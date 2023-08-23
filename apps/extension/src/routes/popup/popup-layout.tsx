3f9ff66e20e9b2a58430d37b563d1485d3ebf99e
Vadim <eugene.s@zpoken.ai>
2023-08-23 21:03:29 +0300

    add card-radial to tailwind config

--- apps/extension/src/routes/popup/popup-layout.tsx
+++ apps/extension/src/routes/popup/popup-layout.tsx
@@ -8,7 +8,7 @@ export const PopupLayout = ({ children }: PopupLayoutProps) => {
   return (
     <div className='relative bg-charcoal'>
       <div className='relative z-10'>{children}</div>
-      <div className='bg-card-radial absolute inset-0 z-0 rounded-2xl p-6 opacity-20' />
+      <div className='absolute inset-0 z-0 rounded-2xl bg-card-radial p-6 opacity-20' />
     </div>
   );
 };
