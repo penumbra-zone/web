3a42eeba3d60bdfd04625e31f18660d7534b4b67
Vadim <eugene.s@zpoken.ai>
2023-08-24 13:02:52 +0300

    fix eslint tailwindcss/classnames-order

--- apps/extension/src/routes/popup/popup-layout.tsx
+++ apps/extension/src/routes/popup/popup-layout.tsx
@@ -2,9 +2,9 @@ import { Outlet } from 'react-router-dom';
 
 export const PopupLayout = () => {
   return (
-    <div className='relative bg-charcoal'>
+    <div className='bg-charcoal relative'>
       <div className='relative z-10'>{<Outlet />}</div>
-      <div className='absolute inset-0 z-0 rounded-2xl bg-card-radial p-6 opacity-20' />
+      <div className='bg-card-radial absolute inset-0 z-0 rounded-2xl p-6 opacity-20' />
     </div>
   );
 };
