01ead32d9370f402b7eb5bcf53e717759ccc8a42
zpoken <82594516+zpoken@users.noreply.github.com>
2023-08-28 12:02:20 -0500

    testing the receipt of different keys (#23)
    * testing the receipt of different keys
    
    * format
    
    * delete value test
    
    ---------
    
    Co-authored-by: Vadim <eugene.s@zpoken.ai>

--- packages/wasm/src/keys.ts
+++ packages/wasm/src/keys.ts
@@ -1,7 +1,21 @@
+import {
+  generate_spend_key,
+  get_address_by_index,
+  get_full_viewing_key,
+  get_short_address_by_index,
+} from '@penumbra-zone/wasm-bundler';
 import { z } from 'zod';
-import { generate_spend_key } from '@penumbra-zone/wasm-bundler';
 
-const SpendKeySchema = z.string();
+const StringSchema = z.string();
 
 export const generateSpendKey = (seedPhrase: string): string =>
-  SpendKeySchema.parse(generate_spend_key(seedPhrase));
+  StringSchema.parse(generate_spend_key(seedPhrase));
+
+export const getFullViewingKey = (spendKey: string): string =>
+  StringSchema.parse(get_full_viewing_key(spendKey));
+
+export const getAddressByIndex = (fullViewingKey: string, index: number): string =>
+  StringSchema.parse(get_address_by_index(fullViewingKey, index));
+
+export const getShortAddressByIndex = (fullViewingKey: string, index: number): string =>
+  StringSchema.parse(get_short_address_by_index(fullViewingKey, index));
