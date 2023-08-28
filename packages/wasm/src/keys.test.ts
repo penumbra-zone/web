c1a9e2e929cae7ee3cca0d09b2a1c86dc544f02b
Vadim <eugene.s@zpoken.ai>
2023-08-28 19:12:08 +0300

    delete value test

--- packages/wasm/src/keys.test.ts
+++ packages/wasm/src/keys.test.ts
@@ -20,13 +20,6 @@ describe('keys', () => {
         generateSpendKey(seedPhrase);
       }).not.toThrow();
     });
-
-    it('equal to penumbraspendkey1fcesjzswatt6g6hkr0jvu6svc8xl7zq3yzzxrnkaz470nvc039ts8c3e4d', () => {
-      const spendKey = generateSpendKey(seedPhrase);
-      expect(spendKey).equal(
-        'penumbraspendkey1fcesjzswatt6g6hkr0jvu6svc8xl7zq3yzzxrnkaz470nvc039ts8c3e4d',
-      );
-    });
   });
 
   describe('generateFullViewingKey()', () => {
@@ -36,14 +29,6 @@ describe('keys', () => {
       expect(() => {
         getFullViewingKey(spendKey);
       }).not.toThrow();
-    });
-
-    it('equal to penumbrafullviewingkey1qjr5suw82hz2tl9jpmurzj7vlc4vwp3zlsc6007c4ckrlmg7nvx567v3mekx4ugf9l6wnmhrw7y6602vrp8ehhw0axxud4saywy4yrsu3masu', () => {
-      const spendKey = generateSpendKey(seedPhrase);
-      const fullViewingKey = getFullViewingKey(spendKey);
-      expect(fullViewingKey).equal(
-        'penumbrafullviewingkey1qjr5suw82hz2tl9jpmurzj7vlc4vwp3zlsc6007c4ckrlmg7nvx567v3mekx4ugf9l6wnmhrw7y6602vrp8ehhw0axxud4saywy4yrsu3masu',
-      );
     });
   });
 
@@ -56,15 +41,6 @@ describe('keys', () => {
         getAddressByIndex(fullViewingKey, 0);
       }).not.toThrow();
     });
-
-    it('equal to penumbrav2t1na5nelwxrfhsfwftm7hefgqrn5qc87kwrs9ydqlans2srau7r23edrq3dsf664efd6c0dys8zyetlfxl64ealxvt7kfqhjflhulwrf3s20lervj8gd602e02qvqwr4nmh2tpyc', () => {
-      const spendKey = generateSpendKey(seedPhrase);
-      const fullViewingKey = getFullViewingKey(spendKey);
-      const zeroAddress = getAddressByIndex(fullViewingKey, 0);
-      expect(zeroAddress).equal(
-        'penumbrav2t1na5nelwxrfhsfwftm7hefgqrn5qc87kwrs9ydqlans2srau7r23edrq3dsf664efd6c0dys8zyetlfxl64ealxvt7kfqhjflhulwrf3s20lervj8gd602e02qvqwr4nmh2tpyc',
-      );
-    });
   });
 
   describe('generateShortAddressByIndex()', () => {
@@ -75,13 +51,6 @@ describe('keys', () => {
       expect(() => {
         getShortAddressByIndex(fullViewingKey, 0);
       }).not.toThrow();
-    });
-
-    it('equal to penumbrav2t1na5nelwxrfhsfwftm7hefgqr…', () => {
-      const spendKey = generateSpendKey(seedPhrase);
-      const fullViewingKey = getFullViewingKey(spendKey);
-      const zeroAddress = getShortAddressByIndex(fullViewingKey, 0);
-      expect(zeroAddress).equal('penumbrav2t1na5nelwxrfhsfwftm7hefgqr…');
     });
   });
 });
