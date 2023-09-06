1122ce90ebf5caeeb64774bc0de542663216e28c
Vadim <eugene.s@zpoken.ai>
2023-09-06 12:56:32 +0300

    delete closed animation

--- packages/ui/components/ui/sheet.tsx
+++ packages/ui/components/ui/sheet.tsx
@@ -23,7 +23,7 @@ const SheetOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SheetPrimitive.Overlay
     className={cn(
-      'fixed inset-0 z-50 bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+      'fixed inset-0 z-50 bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=open]:fade-in-0',
       className,
     )}
     {...props}
@@ -32,21 +32,23 @@ const SheetOverlay = React.forwardRef<
 ));
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName;
 //transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500
-const sheetVariants = cva('fixed z-50 gap-4 bg-background pb-4 pt-5 shadow-lg transition data-[state=open]:duration-500 data-[state=open]:animate-in', {
-  variants: {
-    side: {
-      top: 'inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top',
-      bottom:
-        'inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom',
-      left: 'inset-y-0 left-0 flex h-full w-3/4 flex-col border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left',
-      right:
-        'inset-y-0 right-0 h-full w-3/4  border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm',
+const sheetVariants = cva(
+  'fixed z-50 gap-4 bg-background pb-4 pt-5 shadow-lg transition data-[state=open]:duration-500 data-[state=open]:animate-in',
+  {
+    variants: {
+      side: {
+        top: 'inset-x-0 top-0 border-b data-[state=open]:slide-in-from-top',
+        bottom: 'inset-x-0 bottom-0 border-t data-[state=open]:slide-in-from-bottom',
+        left: 'inset-y-0 left-0 flex h-full w-3/4 flex-col border-r data-[state=open]:slide-in-from-left',
+        right:
+          'inset-y-0 right-0 h-full w-3/4  border-l data-[state=open]:slide-in-from-right sm:max-w-sm',
+      },
+    },
+    defaultVariants: {
+      side: 'right',
     },
   },
-  defaultVariants: {
-    side: 'right',
-  },
-});
+);
 
 interface SheetContentProps
   extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
@@ -89,7 +91,11 @@ const SheetTitle = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Title>,
   React.ComponentPropsWithoutRef<typeof SheetPrimitive.Title>
 >(({ className, ...props }, ref) => (
-  <SheetPrimitive.Title ref={ref} className={cn('font-headline text-base text-white', className)} {...props} />
+  <SheetPrimitive.Title
+    ref={ref}
+    className={cn('font-headline text-base text-white', className)}
+    {...props}
+  />
 ));
 SheetTitle.displayName = SheetPrimitive.Title.displayName;
 
