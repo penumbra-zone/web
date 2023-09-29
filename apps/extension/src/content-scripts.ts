fafcc5278540fa013d0b1c7019756f939a516e54
Vadim <eugene.s@zpoken.ai>
2023-09-29 15:23:08 +0300

    delete retry request

--- apps/extension/src/content-scripts.ts
+++ apps/extension/src/content-scripts.ts
@@ -2,18 +2,9 @@ import { isDappGrpcRequest, isDappGrpcResponse } from 'penumbra-transport';
 
 // Meant to proxy requests between dapp and extension
 
-const sendMsg = async (data: unknown) => {
-  try {
-    await chrome.runtime.sendMessage(data);
-  } catch (error) {
-    console.log(error);
-    await sendMsg(data);
-  }
-};
-
 window.addEventListener('message', ({ data }) => {
   if (isDappGrpcRequest(data)) {
-    void (async () => await sendMsg(data))();
+    void chrome.runtime.sendMessage(data);
   }
 });
 
