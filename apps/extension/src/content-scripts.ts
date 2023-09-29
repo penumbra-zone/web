f517c94424d5190ff2b5a3ea5a48102031a030c8
Vadim <eugene.s@zpoken.ai>
2023-09-29 14:17:53 +0300

    catch error

--- apps/extension/src/content-scripts.ts
+++ apps/extension/src/content-scripts.ts
@@ -2,9 +2,18 @@ import { isDappGrpcRequest, isDappGrpcResponse } from 'penumbra-transport';
 
 // Meant to proxy requests between dapp and extension
 
+const sendMsg = async (data: unknown) => {
+  try {
+    await chrome.runtime.sendMessage(data);
+  } catch (error) {
+    console.log(error);
+    await sendMsg(data);
+  }
+};
+
 window.addEventListener('message', ({ data }) => {
   if (isDappGrpcRequest(data)) {
-    void chrome.runtime.sendMessage(data);
+    void (async () => await sendMsg(data))();
   }
 });
 
--- apps/webapp/app/send/page.tsx
+++ apps/webapp/app/send/page.tsx
@@ -3,7 +3,16 @@ import { FadeTransition } from 'ui';
 export default function Page() {
   return (
     <FadeTransition className='flex min-h-[calc(100vh-82px)] flex-col items-stretch justify-start'>
-      <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'>Send</h1>
+      <div className='grid grid-cols-3 gap-6'>
+        <div />
+        <div className='relative'>
+        <div className='relative z-10 rounded-lg p-5 bg-charcoal-secondary'>
+          
+        </div>
+          <div className='absolute inset-0 z-0 bg-card-radial opacity-20' />
+        </div>
+        <div>asd</div>
+      </div>
     </FadeTransition>
   );
 }
