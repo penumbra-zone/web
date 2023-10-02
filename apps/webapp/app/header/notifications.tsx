5a421c4a2462ec6f2855c7536ddce28ddea1ddc8
Vadim <eugene.s@zpoken.ai>
2023-10-02 17:26:36 +0300

    notification status

--- apps/webapp/app/header/notifications.tsx
+++ apps/webapp/app/header/notifications.tsx
@@ -1,6 +1,10 @@
+'use client';
+
 import { ArrowTopRightIcon } from '@radix-ui/react-icons';
 import { Popover, PopoverContent, PopoverTrigger, Progress } from 'ui';
 import { cn } from 'ui/lib/utils';
+import { useState } from 'react';
+import { BellIcon } from '../../icons';
 import { ResponsiveImage } from '../../shared';
 
 const txs = [
@@ -35,17 +39,36 @@ const txs = [
 ];
 
 export const Notifications = () => {
+  const [status] = useState<'sync' | 'notification' | 'success'>('sync');
+
   return (
     <Popover>
       <PopoverTrigger className='relative'>
-        <div className='absolute right-[2px] top-[5px] z-10 h-[10px] w-[10px] rounded-full bg-sand'></div>
-        <ResponsiveImage src='/bell.svg' alt='Bell' className='h-[30px] w-[30px]' />
+        {status !== 'success' && (
+          <>
+            {status === 'notification' ? (
+              <div className='absolute right-[2px] top-[5px] z-10 h-[11px] w-[11px] rounded-full bg-red'></div>
+            ) : (
+              // <div className='absolute right-[2px] top-[5px] z-10 h-[15px] w-[15px]'>
+              //   <SyncIcon strokeWidth='8' prefix='first' />
+              // </div>
+              <ResponsiveImage
+                src='/sync-bold.svg'
+                alt='Syncing blocks...'
+                className='absolute right-[2px] top-[5px] z-10 h-[15px] w-[15px]'
+              />
+            )}
+          </>
+        )}
+        <div className='h-[30px] w-[30px]'>
+          <BellIcon stroke={status === 'sync' ? '#363434' : '#BDB8B8'} />
+        </div>
       </PopoverTrigger>
       <PopoverContent className='relative flex w-[400px] flex-col gap-10 bg-charcoal-secondary px-[30px] pb-[46px] pt-5'>
         <div className='relative z-10 flex flex-col gap-2'>
           <div className='flex items-center justify-between text-base text-sand'>
             <div className='flex items-center gap-2'>
-              <ResponsiveImage src='/sync.svg' alt='Syncing blocks...' className='h-6 w-6' />
+              <ResponsiveImage src='/sync-thin.svg' alt='Syncing blocks...' className='h-6 w-6' />
               <p className='font-headline font-semibold'>Syncing blocks...</p>
             </div>
             <p className='font-bold'>10982/121312</p>
