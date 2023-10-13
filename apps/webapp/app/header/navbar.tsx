df8d3f9998b9a614935f637111a52ebfd565d2c7
Gabe Rodriguez <grod220@gmail.com>
2023-10-13 19:25:04 +0200

    Gray out disabled nav links

--- apps/webapp/app/header/navbar.tsx
+++ apps/webapp/app/header/navbar.tsx
@@ -9,45 +9,61 @@ const links = [
   {
     href: DappPath.INDEX,
     label: 'Dashboard',
-  },
-  {
-    href: DappPath.SWAP,
-    label: 'Swap',
+    active: true,
   },
   {
     href: DappPath.SEND,
     label: 'Send',
+    active: true,
+  },
+  {
+    href: DappPath.SWAP,
+    label: 'Swap',
+    active: false,
   },
   {
     href: DappPath.POOLS,
     label: 'Pools',
+    active: false,
   },
   {
     href: DappPath.GOVERNANCE,
     label: 'Governance',
+    active: false,
   },
   {
     href: DappPath.STAKING,
     label: 'Staking',
+    active: false,
   },
 ];
+
 export const Navbar = () => {
   const pathname = usePathname();
 
   return (
     <nav className='flex gap-4'>
-      {links.map(i => (
-        <Link
-          key={i.href}
-          href={i.href}
-          className={cn(
-            'font-bold py-[10px] px-[30px]',
-            (pathname as DappPath) === i.href && 'bg-button-gradient-secondary rounded-lg',
-          )}
-        >
-          {i.label}
-        </Link>
-      ))}
+      {links.map(link =>
+        link.active ? (
+          <Link
+            key={link.href}
+            href={link.href}
+            className={cn(
+              'font-bold py-[10px] px-[30px] select-none',
+              (pathname as DappPath) === link.href && 'bg-button-gradient-secondary rounded-lg',
+            )}
+          >
+            {link.label}
+          </Link>
+        ) : (
+          <div
+            key={link.href}
+            className='cursor-not-allowed select-none px-[30px] py-[10px] font-bold text-gray-600'
+          >
+            {link.label}
+          </div>
+        ),
+      )}
     </nav>
   );
 };
