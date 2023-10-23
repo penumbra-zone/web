0b4d70b552662eda4db49e75818c604e99722d56
Gabe Rodriguez <grod220@gmail.com>
2023-10-23 12:51:50 +0200

    Custody routes in place (#125)
    * Authorizing tx
    
    * auth working

---
+++ packages/router/src/grpc/custody/authorize.ts
@@ -0,0 +1,35 @@
+import { GrpcRequest } from '@penumbra-zone/transport';
+import { Box } from '@penumbra-zone/types';
+import { CustodyProtocolService } from '@buf/penumbra-zone_penumbra.connectrpc_es/penumbra/custody/v1alpha1/custody_connect';
+import {
+  AuthorizeRequest,
+  AuthorizeResponse,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/custody/v1alpha1/custody_pb';
+import { authorizePlan, generateSpendKey } from '@penumbra-zone/wasm-ts';
+import { localExtStorage, sessionExtStorage } from '@penumbra-zone/storage';
+import { Key } from '@penumbra-zone/crypto-web';
+
+export const isAuthorizeRequest = (
+  req: GrpcRequest<typeof CustodyProtocolService>,
+): req is AuthorizeRequest => {
+  return req.getType().typeName === AuthorizeRequest.typeName;
+};
+
+export const handleAuthorizeReq = async (req: AuthorizeRequest): Promise<AuthorizeResponse> => {
+  if (!req.plan) throw new Error('No plan included in request');
+
+  const passwordKey = await sessionExtStorage.get('passwordKey');
+  if (!passwordKey) throw new Error('User must login to extension');
+
+  const wallets = await localExtStorage.get('wallets');
+  const { encryptedSeedPhrase } = wallets[0]!.custody;
+
+  const key = await Key.fromJson(passwordKey);
+  const decryptedSeedPhrase = await key.unseal(Box.fromJson(encryptedSeedPhrase));
+  if (!decryptedSeedPhrase) throw new Error('Unable to decrypt seed phrase with password');
+
+  const spendKey = generateSpendKey(decryptedSeedPhrase);
+
+  const authorizationData = authorizePlan(spendKey, req.plan);
+  return new AuthorizeResponse({ data: authorizationData });
+};
--- packages/types/src/transaction.ts
+++ packages/types/src/transaction.ts
@@ -3,6 +3,7 @@ import {
   TransactionView,
 } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/transaction/v1alpha1/transaction_pb';
 import { z } from 'zod';
+import { InnerBase64Schema } from './base64';
 
 export const WasmTransactionInfoSchema = z.object({
   txp: z.unknown(),
@@ -13,3 +14,8 @@ export interface WasmTransactionInfo {
   txp: TransactionPerspective;
   txv: TransactionView;
 }
+
+export const WasmAuthorizeSchema = z.object({
+  effectHash: InnerBase64Schema,
+  spendAuths: z.array(InnerBase64Schema),
+});
