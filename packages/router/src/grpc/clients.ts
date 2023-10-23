02edf33a6fc2e6a4f7bbf58a23676b01d698abe6
Gabe Rodriguez <grod220@gmail.com>
2023-10-23 23:55:20 +0200

    Broadcast tx

--- packages/router/src/grpc/clients.ts
+++ packages/router/src/grpc/clients.ts
@@ -1,10 +1,10 @@
 import { createPromiseClient } from '@connectrpc/connect';
 import { createGrpcWebTransport } from '@connectrpc/connect-web';
 import { testnetConstants } from '@penumbra-zone/constants';
-import { ViewProtocolService } from '@buf/penumbra-zone_penumbra.connectrpc_es/penumbra/view/v1alpha1/view_connect';
+import { TendermintProxyService } from '@buf/penumbra-zone_penumbra.connectrpc_es/penumbra/util/tendermint_proxy/v1alpha1/tendermint_proxy_connect';
 
 const transport = createGrpcWebTransport({
   baseUrl: testnetConstants.grpcEndpoint,
 });
 
-export const viewClient = createPromiseClient(ViewProtocolService, transport);
+export const tendermintClient = createPromiseClient(TendermintProxyService, transport);
