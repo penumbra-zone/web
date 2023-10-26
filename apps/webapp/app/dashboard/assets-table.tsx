d550dc0698117187b280d4483b8e468034137d40
Gabe Rodriguez <grod220@gmail.com>
2023-10-26 15:10:36 +0200

    Improve number formatting

--- apps/webapp/app/dashboard/assets-table.tsx
+++ apps/webapp/app/dashboard/assets-table.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useBalancesWithMetadata } from '../../hooks/sorted-asset';
 import {
   Identicon,
   Table,
@@ -9,50 +10,58 @@ import {
   TableHeader,
   TableRow,
 } from '@penumbra-zone/ui';
-import { useBalancesWithMetadata } from '../../hooks/sorted-asset';
+import { shorten } from '@penumbra-zone/types';
 import { formatNumber } from '../../utils';
 
 export default function AssetsTable() {
-  const { data, error } = useBalancesWithMetadata('usdcValue');
+  const { data, error } = useBalancesWithMetadata();
 
   return (
-    <>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className='text-center'>Account</TableHead>
-            <TableHead className='text-center'>Asset</TableHead>
-            <TableHead className='text-center'>Balance</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {data.map((asset, i) => (
-            <TableRow key={i}>
-              <TableCell>
-                <div className='flex flex-row items-center justify-center gap-2'>
-                  <Identicon name={asset.account.address} className='h-4 w-4 rounded-full' />#
-                  {asset.account.index}
-                </div>
-              </TableCell>
-              <TableCell>
-                <div className='flex flex-col items-center gap-4 '>
-                  <p className=' text-base'>{asset.denomMetadata.display}</p>
-                </div>
-              </TableCell>
-              <TableCell className='text-center font-mono font-light'>
-                <div className='flex flex-col'>
-                  <p className='text-[15px] leading-[22px]'>{formatNumber(asset.balance.amount)}</p>
-                  {/* Enable when pricing ready */}
-                  {/*<p className='text-[15px] font-normal leading-[22px] text-muted-foreground'>*/}
-                  {/*  ${formatNumber(asset.balance.usdcValue)}*/}
-                  {/*</p>*/}
-                </div>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      {error && <div className='text-red-700'>{String(error)}</div>}
-    </>
+    <div className='flex flex-col gap-6'>
+      {data.map(a => {
+        return (
+          <div key={a.index} className='flex flex-col'>
+            <div className='flex flex-col items-center justify-center'>
+              <div className='flex items-center justify-center gap-2'>
+                <h2 className='text-xl font-bold'>Account: #{a.index}</h2>{' '}
+                <Identicon name={a.address} className='h-4 w-4 rounded-full' />
+              </div>
+              <div className='italic text-foreground'>{shorten(a.address, 15)}</div>
+            </div>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className='text-center'>Asset</TableHead>
+                  <TableHead className='text-center'>Balance</TableHead>
+                  <TableHead className='text-center'>Value</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {a.balances.map((asset, i) => (
+                  <TableRow key={i}>
+                    <TableCell>
+                      <div className='flex flex-col items-center gap-4 '>
+                        <p className=' text-base'>{asset.denom}</p>
+                      </div>
+                    </TableCell>
+                    <TableCell className='text-center font-mono font-light'>
+                      <div className='flex flex-col'>
+                        <p className='text-[15px] leading-[22px]'>{formatNumber(asset.amount)}</p>
+                      </div>
+                    </TableCell>
+                    <TableCell className='text-center font-mono font-light'>
+                      <div className='flex flex-col'>
+                        <p className=''>${formatNumber(asset.usdcValue)}</p>
+                      </div>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        );
+      })}
+      {error ? <div className='text-red-700'>{String(error)}</div> : null}
+    </div>
   );
 }
