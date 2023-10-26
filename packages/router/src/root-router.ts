7df34307ebb9d4490d0b3ff336b518ed45a6c5d9
Vadim <eugene.s@zpoken.ai>
2023-10-26 16:48:58 +0300

    check is site connected

--- packages/router/src/root-router.ts
+++ packages/router/src/root-router.ts
@@ -13,29 +13,16 @@ import { localExtStorage } from '@penumbra-zone/storage';
 // Exposed to service worker for listening for internal and external messages
 export const penumbraMessageHandler =
   (services: ServicesInterface) =>
-  (
+  async (
     message: unknown,
     sender: chrome.runtime.MessageSender,
     sendResponse: (response: ServiceWorkerResponse<SwRequestMessage>) => void,
   ) => {
-    if (!allowedRequest(sender)) return;
+    if (!(await allowedRequest(sender, message))) return;
 
     if (isStdRequest(message)) return stdRouter(message, sender, sendResponse, services);
-    else if (isViewServerReq(message)) {
-      void (async () => {
-        const isConnectedSite = (await localExtStorage.get('connectedSites')).includes(
-          sender.origin ?? '',
-        );
-        isConnectedSite && viewServerRouter(message, sender, services);
-      })();
-    } else if (isCustodyServerReq(message)) {
-      void (async () => {
-        const isConnectedSite = (await localExtStorage.get('connectedSites')).includes(
-          sender.origin ?? '',
-        );
-        isConnectedSite && custodyServerRouter(message, sender, services);
-      })();
-    }
+    else if (isViewServerReq(message)) viewServerRouter(message, sender, services);
+    else if (isCustodyServerReq(message)) custodyServerRouter(message, sender, services);
 
     return;
   };
@@ -43,6 +30,15 @@ export const penumbraMessageHandler =
 // Protections to guard for allowed messages only. Requirements:
 // - TODO: If message from dapp, should only be from sites that have received permission via `connect`
 // - If message from extension, ensure it comes from our own and not an external extension
-const allowedRequest = (sender: chrome.runtime.MessageSender): boolean => {
-  return sender.tab?.id !== undefined || sender.id === chrome.runtime.id;
+const allowedRequest = async (
+  sender: chrome.runtime.MessageSender,
+  message: unknown,
+): Promise<boolean> => {
+  // check is connected site only for ViewServerReq and CustodyServerReq
+  const isConnectedSite =
+    isViewServerReq(message) || isCustodyServerReq(message)
+      ? (await localExtStorage.get('connectedSites')).includes(sender.origin ?? '')
+      : true;
+
+  return isConnectedSite && (sender.tab?.id !== undefined || sender.id === chrome.runtime.id);
 };
