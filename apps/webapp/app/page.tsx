2c5fc0b396d8c402d782f3835ff37539dd323e57
Gabe Rodriguez <grod220@gmail.com>
2023-10-26 09:16:25 +0200

    hide unused elements

--- apps/webapp/app/page.tsx
+++ apps/webapp/app/page.tsx
@@ -4,7 +4,6 @@ import { useState } from 'react';
 import { Card, FadeTransition, Tabs, TabsContent, TabsList, TabsTrigger } from '@penumbra-zone/ui';
 import { EduInfoCard } from '../shared';
 import { dashboardTabsHelper } from './dashboard/constants';
-import { NftsTable } from './dashboard/nfts-table';
 import { DashboardPageTab } from './dashboard/types';
 import dynamic from 'next/dynamic';
 
@@ -24,7 +23,7 @@ export default function Page() {
       <div className='relative mx-auto grid max-w-[1276px] grid-cols-3 gap-5'>
         <Card gradient className='col-span-2 row-span-2 flex-1 p-5'>
           <Tabs defaultValue={DashboardPageTab.ASSETS} value={tab}>
-            <TabsList className='mx-auto grid w-[372px] grid-cols-3 gap-4'>
+            <TabsList className='mx-auto grid w-[372px] grid-cols-2 gap-4'>
               <TabsTrigger
                 value={DashboardPageTab.ASSETS}
                 onClick={() => setTab(DashboardPageTab.ASSETS)}
@@ -37,21 +36,12 @@ export default function Page() {
               >
                 Transactions
               </TabsTrigger>
-              <TabsTrigger
-                value={DashboardPageTab.NFTS}
-                onClick={() => setTab(DashboardPageTab.NFTS)}
-              >
-                NFTs
-              </TabsTrigger>
             </TabsList>
             <TabsContent value={DashboardPageTab.ASSETS} className='mt-10'>
               <AssetsTable />
             </TabsContent>
             <TabsContent value={DashboardPageTab.TRANSACTIONS} className='mt-10'>
               <TransactionTable />
-            </TabsContent>
-            <TabsContent value={DashboardPageTab.NFTS}>
-              <NftsTable />
             </TabsContent>
           </Tabs>
         </Card>
