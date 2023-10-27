879d9ecc8d3ad19bd155daf0972ac3ea01d52eee
Vadim <eugene.s@zpoken.ai>
2023-10-27 16:58:00 +0300

    delete log

--- packages/transport/src/create.ts
+++ packages/transport/src/create.ts
@@ -38,8 +38,6 @@ const outputEventListener =
   (event: MessageEvent<unknown>) => {
     if (event.source !== window || !isServiceGrpcResponse(service, event.data)) return;
 
-    console.log(event.data);
-
     const { sequence } = event.data;
 
     if (pending.requests.has(sequence)) {
