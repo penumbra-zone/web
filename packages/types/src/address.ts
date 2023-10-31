6088e45c0146a58be121e37ae0cfc439031c03ec
turbocrime <turbocrime@users.noreply.github.com>
2023-10-30 20:34:13 -0700

    semicolol

--- packages/types/src/address.ts
+++ packages/types/src/address.ts
@@ -9,7 +9,7 @@ export const BECH32_ADDRESS_LENGTH = 143;
 // TODO use later - https://github.com/penumbra-zone/web/pull/63#discussion_r1343992139
 // https://github.com/penumbra-zone/penumbra/blob/8d1644620779ddfd961e58f0f4703318b3d08910/crates/core/keys/src/address.rs#L201-L211
 export const bech32Address = (addr: Address): string =>
-  bech32m.encode(BECH32_PREFIX, bech32m.toWords(addr.inner), BECH32_ADDRESS_LENGTH)
+  bech32m.encode(BECH32_PREFIX, bech32m.toWords(addr.inner), BECH32_ADDRESS_LENGTH);
 
 export const isPenumbraAddr = (addr: string): boolean =>
   addr.length === BECH32_ADDRESS_LENGTH && addr.startsWith(`${BECH32_PREFIX}1`);
