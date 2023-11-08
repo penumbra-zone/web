d079eeb3afab8113ebd5ed6da49b25fa2466b023
Gabe Rodriguez <grod220@gmail.com>
2023-11-08 12:00:43 +0000

    Vite boilerplate (#224)
    * Migrate to vite
    
    * Update lint config
    
    * rename
    
    * more
    
    * fixes

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -4,17 +4,21 @@ module.exports = {
     'eslint:recommended',
     'turbo',
     'plugin:tailwindcss/recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/strict-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:vitest/recommended',
+    'plugin:react-hooks/recommended',
   ],
-  plugins: ['@typescript-eslint', 'turbo', 'vitest'],
+  plugins: ['@typescript-eslint', 'turbo', 'vitest', 'react-refresh'],
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-confusing-void-expression': ['error', { ignoreArrowShorthand: true }],
     '@typescript-eslint/switch-exhaustiveness-check': 'error',
     '@typescript-eslint/no-unnecessary-condition': ['error', { allowConstantLoopConditions: true }],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
   },
   parserOptions: {
     babelOptions: {
@@ -31,4 +35,7 @@ module.exports = {
     webextensions: true,
   },
   ignorePatterns: ['dist/*'],
+  settings: {
+    react: { version: 'detect' },
+  },
 };
