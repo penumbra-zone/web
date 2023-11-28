bca9a2b720559ccc2dde1d5ea116f74649e13475
Valentine <walik19988@gmail.com>
2023-11-29 00:55:31 +0200

    save LPNFT denoms before calling transactionInfo()

--- packages/query/src/block-processor/transactions.ts
+++ packages/query/src/block-processor/transactions.ts
@@ -1,10 +1,23 @@
-import { IndexedDbInterface, noteSourceFromBytes, ParsedNoteSource } from '@penumbra-zone/types';
+import {
+  IndexedDbInterface,
+  noteSourceFromBytes,
+  ParsedNoteSource,
+  ViewServerInterface,
+} from '@penumbra-zone/types';
 import { TendermintQuerier } from '../queriers/tendermint';
 import { decodeTx, transactionInfo } from '@penumbra-zone/wasm-ts/src/transaction';
 import { sha256Hash } from '@penumbra-zone/crypto-web';
 import { NoteSource } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/chain/v1alpha1/chain_pb';
 import { TransactionInfo } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1alpha1/view_pb';
-import { Id } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/transaction/v1alpha1/transaction_pb';
+import {
+  Id,
+  Transaction,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/transaction/v1alpha1/transaction_pb';
+import {
+  PositionState,
+  PositionState_PositionStateEnum,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/dex/v1alpha1/dex_pb';
+import { DenomMetadata } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/asset/v1alpha1/asset_pb';
 
 export class Transactions {
   // These are base64 encoded hex strings
@@ -15,6 +28,7 @@ export class Transactions {
     private fullViewingKey: string,
     private indexedDb: IndexedDbInterface,
     private tendermint: TendermintQuerier,
+    private viewServer: ViewServerInterface,
   ) {}
 
   async add(source?: NoteSource) {
@@ -33,14 +47,12 @@ export class Transactions {
   // it belongs to the user. For that reason, we query the entire block, go through each
   // transaction, and filter to the transaction(s) that belong to the user.
   // We then decode and acquire more info on that transaction to store in the database.
-  async storeTransactionInfo(): Promise<Set<TransactionInfo>> {
-    const newTransactions = new Set<TransactionInfo>();
-
-    if (this.all.size <= 0) return newTransactions;
+  async storeTransactionInfo() {
+    if (this.all.size <= 0) return;
 
     // TODO: Currently bug querying blockHeight 1
     // Tendermint does not allow querying blockHeight 0
-    if (this.blockHeight === 0n || this.blockHeight === 1n) return newTransactions;
+    if (this.blockHeight === 0n || this.blockHeight === 1n) return;
 
     const b = await this.tendermint.getBlock(this.blockHeight);
     if (!b.block?.data?.txs) throw new Error('Unable to query transactions');
@@ -51,6 +63,10 @@ export class Transactions {
 
       if (noteSourcePresent(this.all, noteSource)) {
         const transaction = decodeTx(txBytes);
+
+        // We must store LPNFT denoms before calling transactionInfo()
+        await this.storeLpNft(transaction);
+
         const { txp, txv } = await transactionInfo(
           this.fullViewingKey,
           transaction,
@@ -66,10 +82,33 @@ export class Transactions {
         });
 
         await this.indexedDb.saveTransactionInfo(txToStore);
-        newTransactions.add(txToStore);
       }
     }
-    return newTransactions;
+  }
+
+  // LPNFT asset IDs won't be known to the chain, so we need to pre-populate them in local
+  // registry based on transaction contents.
+  // Necessary to save LPNFT denoms for 'closed' 'withdrawn' and 'claimed' as soon as we detect a positionOpen action
+  async storeLpNft(tx: Transaction) {
+    const processPosition = async (positionState: PositionState_PositionStateEnum) => {
+      if (tx.body?.actions) {
+        for (const action of tx.body.actions) {
+          if (action.action.case === 'positionOpen' && action.action.value.position !== undefined) {
+            const position = action.action.value.position;
+            const state = new PositionState({ state: positionState });
+
+            const denomMetadata: DenomMetadata = this.viewServer.getLpNftDenom(position, state);
+
+            await this.indexedDb.saveAssetsMetadata(denomMetadata);
+          }
+        }
+      }
+    };
+
+    await processPosition(PositionState_PositionStateEnum.OPENED);
+    await processPosition(PositionState_PositionStateEnum.CLOSED);
+    await processPosition(PositionState_PositionStateEnum.WITHDRAWN);
+    await processPosition(PositionState_PositionStateEnum.CLAIMED);
   }
 }
 
--- packages/types/src/block-processor.ts
+++ packages/types/src/block-processor.ts
@@ -1,14 +1,10 @@
-import {
-  SpendableNoteRecord,
-  TransactionInfo,
-} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1alpha1/view_pb';
+import { SpendableNoteRecord } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1alpha1/view_pb';
 import { Nullifier } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/sct/v1alpha1/sct_pb';
 
 export interface BlockProcessorInterface {
   syncBlocks(): Promise<void>;
   autoRetrySync(): Promise<void>;
   storeNewTransactions(blockHeight: bigint, newNotes: SpendableNoteRecord[]): Promise<void>;
-  storeLpNft(newTransactions: Set<TransactionInfo>): Promise<void>;
   handleNullifiers(nullifiers: Nullifier[], blockHeight: bigint): Promise<void>;
   saveSyncProgress(): Promise<void>;
   stopSync(): void;
