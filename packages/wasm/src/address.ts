5afcaddb64f52b28ef63fdf23d75072a98348787
Vadim121197 <52686175+Vadim121197@users.noreply.github.com>
2023-11-30 11:37:58 +0200

    implement IndexByAddress (#263)
    * implement IndexByAddress
    
    * fix address from another fvk
    
    * implement IndexByAddress
    
    * fix address from another fvk
    
    * Implement querying index by address
    
    * lint fix
    
    ---------
    
    Co-authored-by: Vadim <eugene.s@zpoken.ai>
    Co-authored-by: Gabe Rodriguez <grod220@gmail.com>

---
+++ packages/wasm/src/address.ts
@@ -0,0 +1,16 @@
+import { validateSchema } from '@penumbra-zone/types';
+import { z } from 'zod';
+import { get_short_address_by_index, is_controlled_address } from '@penumbra-zone/wasm-bundler';
+import { AddressIndex } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/keys/v1alpha1/keys_pb';
+import { JsonValue } from '@bufbuild/protobuf/dist/esm';
+
+export const getShortAddressByIndex = (fullViewingKey: string, index: number): string =>
+  validateSchema(z.string(), get_short_address_by_index(fullViewingKey, index));
+
+export const isControlledAddress = (
+  fullViewingKey: string,
+  address: string,
+): AddressIndex | undefined => {
+  const res = is_controlled_address(fullViewingKey, address) as JsonValue;
+  return res ? AddressIndex.fromJson(res) : undefined;
+};
