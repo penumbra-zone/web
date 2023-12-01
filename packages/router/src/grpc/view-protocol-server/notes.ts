b89f3ab86c90aa6baf8b167a97e9866613df3baf
Vadim <eugene.s@zpoken.ai>
2023-12-01 10:02:17 +0200

    handleNotesReq refactoring

--- packages/router/src/grpc/view-protocol-server/notes.ts
+++ packages/router/src/grpc/view-protocol-server/notes.ts
@@ -1,61 +1,58 @@
 import {
   NotesRequest,
   NotesResponse,
-  SpendableNoteRecord,
 } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1alpha1/view_pb';
 import { ViewReqMessage } from './router';
-import { ServicesInterface, uint8ArrayToString } from '@penumbra-zone/types';
+import {
+  ServicesInterface,
+  addAmounts,
+  joinLoHiAmount,
+  uint8ArrayToString,
+} from '@penumbra-zone/types';
 import { localExtStorage } from '@penumbra-zone/storage';
+import { Amount } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/num/v1alpha1/num_pb';
 
 export const isNotesRequest = (msg: ViewReqMessage): msg is NotesRequest => {
   return msg.getType().typeName === NotesRequest.typeName;
 };
 
-export const handleNotesReq = async function* (
-  req: NotesRequest,
-  services: ServicesInterface,
-): AsyncIterable<NotesResponse> {
-  const { indexedDb } = await services.getWalletServices();
-  const allNotes = await indexedDb.getAllNotes();
+// If wallet id passed in req, ensure it's in storage
+async function assertWalletIdMatches(req: NotesRequest) {
   const wallets = await localExtStorage.get('wallets');
-
   if (
     req.walletId?.inner &&
     !wallets.find(wallet => wallet.id === uint8ArrayToString(req.walletId!.inner))
   ) {
     throw new Error('Invalid account ID');
   }
+}
 
-  const filteredNotes = allNotes
-    // filter by assetId
-    .filter(note => (req.assetId ? note.note?.value?.assetId?.equals(req.assetId) : note))
-    // filter by addressIndex
-    .filter(note => (req.addressIndex ? note.addressIndex?.equals(req.addressIndex) : note))
-    // filter by heightSpent
-    .filter(note => (!req.includeSpent ? !note.heightSpent : note));
+export const handleNotesReq = async function* (
+  req: NotesRequest,
+  services: ServicesInterface,
+): AsyncIterable<NotesResponse> {
+  await assertWalletIdMatches(req);
 
-  let response: SpendableNoteRecord[];
+  const { indexedDb } = await services.getWalletServices();
+  const allNotes = await indexedDb.getAllNotes();
 
-  if ((req.amountToSpend && (!req.assetId || req.includeSpent)) ?? !req.amountToSpend) {
-    response = filteredNotes;
-  } else {
-    // filter by amountToSpend
-    let sum = 0n;
-    response = [];
+  const { assetId, addressIndex, includeSpent, amountToSpend } = req;
 
-    for (const note of filteredNotes) {
-      sum = sum + (note.note?.value?.amount?.lo ?? 0n);
-      if (sum <= req.amountToSpend.lo) {
-        response.push(note);
-      } else {
-        break;
-      }
-    }
-  }
+  let spent = new Amount();
 
-  yield* response.map(note => {
-    return new NotesResponse({
-      noteRecord: note,
-    });
-  });
+  yield* allNotes
+    .filter(n => (assetId ? n.note?.value?.assetId?.equals(assetId) : true))
+    .filter(n => (addressIndex ? n.addressIndex?.equals(addressIndex) : true))
+    .filter(n => (!includeSpent ? !n.heightSpent : true))
+    .filter(n => {
+      // If set, stop returning notes once the total exceeds this amount.
+      // Ignored if `assetId` is unset or if `includeSpent` is set.
+      if (!(amountToSpend && assetId && !includeSpent)) return true;
+
+      const noteAmount = n.note?.value?.amount ?? new Amount();
+      spent = addAmounts(spent, noteAmount);
+
+      return joinLoHiAmount(spent) <= joinLoHiAmount(amountToSpend);
+    })
+    .map(noteRecord => new NotesResponse({ noteRecord }));
 };
