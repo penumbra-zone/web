3e5c0ba77b4281e510a3ab980af8b5677d1abe21
Vadim <eugene.s@zpoken.ai>
2023-12-06 11:44:19 +0200

    chrome-extension type:module

--- apps/extension/webpack/webpack.dev.cjs
+++ apps/extension/webpack/webpack.dev.cjs
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const common = require('./webpack.common.cjs');
 const webpack = require('webpack');
 
 module.exports = merge(common, {
