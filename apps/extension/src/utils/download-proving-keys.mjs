4584e9435d7c4c22d3b4790f94e85e11446604ba
Tal Derei <talderei99@gmail.com>
2023-12-07 08:48:50 -0800

    modify webpack build config to download proving keys at build time

---
+++ apps/extension/src/utils/download-proving-keys.mjs
@@ -0,0 +1,45 @@
+import fetch from 'node-fetch'
+import * as fs from 'fs';
+import * as path from 'path';
+
+const githubSourceDir =
+  'https://github.com/penumbra-zone/penumbra/raw/main/crates/crypto/proof-params/src/gen/';
+
+const provingKeys = [
+  { file: 'spend_pk.bin' },
+  { file: 'output_pk.bin' },
+  { file: 'swap_pk.bin' },
+  { file: 'swapclaim_pk.bin' },
+  { file: 'nullifier_derivation_pk.bin' },
+  { file: 'delegator_vote_pk.bin' },
+  { file: 'undelegateclaim_pk.bin' },
+];
+
+const binaryFilesDir = path.join('proving-keys');
+
+const downloadProvingKeys = async () => {
+    // Check if `proving-keys` directory exists
+    if (!fs.existsSync(binaryFilesDir)) {
+      fs.mkdirSync(binaryFilesDir, { recursive: true });
+  
+      const promises = provingKeys.map(async ({ file }) => {
+        const response = await fetch(`${githubSourceDir}${file}`);
+        if (!response.ok) throw new Error(`Failed to fetch ${file}`);
+    
+        const buffer = await response.arrayBuffer();
+
+        const outputPath = path.join(binaryFilesDir, file);
+        if (!fs.existsSync(outputPath)) {
+          console.log(`${file}`, "downloaded: ", outputPath)
+          const fileStream = fs.createWriteStream(outputPath, { flags: 'a' });
+          fileStream.write(Buffer.from(buffer));
+          fileStream.end();
+        }
+      });
+  
+      await Promise.all(promises);
+    }
+};
+
+downloadProvingKeys();
+
