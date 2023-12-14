40e01e15f1b8661ed8440b3da2cfa72aa8b9dea6
Tal Derei <talderei99@gmail.com>
2023-12-14 08:47:25 -0800

    destructuring data objects passed to workers

--- apps/extension/src/routes/web-worker/router.ts
+++ apps/extension/src/routes/web-worker/router.ts
@@ -1,17 +1,29 @@
+import { WitnessData } from "@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/transaction/v1alpha1/transaction_pb";
+import { JsonValue } from "@bufbuild/protobuf";
+import { loadProvingKeys } from "@penumbra-zone/wasm-ts/src/utils";
+
 self.addEventListener('message', async function(e) {
     const { type, data } = e.data;
 
-    if (type === 'worker') {                
-        // TODO: Destructure the data object to get individual fields
+    if (type === 'worker') {                       
+        // Destructure the data object to get individual fields
+        const { witnessAndBuildRequest, witness, fullViewingKey } = data 
 
-        // Execute your function using the fields
-        const action = await execute_worker();
+        // Execute worker function using the fields
+        const action = await execute_worker(witnessAndBuildRequest, witness, fullViewingKey);
 
         // Post message back to offscreen document
         self.postMessage(JSON.stringify(action));
     }
 }, false);
 
-async function execute_worker() {
+async function execute_worker(
+    witnessAndBuildRequest: JsonValue, 
+    witness: WitnessData, 
+    fullViewingKey: string
+) {
     console.log('web worker running...');
+
+    // Fetch and load keys into WASM binary
+    await loadProvingKeys();
 }
