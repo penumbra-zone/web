4c1d7f52877e859016d29f97f042fd8b2c91445d
Tal Derei <talderei99@gmail.com>
2023-12-14 08:34:25 -0800

    parallel action builds wip

--- apps/extension/src/routes/offscreen/types.ts
+++ apps/extension/src/routes/offscreen/types.ts
@@ -1,5 +1,6 @@
 import { InternalMessage, InternalRequest, InternalResponse } from '@penumbra-zone/types/src/internal-msg/shared';
-import { AuthorizeAndBuildRequest, AuthorizeAndBuildResponse, WitnessAndBuildRequest } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1alpha1/view_pb';
+import { AuthorizeAndBuildRequest, AuthorizeAndBuildResponse, WitnessAndBuildRequest, WitnessAndBuildResponse } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1alpha1/view_pb';
+import { WitnessData } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/transaction/v1alpha1/transaction_pb';
 
 export type OffscreenMessage = ActionBuildMessage;
 export type OffscreenRequest = InternalRequest<OffscreenMessage>;
@@ -7,8 +8,8 @@ export type OffscreenResponse = InternalResponse<OffscreenMessage>;
 
 export type ActionBuildMessage = InternalMessage<
   'ACTION_AND_BUILD',
-  AuthorizeAndBuildRequest,
-  Promise<AuthorizeAndBuildResponse>
+  { witnessAndBuildRequest: WitnessAndBuildRequest, witness: WitnessData, fullViewingKey: string},
+  Promise<WitnessAndBuildResponse>
 >;
 
 const request: OffscreenRequest['type'][] = ['ACTION_AND_BUILD'];
@@ -23,8 +24,19 @@ export const isOffscreenRequest = (req: unknown): req is OffscreenRequest => {
   );
 };
 
-export const offscreenClient = { buildAction: (arg: WitnessAndBuildRequest) =>
-    sendOffscreenMessage<ActionBuildMessage>({ type: 'ACTION_AND_BUILD', request: arg, target: 'target' }),
+export const offscreenClient = { buildAction: (
+  arg: WitnessAndBuildRequest, 
+  witness: WitnessData, 
+  fullViewingKey: string
+) =>
+    sendOffscreenMessage<ActionBuildMessage>({ 
+      type: 'ACTION_AND_BUILD', 
+      request: {
+        witnessAndBuildRequest: arg, 
+        witness, 
+        fullViewingKey
+      }, 
+      target: 'target' }),
 };
 
 export const sendOffscreenMessage = async <T extends OffscreenMessage>(
--- apps/extension/src/routes/offscreen/window-management.ts
+++ apps/extension/src/routes/offscreen/window-management.ts
@@ -1,5 +1,6 @@
 import { AuthorizeAndBuildRequest, WitnessAndBuildRequest } from "@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1alpha1/view_pb";
 import { offscreenClient } from "./types";
+import { WitnessData } from "@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/transaction/v1alpha1/transaction_pb";
 
 const sw = self as unknown as ServiceWorkerGlobalScope;
 
@@ -7,6 +8,8 @@ const OFFSCREEN_DOCUMENT_PATH = '/offscreen.html';
 
 export const handleOffscreen = async (
   req: WitnessAndBuildRequest, 
+  witness: WitnessData, 
+  fullViewingKey: string
 ): Promise<void> => {
   if (!(await hasDocument())) {
     await chrome.offscreen.createDocument({
@@ -16,7 +19,7 @@ export const handleOffscreen = async (
     });
   }
 
-  const result = await offscreenClient.buildAction(req)
+  const result = await offscreenClient.buildAction(req, witness, fullViewingKey)
   if ('error' in result) throw result.error;
   if (!result.data) throw new Error('Transaction was not approved');
   closeOffscreenDocument()
