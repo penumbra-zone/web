1c96c0b25db14a3fc276e6df7a457bb349f8ef0b
Gabe Rodriguez <grod220@gmail.com>
2023-12-14 16:48:59 -0800

    Await detection impls (#301)
    * Await detection impls
    
    * Await spent status
    
    * Add async generator merge + tests

--- packages/router/src/grpc/view-protocol-server/note-by-commitment.ts
+++ packages/router/src/grpc/view-protocol-server/note-by-commitment.ts
@@ -1,6 +1,7 @@
 import {
   NoteByCommitmentRequest,
   NoteByCommitmentResponse,
+  SpendableNoteRecord,
 } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1alpha1/view_pb';
 import { ViewReqMessage } from './router';
 import { ServicesInterface } from '@penumbra-zone/types';
@@ -16,10 +17,20 @@ export const handleNoteByCommitmentReq = async (
   const { indexedDb } = await services.getWalletServices();
   if (!req.noteCommitment) throw new Error('Missing note commitment in request');
 
-  // TODO  req.await_detection processing
-
   const noteByCommitment = await indexedDb.getNoteByCommitment(req.noteCommitment);
-  if (!noteByCommitment) return new NoteByCommitmentResponse();
+  if (noteByCommitment) return new NoteByCommitmentResponse({ spendableNote: noteByCommitment });
+  if (!req.awaitDetection) throw new Error('Note not found');
 
-  return new NoteByCommitmentResponse({ spendableNote: noteByCommitment });
+  // Wait until our DB encounters a new note with this commitment
+  const response = new NoteByCommitmentResponse();
+  const subscription = indexedDb.subscribe('SPENDABLE_NOTES');
+
+  for await (const update of subscription) {
+    const note = SpendableNoteRecord.fromJson(update.value);
+    if (note.noteCommitment?.equals(req.noteCommitment)) {
+      response.spendableNote = note;
+      break;
+    }
+  }
+  return response;
 };
