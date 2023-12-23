92958eaae908f4ac8943d1ee313fbe29794f524f
turbocrime <turbocrime@users.noreply.github.com>
2023-12-23 14:33:54 -0800

    lifecycle wip

--- packages/router/src/grpc/offscreen-client.ts
+++ packages/router/src/grpc/offscreen-client.ts
@@ -12,41 +12,60 @@ import {
 
 const OFFSCREEN_DOCUMENT_PATH = '/offscreen.html';
 
-export const offscreenClient = {
-  buildAction: async (
-    arg: WitnessAndBuildRequest,
-    witness: WitnessData,
-    fullViewingKey: string,
-  ): Promise<Action[]> => {
-    const buildRes = await sendOffscreenMessage<ActionBuildMessage>({
-      type: 'BUILD_ACTION',
-      request: {
-        transactionPlan: arg.transactionPlan!.toJson() as ActionBuildRequest['transactionPlan'],
-        witness: witness.toJson() as ActionBuildRequest['witness'],
-        fullViewingKey,
-      },
+let active = 0;
+
+const activateOffscreen = async () => {
+  // @ts-expect-error: no types available yet
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  const getContexts = chrome.runtime.getContexts as (x: unknown) => Promise<unknown[]>;
+  const offscreenExists = getContexts({
+    contextTypes: ['OFFSCREEN_DOCUMENT'],
+    documentUrls: [OFFSCREEN_DOCUMENT_PATH],
+  }).then(contexts => contexts.length > 0);
+
+  if (active && (await offscreenExists)) active++;
+  else {
+    active++;
+    await chrome.offscreen.createDocument({
+      url: OFFSCREEN_DOCUMENT_PATH,
+      reasons: [chrome.offscreen.Reason.WORKERS],
+      justification: 'Manages Penumbra transaction WASM workers',
     });
-    if ('error' in buildRes) throw new Error(String(buildRes.error));
-    const actions = buildRes.data.map(a => Action.fromJson(a));
-    return actions;
-  },
+  }
 };
 
-export const sendOffscreenMessage = async <T extends OffscreenMessage>(
+const releaseOffscreen = () => {
+  active--;
+  if (!active) void chrome.offscreen.closeDocument();
+};
+
+const sendOffscreenMessage = async <T extends OffscreenMessage>(
   req: InternalRequest<T>,
 ): Promise<InternalResponse<ActionBuildMessage>> => {
-  await chrome.offscreen.createDocument({
-    url: OFFSCREEN_DOCUMENT_PATH,
-    reasons: [chrome.offscreen.Reason.WORKERS],
-    justification: 'Manages Penumbra transaction WASM workers',
-  });
-
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
-  const result = (await chrome.runtime.sendMessage({
-    ...req,
-  })) satisfies InternalResponse<ActionBuildMessage>;
-
-  await chrome.offscreen.closeDocument();
-
-  return result;
+  try {
+    await activateOffscreen();
+    return await chrome.runtime.sendMessage<InternalRequest<T>, InternalResponse<T>>(req);
+  } finally {
+    releaseOffscreen();
+  }
 };
+
+const buildAction = async (
+  arg: WitnessAndBuildRequest,
+  witness: WitnessData,
+  fullViewingKey: string,
+): Promise<Action[]> => {
+  const buildRes = await sendOffscreenMessage<ActionBuildMessage>({
+    type: 'BUILD_ACTION',
+    request: {
+      transactionPlan: arg.transactionPlan!.toJson() as ActionBuildRequest['transactionPlan'],
+      witness: witness.toJson() as ActionBuildRequest['witness'],
+      fullViewingKey,
+    },
+  });
+  if ('error' in buildRes) throw new Error(String(buildRes.error));
+  const actions = buildRes.data.map(a => Action.fromJson(a));
+  return actions;
+};
+
+export const offscreenClient = { buildAction };
