194683278d1dd63792a6668951fa47cd89282709
turbocrime <turbocrime@users.noreply.github.com>
2024-01-02 10:06:04 -0800

    replace todo

--- packages/router/src/grpc/view-protocol-server/assets.ts
+++ packages/router/src/grpc/view-protocol-server/assets.ts
@@ -1,6 +1,7 @@
 import type { Impl } from '.';
 import { servicesCtx } from '../../ctx';
 
+// TODO: Implement filters
 export const assets: Impl['assets'] = async function* (_, ctx) {
   const services = ctx.values.get(servicesCtx);
   const { indexedDb } = await services.getWalletServices();
