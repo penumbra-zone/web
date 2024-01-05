7de6f8a14974b26315c1f7cde49865ba7932c4e3
Jesse Pinho <jesse@jessepinho.com>
2024-01-04 21:55:20 -0800

    Simplify test suite structure

--- packages/ui/components/ui/tx/view/address-view.test.tsx
+++ packages/ui/components/ui/tx/view/address-view.test.tsx
@@ -40,46 +40,38 @@ const addressViewWithNormalAddress = new AddressView({
 
 describe('<AddressViewComponent />', () => {
   describe('when `copyable` is `true`', () => {
-    describe('when the address is a one-time address', () => {
-      test('does not show the copy icon', () => {
-        const { queryByTestId } = render(
-          <AddressViewComponent view={addressViewWithOneTimeAddress} copyable />,
-        );
+    test('does not show the copy icon when the address is a one-time address', () => {
+      const { queryByTestId } = render(
+        <AddressViewComponent view={addressViewWithOneTimeAddress} copyable />,
+      );
 
-        expect(queryByTestId('AddressView__CopyIcon')).toBeNull();
-      });
+      expect(queryByTestId('AddressView__CopyIcon')).toBeNull();
     });
 
-    describe('when the address is not a one-time address', () => {
-      test('shows the copy icon', () => {
-        const { queryByTestId } = render(
-          <AddressViewComponent view={addressViewWithNormalAddress} copyable />,
-        );
+    test('shows the copy icon when the address is not a one-time address', () => {
+      const { queryByTestId } = render(
+        <AddressViewComponent view={addressViewWithNormalAddress} copyable />,
+      );
 
-        expect(queryByTestId('AddressView__CopyIcon')).not.toBeNull();
-      });
+      expect(queryByTestId('AddressView__CopyIcon')).not.toBeNull();
     });
   });
 
   describe('when `copyable` is `false`', () => {
-    describe('when the address is a one-time address', () => {
-      test('does not show the copy icon', () => {
-        const { queryByTestId } = render(
-          <AddressViewComponent view={addressViewWithOneTimeAddress} copyable={false} />,
-        );
+    test('does not show the copy icon when the address is a one-time address', () => {
+      const { queryByTestId } = render(
+        <AddressViewComponent view={addressViewWithOneTimeAddress} copyable={false} />,
+      );
 
-        expect(queryByTestId('AddressView__CopyIcon')).toBeNull();
-      });
+      expect(queryByTestId('AddressView__CopyIcon')).toBeNull();
     });
 
-    describe('when the address is not a one-time address', () => {
-      test('does not show the copy icon', () => {
-        const { queryByTestId } = render(
-          <AddressViewComponent view={addressViewWithNormalAddress} copyable={false} />,
-        );
+    test('does not show the copy icon when the address is not a one-time address', () => {
+      const { queryByTestId } = render(
+        <AddressViewComponent view={addressViewWithNormalAddress} copyable={false} />,
+      );
 
-        expect(queryByTestId('AddressView__CopyIcon')).toBeNull();
-      });
+      expect(queryByTestId('AddressView__CopyIcon')).toBeNull();
     });
   });
 });
