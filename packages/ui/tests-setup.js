f54ad3e9e288ab9bedd85433d869f20da808a6af
Jesse Pinho <jesse@jessepinho.com>
2024-01-04 21:22:30 -0800

    Clarify comment

--- packages/ui/tests-setup.js
+++ packages/ui/tests-setup.js
@@ -2,6 +2,7 @@ import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 
 afterEach(() => {
-  // Clear anything rendered by jsdom.
+  // Clear anything rendered by jsdom. (Without this, previous tests can leave
+  // React nodes in the DOM, which can interfere with subsequent tests.)
   cleanup();
 });
