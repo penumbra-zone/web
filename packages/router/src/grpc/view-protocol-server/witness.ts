90f75737a2e05a9cffde23f13b06a4b5ce351191
valentine <valentyn1789@gmail.com>
2024-01-08 18:35:36 +0200

    view service:  impl witness

---
+++ packages/router/src/grpc/view-protocol-server/witness.ts
@@ -0,0 +1,19 @@
+import type { Impl } from '.';
+import { servicesCtx } from '../../ctx';
+
+import { getWitness } from '@penumbra-zone/wasm-ts';
+
+import { ConnectError, Code } from '@connectrpc/connect';
+
+export const witness: Impl['witness'] = async (req, ctx) => {
+  const services = ctx.values.get(servicesCtx);
+
+  if (!req.transactionPlan) throw new ConnectError('No tx plan in request', Code.InvalidArgument);
+
+  const { indexedDb } = await services.getWalletServices();
+  const sct = await indexedDb.getStateCommitmentTree();
+
+  const witnessData = getWitness(req.transactionPlan, sct);
+
+  return { witnessData };
+};
