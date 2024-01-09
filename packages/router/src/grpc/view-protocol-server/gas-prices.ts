eb608656e3db020e3165cae27ad72fdf18350ef5
Jesse Pinho <jesse@jessepinho.com>
2024-01-08 17:19:23 -0800

    Simplify the gasPrices method

--- packages/router/src/grpc/view-protocol-server/gas-prices.ts
+++ packages/router/src/grpc/view-protocol-server/gas-prices.ts
@@ -1,15 +1,13 @@
 import type { Impl } from '.';
 
-import { GasPricesResponse } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1alpha1/view_pb';
-import { GasPrices } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/fee/v1alpha1/fee_pb';
-
 export const gasPrices: Impl['gasPrices'] = () => {
-  return new GasPricesResponse({
-    gasPrices: new GasPrices({
+  /** @todo Replace this stub with real gas prices. */
+  return {
+    gasPrices: {
       blockSpacePrice: 1n,
       compactBlockSpacePrice: 1n,
       executionPrice: 1n,
       verificationPrice: 1n,
-    }),
-  });
+    },
+  };
 };
