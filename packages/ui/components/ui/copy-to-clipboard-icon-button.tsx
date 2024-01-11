6294ec029c974a4dfc45494e9159c1d20a9ca1e0
Jesse Pinho <jesse@jessepinho.com>
2024-01-11 14:45:14 -0800

    Fix failing tests

--- packages/ui/components/ui/copy-to-clipboard-icon-button.tsx
+++ packages/ui/components/ui/copy-to-clipboard-icon-button.tsx
@@ -8,7 +8,12 @@ export const CopyToClipboardIconButton = ({ text }: { text: string }) => {
   return (
     <CopyToClipboard
       text={text}
-      label={<CopyIcon className='text-muted-foreground h-4 w-4 hover:opacity-50' />}
+      label={
+        <CopyIcon
+          className='text-muted-foreground h-4 w-4 hover:opacity-50'
+          data-testid='CopyToClipboardIconButton__icon'
+        />
+      }
       className='h-4 w-4'
     />
   );
