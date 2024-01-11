b981704c311e2deb9ba1157fc7252eb08fb80096
Jesse Pinho <jesse@jessepinho.com>
2024-01-11 15:17:09 -0800

    Use the new Address component inside AddressView

--- packages/ui/components/ui/tx/view/address-view.tsx
+++ packages/ui/components/ui/tx/view/address-view.tsx
@@ -1,6 +1,7 @@
+import { Address } from '../../address';
 import { AddressIcon } from '../../address-icon';
 import { AddressView } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/keys/v1alpha1/keys_pb';
-import { bech32Address, shortenAddress } from '@penumbra-zone/types';
+import { bech32Address } from '@penumbra-zone/types';
 import { CopyToClipboardIconButton } from '../../copy-to-clipboard-icon-button';
 
 interface AddressViewProps {
@@ -13,8 +14,7 @@ interface AddressViewProps {
 export const AddressViewComponent = ({ view, copyable = true }: AddressViewProps) => {
   if (!view?.addressView.value?.address) return <></>;
 
-  const encoded = bech32Address(view.addressView.value.address);
-  const display = shortenAddress(encoded);
+  const encodedAddress = bech32Address(view.addressView.value.address);
 
   const accountIndex =
     view.addressView.case === 'visible' ? view.addressView.value.index?.account : undefined;
@@ -31,7 +31,7 @@ export const AddressViewComponent = ({ view, copyable = true }: AddressViewProps
     <div className='flex items-center gap-2'>
       {accountIndex !== undefined ? (
         <>
-          <AddressIcon address={encoded} size={14} />
+          <AddressIcon address={encodedAddress} size={14} />
 
           <span className='font-bold'>
             {addressIndexLabel}
@@ -39,10 +39,10 @@ export const AddressViewComponent = ({ view, copyable = true }: AddressViewProps
           </span>
         </>
       ) : (
-        <div className='font-mono text-sm italic text-foreground'>{display}</div>
+        <Address address={encodedAddress} />
       )}
 
-      {copyable && <CopyToClipboardIconButton text={encoded} />}
+      {copyable && <CopyToClipboardIconButton text={encodedAddress} />}
     </div>
   );
 };
