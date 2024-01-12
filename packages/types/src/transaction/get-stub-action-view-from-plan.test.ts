4ae58349268669ee9639dd0f79517c2418880b8f
Jesse Pinho <jesse@jessepinho.com>
2024-01-12 14:21:05 -0800

    Assert more properties exist

--- packages/types/src/transaction/get-stub-action-view-from-plan.test.ts
+++ packages/types/src/transaction/get-stub-action-view-from-plan.test.ts
@@ -3,6 +3,9 @@ import { getStubActionViewFromPlan } from './get-stub-action-view-from-plan';
 import { ActionPlan } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/transaction/v1alpha1/transaction_pb';
 
 describe('getStubActionViewFromPlan()', () => {
+  const address =
+    'penumbra147mfall0zr6am5r45qkwht7xqqrdsp50czde7empv7yq2nk3z8yyfh9k9520ddgswkmzar22vhz9dwtuem7uxw0qytfpv7lk3q9dp8ccaw2fn5c838rfackazmgf3ahh09cxmz';
+
   describe('`spend` action', () => {
     test('throws if the address is missing', () => {
       const actionPlan = new ActionPlan({
@@ -14,7 +17,38 @@ describe('getStubActionViewFromPlan()', () => {
         },
       });
 
-      expect(() => getStubActionViewFromPlan({})(actionPlan)).toThrow('No address in action plan');
+      expect(() => getStubActionViewFromPlan({})(actionPlan)).toThrow('No address in spend plan');
+    });
+
+    test('throws if the amount is missing', () => {
+      const actionPlan = new ActionPlan({
+        action: {
+          case: 'spend',
+          value: {
+            note: {
+              address: { altBech32m: address },
+            },
+          },
+        },
+      });
+
+      expect(() => getStubActionViewFromPlan({})(actionPlan)).toThrow('No amount in spend plan');
+    });
+
+    test('throws if the asset ID is missing', () => {
+      const actionPlan = new ActionPlan({
+        action: {
+          case: 'spend',
+          value: {
+            note: {
+              address: { altBech32m: address },
+              value: { amount: { hi: 1n, lo: 0n } },
+            },
+          },
+        },
+      });
+
+      expect(() => getStubActionViewFromPlan({})(actionPlan)).toThrow('No asset ID in spend plan');
     });
   });
 });
--- packages/types/src/transaction/get-stub-action-view-from-plan.ts
+++ packages/types/src/transaction/get-stub-action-view-from-plan.ts
@@ -3,43 +3,56 @@ import {
   ActionView,
 } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/transaction/v1alpha1/transaction_pb';
 import { DenomMetadata } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/asset/v1alpha1/asset_pb';
+import {
+  SpendPlan,
+  SpendView,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/shielded_pool/v1alpha1/shielded_pool_pb';
+
+const getSpendView = (
+  spendPlan: SpendPlan,
+  metadataByAssetId: Record<string, DenomMetadata>,
+): SpendView => {
+  if (!spendPlan.note?.address) throw new Error('No address in spend plan');
+  if (!spendPlan.note.value?.amount) throw new Error('No amount in spend plan');
+  if (!spendPlan.note.value.assetId) throw new Error('No asset ID in spend plan');
+
+  return new SpendView({
+    spendView: {
+      case: 'visible',
+      value: {
+        note: {
+          address: {
+            addressView: {
+              case: 'opaque',
+              value: { address: spendPlan.note.address },
+            },
+          },
+          value: {
+            valueView: {
+              case: 'knownDenom',
+              value: {
+                amount: { hi: 1n, lo: 0n },
+                denom: {
+                  penumbraAssetId: {},
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+};
 
 export const getStubActionViewFromPlan =
   (metadataByAssetId: Record<string, DenomMetadata>) =>
   (actionPlan: ActionPlan): ActionView => {
     switch (actionPlan.action.case) {
       case 'spend':
-        if (!actionPlan.action.value.note?.address) throw new Error('No address in action plan');
-
         return new ActionView({
           actionView: {
             case: 'spend',
-            value: {
-              spendView: {
-                case: 'visible',
-                value: {
-                  note: {
-                    address: {
-                      addressView: {
-                        case: 'opaque',
-                        value: { address: actionPlan.action.value.note.address },
-                      },
-                    },
-                    value: {
-                      valueView: {
-                        case: 'knownDenom',
-                        value: {
-                          amount: { hi: 1n, lo: 0n },
-                          denom: {
-                            penumbraAssetId: {},
-                          },
-                        },
-                      },
-                    },
-                  },
-                },
-              },
-            },
+            value: getSpendView(actionPlan.action.value, metadataByAssetId),
           },
         });
     }
