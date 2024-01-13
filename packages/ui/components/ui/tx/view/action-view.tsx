6ef25bf0e8c70f0432b8eeaee41bcc4d197e9d83
Jesse Pinho <jesse@jessepinho.com>
2024-01-12 20:43:12 -0800

    Simplify ActionViewComponent

--- packages/ui/components/ui/tx/view/action-view.tsx
+++ packages/ui/components/ui/tx/view/action-view.tsx
@@ -3,6 +3,38 @@ import { SpendViewComponent } from './spend';
 import { OutputViewComponent } from './output';
 import { ActionView } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/transaction/v1alpha1/transaction_pb';
 
+const CASE_TO_LABEL: Record<string, string> = {
+  daoDeposit: 'DAO Deposit',
+  daoOutput: 'DAO Output',
+  daoSpend: 'DAO Spend',
+  delegate: 'Delegate',
+  delegatorVote: 'Delegator Vote',
+  ibcRelayAction: 'IBC Relay Action',
+  ics20Withdrawal: 'ICS20 Withdrawal',
+  positionClose: 'Position Close',
+  positionOpen: 'Position Open',
+  positionRewardClaim: 'Position Reward Claim',
+  positionWithdraw: 'Position Withdraw',
+  proposalDepositClaim: 'Proposal Deposit Claim',
+  proposalSubmit: 'Proposal Submit',
+  proposalWithdraw: 'Proposal Withdraw',
+  swap: 'Swap',
+  swapClaim: 'Swap Claim',
+  undelegate: 'Undelegate',
+  undelegateClaim: 'Undelegate Claim',
+  validatorDefinition: 'Validator Definition',
+  validatorVote: 'Validator Vote',
+};
+
+const getLabelForActionCase = (actionCase: string | undefined): string => {
+  if (!actionCase) return '';
+
+  const label = CASE_TO_LABEL[actionCase];
+  if (label) return label;
+
+  return String(actionCase);
+};
+
 export const ActionViewComponent = ({ av: { actionView } }: { av: ActionView }) => {
   switch (actionView.case) {
     case 'spend':
@@ -11,67 +43,7 @@ export const ActionViewComponent = ({ av: { actionView } }: { av: ActionView }) 
     case 'output':
       return <OutputViewComponent value={actionView.value} />;
 
-    case 'swap':
-      return <ViewBox label='Swap' />;
-
-    case 'swapClaim':
-      return <ViewBox label='Swap Claim' />;
-
-    case 'ics20Withdrawal':
-      return <ViewBox label='ICS20 Withdrawal' />;
-
-    case 'delegate':
-      return <ViewBox label='Delegate' />;
-
-    case 'undelegate':
-      return <ViewBox label='Undelegate' />;
-
-    case 'undelegateClaim':
-      return <ViewBox label='Undelegate Claim' />;
-
-    case 'validatorDefinition':
-      return <ViewBox label='Validator Definition' />;
-
-    case 'ibcRelayAction':
-      return <ViewBox label='IBC Relay Action' />;
-
-    case 'proposalSubmit':
-      return <ViewBox label='Proposal Submit' />;
-
-    case 'proposalWithdraw':
-      return <ViewBox label='Proposal Withdraw' />;
-
-    case 'validatorVote':
-      return <ViewBox label='Validator Vote' />;
-
-    case 'delegatorVote':
-      return <ViewBox label='Delegator Vote' />;
-
-    case 'proposalDepositClaim':
-      return <ViewBox label='Proposal Deposit Claim' />;
-
-    case 'positionOpen':
-      return <ViewBox label='Position Open' />;
-
-    case 'positionClose':
-      return <ViewBox label='Position Close' />;
-
-    case 'positionWithdraw':
-      return <ViewBox label='Position Withdraw' />;
-
-    case 'positionRewardClaim':
-      return <ViewBox label='Position Reward Claim' />;
-
-    case 'daoSpend':
-      return <ViewBox label='DAO Spend' />;
-
-    case 'daoOutput':
-      return <ViewBox label='DAO Output' />;
-
-    case 'daoDeposit':
-      return <ViewBox label='DAO Deposit' />;
-
     default:
-      return <ViewBox label={String(actionView.case)} />;
+      return <ViewBox label={getLabelForActionCase(actionView.case)} />;
   }
 };
