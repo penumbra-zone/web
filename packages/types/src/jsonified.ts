688f88a04e46967160fe882b1c78f216036bb776
turbocrime <turbocrime@users.noreply.github.com>
2024-01-15 12:53:14 -0800

    improved - stringify numeric keys - undefined never - single type def

--- packages/types/src/jsonified.ts
+++ packages/types/src/jsonified.ts
@@ -1,16 +1,17 @@
 import type { JsonValue, Message, PlainMessage } from '@bufbuild/protobuf';
 
-export type Jsonified<T> = T extends JsonValue
-  ? T
-  : {
-      // eslint-disable-next-line @typescript-eslint/ban-types -- we're just deleting this fn, it doesn't matter
-      [P in keyof T as T[P] extends Function ? never : P]: JsonField<T[P]>;
-    };
-
 // prettier-ignore
-type JsonField<F> = F extends (JsonValue) ? F
-                  : F extends (Date| Uint8Array | bigint) ? string
-                  : F extends (infer U)[] ? JsonField<U>[]
-                  : F extends Message<infer U> ? Jsonified<PlainMessage<U>>
-                  : F extends object ? Jsonified<F>
-                  : F;
+export type Jsonified<T> = T extends JsonValue ? T
+                         : T extends (Date | Uint8Array | bigint) ? string
+                         : T extends (infer U)[] ? Jsonified<U>[]
+                         : T extends Message<infer U> ? Jsonified<PlainMessage<U>>
+                         : T extends object ? {
+                            [P in keyof T as
+                              // eslint-disable-next-line @typescript-eslint/ban-types
+                              T[P] extends (Function) ? never
+                            : P extends string ? P
+                            : P extends number ? `${P}`
+                            : never
+                            ]: Jsonified<T[P]>;
+                          } 
+                         : T extends undefined ? never : T;
