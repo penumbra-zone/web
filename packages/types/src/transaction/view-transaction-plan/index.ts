4f0925e39c419bf28684cefb5122566fcfd9e97d
Jesse Pinho <jesse@jessepinho.com>
2024-01-15 15:21:03 -0800

    Document viewTransactionPlan()

--- packages/types/src/transaction/view-transaction-plan/index.ts
+++ packages/types/src/transaction/view-transaction-plan/index.ts
@@ -6,6 +6,16 @@ import {
 } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/transaction/v1alpha1/transaction_pb';
 import { viewActionPlan } from './view-action-plan';
 
+/**
+ * Given a `TransactionPlan`, returns a `TransactionView` that can be passed to
+ * a `<TransactionViewComponent />` so as to render the plan as though it's an
+ * already-completed transaction.
+ *
+ * Note that, since it is of course _not_ actually a completed transaction, the
+ * `TransactionView` is sort of a stub -- that is, it will be missing some
+ * properties. Its main purpose is to be able to render the transaction plan,
+ * not to be exhaustive.
+ */
 export const viewTransactionPlan = (
   txPlan: TransactionPlan,
   denomMetadataByAssetId: Record<string, Jsonified<DenomMetadata>>,
