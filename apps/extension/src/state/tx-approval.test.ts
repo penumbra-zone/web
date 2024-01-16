6fa0448fe3418c54c0296e3aadb44183420e69b5
Jesse Pinho <jesse@jessepinho.com>
2024-01-16 08:59:02 -0800

    Fix test name/structure

--- apps/extension/src/state/tx-approval.test.ts
+++ apps/extension/src/state/tx-approval.test.ts
@@ -14,44 +14,44 @@ describe('TX Approval Slice', () => {
     useStore = create<AllSlices>()(initializeStore(mockSessionExtStorage(), mockLocalExtStorage()));
   });
 
-  test('returns undefined', () => {
-    const returnAddress =
-      'penumbra147mfall0zr6am5r45qkwht7xqqrdsp50czde7empv7yq2nk3z8yyfh9k9520ddgswkmzar22vhz9dwtuem7uxw0qytfpv7lk3q9dp8ccaw2fn5c838rfackazmgf3ahh09cxmz';
-    const chainId = 'testnet';
-    const expiryHeight = 100n;
-
-    const validTxnPlan = new TransactionPlan({
-      fee: { amount: { hi: 1n, lo: 0n } },
-      memoPlan: {
-        plaintext: {
-          returnAddress: {
-            altBech32m: returnAddress,
-          },
-          text: 'Memo text here',
-        },
-      },
-      chainId,
-      expiryHeight,
-    });
-
-    useStore.setState({
-      txApproval: {
-        authorizeRequest: {
-          preAuthorizations: [],
-          plan: validTxnPlan.toJson() as Jsonified<TransactionPlan>,
-        },
-        denomMetadataByAssetId: {},
-      },
-    });
-
-    const state = useStore.getState();
-    const result = transactionViewSelector(state);
-    const expected = viewTransactionPlan(validTxnPlan, {});
-
-    expect(result!.equals(expected)).toBe(true);
-  });
-
   describe('transactionViewSelector', () => {
+    test('returns the result of `viewTransactionPlan`', () => {
+      const returnAddress =
+        'penumbra147mfall0zr6am5r45qkwht7xqqrdsp50czde7empv7yq2nk3z8yyfh9k9520ddgswkmzar22vhz9dwtuem7uxw0qytfpv7lk3q9dp8ccaw2fn5c838rfackazmgf3ahh09cxmz';
+      const chainId = 'testnet';
+      const expiryHeight = 100n;
+
+      const validTxnPlan = new TransactionPlan({
+        fee: { amount: { hi: 1n, lo: 0n } },
+        memoPlan: {
+          plaintext: {
+            returnAddress: {
+              altBech32m: returnAddress,
+            },
+            text: 'Memo text here',
+          },
+        },
+        chainId,
+        expiryHeight,
+      });
+
+      useStore.setState({
+        txApproval: {
+          authorizeRequest: {
+            preAuthorizations: [],
+            plan: validTxnPlan.toJson() as Jsonified<TransactionPlan>,
+          },
+          denomMetadataByAssetId: {},
+        },
+      });
+
+      const state = useStore.getState();
+      const result = transactionViewSelector(state);
+      const expected = viewTransactionPlan(validTxnPlan, {});
+
+      expect(result!.equals(expected)).toBe(true);
+    });
+
     describe('when `authorizeRequest` is undefined', () => {
       beforeEach(() => {
         useStore.setState({ txApproval: {} });
