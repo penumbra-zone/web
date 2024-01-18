13440a3eaaf8f471da9c66b7bed6c26c00f0d9cd
Jesse Pinho <jesse@jessepinho.com>
2024-01-18 12:41:52 -0800

    Fix asset paths (#374)
    * Fix paths in vite/index.html/assets
    
    * Fix paths to various other icons

--- apps/webapp/src/components/dashboard/constants.ts
+++ apps/webapp/src/components/dashboard/constants.ts
@@ -10,17 +10,17 @@ export const dashboardTabs = [
 
 export const dashboardTabsHelper: DashboardTabMap = {
   [PagePath.DASHBOARD]: {
-    src: '/funds-gradient.svg',
+    src: './funds-gradient.svg',
     label: 'Asset Balances',
     content: EduPanel.ASSETS,
   },
   [PagePath.TRANSACTIONS]: {
-    src: '/receive-gradient.svg',
+    src: './receive-gradient.svg',
     label: 'Transaction history',
     content: EduPanel.TRANSACTIONS_LIST,
   },
   [PagePath.NFTS]: {
-    src: '/ibc-gradient.svg',
+    src: './ibc-gradient.svg',
     label: 'NFTs history',
     content: EduPanel.TEMP_FILLER,
   },
