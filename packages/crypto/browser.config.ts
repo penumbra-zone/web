f35af188d6e912b194d296363c86b2e2d4111b0e
Jesse Pinho <jesse@jessepinho.com>
2024-01-19 17:32:00 -0800

    Use JSDom instead of Chromium

--- packages/crypto/browser.config.ts
+++ packages/crypto/browser.config.ts
@@ -4,11 +4,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   test: {
     include: ['**/encryption.test.ts', '**/sha256.test.ts'],
-    browser: {
-      name: 'chromium',
-      provider: 'playwright',
-      enabled: true,
-      headless: true,
-    },
+    environment: 'jsdom',
   },
 });
