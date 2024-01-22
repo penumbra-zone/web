fd6c83e0b6fe94e224c4dcfe075a15e8d3220f2e
valentine <valentyn1789@gmail.com>
2024-01-22 19:08:48 +0200

    resolve conflicts

--- packages/router/src/grpc/view-protocol-server/notes-for-voting.ts
+++ packages/router/src/grpc/view-protocol-server/notes-for-voting.ts
@@ -1,10 +1,7 @@
 import type { Impl } from '.';
-import { assertWalletIdCtx, servicesCtx } from '../../ctx';
+import { servicesCtx } from '../../ctx';
 
 export const notesForVoting: Impl['notesForVoting'] = async function* (req, ctx) {
-  const assertWalletId = ctx.values.get(assertWalletIdCtx);
-  await assertWalletId(req.walletId);
-
   const services = ctx.values.get(servicesCtx);
   const { indexedDb } = await services.getWalletServices();
   const responses = await indexedDb.getNotesForVoting(req.addressIndex, req.votableAtHeight);
