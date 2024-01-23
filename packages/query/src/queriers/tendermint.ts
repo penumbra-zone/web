cda0af499363fc44469e1dd36f6b82da604ef9e2
Gabe Rodriguez <grod220@gmail.com>
2024-01-22 23:09:32 -0600

    Warn on unknown tendermint log (#383)
    * Warn on unknown tendermint log
    
    * review updates

--- packages/query/src/queriers/tendermint.ts
+++ packages/query/src/queriers/tendermint.ts
@@ -5,6 +5,9 @@ import { TendermintQuerierInterface } from '@penumbra-zone/types';
 import { TransactionId } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/txhash/v1alpha1/txhash_pb';
 import { Transaction } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/transaction/v1alpha1/transaction_pb';
 
+// Should add more errors as we discover them
+const knownTendermintErrors = ['proof did not verify', 'is not a valid field element'];
+
 export class TendermintQuerier implements TendermintQuerierInterface {
   private readonly client: PromiseClient<typeof TendermintProxyService>;
 
@@ -21,16 +24,20 @@ export class TendermintQuerier implements TendermintQuerierInterface {
     const params = tx.toBinary();
     // Note that "synchronous" here means "wait for the tx to be accepted by
     // the fullnode", not "wait for the tx to be included on chain.
-    const res = await this.client.broadcastTxSync({ params });
-    // TODO: check res.code? other failure states?
-    if (res.log.length) throw new Error(`Tendermint: ${res.log}`);
-    return new TransactionId({ inner: res.hash });
+    const { hash, log } = await this.client.broadcastTxSync({ params });
+
+    if (log) {
+      if (knownTendermintErrors.some(e => log.includes(e))) {
+        throw new Error(`Tendermint: ${log}`);
+      } else {
+        console.warn(log);
+      }
+    }
+    return new TransactionId({ inner: hash });
   }
 
   async getTransaction(txId: TransactionId): Promise<{ height: bigint; transaction: Transaction }> {
     const res = await this.client.getTx({ hash: txId.inner });
-    // TODO: check res.code? other failure states?
-    if (res.txResult?.log.length) throw new Error(`Tendermint: ${res.txResult.log}`);
     const transaction = Transaction.fromBinary(res.tx);
     return { height: res.height, transaction };
   }
