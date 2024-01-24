bb753632679af80107d8264b0c4320bac9f19eb6
Tal Derei <talderei99@gmail.com>
2024-01-23 20:33:00 -0800

    cleanup client context impl

--- packages/router/src/grpc/view-protocol-server/authorize-and-build.ts
+++ packages/router/src/grpc/view-protocol-server/authorize-and-build.ts
@@ -3,26 +3,14 @@ import { custodyCtx, servicesCtx } from '../../ctx';
 import { offscreenClient } from '../offscreen-client';
 import { buildParallel, getWitness } from '@penumbra-zone/wasm-ts';
 import { ConnectError, Code } from '@connectrpc/connect';
-import { AuthorizeRequest } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/custody/v1alpha1/custody_pb';
-import { authorize } from '../custody/authorize';
-import { AuthorizationData } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/transaction/v1alpha1/transaction_pb';
 
 export const authorizeAndBuild: Impl['authorizeAndBuild'] = async (req, ctx) => {
   if (!req.transactionPlan) throw new ConnectError('No tx plan in request', Code.InvalidArgument);
 
-  // Request authorization data without awaiting
-  // const authorizationDataPromise = authorize(
-  //   new AuthorizeRequest({ plan: req.transactionPlan }),
-  //   ctx,
-  // )!;
-
-  // Instance of gRPC client (custodyClient) for the Custody Protocol service
   const custodyClient = ctx.values.get(custodyCtx);
 
-  // Request authorization data (remove await)
-  const { data: authorizationData } = await custodyClient!.authorize({ plan: req.transactionPlan });
+  const authorizationData = custodyClient!.authorize({ plan: req.transactionPlan });
   if (!authorizationData) throw new Error('no authorization data in response');
-  console.log("authorizationData is: ", authorizationData)
 
   const services = ctx.values.get(servicesCtx);
   const {
@@ -43,8 +31,7 @@ export const authorizeAndBuild: Impl['authorizeAndBuild'] = async (req, ctx) => 
     batchActions,
     req.transactionPlan,
     witnessData,
-    // (await authorizationDataPromise).data as AuthorizationData,
-    authorizationData
+    (await authorizationData).data! 
   );
 
   return { transaction };
