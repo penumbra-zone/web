0e52f227224714218c1f8c11eed2f4de2295631d
Jesse Pinho <jesse@jessepinho.com>
2024-01-24 15:43:05 -0800

    Reenable crypto browser tests

--- packages/crypto/browser.config.ts
+++ packages/crypto/browser.config.ts
@@ -3,12 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
-    include: ['**/sha256.test.ts'],
-    /**
-     * @todo: Get the below tests to pass reliably in CI, then uncomment them.
-     * @see https://github.com/penumbra-zone/web/issues/379
-     */
-    // include: ['**/encryption.test.ts', '**/sha256.test.ts'],
+    include: ['**/encryption.test.ts', '**/sha256.test.ts'],
     browser: {
       name: 'chromium',
       provider: 'playwright',
