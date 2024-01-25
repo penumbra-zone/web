47eb12ce6a2ab9d5a70446fc0e94c5c8f70ef8d4
turbocrime <turbocrime@users.noreply.github.com>
2024-01-24 17:39:05 -0800

    organize extension client creation tools

--- apps/extension/src/hooks/chain-id.ts
+++ apps/extension/src/hooks/chain-id.ts
@@ -1,8 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
-import { grpcClient } from '../clients';
+import { viewClient } from '../clients/extension-page';
 
 export const getChainId = async (): Promise<string> => {
-  const res = await grpcClient.appParameters({});
+  const res = await viewClient.appParameters({});
   if (!res.parameters?.chainParams) throw new Error('No chain params in response');
 
   return res.parameters.chainParams.chainId;
