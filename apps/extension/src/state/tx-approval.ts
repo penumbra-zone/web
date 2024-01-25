080c97c6cb902ee99e90252d4ebfe1207cf86a7a
turbocrime <turbocrime@users.noreply.github.com>
2024-01-25 10:55:42 -0800

    simplify stringified, add comments

--- apps/extension/src/state/tx-approval.ts
+++ apps/extension/src/state/tx-approval.ts
@@ -1,13 +1,13 @@
 import type { TransactionView } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/transaction/v1alpha1/transaction_pb';
 import type { AuthorizeRequest } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/custody/v1alpha1/custody_pb';
-import type { Jsonified, Stringified } from '@penumbra-zone/types';
+import type { Stringified } from '@penumbra-zone/types';
 import { AllSlices, SliceCreator } from './index';
 import { MessageResponder } from '@penumbra-zone/types/src/internal-msg/shared';
 import { TxApproval } from '@penumbra-zone/types/src/internal-msg/tx-approval';
 
 export interface TxApprovalSlice {
-  authorizeRequest?: Stringified<Jsonified<AuthorizeRequest>>;
-  transactionViewFromPlan?: Stringified<Jsonified<TransactionView>>;
+  authorizeRequest?: Stringified<AuthorizeRequest>;
+  transactionViewFromPlan?: Stringified<TransactionView>;
   // Holding the message responder function. Service worker will be "awaiting" the call of this.
   responder?: MessageResponder<TxApproval>;
 }
--- packages/types/src/jsonified.ts
+++ packages/types/src/jsonified.ts
@@ -18,21 +18,44 @@ export const isJsonValue = (res: unknown): res is JsonValue =>
   isJsonObject(res) ||
   isJsonArray(res);
 
+/**
+ * This is used to suggest the purpose of a string to a human. It provides no
+ * actual type assistance.
+ */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export type Stringified<J> = J extends Jsonified<infer _T> ? string : never;
+export type Stringified<J> = Jsonified<J> extends JsonValue ? string : never;
 
+/**
+ * This is used to approximate a json-compatible version of a type. Mostly, it
+ * is for *suggesting* what a parameter or return *should* represent, *to a
+ * human*. It performs no actual data conversion, and makes assumptions about
+ * how some json-incompatible types will be converted.
+ *
+ * Our idb schema uses it for identifying key/index types generated from stored
+ * object fields.
+ *
+ * It's based on PlainMessage from `@bufbuild/protobuf`, but notably we treat
+ * any `Message<T>` as a black-box `JsonObject`, to avoid the complex and
+ * configurable details of protojson.
+ *
+ * - members of `JsonValue` are directly equivalent
+ * - `Date`, `Uint8Array`, and `bigint` are assumed to stringify
+ * - Arrays are recursively Jsonified.
+ * - `AnyMessage` becomes generic `JsonObject`
+ * - other objects maintain structure, and filter out functions
+ */
 // prettier-ignore
-export type Jsonified<T> = T extends JsonValue ? T
-                         : T extends (Date | Uint8Array | bigint) ? string
-                         : T extends (infer U)[] ? Jsonified<U>[]
-                         : T extends AnyMessage ? JsonObject
-                         : T extends object ? {
-                            [P in keyof T as
+export type Jsonified<T> = T extends JsonValue ? T                          // JsonValue members equivalent
+                         : T extends (Date | Uint8Array | bigint) ? string  // these types stringify
+                         : T extends (infer U)[] ? Jsonified<U>[]           // recurse into array members
+                         : T extends AnyMessage ? JsonObject                // AnyMessage is a black box
+                         : T extends object ? {                             // any object...
+                            [P in keyof T as                                // ...index into...
                               // eslint-disable-next-line @typescript-eslint/ban-types
-                              T[P] extends (Function) ? never
-                            : P extends string ? P
-                            : P extends number ? `${P}`
-                            : never
-                            ]: Jsonified<T[P]>;
+                              T[P] extends (Function) ? never               // ...strip function members
+                            : P extends string ? P                          // ...keep string keys
+                            : P extends number ? `${P}`                     // ...stringify number keys
+                            : never                                         // ...strip symbol keys
+                            ]: Jsonified<T[P]>;                             // ...recurse object members
                           } 
-                         : T extends undefined ? never : T;
+                         : T extends undefined ? never : T; // undefined not allowed
