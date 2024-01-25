4a91ef50743822d3b994e775a56a946016fa5601
turbocrime <turbocrime@users.noreply.github.com>
2024-01-25 10:28:27 -0800

    JsonValue type guard

--- packages/types/src/jsonified.ts
+++ packages/types/src/jsonified.ts
@@ -1,5 +1,23 @@
 import type { JsonValue, JsonObject, AnyMessage } from '@bufbuild/protobuf';
 
+export const isJsonObject = (res: unknown): res is JsonObject =>
+  res != null &&
+  typeof res === 'object' &&
+  !Array.isArray(res) &&
+  Object.keys(res).every(k => typeof k === 'string') &&
+  Object.values(res).every(isJsonValue);
+
+export const isJsonArray = (res: unknown): res is JsonValue[] =>
+  res != null && Array.isArray(res) && res.every(isJsonValue);
+
+export const isJsonValue = (res: unknown): res is JsonValue =>
+  res === null ||
+  typeof res === 'string' ||
+  typeof res === 'number' ||
+  typeof res === 'boolean' ||
+  isJsonObject(res) ||
+  isJsonArray(res);
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export type Stringified<J> = J extends Jsonified<infer _T> ? string : never;
 
