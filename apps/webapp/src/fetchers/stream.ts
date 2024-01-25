9858e219f0c9207a79d181646b6d0e55408718e2
Jesse Pinho <jesse@jessepinho.com>
2024-01-25 15:07:01 -0800

    Reject with an error in streamToPromise; write tests for this

--- apps/webapp/src/fetchers/stream.ts
+++ apps/webapp/src/fetchers/stream.ts
@@ -56,13 +56,19 @@ export const useCollectedStream = <T>(query: AsyncIterable<T>): CollectedStreamQ
 // Note: If the stream is unending, this will not resolve.
 //       This is only useful if you are collecting all of the fixed set of results together.
 export const streamToPromise = <T>(query: AsyncIterable<T>): Promise<T[]> => {
-  return new Promise<T[]>(resolve => {
+  return new Promise<T[]>((resolve, reject) => {
     void (async function () {
       const result: T[] = [];
-      for await (const res of query) {
-        result.push(res);
+      try {
+        for await (const res of query) {
+          result.push(res);
+        }
+        resolve(result);
+      } catch (e) {
+        if (e instanceof Error) reject(e);
+        else if (typeof e === 'string') reject(new Error(e));
+        else reject(new Error('Unknown error in `streamToPromise`'));
       }
-      resolve(result);
     })();
   });
 };
