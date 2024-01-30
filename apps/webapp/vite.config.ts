4b7ef81b002becb48ba35a3db28e0911ff40fc51
turbocrime <turbocrime@users.noreply.github.com>
2024-01-29 19:21:40 -0800

    webapp bundler chunking

--- apps/webapp/vite.config.ts
+++ apps/webapp/vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
+  build: {
+    rollupOptions: {
+      output: {
+        // configure additional chunks based on file path
+        manualChunks: (id: string) => {
+          if (id.includes('@buf')) return 'protobuf';
+          return;
+        },
+      },
+    },
+  },
 });
