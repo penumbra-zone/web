9c1d36807d32d35a59f02306e2e40f8febac1e25
valentine <valentyn1789@gmail.com>
2024-02-06 13:02:32 +0200

    revert error code

--- packages/router/src/grpc/view-protocol-server/fmd-parameters.ts
+++ packages/router/src/grpc/view-protocol-server/fmd-parameters.ts
@@ -7,6 +7,6 @@ export const fMDParameters: Impl['fMDParameters'] = async (_, ctx) => {
   const services = ctx.values.get(servicesCtx);
   const { indexedDb } = await services.getWalletServices();
   const parameters = await indexedDb.getFmdParams();
-  if (!parameters) throw new ConnectError('No FMD parameters', Code.Unavailable);
+  if (!parameters) throw new ConnectError('No FMD parameters', Code.FailedPrecondition);
   return { parameters };
 };
