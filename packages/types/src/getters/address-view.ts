2a1dae2e8855535067a480f6bf3a9e3cbcb65d6f
Jesse Pinho <jesse@jessepinho.com>
2024-02-09 15:40:15 -0800

    Fix some broken tests/etc. in the send slice

--- packages/types/src/getters/address-view.ts
+++ packages/types/src/getters/address-view.ts
@@ -1,4 +1,5 @@
 import {
+  Address,
   AddressIndex,
   AddressView,
 } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/keys/v1/keys_pb';
@@ -6,4 +7,8 @@ import { createGetter } from './utils/create-getter';
 
 export const getAddressIndex = createGetter<AddressView, AddressIndex>(addressView =>
   addressView?.addressView.case === 'decoded' ? addressView.addressView.value.index : undefined,
+);
+
+export const getAddress = createGetter<AddressView, Address>(
+  addressView => addressView?.addressView.value?.address,
 );
