7df79e83cf66a5f084d3e94f94ec2677adb22968
Gabe Rodriguez <grod220@gmail.com>
2024-02-12 15:09:54 +0100

    Top level packages

--- packages/wasm/compile-crate.ts
+++ packages/wasm/compile-crate.ts
@@ -7,14 +7,14 @@ const TARGETS = ['nodejs', 'bundler']; // can also compile to "web" (not used cu
 TARGETS.forEach(target => {
   // Run wasm-pack for each target
   execSync(
-    `wasm-pack build ./crate --release --target ${target} --out-name index --out-dir ./dist/${target}`,
+    `wasm-pack build ./crate --release --target ${target} --out-name index --out-dir ../../wasm-${target}`,
     {
       stdio: 'inherit',
     },
   );
 
   // Rename package to target-specific names
-  const packageJsonPath = path.join(process.cwd(), `crate/dist/${target}/package.json`);
+  const packageJsonPath = path.join(process.cwd(), `../wasm-${target}/package.json`);
   const packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf-8')) as { name: string };
   packageJson.name = `@penumbra-zone/wasm-${target}`;
   writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf-8');
