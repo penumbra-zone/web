8718a18982af35f2c7437de11ddb31cfa6977560
valentine <valentyn1789@gmail.com>
2024-02-13 20:19:32 +0200

    Temporarily fix IDB streaming for transactionInfo and assets

--- packages/router/src/grpc/view-protocol-server/assets.ts
+++ packages/router/src/grpc/view-protocol-server/assets.ts
@@ -1,6 +1,7 @@
 import type { Impl } from '.';
 import { servicesCtx } from '../../ctx';
 import { assetPatterns } from '@penumbra-zone/constants';
+import { Metadata } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/asset/v1/asset_pb';
 
 export const assets: Impl['assets'] = async function* (req, ctx) {
   const services = ctx.values.get(servicesCtx);
@@ -46,8 +47,15 @@ export const assets: Impl['assets'] = async function* (req, ctx) {
     })),
   ].filter(i => i.includeReq);
 
+  // NOTE: https://github.com/penumbra-zone/web/issues/495
+  //      Results collected a temp workaround until issue is resolved.
+  const assets: { denomMetadata: Metadata }[] = [];
   for await (const metadata of indexedDb.iterateAssetsMetadata()) {
     if (filtered && !patterns.find(p => p.pattern.test(metadata.display))) continue;
-    yield { denomMetadata: metadata };
+    assets.push({ denomMetadata: metadata });
+  }
+
+  for (const asset of assets) {
+    yield asset;
   }
 };
