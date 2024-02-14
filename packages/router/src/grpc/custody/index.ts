f2f9c076a4c71b460f12c1f29e8792d38c38165f
Gabe Rodriguez <grod220@gmail.com>
2024-02-14 03:02:04 +0100

    Remove fvk export (#505)

--- packages/router/src/grpc/custody/index.ts
+++ packages/router/src/grpc/custody/index.ts
@@ -1,11 +1,9 @@
 import type { CustodyService } from '@buf/penumbra-zone_penumbra.connectrpc_es/penumbra/custody/v1/custody_connect';
 import type { ServiceImpl } from '@connectrpc/connect';
 import { authorize } from './authorize';
-import { exportFullViewingKey } from './export-full-viewing-key';
 
 export type Impl = ServiceImpl<typeof CustodyService>;
 
-export const custodyImpl: Omit<Impl, 'confirmAddress'> = {
+export const custodyImpl: Omit<Impl, 'confirmAddress' | 'exportFullViewingKey'> = {
   authorize,
-  exportFullViewingKey,
 };
