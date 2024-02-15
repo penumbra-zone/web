922c11d0dca273a561fc81b52232654d76949fce
Jesse Pinho <jesse@jessepinho.com>
2024-02-15 12:17:44 -0800

    Refactor ExtensionNotInstalled to use SplashPage

--- apps/webapp/src/components/extension-not-installed.tsx
+++ apps/webapp/src/components/extension-not-installed.tsx
@@ -1,6 +1,5 @@
-import { Button } from '@penumbra-zone/ui';
+import { Button, SplashPage } from '@penumbra-zone/ui';
 import { HeadTag } from './metadata/head-tag';
-import { EduInfoCard } from './shared/edu-panels/edu-info-card';
 
 const CHROME_EXTENSION_ID = 'lkpmkhpnhknhmibgnmmhdhgdilepfghe';
 
@@ -8,26 +7,20 @@ export const ExtensionNotInstalled = () => {
   return (
     <>
       <HeadTag />
-      <main className='flex h-screen items-center justify-center'>
-        <EduInfoCard
-          className='order-1 md:order-2'
-          src='./receive-gradient.svg'
-          label='Install our extension'
-        >
-          <div className='flex items-center gap-4'>
-            You need to install the Penumbra Chrome extension to use this app.
-            <Button asChild className='px-4 text-white'>
-              <a
-                href={`https://chrome.google.com/webstore/detail/penumbra-wallet/${CHROME_EXTENSION_ID}`}
-                target='_blank'
-                rel='noreferrer'
-              >
-                Install
-              </a>
-            </Button>
-          </div>
-        </EduInfoCard>
-      </main>
+      <SplashPage title='Install our extension'>
+        <div className='flex items-center justify-between gap-4'>
+          To get started, install the Penumbra Chrome extension.
+          <Button asChild className='px-4 text-white'>
+            <a
+              href={`https://chrome.google.com/webstore/detail/penumbra-wallet/${CHROME_EXTENSION_ID}`}
+              target='_blank'
+              rel='noreferrer'
+            >
+              Install
+            </a>
+          </Button>
+        </div>
+      </SplashPage>
     </>
   );
 };
