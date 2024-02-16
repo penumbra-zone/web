766cc6e318eda539e417d1b0fa2ba6f8b799dce5
Jesse Pinho <jesse@jessepinho.com>
2024-02-16 13:59:37 -0800

    Add a styling fix

--- packages/ui/components/ui/toaster.tsx
+++ packages/ui/components/ui/toaster.tsx
@@ -1,5 +1,7 @@
 import { Toaster as Sonner } from 'sonner';
 
+import './toaster.css';
+
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
