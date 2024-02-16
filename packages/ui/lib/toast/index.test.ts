9f56d56be110e3543d63d0ebe111ab4f0dad3581
Jesse Pinho <jesse@jessepinho.com>
2024-02-15 16:22:30 -0800

    Build out a Toast class for interacting more easily with toasts

---
+++ packages/ui/lib/toast/index.test.ts
@@ -0,0 +1,144 @@
+import { Mock, beforeEach, describe, expect, it, vi } from 'vitest';
+import { Toast } from '.';
+
+const TOAST_FN_PROPERTIES = vi.hoisted(
+  () => ['success', 'info', 'warning', 'error', 'message', 'dismiss', 'loading'] as const,
+);
+
+const MOCK_TOAST_ID = vi.hoisted(() => 1);
+
+type MockWithToastFnProperties = Mock & Record<(typeof TOAST_FN_PROPERTIES)[number], Mock>;
+
+const mockToastFn = vi.hoisted<MockWithToastFnProperties>(() => {
+  const mock = vi.fn().mockReturnValue(1) as MockWithToastFnProperties;
+  TOAST_FN_PROPERTIES.forEach(
+    property => (mock[property] = vi.fn().mockReturnValue(MOCK_TOAST_ID)),
+  );
+
+  return mock;
+});
+
+vi.mock('sonner', async () => ({
+  ...(await vi.importActual('sonner')),
+  toast: mockToastFn,
+}));
+
+describe('Toast', () => {
+  beforeEach(() => {
+    mockToastFn.mockClear();
+    TOAST_FN_PROPERTIES.forEach(property => mockToastFn[property].mockClear());
+  });
+
+  it('does not show the toast until .show() is called', () => {
+    const toast = new Toast().message('Hello, world!').description('Description here');
+
+    expect(mockToastFn).not.toHaveBeenCalled();
+    toast.show();
+    expect(mockToastFn).toHaveBeenCalledTimes(1);
+  });
+
+  describe('.show()', () => {
+    it('shows the toast with the configured message and description', () => {
+      new Toast().message('Hello, world!').description('Description here').show();
+
+      expect(mockToastFn).toHaveBeenCalledWith(
+        'Hello, world!',
+        expect.objectContaining({
+          description: 'Description here',
+        }),
+      );
+    });
+
+    describe('when called on an already-visible toast', () => {
+      let toast: Toast;
+
+      beforeEach(() => {
+        toast = new Toast().message('Hello, world!').show();
+      });
+
+      it("updates the toast with the existing toast's ID", () => {
+        toast.message('New message here').show();
+
+        expect(mockToastFn).toHaveBeenCalledWith('New message here', { id: MOCK_TOAST_ID });
+      });
+    });
+  });
+
+  describe('.message()', () => {
+    it('changes the message once .show() is called', () => {
+      const toast = new Toast().message('Hello, world!').description('Description here').show();
+      toast.message('New message!').show();
+
+      expect(mockToastFn).toHaveBeenCalledTimes(2);
+      expect(mockToastFn).toHaveBeenLastCalledWith(
+        'New message!',
+        expect.objectContaining({
+          description: 'Description here',
+        }),
+      );
+    });
+  });
+
+  describe('.description()', () => {
+    it('changes the description once .show() is called', () => {
+      const toast = new Toast().message('Hello, world!').description('Description here').show();
+      toast.description('New description!').show();
+
+      expect(mockToastFn).toHaveBeenCalledTimes(2);
+      expect(mockToastFn).toHaveBeenLastCalledWith(
+        'Hello, world!',
+        expect.objectContaining({
+          description: 'New description!',
+        }),
+      );
+    });
+  });
+
+  describe('.dismiss()', () => {
+    it('dismisses the toast', () => {
+      const toast = new Toast().message('Hello, world!').description('Description here').show();
+      toast.dismiss();
+
+      expect(mockToastFn.dismiss).toHaveBeenCalled();
+    });
+  });
+
+  describe('.default()', () => {
+    it('switches to use the default toast function', () => {
+      const toast = new Toast()
+        // Switch to error first, to verify that it switches back to the default
+        .error()
+        .message('Hello, world!')
+        .description('Description here')
+        .show();
+
+      toast.default().show();
+
+      expect(mockToastFn).toHaveBeenCalledTimes(1);
+      expect(mockToastFn).toHaveBeenCalledWith(
+        'Hello, world!',
+        expect.objectContaining({
+          description: 'Description here',
+        }),
+      );
+    });
+  });
+
+  (['success', 'info', 'warning', 'error', 'loading'] as const).forEach(toastFn => {
+    describe(`.${toastFn}()`, () => {
+      it(`switches to use the ${toastFn} toast function`, () => {
+        const toast = new Toast().message('Hello, world!').description('Description here').show();
+
+        toast[toastFn]().show();
+
+        expect(mockToastFn[toastFn]).toHaveBeenCalledTimes(1);
+        expect(mockToastFn[toastFn]).toHaveBeenCalledWith(
+          'Hello, world!',
+          expect.objectContaining({
+            description: 'Description here',
+          }),
+        );
+      });
+    });
+  });
+});
---
+++ packages/ui/lib/toast/index.ts
@@ -0,0 +1,109 @@
+import { ReactNode } from 'react';
+import { ExternalToast, toast } from 'sonner';
+
+type ToastId = string | number;
+
+/**
+ * A nice wrapper around the `toast()` function from Sonner that uses the
+ * builder pattern to construct a toast and update it as needed, without having
+ * to keep track of a toast ID.
+ *
+ * @example
+ * ```ts
+ * const toast = new Toast().message('This is a toast').show();
+ *
+ * // later...
+ *
+ * toast
+ *   .error() // Style the toast as an error
+ *   .message('Uh oh -- something went wrong!') // Update the message
+ *   .description('Here are the error details') // Update the description
+ *   .show();
+ * ```
+ *
+ * Note that the toast only gets shown or updated when calling `show()`. That
+ * way, you can configure your toast before anything gets rendered to the user.
+ *
+ * @example
+ * ```ts
+ * // Nothing shown to the user yet:
+ * const toast = new Toast().message('This is a toast');
+ *
+ * // Still nothing shown to the user:
+ * toast.description('Here is a description.');
+ *
+ * // Now the user sees a toast with the above message and description:
+ * toast.show();
+ *
+ * // After this line, the toast still hasn't changed at all, since we haven't
+ * // called `.show()` yet:
+ * toast.error().message('Uh oh!').description('Houston, we have a problem');
+ *
+ * // Finally, the toast updates to show the error message:
+ * toast.show()
+ * ```
+ */
+export class Toast {
+  private toastId: ToastId | undefined;
+  private toastFn: (message: string | React.ReactNode, data?: ExternalToast) => string | number =
+    toast;
+  private _message: ReactNode | undefined;
+  private _description: ReactNode | undefined;
+
+  show() {
+    this.toastId = this.toastFn(this._message, {
+      description: this._description,
+      id: this.toastId,
+    });
+
+    return this;
+  }
+
+  dismiss() {
+    // Only call `toast.dismiss()` if we have a toast ID, since passing
+    // `undefined` to `toast.dismiss()` results in _all_ toasts being dismissed.
+    if (this.toastId) toast.dismiss(this.toastId);
+
+    return this;
+  }
+
+  message(message: ReactNode): this {
+    this._message = message;
+    return this;
+  }
+
+  description(description: ReactNode | undefined): this {
+    this._description = description;
+    return this;
+  }
+
+  default(): this {
+    this.toastFn = toast;
+    return this;
+  }
+
+  success(): this {
+    this.toastFn = toast.success;
+    return this;
+  }
+
+  info(): this {
+    this.toastFn = toast.info;
+    return this;
+  }
+
+  warning(): this {
+    this.toastFn = toast.warning;
+    return this;
+  }
+
+  error(): this {
+    this.toastFn = toast.error;
+    return this;
+  }
+
+  loading(): this {
+    this.toastFn = toast.loading;
+    return this;
+  }
+}
