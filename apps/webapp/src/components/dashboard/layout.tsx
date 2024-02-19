dee2f12d9cf493e65e6cef605762d061dd3d5dec
Gabe Rodriguez <grod220@gmail.com>
2024-02-19 14:25:28 +0100

    Show tooltip icon no matter the toggle state

--- apps/webapp/src/components/dashboard/layout.tsx
+++ apps/webapp/src/components/dashboard/layout.tsx
@@ -13,7 +13,7 @@ export const DashboardLayout = () => {
     <div className='grid gap-6 md:gap-4 lg:grid-cols-3 xl:gap-5'>
       <Card
         gradient
-        className='order-2 flex-1 p-5 md:p-4 lg:order-1 lg:col-span-2 lg:row-span-2 xl:p-5'
+        className='order-2 flex flex-1 flex-col p-5 md:p-4 lg:order-1 lg:col-span-2 lg:row-span-2 xl:p-5'
       >
         <Tabs tabs={dashboardTabs} activeTab={pathname} className='mx-auto w-[75%] lg:w-[372px]' />
         <Outlet />
