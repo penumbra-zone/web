20242571111849408b1f1dc926e3568923117bbf
Jesse Pinho <jesse@jessepinho.com>
2024-02-20 15:42:50 -0800

    Refactor the successful login page to use SplashPage

--- apps/extension/src/routes/page/index.tsx
+++ apps/extension/src/routes/page/index.tsx
@@ -1,15 +1,6 @@
 import { redirect } from 'react-router-dom';
 import { PagePath } from './paths';
-import {
-  Button,
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-  CompressedVideoLogo,
-  FadeTransition,
-} from '@penumbra-zone/ui';
+import { Button, SplashPage } from '@penumbra-zone/ui';
 import { localExtStorage } from '@penumbra-zone/storage';
 
 // Because Zustand initializes default empty (prior to persisted storage synced),
@@ -25,32 +16,20 @@ export const pageIndexLoader = async () => {
 
 export const PageIndex = () => {
   return (
-    <FadeTransition>
-      <div className='absolute inset-0 flex w-screen items-center justify-center'>
-        <CompressedVideoLogo noWords className='w-[850px]' />
-      </div>
-      <Card className='w-[650px] p-6' gradient>
-        <CardHeader>
-          <CardTitle className='bg-gradient-to-r from-teal-400 via-neutral-300 to-orange-400 bg-clip-text text-6xl text-transparent opacity-80'>
-            Successful login
-          </CardTitle>
-          <CardDescription>
-            Use your account to transact, stake, swap or market make. All of it is shielded and
-            private.
-          </CardDescription>
-        </CardHeader>
-        <CardContent className='grid gap-4'>
-          <Button
-            variant='gradient'
-            onClick={() => {
-              window.open('https://app.testnet.penumbra.zone/', '_blank');
-              window.close();
-            }}
-          >
-            Visit testnet web app
-          </Button>
-        </CardContent>
-      </Card>
-    </FadeTransition>
+    <SplashPage
+      title='Successful login'
+      description='Use your account to transact, stake, swap or market make.'
+    >
+      <Button
+        variant='gradient'
+        className='w-full'
+        onClick={() => {
+          window.open('https://app.testnet.penumbra.zone/', '_blank');
+          window.close();
+        }}
+      >
+        Visit testnet web app
+      </Button>
+    </SplashPage>
   );
 };
