6d8b64af1085303a00f39ba9cbfec8d12cf81e49
Jesse Pinho <jesse@jessepinho.com>
2024-02-21 14:44:11 -0800

    Rename type/helper

--- apps/webapp/src/fetchers/balances/by-account.ts
+++ apps/webapp/src/fetchers/balances/by-account.ts
@@ -4,16 +4,13 @@ import {
 } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/keys/v1/keys_pb';
 import { AssetBalance, getAssetBalances } from './index.ts';
 
-export interface AccountGroupedBalances {
+export interface BalancesByAccount {
   index: AddressIndex;
   address: Address;
   balances: AssetBalance[];
 }
 
-const groupByAccount = (
-  acc: AccountGroupedBalances[],
-  curr: AssetBalance,
-): AccountGroupedBalances[] => {
+const groupByAccount = (acc: BalancesByAccount[], curr: AssetBalance): BalancesByAccount[] => {
   if (curr.address.addressView.case !== 'decoded') throw new Error('address is not decoded');
   if (!curr.address.addressView.value.address) throw new Error('no address in address view');
   if (!curr.address.addressView.value.index) throw new Error('no index in address view');
@@ -34,7 +31,7 @@ const groupByAccount = (
   return acc;
 };
 
-export const getBalancesByAccount = async (): Promise<AccountGroupedBalances[]> => {
+export const getBalancesByAccount = async (): Promise<BalancesByAccount[]> => {
   const balances = await getAssetBalances();
   return balances.reduce(groupByAccount, []);
 };
