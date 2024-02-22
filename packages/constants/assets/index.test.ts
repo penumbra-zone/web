1bd0672e50a0a3a30bfc402fec47109f65001baf
Jesse Pinho <jesse@jessepinho.com>
2024-02-22 15:05:47 -0800

    Fix token regexes

--- packages/constants/assets/index.test.ts
+++ packages/constants/assets/index.test.ts
@@ -4,47 +4,51 @@ import { assetPatterns } from '.';
 describe('assetPatterns', () => {
   describe('lpNftPattern', () => {
     it('matches when a string begins with `lpnft_`', () => {
-      expect(assetPatterns.lpNftPattern.test('lpnft_abc123')).toBe(true);
+      expect(assetPatterns.lpNft.test('lpnft_abc123')).toBe(true);
     });
 
     it('does not match when a string contains, but does not begin with, `lpnft_`', () => {
-      expect(assetPatterns.lpNftPattern.test('ibc-transfer/channel-1234/lpnft_abc123')).toBe(false);
+      expect(assetPatterns.lpNft.test('ibc-transfer/channel-1234/lpnft_abc123')).toBe(false);
     });
   });
 
   describe('delegationTokenPattern', () => {
-    it('matches when a string begins with `delegation_`', () => {
-      expect(assetPatterns.delegationTokenPattern.test('delegation_abc123')).toBe(true);
+    it('matches when a string is a valid delegation token name', () => {
+      expect(assetPatterns.delegationToken.test('delegation_penumbravalid1abc123')).toBe(true);
     });
 
-    it('does not match when a string contains, but does not begin with, `delegation_`', () => {
+    it('does not match when a string contains, but does not begin with, a valid delegation token name', () => {
       expect(
-        assetPatterns.delegationTokenPattern.test('ibc-transfer/channel-1234/delegation_abc123'),
+        assetPatterns.delegationToken.test(
+          'ibc-transfer/channel-1234/delegation_penumbravalid1abc123',
+        ),
       ).toBe(false);
     });
   });
 
   describe('proposalNftPattern', () => {
     it('matches when a string begins with `proposal_`', () => {
-      expect(assetPatterns.proposalNftPattern.test('proposal_abc123')).toBe(true);
+      expect(assetPatterns.proposalNft.test('proposal_abc123')).toBe(true);
     });
 
     it('does not match when a string contains, but does not begin with, `proposal_`', () => {
-      expect(
-        assetPatterns.proposalNftPattern.test('ibc-transfer/channel-1234/proposal_abc123'),
-      ).toBe(false);
+      expect(assetPatterns.proposalNft.test('ibc-transfer/channel-1234/proposal_abc123')).toBe(
+        false,
+      );
     });
   });
 
   describe('unbondingTokenPattern', () => {
-    it('matches when a string begins with `uunbonding_epoch_N_`', () => {
-      expect(assetPatterns.unbondingTokenPattern.test('uunbonding_epoch_1_abc123')).toBe(true);
+    it('matches when a string is a valid unbonding token name', () => {
+      expect(assetPatterns.unbondingToken.test('uunbonding_epoch_1_penumbravalid1abc123')).toBe(
+        true,
+      );
     });
 
-    it('does not match when a string contains, but does not begin with, `uunbonding_epoch_N_`', () => {
+    it('does not match when a string contains, but does not begin with, a valid unbonding token name', () => {
       expect(
-        assetPatterns.unbondingTokenPattern.test(
-          'ibc-transfer/channel-1234/uunbonding_epoch_1_abc123',
+        assetPatterns.unbondingToken.test(
+          'ibc-transfer/channel-1234/uunbonding_epoch_1_penumbravalid1abc123',
         ),
       ).toBe(false);
     });
@@ -52,13 +56,13 @@ describe('assetPatterns', () => {
 
   describe('votingReceiptPattern', () => {
     it('matches when a string begins with `voted_on_`', () => {
-      expect(assetPatterns.votingReceiptPattern.test('voted_on_abc123')).toBe(true);
+      expect(assetPatterns.votingReceipt.test('voted_on_abc123')).toBe(true);
     });
 
     it('does not match when a string contains, but does not begin with, `voted_on_`', () => {
-      expect(
-        assetPatterns.votingReceiptPattern.test('ibc-transfer/channel-1234/voted_on_abc123'),
-      ).toBe(false);
+      expect(assetPatterns.votingReceipt.test('ibc-transfer/channel-1234/voted_on_abc123')).toBe(
+        false,
+      );
     });
   });
 });
--- packages/constants/assets/index.ts
+++ packages/constants/assets/index.ts
@@ -20,11 +20,26 @@ export interface UnbondingCaptureGroups {
   id: string;
 }
 
-// Source of truth for regex patterns: https://github.com/penumbra-zone/penumbra/blob/main/crates/core/asset/src/asset/registry.rs
+/**
+ * Call `.test()` on these RegExp patterns to test whether a token is of a given type.
+ *
+ * NOTE - SECURITY IMPLICATIONS: These RegExps each assert that the given prefix
+ * is at the _beginning_ of the string. This ensures that they are
+ * differentiated from IBC deposits with the same asset name. Penumbra prefixes
+ * IBC deposit assets with the provenance of the token, so that if someone
+ * creates a token called, e.g., `delegation_whatever` and sends it to a
+ * Penumbra user via IBC, it will show up in Penumbra as
+ * `transfer/channel-1234/delegation_whatever`. Thus, asserting that the
+ * `delegation_` prefix occurs at the _beginning_ of the string, as we're doing
+ * below, ensures that it's actually the type we expect it to be.
+ *
+ * Source of truth for regex patterns:
+ * https://github.com/penumbra-zone/penumbra/blob/main/crates/core/asset/src/asset/registry.rs
+ */
 export const assetPatterns: AssetPattens = {
   lpNft: new RegExp(/^lpnft_/),
   delegationToken: new RegExp(
-    /.*delegation_(?<bech32IdentityKey>penumbravalid1(?<id>[a-zA-HJ-NP-Z0-9]+))$/,
+    /^delegation_(?<bech32IdentityKey>penumbravalid1(?<id>[a-zA-HJ-NP-Z0-9]+))$/,
   ),
   proposalNft: new RegExp(/^proposal_/),
   /**
@@ -32,7 +47,7 @@ export const assetPatterns: AssetPattens = {
    * the extra `u` at the beginning.
    */
   unbondingToken: new RegExp(
-    /.*unbonding_epoch_(?<epoch>[0-9]+)_penumbravalid1(?<id>[a-zA-HJ-NP-Z0-9]+)$/,
+    /^uunbonding_epoch_(?<epoch>[0-9]+)_penumbravalid1(?<id>[a-zA-HJ-NP-Z0-9]+)$/,
   ),
   votingReceipt: new RegExp(/^voted_on_/),
 };
