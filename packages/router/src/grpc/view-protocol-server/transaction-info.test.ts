39604792b7671b253e3b724fb90d1f1f168c1fb3
Valentine <walik19988@gmail.com>
2024-02-22 10:28:49 +0000

    add tests for transaction-info (#584)

---
+++ packages/router/src/grpc/view-protocol-server/transaction-info.test.ts
@@ -0,0 +1,120 @@
+import { ViewService } from '@buf/penumbra-zone_penumbra.connectrpc_es/penumbra/view/v1/view_connect';
+import { servicesCtx } from '../../ctx';
+
+import { createContextValues, createHandlerContext, HandlerContext } from '@connectrpc/connect';
+
+import { beforeEach, describe, expect, test, vi } from 'vitest';
+
+import {
+  TransactionInfo,
+  TransactionInfoRequest,
+  TransactionInfoResponse,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1/view_pb';
+import { IndexedDbMock, MockServices } from './test-utils';
+import { Services } from '@penumbra-zone/services';
+import { transactionInfo } from './transaction-info';
+
+describe('TransactionInfo request handler', () => {
+  let mockServices: MockServices;
+  let mockCtx: HandlerContext;
+  let mockIndexedDb: IndexedDbMock;
+  let req: TransactionInfoRequest;
+
+  beforeEach(() => {
+    vi.resetAllMocks();
+
+    const mockIterateTransactionInfo = {
+      next: vi.fn(),
+      [Symbol.asyncIterator]: () => mockIterateTransactionInfo,
+    };
+
+    mockIndexedDb = {
+      iterateTransactionInfo: () => mockIterateTransactionInfo,
+    };
+
+    mockServices = {
+      getWalletServices: vi.fn(() => Promise.resolve({ indexedDb: mockIndexedDb })),
+    };
+
+    mockCtx = createHandlerContext({
+      service: ViewService,
+      method: ViewService.methods.transactionInfo,
+      protocolName: 'mock',
+      requestMethod: 'MOCK',
+      contextValues: createContextValues().set(servicesCtx, mockServices as unknown as Services),
+    });
+
+    for (const record of testData) {
+      mockIterateTransactionInfo.next.mockResolvedValueOnce({
+        value: record,
+      });
+    }
+    mockIterateTransactionInfo.next.mockResolvedValueOnce({
+      done: true,
+    });
+    req = new TransactionInfoRequest();
+  });
+
+  test('should get all transactions if startHeight and endHeight are not set in request', async () => {
+    const responses: TransactionInfoResponse[] = [];
+    for await (const res of transactionInfo(req, mockCtx)) {
+      responses.push(new TransactionInfoResponse(res));
+    }
+    expect(responses.length).toBe(4);
+  });
+
+  test('should get only transactions whose height is not greater than endHeight', async () => {
+    const responses: TransactionInfoResponse[] = [];
+    req.endHeight = 2525n;
+    for await (const res of transactionInfo(req, mockCtx)) {
+      responses.push(new TransactionInfoResponse(res));
+    }
+    expect(responses.length).toBe(3);
+  });
+
+  test('should receive only transactions whose height is not less than startHeight', async () => {
+    const responses: TransactionInfoResponse[] = [];
+    req.startHeight = 2525n;
+    for await (const res of transactionInfo(req, mockCtx)) {
+      responses.push(new TransactionInfoResponse(res));
+    }
+    expect(responses.length).toBe(2);
+  });
+
+  test('should receive only transactions whose height is between startHeight and endHeight inclusive', async () => {
+    const responses: TransactionInfoResponse[] = [];
+    req.startHeight = 998n;
+    req.endHeight = 2525n;
+    for await (const res of transactionInfo(req, mockCtx)) {
+      responses.push(new TransactionInfoResponse(res));
+    }
+    expect(responses.length).toBe(2);
+  });
+});
+
+const testData: TransactionInfo[] = [
+  TransactionInfo.fromJson({
+    height: '222',
+    id: {
+      inner: '1MI8IG5D3MQj3s1j0MXTwCQtAaVbwTlPkW8Qdz1EVIo=',
+    },
+  }),
+  TransactionInfo.fromJson({
+    height: '1000',
+    id: {
+      inner: '2MI8IG5D3MQj3s1j0MXTwCQtAaVbwTlPkW8Qdz1EVIo=',
+    },
+  }),
+  TransactionInfo.fromJson({
+    height: '2525',
+    id: {
+      inner: '3MI8IG5D3MQj3s1j0MXTwCQtAaVbwTlPkW8Qdz1EVIo=',
+    },
+  }),
+  TransactionInfo.fromJson({
+    height: '12255',
+    id: {
+      inner: '4MI8IG5D3MQj3s1j0MXTwCQtAaVbwTlPkW8Qdz1EVIo=',
+    },
+  }),
+];
