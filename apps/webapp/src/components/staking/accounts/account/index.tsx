fa05271da093f349f840311f5013022ed6789c90
Jesse Pinho <jesse@jessepinho.com>
2024-02-22 17:49:58 -0800

    Extract lots of logic to a hook

--- apps/webapp/src/components/staking/accounts/account/index.tsx
+++ apps/webapp/src/components/staking/accounts/account/index.tsx
@@ -1,82 +1,21 @@
-import {
-  bech32IdentityKey,
-  getDisplayDenomFromView,
-  getIdentityKeyFromValidatorInfo,
-} from '@penumbra-zone/types';
 import { Card, CardContent, CardHeader, CardTitle } from '@penumbra-zone/ui';
 import { BalancesByAccount } from '../../../../fetchers/balances/by-account';
 import { ValueViewComponent } from '@penumbra-zone/ui/components/ui/tx/view/value';
-import { useContext, useMemo } from 'react';
-import { STAKING_TOKEN, assetPatterns } from '@penumbra-zone/constants';
-import { ValidatorInfoContext } from '../../validator-info-context';
-import { ValidatorInfo } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/stake/v1/stake_pb';
-import { ValueView } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/asset/v1/asset_pb';
 import { ValidatorsTable } from '../../validators-table';
-import { AssetBalance } from '../../../../fetchers/balances';
 import { StakingActions } from '.././staking-actions';
-import { getBech32IdentityKeyFromValueView } from './helpers';
-
-const hasDelegationOrUnbondingTokens =
-  (delegationBalances: AssetBalance[], unbondingBalances: AssetBalance[]) =>
-  (validatorInfo: ValidatorInfo): boolean => {
-    const identityKey = bech32IdentityKey(getIdentityKeyFromValidatorInfo(validatorInfo));
-    const hasDelegationTokens = delegationBalances.some(
-      ({ value }) => getBech32IdentityKeyFromValueView(value) === identityKey,
-    );
-    const hasUnbondingTokens = unbondingBalances.some(
-      ({ value }) => getBech32IdentityKeyFromValueView(value) === identityKey,
-    );
-
-    return hasDelegationTokens || hasUnbondingTokens;
-  };
+import { useValidatorsForAccount } from './use-validators-for-account';
 
 export const Account = ({ account }: { account: BalancesByAccount }) => {
-  const { unstakedBalance, delegationBalances, unbondingBalances } = useMemo(
-    () => ({
-      unstakedBalance: account.balances.find(
-        balance => getDisplayDenomFromView(balance.value) === STAKING_TOKEN,
-      ),
-      delegationBalances: account.balances.filter(balance =>
-        assetPatterns.delegationToken.test(getDisplayDenomFromView(balance.value)),
-      ),
-      unbondingBalances: account.balances.filter(balance =>
-        assetPatterns.unbondingToken.test(getDisplayDenomFromView(balance.value)),
-      ),
-    }),
-    [account.balances],
-  );
-
   const {
-    validatorInfos: unfilteredValidatorInfos,
-    votingPowerByValidatorInfo,
-    loading,
     error,
-  } = useContext(ValidatorInfoContext);
-
-  const validatorInfos = unfilteredValidatorInfos.filter(
-    hasDelegationOrUnbondingTokens(delegationBalances, unbondingBalances),
-  );
-
-  const tokensByValidatorInfo: Map<
-    ValidatorInfo,
-    { delegation?: ValueView; unbonding?: ValueView }
-  > = validatorInfos.reduce((prev, curr) => {
-    const validatorIdentityKey = bech32IdentityKey(getIdentityKeyFromValidatorInfo(curr));
-
-    const tokens = {
-      delegation: delegationBalances.find(
-        balance => getBech32IdentityKeyFromValueView(balance.value) === validatorIdentityKey,
-      )?.value,
-      unbonding: unbondingBalances.find(
-        balance => getBech32IdentityKeyFromValueView(balance.value) === validatorIdentityKey,
-      )?.value,
-    };
-    prev.set(curr, tokens);
-
-    return prev;
-  }, new Map<ValidatorInfo, { delegation?: ValueView; unbonding?: ValueView }>());
-
-  const shouldRender = !!unstakedBalance || !!validatorInfos.length;
+    loading,
+    shouldRender,
+    delegationTokensByValidatorInfo,
+    unbondingTokensByValidatorInfo,
+    validatorInfos,
+    votingPowerByValidatorInfo,
+    unstakedTokens: unstakedBalance,
+  } = useValidatorsForAccount(account.balances);
 
   if (!shouldRender) return null;
 
@@ -88,7 +27,7 @@ export const Account = ({ account }: { account: BalancesByAccount }) => {
       <CardContent>
         {unstakedBalance && (
           <div className='flex gap-1'>
-            <ValueViewComponent view={unstakedBalance.value} />
+            <ValueViewComponent view={unstakedBalance} />
             <span>available to delegate</span>
           </div>
         )}
@@ -103,8 +42,8 @@ export const Account = ({ account }: { account: BalancesByAccount }) => {
               renderStaking={validatorInfo => (
                 <StakingActions
                   canDelegate={!!unstakedBalance}
-                  delegationTokens={tokensByValidatorInfo.get(validatorInfo)?.delegation}
-                  unbondingTokens={tokensByValidatorInfo.get(validatorInfo)?.unbonding}
+                  delegationTokens={delegationTokensByValidatorInfo.get(validatorInfo)}
+                  unbondingTokens={unbondingTokensByValidatorInfo.get(validatorInfo)}
                 />
               )}
             />
---
+++ apps/webapp/src/components/staking/accounts/account/use-validators-for-account.ts
@@ -0,0 +1,169 @@
+import { ValidatorInfo } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/stake/v1/stake_pb';
+import { useContext, useMemo } from 'react';
+import { ValidatorInfoContext } from '../../validator-info-context';
+import {
+  bech32IdentityKey,
+  getDisplayDenomFromView,
+  getIdentityKeyFromValidatorInfo,
+} from '@penumbra-zone/types';
+import { AssetBalance } from '../../../../fetchers/balances';
+import { getBech32IdentityKeyFromValueView } from './helpers';
+import { STAKING_TOKEN, assetPatterns } from '@penumbra-zone/constants';
+import { ValueView } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/asset/v1/asset_pb';
+
+const hasDelegationOrUnbondingTokens =
+  (delegationBalances: AssetBalance[], unbondingBalances: AssetBalance[]) =>
+  (validatorInfo: ValidatorInfo): boolean => {
+    const identityKey = bech32IdentityKey(getIdentityKeyFromValidatorInfo(validatorInfo));
+    const hasDelegationTokens = delegationBalances.some(
+      ({ value }) => getBech32IdentityKeyFromValueView(value) === identityKey,
+    );
+    const hasUnbondingTokens = unbondingBalances.some(
+      ({ value }) => getBech32IdentityKeyFromValueView(value) === identityKey,
+    );
+
+    return hasDelegationTokens || hasUnbondingTokens;
+  };
+
+const getTokensByValidatorInfo = (
+  validatorInfos: ValidatorInfo[],
+  delegationBalances: AssetBalance[],
+  unbondingBalances: AssetBalance[],
+) => {
+  const delegationTokensByValidatorInfo = new Map<ValidatorInfo, ValueView | undefined>();
+  const unbondingTokensByValidatorInfo = new Map<ValidatorInfo, ValueView | undefined>();
+
+  validatorInfos.forEach(validatorInfo => {
+    const validatorIdentityKey = bech32IdentityKey(getIdentityKeyFromValidatorInfo(validatorInfo));
+
+    delegationTokensByValidatorInfo.set(
+      validatorInfo,
+      delegationBalances.find(
+        balance => getBech32IdentityKeyFromValueView(balance.value) === validatorIdentityKey,
+      )?.value,
+    );
+    unbondingTokensByValidatorInfo.set(
+      validatorInfo,
+      unbondingBalances.find(
+        balance => getBech32IdentityKeyFromValueView(balance.value) === validatorIdentityKey,
+      )?.value,
+    );
+  });
+
+  return { delegationTokensByValidatorInfo, unbondingTokensByValidatorInfo };
+};
+
+interface UseValidatorsForAccount {
+  /**
+   * Any error in loading validators.
+   */
+  error: unknown;
+
+  /**
+   * Whether we're currently still loading validators.
+   */
+  loading: boolean;
+
+  /**
+   * Whether we should render anything for this account. Will be `true` if
+   * this account has any unstaked tokens, OR any delegation tokens bonded to
+   * an active validator, OR any unbonding tokens from an active validator.
+   */
+  shouldRender: boolean;
+
+  /**
+   * A `Map` of `ValidatorInfo`s to `ValueView`s containing this account's
+   * balance of delegation tokens to that validator. Will be `undefined` if this
+   * account has no delegation tokens for the validator.
+   */
+  delegationTokensByValidatorInfo: Map<ValidatorInfo, ValueView | undefined>;
+
+  /**
+   * A `Map` of `ValidatorInfo`s to `ValueView`s containing this account's
+   * balance of unbonding tokens to that validator. Will be `undefined` if this
+   * account has no delegation tokens for the validator.
+   */
+  unbondingTokensByValidatorInfo: Map<ValidatorInfo, ValueView | undefined>;
+
+  /**
+   * An array of `ValidatorInfo`s that this account has delegation tokens in, or
+   * unbonding tokens from. Use this array to render validators relevant to this
+   * account.
+   */
+  validatorInfos: ValidatorInfo[];
+
+  /**
+   * A `Map` of `ValidatorInfo`s to their voting power, expressed as an integer
+   * percentage between 0-100.
+   */
+  votingPowerByValidatorInfo: Map<ValidatorInfo, number>;
+
+  /**
+   * A `ValueView` representing this account's amount of unstaked tokens --
+   * i.e., tokens available for staking in a validator. Will be `undefined` if
+   * this account has no unstaked tokens.
+   */
+  unstakedTokens: ValueView | undefined;
+}
+
+/**
+ * This hook is a bit of a bear, but at present, there doesn't seem to be a
+ * better way to do it. (If you find a better way, feel free to refactor.) To
+ * optimize performance, it makes heavy use of `useMemo()`.
+ */
+export const useValidatorsForAccount = (assetBalances: AssetBalance[]): UseValidatorsForAccount => {
+  // First, grab validator infos from context.
+  const {
+    validatorInfos: unfilteredValidatorInfos,
+    votingPowerByValidatorInfo,
+    loading,
+    error,
+  } = useContext(ValidatorInfoContext);
+
+  // Then, grab the unstaked tokens, delegation tokens, and unbonding tokens
+  // from this account.
+  const { unstakedTokens, delegationBalances, unbondingBalances } = useMemo(
+    () => ({
+      unstakedTokens: assetBalances.find(
+        balance => getDisplayDenomFromView(balance.value) === STAKING_TOKEN,
+      )?.value,
+      delegationBalances: assetBalances.filter(balance =>
+        assetPatterns.delegationToken.test(getDisplayDenomFromView(balance.value)),
+      ),
+      unbondingBalances: assetBalances.filter(balance =>
+        assetPatterns.unbondingToken.test(getDisplayDenomFromView(balance.value)),
+      ),
+    }),
+    [assetBalances],
+  );
+
+  // Next, filter validator infos for just those that we have delegation or
+  // unbonding tokens for.
+  const validatorInfos = useMemo(
+    () =>
+      unfilteredValidatorInfos.filter(
+        hasDelegationOrUnbondingTokens(delegationBalances, unbondingBalances),
+      ),
+    [unfilteredValidatorInfos, delegationBalances, unbondingBalances],
+  );
+
+  // Lastly, create `Map`s of validator infos to the delegation and unbonding
+  // tokens we hold.
+  const { delegationTokensByValidatorInfo, unbondingTokensByValidatorInfo } = useMemo(
+    () => getTokensByValidatorInfo(validatorInfos, delegationBalances, unbondingBalances),
+    [validatorInfos, delegationBalances, unbondingBalances],
+  );
+
+  const shouldRender = !!unstakedTokens || !!validatorInfos.length;
+
+  return {
+    validatorInfos,
+    delegationTokensByValidatorInfo,
+    unbondingTokensByValidatorInfo,
+    votingPowerByValidatorInfo,
+    unstakedTokens,
+    loading,
+    error,
+    shouldRender,
+  };
+};
