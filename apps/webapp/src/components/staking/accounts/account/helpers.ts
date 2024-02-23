d0bceb428be0bed8df4a730923acf5755999683f
Jesse Pinho <jesse@jessepinho.com>
2024-02-22 20:39:10 -0800

    Add docs

--- apps/webapp/src/components/staking/accounts/account/helpers.ts
+++ apps/webapp/src/components/staking/accounts/account/helpers.ts
@@ -2,6 +2,15 @@ import { ValueView } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core
 import { assetPatterns } from '@penumbra-zone/constants';
 import { getDisplayDenomFromView } from '@penumbra-zone/types';
 
+/**
+ * When given a `ValueView` with a delegation token or an unbonding token,
+ * returns the bech32 representation of the validator's identity key that the
+ * delegation/unbonding token is tied to. Throws if passed a value view that
+ * doesn't contain a delegation or unbonding token.
+ *
+ * This is useful for when you're trying to match a delegation or unbonding
+ * token to a given validator by identity key.
+ */
 export const getBech32IdentityKeyFromValueView = (valueView: ValueView): string => {
   const displayDenom = getDisplayDenomFromView(valueView);
 
