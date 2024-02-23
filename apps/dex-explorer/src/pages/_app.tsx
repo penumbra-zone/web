e61e04ba1a03c44b385f2d5a3abd6eb9e6c9a884
Phil <pbal@fordham.edu>
2024-02-22 18:06:56 -0700

    Some cleaner data rendering

--- apps/dex-explorer/src/pages/_app.tsx
+++ apps/dex-explorer/src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppProps } from "next/app";
 import "@/global.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
