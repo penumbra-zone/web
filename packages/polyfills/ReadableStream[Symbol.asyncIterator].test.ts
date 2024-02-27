9f3f597f5cb7bcaf83622b791fd41a15c95ba53a
turbocrime <turbocrime@users.noreply.github.com>
2024-02-27 14:58:55 -0800

    more iteration tests

--- packages/polyfills/ReadableStream[Symbol.asyncIterator].test.ts
+++ packages/polyfills/ReadableStream[Symbol.asyncIterator].test.ts
@@ -21,7 +21,26 @@ describe('ReadableStream[Symbol.asyncIterator]', () => {
     expect(values).toEqual([1, 2, 3]);
   });
 
-  test('ReadableStream can be yielded to by a generator', async () => {
+  test("ReadableStream can't be iterated with synchronous `for`", () => {
+    const stream = new ReadableStream<number>({
+      start(controller) {
+        controller.enqueue(1);
+        controller.enqueue(2);
+        controller.enqueue(3);
+        controller.close();
+      },
+    });
+
+    const values = [];
+    try {
+      // @ts-expect-error - this should be an error
+      for (const value of stream) values.push(value);
+    } catch (e) {
+      expect(e).toBeInstanceOf(TypeError);
+    }
+  });
+
+  test('ReadableStream can be yielded to by an async generator', async () => {
     async function* genFn() {
       yield* new ReadableStream<number>({
         start(controller) {
@@ -36,5 +55,20 @@ describe('ReadableStream[Symbol.asyncIterator]', () => {
     const values = [];
     for await (const value of genFn()) values.push(value);
     expect(values).toEqual([1, 2, 3]);
+  });
+
+  test("ReadableStream can't be yielded to by a synchronous generator", async () => {
+    function* genFn() {
+      // @ts-expect-error - this should be an error
+      yield* new ReadableStream<number>({
+        start(controller) {
+          controller.enqueue(1);
+          controller.enqueue(2);
+          controller.enqueue(3);
+          controller.close();
+        },
+      });
+    }
+    expect(genFn()).toThrow(TypeError);
   });
 });
