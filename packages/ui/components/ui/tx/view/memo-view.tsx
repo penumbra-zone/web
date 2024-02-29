a8373ffa0c0534059487c496ca17ba968992f2da
turbocrime <turbocrime@users.noreply.github.com>
2024-02-29 02:25:20 -0800

    remove condition to transaction.tsx, use switch

--- packages/ui/components/ui/tx/view/memo-view.tsx
+++ packages/ui/components/ui/tx/view/memo-view.tsx
@@ -2,33 +2,32 @@ import { MemoView } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/
 import { AddressViewComponent } from './address-view';
 import { ViewBox, ViewSection } from './viewbox';
 
-export const MemoViewComponent = ({ memo }: { memo: MemoView | undefined }) => {
-  if (!memo?.memoView.case) return null;
-  if (memo.memoView.case === 'visible') {
-    const mv = memo.memoView.value;
-    const text = mv.plaintext?.text;
-
-    return (
-      <ViewSection heading='Memo'>
-        <ViewBox
-          label='Memo Text'
-          visibleContent={
-            /* Ensure that an empty memo string is rendered as a blank space, highlighting absence */
-            text ? <div>{text}</div> : <div>&nbsp;</div>
-          }
-        />
-        <ViewBox
-          label='Sender Return Address'
-          visibleContent={<AddressViewComponent view={mv.plaintext!.returnAddress} />}
-        />
-      </ViewSection>
-    );
+export const MemoViewComponent = ({ memo: { memoView } }: { memo: MemoView }) => {
+  switch (memoView.case) {
+    case 'visible':
+      return (
+        <ViewSection heading='Memo'>
+          <ViewBox
+            label='Memo Text'
+            visibleContent={
+              // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+              <div>{memoView.value.plaintext?.text || '&nbsp;'}</div>
+            }
+          />
+          <ViewBox
+            label='Sender Return Address'
+            visibleContent={<AddressViewComponent view={memoView.value.plaintext!.returnAddress} />}
+          />
+        </ViewSection>
+      );
+    case 'opaque':
+      return (
+        <ViewSection heading='Memo'>
+          <ViewBox label='Memo Text' />
+          <ViewBox label='Sender Return Address' />
+        </ViewSection>
+      );
+    default:
+      return <span>Invalid MemoView</span>;
   }
-
-  return (
-    <ViewSection heading='Memo'>
-      <ViewBox label='Memo Text' />
-      <ViewBox label='Sender Return Address' />
-    </ViewSection>
-  );
 };
