5ae475fcdb83b40e528705de7c17db15139aa9d8
turbocrime <turbocrime@users.noreply.github.com>
2024-03-01 20:25:12 -0800

    updated approval

--- apps/extension/src/approve-origin.ts
+++ apps/extension/src/approve-origin.ts
@@ -1,16 +1,17 @@
 import { JsonValue } from '@bufbuild/protobuf';
 import { ConnectError } from '@connectrpc/connect';
 import { errorFromJson } from '@connectrpc/connect/protocol-connect';
-import { localExtStorage } from '@penumbra-zone/storage';
+import { OriginRecord, localExtStorage } from '@penumbra-zone/storage';
 import { OriginApproval, PopupType } from './message/popup';
 import { popup } from './popup';
+import Map from '@penumbra-zone/polyfills/Map.groupBy';
 
 export const originAlreadyApproved = async (url: string): Promise<boolean> => {
   // parses the origin and returns a consistent format
   const urlOrigin = new URL(url).origin;
   // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
   const connectedSites = await localExtStorage.get('connectedSites');
-  return Boolean(connectedSites[urlOrigin]);
+  return Boolean(connectedSites.find(site => site.origin === urlOrigin)?.attitude);
 };
 
 export const approveOrigin = async ({
@@ -23,9 +24,14 @@ export const approveOrigin = async ({
 
   // parses the origin and returns a consistent format
   const urlOrigin = new URL(senderOrigin).origin;
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
   const connectedSites = await localExtStorage.get('connectedSites');
-  if (typeof connectedSites[urlOrigin] === 'boolean') return Boolean(connectedSites[urlOrigin]);
+
+  const siteRecords = Map.groupBy(connectedSites, site => site.origin === urlOrigin);
+  const irrelevant = siteRecords.get(false) ?? []; // we need to handle these in order to write back to storage
+  const [existingRecord, ...extraRecords] = siteRecords.get(true) ?? [];
+
+  if (extraRecords.length) throw new Error('Multiple records for the same origin');
+  if (existingRecord) return Boolean(existingRecord.attitude);
 
   const res = await popup<OriginApproval>({
     type: PopupType.OriginApproval,
@@ -35,10 +41,18 @@ export const approveOrigin = async ({
       title: tab.title,
     },
   });
+
   if ('error' in res)
     throw errorFromJson(res.error as JsonValue, undefined, ConnectError.from(res));
 
-  connectedSites[urlOrigin] = res.data.attitude;
-  void localExtStorage.set('connectedSites', connectedSites); // TODO: is there a race condition here?
-  return Boolean(connectedSites[urlOrigin]);
+  const createRecord: OriginRecord = {
+    ...res.data,
+    date: Date.now(),
+  };
+
+  // TODO: is there a race condition here?
+  // if something has written after our initial read, we'll clobber them
+  void localExtStorage.set('connectedSites', [createRecord, ...irrelevant]);
+
+  return Boolean(createRecord.attitude);
 };
