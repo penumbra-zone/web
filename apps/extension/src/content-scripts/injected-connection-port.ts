072e862e42a87f56c830558bd02213d40c5da71e
turbocrime <turbocrime@users.noreply.github.com>
2024-03-04 08:31:26 -0800

    comment content script

--- apps/extension/src/content-scripts/injected-connection-port.ts
+++ apps/extension/src/content-scripts/injected-connection-port.ts
@@ -2,6 +2,7 @@ import { Prax } from '../message/prax';
 import { PraxConnectionPort } from './message';
 import { CRSessionClient } from '@penumbra-zone/transport-chrome/session-client';
 
+// this inits the session client that transports messages on the DOM channel through the Chrome runtime
 const initOnce = (req: unknown, _sender: chrome.runtime.MessageSender, respond: () => void) => {
   if (req !== Prax.InitConnection) return false;
   chrome.runtime.onMessage.removeListener(initOnce);
