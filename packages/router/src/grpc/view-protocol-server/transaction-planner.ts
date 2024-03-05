bd6081c19de24807d09d3fb71eacae319febd96f
valentine <valentyn1789@gmail.com>
2024-03-05 02:10:48 +0200

    Throw errors if we have no data to build TxPlan

--- packages/router/src/grpc/view-protocol-server/transaction-planner.ts
+++ packages/router/src/grpc/view-protocol-server/transaction-planner.ts
@@ -1,6 +1,7 @@
 import type { Impl } from '.';
 import { servicesCtx } from '../../ctx';
 import { planTransaction } from '@penumbra-zone/wasm';
+import { Code, ConnectError } from '@connectrpc/connect';
 
 export const transactionPlanner: Impl['transactionPlanner'] = async (req, ctx) => {
   const services = ctx.values.get(servicesCtx);
@@ -8,6 +9,14 @@ export const transactionPlanner: Impl['transactionPlanner'] = async (req, ctx) =
     indexedDb,
     viewServer: { fullViewingKey },
   } = await services.getWalletServices();
+
+  const fmdParams = await indexedDb.getFmdParams();
+  if (!fmdParams) throw new ConnectError('FmdParameters not available', Code.FailedPrecondition);
+  const { chainId, sctParams } = (await indexedDb.getAppParams()) ?? {};
+  if (!sctParams) throw new ConnectError('SctParameters not available', Code.FailedPrecondition);
+  if (!chainId) throw new ConnectError('ChainId not available', Code.FailedPrecondition);
+  const gasPrices = await indexedDb.getGasPrices();
+  if (!gasPrices) throw new ConnectError('Gas prices is not available', Code.FailedPrecondition);
 
   const idbConstants = indexedDb.constants();
 
