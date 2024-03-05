297a5af2c67c4eee9a5b411f3c653db991757c79
Phil <pbal@fordham.edu>
2024-03-05 15:00:12 -0700

    Indexer endpt

--- apps/dex-explorer/src/constants/configConstants.ts
+++ apps/dex-explorer/src/constants/configConstants.ts
@@ -1,9 +1,12 @@
 export interface GrpcConfig {
   grpcEndpoint: string;
+  indexerEndpoint: string;
 }
 
 const defaultPenumbraGrpcEndpoint = "https://grpc.testnet.penumbra.zone";
+const defaultIndexerEndpoint = "postgresql://penumbra:penumbra@db.testnet-preview.penumbra.zone:5432/penumbra?sslmode=disable"
 
 export const testnetConstants: GrpcConfig = {
   grpcEndpoint: process.env.PENUMBRA_GRPC_ENDPOINT ? process.env.PENUMBRA_GRPC_ENDPOINT : defaultPenumbraGrpcEndpoint,
+  indexerEndpoint: process.env.PENUMBRA_INDEXER_ENDPOINT ? process.env.PENUMBRA_INDEXER_ENDPOINT : defaultIndexerEndpoint
 };
