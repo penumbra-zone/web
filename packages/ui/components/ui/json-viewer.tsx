91d1123f27c55260d3664855db9b26d2d54d273b
Gabe Rodriguez <grod220@gmail.com>
2024-03-06 23:11:06 +0100

    Use react-json-view-lite

--- packages/ui/components/ui/json-viewer.tsx
+++ packages/ui/components/ui/json-viewer.tsx
@@ -1,39 +1,29 @@
-import { JsonViewer as TextaJsonViewer, NamedColorspace } from '@textea/json-viewer';
-
 import type { JsonObject, JsonValue } from '@bufbuild/protobuf';
-
-export const customTheme: NamedColorspace = {
-  scheme: 'custom',
-  author: 'penumbra',
-  base00: '#000000', // Background Color
-  base01: '#ffffff',
-  base02: '#212121', // Nested section border color
-  base03: '#ffffff',
-  base04: '#464646', // Item count Color
-  base05: '#ffffff',
-  base06: '#ffffff',
-  base07: '#ffffff',
-  base08: '#ffffff',
-  base09: '#ff7900', // Value Color
-  base0A: '#ffffff',
-  base0B: '#98c379',
-  base0C: '#09eed1', // Array index Color
-  base0D: '#ffffff',
-  base0E: '#ffffff',
-  base0F: '#ffffff',
-};
+import { JsonView } from 'react-json-view-lite';
 
 export const JsonViewer = ({ jsonObj }: { jsonObj: JsonObject | JsonValue[] }) => {
   return (
     <div className='mt-5 rounded bg-black p-5'>
-      <TextaJsonViewer
-        value={jsonObj}
-        style={{ fontFamily: 'Iosevka Term' }}
-        theme={customTheme}
-        rootName={false}
-        enableClipboard={true}
-        defaultInspectDepth={2}
-        quotesOnKeys={false}
+      <JsonView
+        data={jsonObj}
+        shouldExpandNode={level => level < 2}
+        style={{
+          container: 'bg-black whitespace-pre-wrap break-words font-mono -mx-4',
+          basicChildStyle: 'mx-4 py-[2px]',
+          label: 'font-semibold mr-1.5 text-gray-200',
+          nullValue: 'text-red-600',
+          undefinedValue: 'text-red-600',
+          stringValue: 'text-amber-600',
+          booleanValue: 'text-purple-600',
+          numberValue: 'text-teal-600',
+          otherValue: 'text-blue-600',
+          punctuation: 'text-gray-500 mr-1.5',
+          collapseIcon:
+            'text-teal-200 text-[10px] mr-1.5 select-none cursor-pointer after:content-["▼"]',
+          expandIcon:
+            'text-teal-200 text-[10px] mr-1.5 select-none cursor-pointer after:content-["▶"]',
+          collapsedContent: 'text-amber-600 mr-1.5 after:content-["..."] after:text-xs',
+        }}
       />
     </div>
   );
