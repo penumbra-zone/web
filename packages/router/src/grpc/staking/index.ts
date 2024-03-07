45fd7488c0fce53569289d642668a68a0e015b77
Jesse Pinho <jesse@jessepinho.com>
2024-03-07 11:08:51 -0800

    Add a validatorPenalty method handler to our impl of Staking

--- packages/router/src/grpc/staking/index.ts
+++ packages/router/src/grpc/staking/index.ts
@@ -1,7 +1,12 @@
 import { QueryService as StakingService } from '@buf/penumbra-zone_penumbra.connectrpc_es/penumbra/core/component/stake/v1/stake_connect';
 import { ServiceImpl } from '@connectrpc/connect';
+
 import { validatorInfo } from './validator-info';
+import { validatorPenalty } from './validator-penalty';
 
 export type Impl = ServiceImpl<typeof StakingService>;
 
-export const stakingImpl: Pick<Impl, 'validatorInfo'> = { validatorInfo };
+export const stakingImpl: Pick<Impl, 'validatorInfo' | 'validatorPenalty'> = {
+  validatorInfo,
+  validatorPenalty,
+};
