09baa374009f01e180bea75a251a2e92de073a45
Jesse Pinho <jesse@jessepinho.com>
2024-03-06 19:02:29 -0800

    Implement StakingService in the extension

---
+++ packages/router/src/grpc/staking/validator-info.ts
@@ -0,0 +1,22 @@
+import { getStateEnumFromValidatorInfo } from '@penumbra-zone/getters';
+import { Impl } from '.';
+import { servicesCtx } from '../../ctx';
+import {
+  ValidatorInfoResponse,
+  ValidatorState_ValidatorStateEnum,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/stake/v1/stake_pb';
+
+export const validatorInfo: Impl['validatorInfo'] = async function* (req, ctx) {
+  const services = ctx.values.get(servicesCtx);
+  const { indexedDb } = await services.getWalletServices();
+
+  for await (const validatorInfo of indexedDb.iterateValidatorInfos()) {
+    if (
+      !req.showInactive &&
+      getStateEnumFromValidatorInfo(validatorInfo) === ValidatorState_ValidatorStateEnum.INACTIVE
+    )
+      continue;
+
+    yield new ValidatorInfoResponse({ validatorInfo });
+  }
+};
