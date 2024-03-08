9223a3cd1aced0e5796b3775509ae8f3bc7f1612
Jesse Pinho <jesse@jessepinho.com>
2024-03-08 11:59:56 -0800

    Simplify use of state in Delegations; remove unused props

--- apps/minifront/src/components/staking/account/delegations.tsx
+++ apps/minifront/src/components/staking/account/delegations.tsx
@@ -1,34 +1,25 @@
 import { getIdentityKeyFromValueView } from '@penumbra-zone/getters';
 import { VotingPowerAsIntegerPercentage, bech32IdentityKey } from '@penumbra-zone/types';
-import { useStore } from '../../../state';
-import { stakingSelector } from '../../../state/staking';
+import { AllSlices, useStore } from '../../../state';
 import { DelegationValueView } from './delegation-value-view';
 import { ValueView } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/asset/v1/asset_pb';
+import { useShallow } from 'zustand/react/shallow';
 
 const getVotingPowerAsIntegerPercentage = (
   votingPowerByValidatorInfo: Record<string, VotingPowerAsIntegerPercentage>,
   delegation: ValueView,
 ) => votingPowerByValidatorInfo[bech32IdentityKey(getIdentityKeyFromValueView(delegation))];
 
-export const Delegations = () => {
-  const {
-    account,
-    delegationsByAccount,
-    unstakedTokensByAccount,
-    votingPowerByValidatorInfo,
+const delegationsSelector = (state: AllSlices) => ({
+  delegations: state.staking.delegationsByAccount.get(state.staking.account) ?? [],
+  unstakedTokens: state.staking.unstakedTokensByAccount.get(state.staking.account),
+  votingPowerByValidatorInfo: state.staking.votingPowerByValidatorInfo,
+});
 
-    action,
-    amount,
-    delegate,
-    loading,
-    onClickActionButton,
-    onClose,
-    setAmount,
-    undelegate,
-    validatorInfo,
-  } = useStore(stakingSelector);
-  const unstakedTokens = unstakedTokensByAccount.get(account);
-  const delegations = delegationsByAccount.get(account) ?? [];
+export const Delegations = () => {
+  const { delegations, unstakedTokens, votingPowerByValidatorInfo } = useStore(
+    useShallow(delegationsSelector),
+  );
 
   return (
     <div className='mt-8 flex flex-col gap-8'>
@@ -41,15 +32,6 @@ export const Delegations = () => {
             votingPowerByValidatorInfo,
             delegation,
           )}
-          amount={amount}
-          delegate={delegate}
-          onClickActionButton={onClickActionButton}
-          onClose={onClose}
-          setAmount={setAmount}
-          undelegate={undelegate}
-          action={action}
-          validatorInfo={validatorInfo}
-          loading={loading}
         />
       ))}
     </div>
