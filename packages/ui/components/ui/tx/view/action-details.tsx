a45c3bc5f5ca00c6a92b5b9799f5a726d233fba2
Jesse Pinho <jesse@jessepinho.com>
2024-03-08 14:43:07 -0800

    Create ActionDetails component

---
+++ packages/ui/components/ui/tx/view/action-details.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from 'react';
+
+/**
+ * Render key/value pairs inside a `<ViewBox />`.
+ *
+ * @example
+ * ```tsx
+ * <ActionDetails>
+ *   <ActionDetails.Row label="Validator">
+ *     <IdentityKeyComponent identityKey={identityKey} />
+ *   </ActionDetails.Row>
+ * </ActionDetails>
+ * ```
+ */
+export const ActionDetails = ({ children }: { children: ReactNode }) => {
+  return <div className='flex flex-col gap-2'>{children}</div>;
+};
+
+const ActionDetailsRow = ({ label, children }: { label: string; children: ReactNode }) => {
+  return (
+    <div className='flex items-center justify-between'>
+      <span className='break-keep'>{label}</span>
+      <span className='mx-2 h-[1px] min-w-8 grow border-b-[1px] border-dotted border-light-brown' />
+      {children}
+    </div>
+  );
+};
+
+ActionDetails.Row = ActionDetailsRow;
