2e027a5113f4cbe7754149c77dd8a7b0e9cb1a63
turbocrime <turbocrime@users.noreply.github.com>
2024-03-08 09:16:17 -0800

    more compact https

--- apps/extension/src/shared/components/display-origin-url.tsx
+++ apps/extension/src/shared/components/display-origin-url.tsx
@@ -1,13 +1,16 @@
 export const DisplayOriginURL = ({ url: { protocol, hostname, port } }: { url: URL }) => (
   <span className='font-mono'>
-    <span className='inline-block tracking-[-0.1em] brightness-90 saturate-50'>
-      {protocol}
-      {'//'}
+    <span className='inline-block tracking-[-0.11em] brightness-90 saturate-50'>
+      {protocol.split(':')[0]}
+      <span className='tracking-[-0.21em]'>{':/'}</span>
+      {'/'}
     </span>
     <span className='tracking-[-0.05em] brightness-150 saturate-150'>{hostname}</span>
     {port ? (
       <>
-        <span className='inline-block tracking-[-0.075em]  brightness-90 saturate-50'> {':'} </span>
+        <span className='ml-[-0.05em] inline-block tracking-[-0.11em] brightness-90 saturate-50'>
+          {':'}
+        </span>
         <span className='tracking-[-0.05em] brightness-150 saturate-150'>{port}</span>
       </>
     ) : null}
