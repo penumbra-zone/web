50027a53bfd5b8dd1cb4cb90abb7d65872446642
Jesse Pinho <jesse@jessepinho.com>
2024-03-08 14:54:01 -0800

    Extract Separator component

--- packages/ui/components/ui/tx/view/action-details.tsx
+++ packages/ui/components/ui/tx/view/action-details.tsx
@@ -16,11 +16,17 @@ export const ActionDetails = ({ children }: { children: ReactNode }) => {
   return <div className='flex flex-col gap-2'>{children}</div>;
 };
 
+const Separator = () => (
+  <div className='mx-2 h-[1px] min-w-8 grow border-b-[1px] border-dotted border-light-brown' />
+);
+
 const ActionDetailsRow = ({ label, children }: { label: string; children: ReactNode }) => {
   return (
     <div className='flex items-center justify-between'>
       <span className='break-keep'>{label}</span>
-      <span className='mx-2 h-[1px] min-w-8 grow border-b-[1px] border-dotted border-light-brown' />
+
+      <Separator />
+
       {children}
     </div>
   );
