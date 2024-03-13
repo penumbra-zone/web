288bd30b2be6755d57a818040f824fc24aa610e5
Jesse Pinho <jesse@jessepinho.com>
2024-03-13 16:25:53 -0700

    Render a 404 page

---
+++ apps/minifront/src/components/not-found.tsx
@@ -0,0 +1,5 @@
+import { SplashPage } from '@penumbra-zone/ui';
+
+export const NotFound = () => {
+  return <SplashPage title='404'>That page could not be found. </SplashPage>;
+};
--- apps/minifront/src/components/shared/error-boundary.tsx
+++ apps/minifront/src/components/shared/error-boundary.tsx
@@ -1,13 +1,15 @@
-import { useRouteError } from 'react-router-dom';
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
 import { PraxNotConnectedError } from '@penumbra-zone/client';
 import { ExtensionNotConnected } from '../extension-not-connected';
+import { NotFound } from '../not-found';
 
 export const ErrorBoundary = () => {
   const error = useRouteError();
 
   if (error instanceof PraxNotConnectedError) return <ExtensionNotConnected />;
+  if (isRouteErrorResponse(error) && error.status === 404) return <NotFound />;
 
-  console.error(error);
+  console.error('ErrorBoundary caught error:', error);
 
   return (
     <div className='text-red'>
