c7f5b9f1c0204d3aa2043c591bcd3d9526d69fee
Gabe Rodriguez <grod220@gmail.com>
2024-03-13 22:07:00 +0100

    Fix null bug

--- apps/extension/src/routes/popup/popup-needs.ts
+++ apps/extension/src/routes/popup/popup-needs.ts
@@ -2,17 +2,19 @@ import { redirect } from 'react-router-dom';
 import { PopupPath } from './paths';
 import { localExtStorage, sessionExtStorage } from '@penumbra-zone/storage';
 
-export const needsLogin = async (): Promise<Response | undefined> => {
+export const needsLogin = async (): Promise<Response | null> => {
   const password = await sessionExtStorage.get('passwordKey');
-  if (password) return;
+  if (password) return null;
 
   return redirect(PopupPath.LOGIN);
 };
 
 export const needsOnboard = async () => {
   const wallets = await localExtStorage.get('wallets');
-  if (wallets.length) return;
+  if (wallets.length) return null;
 
   void chrome.runtime.openOptionsPage();
   window.close();
+
+  return null;
 };
