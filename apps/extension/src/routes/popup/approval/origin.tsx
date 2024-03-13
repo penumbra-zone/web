c02348796e382adc08af1935df90179516574b94
turbocrime <134443988+turbocrime@users.noreply.github.com>
2024-03-13 12:37:00 -0700

    always display entire origin url (#726)

--- apps/extension/src/routes/popup/approval/origin.tsx
+++ apps/extension/src/routes/popup/approval/origin.tsx
@@ -82,8 +82,8 @@ export const OriginApproval = () => {
                     )}
                   </div>
                 </div>
-                <div className='z-30 flex h-11 w-full items-center overflow-x-scroll rounded-lg bg-background p-2 text-muted-foreground [scrollbar-color:red_red]'>
-                  <div className='mx-auto items-center p-2 text-center'>
+                <div className='z-30 flex min-h-11 w-full items-center overflow-x-scroll rounded-lg bg-background p-2 text-muted-foreground [scrollbar-color:red_red]'>
+                  <div className='mx-auto items-center p-2 text-center leading-[0.8em]'>
                     <DisplayOriginURL url={new URL(requestOrigin)} />
                   </div>
                 </div>
