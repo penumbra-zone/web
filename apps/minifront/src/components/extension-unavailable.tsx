2d09088f13e0d8fcf6ef25d876fa9e89e5bb591b
Jesse Pinho <jesse@jessepinho.com>
2024-03-14 12:19:30 -0700

    Link to the node's status page from the error page

--- apps/minifront/src/components/extension-unavailable.tsx
+++ apps/minifront/src/components/extension-unavailable.tsx
@@ -1,6 +1,9 @@
 import { SplashPage } from '@penumbra-zone/ui';
 import { HeadTag } from './metadata/head-tag';
 
+const NODE_STATUS_PAGE_URL =
+  window.location.hostname === 'localhost' ? 'http://localhost:5174' : '/';
+
 export const ExtensionUnavailable = () => {
   return (
     <>
@@ -8,7 +11,12 @@ export const ExtensionUnavailable = () => {
 
       <SplashPage title='Penumbra extension unavailble'>
         {`We can't currently connect to the Penumbra extension. This could be because the RPC node
-        that you're connected to is down. Please try reloading this page; and if that doesn't work,
+        that you're connected to is down. Please check `}
+        <a href={NODE_STATUS_PAGE_URL} className='underline'>
+          the node&apos;s status page
+        </a>
+        ;
+        {` and if that doesn't work,
         please consider using a different RPC node.`}
       </SplashPage>
     </>
