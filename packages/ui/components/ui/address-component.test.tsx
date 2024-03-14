14cbc874fd2b9b28983d203b2cd07bfcbbd275fe
Jesse Pinho <jesse@jessepinho.com>
2024-03-14 16:12:36 -0700

    Update tests

--- packages/ui/components/ui/address-component.test.tsx
+++ packages/ui/components/ui/address-component.test.tsx
@@ -3,28 +3,27 @@ import { describe, expect, test } from 'vitest';
 import { render } from '@testing-library/react';
 import { Address } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/keys/v1/keys_pb';
 import { bech32ToUint8Array } from '@penumbra-zone/types/src/address';
-import { shortenAddress } from '@penumbra-zone/types/src/string';
 
 describe('<AddressComponent />', () => {
   const address =
     'penumbra1u7dk4qw6fz3vlwyjl88vlj6gqv4hcmz2vesm87t7rm0lvwmgqqkrp3zrdmfg6et86ggv4nwmnc8vy39uxyacwm8g7trk77ad0c8n4qt76ncvuukx6xlj8mskhyjpn4twkpwwl2';
   const pbAddress = new Address({ inner: bech32ToUint8Array(address) });
 
-  test('renders the shortened address', () => {
+  test('renders the address', () => {
     const { baseElement } = render(<AddressComponent address={pbAddress} />);
 
-    expect(baseElement).toHaveTextContent(shortenAddress(address));
+    expect(baseElement).toHaveTextContent(address);
   });
 
   test('uses text-muted-foreground for non-ephemeral addresses', () => {
     const { getByText } = render(<AddressComponent address={pbAddress} />);
 
-    expect(getByText(shortenAddress(address))).toHaveClass('text-muted-foreground');
+    expect(getByText(address)).toHaveClass('text-muted-foreground');
   });
 
   test('uses colored text for ephemeral addresses', () => {
     const { getByText } = render(<AddressComponent address={pbAddress} ephemeral />);
 
-    expect(getByText(shortenAddress(address))).toHaveClass('text-[#8D5728]');
+    expect(getByText(address)).toHaveClass('text-[#8D5728]');
   });
 });
