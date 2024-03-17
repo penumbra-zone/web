e6cf284bac4bf863b73da5ab91271b45bf072e27
Phil <pbal@fordham.edu>
2024-03-16 19:40:25 -0600

    checkpoint:

--- src/components/charts/depthChart.tsx
+++ src/components/charts/depthChart.tsx
@@ -15,16 +15,7 @@ interface DepthChartProps {
 const DepthChart = ({ buySideData, sellSideData }: DepthChartProps) => {
   return (
     <>
-      <VStack align={"left"} spacing={2}>
-        <Text fontSize={"large"} fontWeight={"bold"} paddingBottom=".2em">
-          Depth Chart
-        </Text>
-        <HStack align={"center"} spacing={2}>
-          <Text fontSize={"small"} fontFamily={"monospace"}>
-            Data{" "}
-          </Text>
-        </HStack>
-      </VStack>
+    <Text>Hmm</Text>
     </>
   );
 };
