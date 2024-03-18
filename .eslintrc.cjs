36684792a7fc6902594c7b8c6d343c8a4bd71717
Tal Derei <talderei99@gmail.com>
2024-03-17 20:41:42 -0700

    remove custom

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,6 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-custom`
   extends: [
-    'custom',
     'prettier',
     'eslint:recommended',
     'turbo',
