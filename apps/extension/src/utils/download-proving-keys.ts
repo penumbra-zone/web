f03d5e6cecee82c06a67f32d30b5148796471140
Tal Derei <talderei99@gmail.com>
2024-03-20 16:39:56 -0700

    address partial comments

--- apps/extension/src/utils/download-proving-keys.ts
+++ apps/extension/src/utils/download-proving-keys.ts
@@ -12,13 +12,16 @@ const main = () => {
 
   console.log('Downloading keys', VERSION_TAG, Object.values(provingKeys).join(', '));
 
+  // Check if the --force flag is provided in the command-line arguments
+  const forceDownload = process.argv.includes('--force');
+
   fs.mkdirSync(binDir, { recursive: true });
   const downloads = Object.values(provingKeys).map(async name => {
     const file = `${name}_pk.bin`;
     const outputPath = path.join(binDir, file);
 
     // Check if the file already exists
-    if (fs.existsSync(outputPath)) {
+    if (!forceDownload && fs.existsSync(outputPath)) {
       console.log(`${file} already downloaded.`);
       return;
     }
