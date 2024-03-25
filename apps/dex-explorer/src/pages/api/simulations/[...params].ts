34435bd872372112c8e2eb7a8fe94c16d55b8ce5
Phil <30321052+philipjames44@users.noreply.github.com>
2024-03-25 15:48:18 -0600

    Migrate grpc queries server side (#7)
    * Migrate grpc queries server side
    
    * fix lp view
    
    * Shielded pool -> server side
    
    * fix rendering for if position not found
    
    * Move sim queries server side

---
+++ apps/dex-explorer/src/pages/api/simulations/[...params].ts
@@ -0,0 +1,94 @@
+// pages/api/simulations/[...params].ts
+import { testnetConstants } from "../../../constants/configConstants";
+import { SimulationQuerier } from "@/utils/protos/services/dex/simulated-trades";
+import { base64ToUint8Array } from "../../../utils/math/base64";
+import {
+  SimulateTradeRequest,
+  SimulateTradeRequest_Routing_SingleHop,
+  SimulateTradeResponse,
+  SwapExecution,
+} from "@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/dex/v1/dex_pb";
+import { tokenConfigMapOnSymbol } from "@/constants/tokenConstants";
+import { joinLoHi, splitLoHi } from "@/utils/math/hiLo";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function simulationHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const params = req.query.params as string[];
+
+  const token1 = params[0] || null;
+  const token2 = params[1] || null;
+  const amountIn = params[2] || null;
+  const singleHop = params[3] || null;
+
+  let isSingleHop = false;
+
+  try {
+    if (!token1 || !token2 || !amountIn) {
+      return res.status(400).json({ error: "Invalid query parameters" });
+    }
+
+    if (String(singleHop).toLocaleLowerCase() === "singlehop") {
+      isSingleHop = true;
+    }
+
+    // Get token 1 & 2
+    const asset1Token = tokenConfigMapOnSymbol[token1];
+    const asset2Token = tokenConfigMapOnSymbol[token2];
+
+    const sim_querier = new SimulationQuerier({
+      grpcEndpoint: testnetConstants.grpcEndpoint,
+    });
+
+    const amtIn = splitLoHi(BigInt(Number(amountIn) * 10 ** asset1Token.decimals));
+
+    let simRequest = new SimulateTradeRequest({});
+    if (!isSingleHop) {
+      simRequest = new SimulateTradeRequest({
+        input: {
+          assetId: {
+            inner: base64ToUint8Array(asset1Token.inner),
+          },
+          amount: {
+            lo: amtIn.lo,
+            hi: amtIn.hi,
+          },
+        },
+        output: {
+          inner: base64ToUint8Array(asset2Token.inner),
+        },
+      });
+    } else {
+      simRequest = new SimulateTradeRequest({
+        input: {
+          assetId: {
+            inner: base64ToUint8Array(asset1Token.inner),
+          },
+          amount: {
+            lo: amtIn.lo,
+            hi: amtIn.hi,
+          },
+        },
+        output: {
+          inner: base64ToUint8Array(asset2Token.inner),
+        },
+        routing: {
+          setting: {
+            case: "singleHop",
+            value: SimulateTradeRequest_Routing_SingleHop,
+          },
+        },
+      });
+    }
+
+    const data = await sim_querier.simulateTrade(simRequest);
+    res.status(200).json(data as SwapExecution);
+  } catch (error) {
+    console.error("Error simulation trade grpc data:", error);
+    res
+      .status(500)
+      .json({ error: `Error simualtion trade grpc data: ${error}` });
+  }
+}
