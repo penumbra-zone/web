eafdce070a8afac5797ec5637deee4fdb31730e4
Gabe Rodriguez <grod220@gmail.com>
2024-03-27 13:22:58 +0100

    Clarify wasm func naming (#833)
    * Clarify wasm func naming
    
    * review updates

--- packages/wasm/src/address.ts
+++ packages/wasm/src/address.ts
@@ -1,10 +1,9 @@
-import { get_short_address_by_index, is_controlled_address } from '../wasm';
+import { get_index_by_address, get_short_address_by_index } from '../wasm';
 import {
   Address,
   AddressIndex,
 } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/keys/v1/keys_pb';
 import { JsonValue } from '@bufbuild/protobuf';
-import { bech32Address } from '@penumbra-zone/bech32/src/address';
 
 export const getShortAddressByIndex = (fullViewingKey: string, index: number) =>
   get_short_address_by_index(fullViewingKey, index) as string;
@@ -13,9 +12,14 @@ export const getAddressIndexByAddress = (
   fullViewingKey: string,
   address: Address,
 ): AddressIndex | undefined => {
-  const res = is_controlled_address(fullViewingKey, bech32Address(address)) as JsonValue;
+  const res = get_index_by_address(fullViewingKey, address.toJson()) as JsonValue;
   return res ? AddressIndex.fromJson(res) : undefined;
 };
 
-export const isControlledAddress = (fullViewingKey: string, address?: Address): boolean =>
-  address ? Boolean(is_controlled_address(fullViewingKey, bech32Address(address))) : false;
+// Only an address controlled by the FVK can view its index
+export const isControlledAddress = (fullViewingKey: string, address?: Address): boolean => {
+  if (!address) return false;
+
+  const viewableIndex = get_index_by_address(fullViewingKey, address.toJson()) as JsonValue;
+  return Boolean(viewableIndex);
+};
