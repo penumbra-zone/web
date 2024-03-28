4e47dc9b9d8705f93f8eb7271177620874a4e575
Tal Derei <talderei99@gmail.com>
2024-03-27 22:45:25 -0700

    update version tag

---
+++ packages/wasm/utils/download-proving-keys.ts
@@ -0,0 +1,46 @@
+import fetch from 'node-fetch';
+import * as fs from 'fs';
+import * as path from 'path';
+import { provingKeys } from '@penumbra-zone/wasm/src/proving-keys';
+
+const main = () => {
+  const VERSION_TAG = 'v0.70.3';
+
+  const githubSourceDir = `https://github.com/penumbra-zone/penumbra/raw/${VERSION_TAG}/crates/crypto/proof-params/src/gen/`;
+
+  const binDir = path.join('bin');
+
+  console.log('Downloading keys', VERSION_TAG, Object.values(provingKeys).join(', '));
+
+  // Check if the --force flag is provided in the command-line arguments
+  const forceDownload = process.argv.includes('--force');
+
+  fs.mkdirSync(binDir, { recursive: true });
+  const downloads = Object.values(provingKeys).map(async name => {
+    const file = `${name}_pk.bin`;
+    const outputPath = path.join(binDir, file);
+
+    // Check if the file already exists
+    if (!forceDownload && fs.existsSync(outputPath)) {
+      console.log(`${file} already downloaded.`);
+      return;
+    }
+
+    const downloadPath = new URL(`${githubSourceDir}${file}`);
+
+    const response = await fetch(downloadPath);
+    if (!response.ok) throw new Error(`Failed to fetch ${file}`);
+
+    const fileStream = fs.createWriteStream(outputPath, { flags: 'w' });
+    fileStream.write(Buffer.from(await response.arrayBuffer()));
+    fileStream.end().close(() => {
+      const size = fs.statSync(outputPath).size;
+      const sizeMB = size / 1024 / 1024;
+      console.log(`Downloaded ${sizeMB.toFixed(2)}MiB ${outputPath}`);
+    });
+  });
+
+  void Promise.allSettled(downloads);
+};
+
+main();
