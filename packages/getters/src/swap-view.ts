33b5d1e387be23f50d30472ccc86fad17c06e728
Jesse Pinho <jesse@jessepinho.com>
2024-03-29 17:25:17 -0700

    More getters

--- packages/getters/src/swap-view.ts
+++ packages/getters/src/swap-view.ts
@@ -1,7 +1,7 @@
 import { SwapView } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/dex/v1/dex_pb';
 import { createGetter } from './utils/create-getter';
 import { getValue } from './note-view';
-import { getDelta1I, getDelta2I } from './swap-plaintext';
+import { getClaimFee, getDelta1I, getDelta2I } from './swap-plaintext';
 
 export const getOutput1 = createGetter((swapView?: SwapView) =>
   swapView?.swapView.case === 'visible' ? swapView.swapView.value.output1 : undefined,
@@ -19,6 +19,8 @@ export const getSwapPlaintext = createGetter((swapView?: SwapView) =>
   swapView?.swapView.case === 'visible' ? swapView.swapView.value.swapPlaintext : undefined,
 );
 
+export const getClaimFeeFromSwapView = getSwapPlaintext.pipe(getClaimFee);
+
 export const getDelta1IFromSwapView = getSwapPlaintext.pipe(getDelta1I);
 export const getDelta2IFromSwapView = getSwapPlaintext.pipe(getDelta2I);
 
@@ -28,4 +30,21 @@ export const getAsset1Metadata = createGetter((swapView?: SwapView) =>
 
 export const getAsset2Metadata = createGetter((swapView?: SwapView) =>
   swapView?.swapView.case === 'visible' ? swapView.swapView.value.asset2Metadata : undefined,
+);
+
+export const getClaimTx = createGetter((swapView?: SwapView) =>
+  swapView?.swapView.case === 'visible' ? swapView.swapView.value.claimTx : undefined,
+);
+
+/**
+ * This is a sort of odd getter. It looks in both `output1` and `output2` for
+ * the output address. We could get the address from `claimAddress` under the
+ * `swapPlaintext` property, but that's just an `Address`, not an `AddressView`,
+ * and we want to display the address view because that includes details like
+ * the account index.
+ */
+export const getAddressView = createGetter((swapView?: SwapView) =>
+  swapView?.swapView.case === 'visible'
+    ? swapView.swapView.value.output1?.address ?? swapView.swapView.value.output2?.address
+    : undefined,
 );
