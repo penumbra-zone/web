a52d3efa18b4ebf8413e295b7a56859728be22b5
Jesse Pinho <jesse@jessepinho.com>
2024-03-30 13:40:43 -0700

    Generate zero-value views on the fly; handle those in OneWaySwap

--- packages/ui/components/ui/tx/view/swap/one-way-swap.tsx
+++ packages/ui/components/ui/tx/view/swap/one-way-swap.tsx
@@ -1,6 +1,8 @@
 import { ValueViewComponent } from '../value';
 import { ArrowRight } from 'lucide-react';
 import { ValueView } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/asset/v1/asset_pb';
+import { getAmount, getSymbolFromValueView } from '@penumbra-zone/getters/src/value-view';
+import { isZero } from '@penumbra-zone/types/src/amount';
 
 /**
  * Renders a one-way swap (which should be the only kind of swap that ever
@@ -9,13 +11,15 @@ import { ValueView } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core
  * 1.23INPUT -> 4.56OUTPUT (7.89 unfilled)
  */
 export const OneWaySwap = ({ input, output }: { input: ValueView; output: ValueView }) => {
+  const outputAmount = getAmount(output);
+
   return (
     <div className='flex items-center gap-2'>
       <ValueViewComponent view={input} />
 
       <ArrowRight />
 
-      <ValueViewComponent view={output} />
+      {isZero(outputAmount) ? getSymbolFromValueView(output) : <ValueViewComponent view={output} />}
     </div>
   );
 };
