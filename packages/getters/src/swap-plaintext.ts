ed5f8a150ad64f96ceab97426c14034e0a17ad92
Jesse Pinho <jesse@jessepinho.com>
2024-03-29 17:25:17 -0700

    More getters

--- packages/getters/src/swap-plaintext.ts
+++ packages/getters/src/swap-plaintext.ts
@@ -3,3 +3,4 @@ import { createGetter } from './utils/create-getter';
 
 export const getDelta1I = createGetter((swapPlaintext?: SwapPlaintext) => swapPlaintext?.delta1I);
 export const getDelta2I = createGetter((swapPlaintext?: SwapPlaintext) => swapPlaintext?.delta2I);
+export const getClaimFee = createGetter((swapPlaintext?: SwapPlaintext) => swapPlaintext?.claimFee);
