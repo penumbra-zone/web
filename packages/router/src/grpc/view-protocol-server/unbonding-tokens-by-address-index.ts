f85da16ac33d2b867e5e18e32d30ba593dae92ed
Jesse Pinho <jesse@jessepinho.com>
2024-03-30 17:17:48 -0700

    Start building out logic

--- packages/router/src/grpc/view-protocol-server/unbonding-tokens-by-address-index.ts
+++ packages/router/src/grpc/view-protocol-server/unbonding-tokens-by-address-index.ts
@@ -1,6 +1,26 @@
+import {
+  BalancesRequest,
+  BalancesResponse,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1/view_pb';
 import { Impl } from '.';
+import { balances } from './balances';
+import Array from '@penumbra-zone/polyfills/src/Array.fromAsync';
+import { assetPatterns } from '@penumbra-zone/constants/src/assets';
+import { getDisplayFromBalancesResponse } from '@penumbra-zone/getters/src/balances-response';
+import { PartialMessage } from '@bufbuild/protobuf';
+
+const forUnbondingTokens = (balancesResponse: PartialMessage<BalancesResponse>) =>
+  assetPatterns.unbondingToken.matches(
+    getDisplayFromBalancesResponse(new BalancesResponse(balancesResponse)),
+  );
 
 export const unbondingTokensByAddressIndex: Impl['unbondingTokensByAddressIndex'] = async (
   req,
   ctx,
-) => {};
+) => {
+  const allBalances = await Array.fromAsync(
+    balances(new BalancesRequest({ accountFilter: { account: 0 } }), ctx),
+  );
+
+  const unbondingTokenBalances = allBalances.filter(forUnbondingTokens);
+};
