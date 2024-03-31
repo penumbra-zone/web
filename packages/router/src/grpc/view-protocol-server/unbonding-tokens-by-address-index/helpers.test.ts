f764fa910d1dbec448fd6f21b54e3e78aa962bf0
Jesse Pinho <jesse@jessepinho.com>
2024-03-30 17:47:41 -0700

    Create isClaimable helper

---
+++ packages/router/src/grpc/view-protocol-server/unbonding-tokens-by-address-index/helpers.test.ts
@@ -0,0 +1,95 @@
+import { describe, expect, it, vi } from 'vitest';
+import { isClaimable } from './helpers';
+import {
+  AppParametersResponse,
+  BalancesResponse,
+  StatusResponse,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1/view_pb';
+import { createHandlerContext } from '@connectrpc/connect';
+import { ViewService } from '@buf/penumbra-zone_penumbra.connectrpc_es/penumbra/view/v1/view_connect';
+
+const mockAppParameters = vi.hoisted(() => vi.fn());
+vi.mock('../app-parameters', () => ({
+  appParameters: mockAppParameters,
+}));
+
+const mockStatus = vi.hoisted(() => vi.fn());
+vi.mock('../status', () => ({
+  status: mockStatus,
+}));
+
+const mockCtx = createHandlerContext({
+  service: ViewService,
+  method: ViewService.methods.unbondingTokensByAddressIndex,
+  protocolName: 'mock',
+  requestMethod: 'MOCK',
+  url: '/mock',
+});
+
+describe('isClaimable()', () => {
+  it("returns `true` when we've passed the unbonding delay period", async () => {
+    mockAppParameters.mockResolvedValue(
+      new AppParametersResponse({
+        parameters: {
+          stakeParams: {
+            unbondingDelay: 100n,
+          },
+        },
+      }),
+    );
+
+    mockStatus.mockResolvedValue(
+      new StatusResponse({
+        fullSyncHeight: 200n,
+      }),
+    );
+
+    const balancesResponse = new BalancesResponse({
+      balanceView: {
+        valueView: {
+          case: 'knownAssetId',
+          value: {
+            metadata: {
+              display: 'unbonding_start_at_100_penumbravalid1abc123',
+            },
+          },
+        },
+      },
+    });
+
+    await expect(isClaimable(balancesResponse, mockCtx)).resolves.toBe(true);
+  });
+
+  it("returns `false` when we haven't yet passed the unbonding delay period", async () => {
+    mockAppParameters.mockResolvedValue(
+      new AppParametersResponse({
+        parameters: {
+          stakeParams: {
+            unbondingDelay: 100n,
+          },
+        },
+      }),
+    );
+
+    mockStatus.mockResolvedValue(
+      new StatusResponse({
+        fullSyncHeight: 200n,
+      }),
+    );
+
+    const balancesResponse = new BalancesResponse({
+      balanceView: {
+        valueView: {
+          case: 'knownAssetId',
+          value: {
+            metadata: {
+              display: 'unbonding_start_at_150_penumbravalid1abc123',
+            },
+          },
+        },
+      },
+    });
+
+    await expect(isClaimable(balancesResponse, mockCtx)).resolves.toBe(false);
+  });
+});
--- packages/router/src/grpc/view-protocol-server/unbonding-tokens-by-address-index/helpers.ts
+++ packages/router/src/grpc/view-protocol-server/unbonding-tokens-by-address-index/helpers.ts
@@ -1,9 +1,38 @@
-import { BalancesResponse } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1/view_pb';
+import {
+  AppParametersRequest,
+  BalancesResponse,
+  StatusRequest,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1/view_pb';
 import { PartialMessage } from '@bufbuild/protobuf';
+import { HandlerContext } from '@connectrpc/connect';
 import { assetPatterns } from '@penumbra-zone/constants/src/assets';
 import { getDisplayFromBalancesResponse } from '@penumbra-zone/getters/src/balances-response';
+import { status } from '../status';
+import { appParameters } from '../app-parameters';
 
 export const isUnbondingTokenBalance = (balancesResponse: PartialMessage<BalancesResponse>) =>
   assetPatterns.unbondingToken.matches(
     getDisplayFromBalancesResponse(new BalancesResponse(balancesResponse)),
   );
+
+export const isClaimable = async (
+  balancesResponse: PartialMessage<BalancesResponse>,
+  ctx: HandlerContext,
+): Promise<boolean> => {
+  const [{ fullSyncHeight }, { parameters }] = await Promise.all([
+    status(new StatusRequest(), ctx),
+    appParameters(new AppParametersRequest(), ctx),
+  ]);
+
+  if (!fullSyncHeight || !parameters?.stakeParams?.unbondingDelay) return false;
+
+  const display = getDisplayFromBalancesResponse(new BalancesResponse(balancesResponse));
+  const unbondingStartHeight = assetPatterns.unbondingToken.capture(display);
+
+  if (unbondingStartHeight?.startAt) {
+    const blocksSinceUnbondingStarted = fullSyncHeight - BigInt(unbondingStartHeight.startAt);
+    return blocksSinceUnbondingStarted >= parameters.stakeParams.unbondingDelay;
+  }
+
+  return false;
+};
