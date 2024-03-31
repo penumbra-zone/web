af2781d91be12255f94b245da4e4b0ed384d1dfc
Jesse Pinho <jesse@jessepinho.com>
2024-03-30 17:53:51 -0700

    Build out rest of RPC method impl

--- packages/router/src/grpc/view-protocol-server/unbonding-tokens-by-address-index/index.ts
+++ packages/router/src/grpc/view-protocol-server/unbonding-tokens-by-address-index/index.ts
@@ -1,16 +1,35 @@
-import { BalancesRequest } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1/view_pb';
+import {
+  BalancesRequest,
+  UnbondingTokensByAddressIndexRequest_Filter,
+  UnbondingTokensByAddressIndexResponse,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1/view_pb';
 import { Impl } from '..';
 import { balances } from '../balances';
-import Array from '@penumbra-zone/polyfills/src/Array.fromAsync';
-import { isUnbondingTokenBalance } from './helpers';
+import { getIsClaimable, isUnbondingTokenBalance } from './helpers';
 
-export const unbondingTokensByAddressIndex: Impl['unbondingTokensByAddressIndex'] = async (
-  req,
-  ctx,
-) => {
-  const allBalances = await Array.fromAsync(
-    balances(new BalancesRequest({ accountFilter: { account: 0 } }), ctx),
-  );
+export const unbondingTokensByAddressIndex: Impl['unbondingTokensByAddressIndex'] =
+  async function* (req, ctx) {
+    for await (const balancesResponse of balances(
+      new BalancesRequest({ accountFilter: req.addressIndex }),
+      ctx,
+    )) {
+      if (!isUnbondingTokenBalance(balancesResponse)) continue;
+      const claimable = await getIsClaimable(balancesResponse, ctx);
 
-  const unbondingTokenBalances = allBalances.filter(isUnbondingTokenBalance);
-};
+      if (req.filter === UnbondingTokensByAddressIndexRequest_Filter.CLAIMABLE && !claimable) {
+        continue;
+      }
+
+      if (
+        req.filter === UnbondingTokensByAddressIndexRequest_Filter.NOT_YET_CLAIMABLE &&
+        claimable
+      ) {
+        continue;
+      }
+
+      yield new UnbondingTokensByAddressIndexResponse({
+        claimable,
+        valueView: balancesResponse.balanceView,
+      });
+    }
+  };
