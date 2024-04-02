4b6e697139cfe2d1ed90b06f2e2e292b7c6aaa66
Jesse Pinho <jesse@jessepinho.com>
2024-04-01 17:59:19 -0700

    Add submit button and link to edit list

--- apps/extension/src/routes/page/onboarding/set-rpc-endpoint.tsx
+++ apps/extension/src/routes/page/onboarding/set-rpc-endpoint.tsx
@@ -3,12 +3,15 @@ import { FadeTransition } from '@penumbra-zone/ui/components/ui/fade-transition'
 import { RPC_ENDPOINTS } from '../../../shared/rpc-endpoints';
 import { useMemo, useState } from 'react';
 import { SelectList } from '@penumbra-zone/ui/components/ui/select-list';
+import { Button } from '@penumbra-zone/ui/components/ui/button';
 
 const randomSort = () => (Math.random() >= 0.5 ? 1 : -1);
 
 export const SetRpcEndpoint = () => {
   const randomlySortedEndpoints = useMemo(() => [...RPC_ENDPOINTS].sort(randomSort), []);
   const [selectedEndpointUrl, setSelectedEndpointUrl] = useState(randomlySortedEndpoints[0]?.url);
+
+  const handleSubmit = () => {};
 
   return (
     <FadeTransition>
@@ -20,18 +23,33 @@ export const SetRpcEndpoint = () => {
           </CardDescription>
         </CardHeader>
 
-        <SelectList>
-          {randomlySortedEndpoints.map(rpcEndpoint => (
-            <SelectList.Option
-              key={rpcEndpoint.url}
-              label={rpcEndpoint.name}
-              secondaryText={rpcEndpoint.url}
-              onSelect={setSelectedEndpointUrl}
-              value={rpcEndpoint.url}
-              isSelected={rpcEndpoint.url === selectedEndpointUrl}
-            />
-          ))}
-        </SelectList>
+        <form className='mt-6 flex flex-col gap-4'>
+          <SelectList>
+            {randomlySortedEndpoints.map(rpcEndpoint => (
+              <SelectList.Option
+                key={rpcEndpoint.url}
+                label={rpcEndpoint.name}
+                secondaryText={rpcEndpoint.url}
+                onSelect={setSelectedEndpointUrl}
+                value={rpcEndpoint.url}
+                isSelected={rpcEndpoint.url === selectedEndpointUrl}
+              />
+            ))}
+          </SelectList>
+
+          <Button variant='gradient' className='mt-2' onClick={handleSubmit}>
+            Next
+          </Button>
+        </form>
+
+        <a
+          href='https://github.com/penumbra-zone/web/blob/main/apps/extension/src/shared/rpc-endpoints.ts'
+          target='_blank'
+          rel='noreferrer'
+          className='mt-6 block text-right text-xs text-muted-foreground'
+        >
+          Add to this list
+        </a>
       </Card>
     </FadeTransition>
   );
