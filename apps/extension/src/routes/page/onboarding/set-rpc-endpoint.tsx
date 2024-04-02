0d653d17bbdd0e0fc671e30fc9bb545b257b7eb0
Jesse Pinho <jesse@jessepinho.com>
2024-04-01 17:49:14 -0700

    Build out RPC UI

--- apps/extension/src/routes/page/onboarding/set-rpc-endpoint.tsx
+++ apps/extension/src/routes/page/onboarding/set-rpc-endpoint.tsx
@@ -1,11 +1,37 @@
-import { Card } from '@penumbra-zone/ui/components/ui/card';
+import { Card, CardDescription, CardHeader, CardTitle } from '@penumbra-zone/ui/components/ui/card';
 import { FadeTransition } from '@penumbra-zone/ui/components/ui/fade-transition';
+import { RPC_ENDPOINTS } from '../../../shared/rpc-endpoints';
+import { useMemo, useState } from 'react';
+import { SelectList } from '@penumbra-zone/ui/components/ui/select-list';
+
+const randomSort = () => (Math.random() >= 0.5 ? 1 : -1);
 
 export const SetRpcEndpoint = () => {
+  const randomlySortedEndpoints = useMemo(() => [...RPC_ENDPOINTS].sort(randomSort), []);
+  const [selectedEndpointUrl, setSelectedEndpointUrl] = useState(randomlySortedEndpoints[0]?.url);
+
   return (
     <FadeTransition>
       <Card className='w-[400px]' gradient>
-        <div>Hello, world!</div>
+        <CardHeader>
+          <CardTitle>Select your preferred RPC endpoint</CardTitle>
+          <CardDescription>
+            If you&apos;re unsure which one to choose, leave this option set to the default.
+          </CardDescription>
+        </CardHeader>
+
+        <SelectList>
+          {randomlySortedEndpoints.map(rpcEndpoint => (
+            <SelectList.Option
+              key={rpcEndpoint.url}
+              label={rpcEndpoint.name}
+              secondaryText={rpcEndpoint.url}
+              onSelect={setSelectedEndpointUrl}
+              value={rpcEndpoint.url}
+              isSelected={rpcEndpoint.url === selectedEndpointUrl}
+            />
+          ))}
+        </SelectList>
       </Card>
     </FadeTransition>
   );
