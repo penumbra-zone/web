c3c7aa4970ad981e8261ca21e2db27ee26c69589
Jesse Pinho <jesse@jessepinho.com>
2024-04-04 15:15:46 -0700

    Add wrapping:

--- apps/minifront/src/components/dashboard/assets-table/index.tsx
+++ apps/minifront/src/components/dashboard/assets-table/index.tsx
@@ -53,7 +53,7 @@ export default function AssetsTable() {
               {a.balances.map((assetBalance, index) => (
                 <TableRow key={index}>
                   <TableCell>
-                    <div className='flex gap-2'>
+                    <div className='flex flex-wrap gap-2'>
                       <ValueViewComponent view={assetBalance.balanceView} />
                       <EquivalentValues valueView={assetBalance.balanceView} />
                     </div>
