dafde2541ffe9e8a54659d5dcce18fe313340d37
turbocrime <turbocrime@users.noreply.github.com>
2024-04-05 12:15:55 -0500

    revert footer style

--- apps/minifront/src/components/footer/footer.tsx
+++ apps/minifront/src/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import { RightsMessage } from './rights-message';
 
 export const Footer = () => (
   <footer className='w-full bg-gradient-to-b from-transparent to-black to-40% pt-[3em]'>
-    <div className='text-center text-stone-700 hover:text-stone-600'>
+    <div className='text-center text-stone-700'>
       <RightsMessage />
       <MinifrontVersion />
     </div>
