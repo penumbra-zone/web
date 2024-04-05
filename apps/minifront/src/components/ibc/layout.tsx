f1cb279a00482c714ced3d909b72c548b231e123
turbocrime <turbocrime@users.noreply.github.com>
2024-04-05 12:16:13 -0500

    im going to delete this anyway

--- apps/minifront/src/components/ibc/layout.tsx
+++ apps/minifront/src/components/ibc/layout.tsx
@@ -3,9 +3,8 @@ import { IbcInForm } from './ibc-in-form';
 import { IbcOutForm } from './ibc-out-form';
 
 export const IbcLayout = () => {
-  //div className='flex flex-1 flex-col gap-4 md:flex-row md:place-content-between'>
   return (
-    <div className='flex flex-1 flex-col gap-4 md:flex-row  md:place-content-around'>
+    <div className='flex flex-1 flex-col gap-4 md:flex-row md:place-content-around'>
       <Card className='md:self-start'>
         <IbcInForm />
       </Card>
