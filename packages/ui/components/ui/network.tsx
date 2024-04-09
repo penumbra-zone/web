8048d280db5f73e9baad5e5fa61ff805b182f56e
Jesse Pinho <jesse@jessepinho.com>
2024-04-09 15:45:44 -0700

    Add an optional href prop for Network

--- packages/ui/components/ui/network.tsx
+++ packages/ui/components/ui/network.tsx
@@ -1,19 +1,31 @@
+import { ConditionalWrap } from './conditional-wrap';
+
 export interface NetworkProps {
   name: string;
   connectIndicator?: boolean;
+  href?: string;
 }
 
-export const Network = ({ name, connectIndicator = true }: NetworkProps) => {
+export const Network = ({ name, href, connectIndicator = true }: NetworkProps) => {
   return (
-    <div
-      className={
-        'flex items-center justify-between gap-4 rounded-lg border bg-background px-5 py-[7px] font-bold text-muted-foreground md:px-[25px] xl:px-[18px]'
-      }
-    >
-      {connectIndicator && (
-        <div className='-mx-1 h-4 w-1 rounded-sm bg-gradient-to-b from-cyan-400 to-emerald-400'></div>
+    <ConditionalWrap
+      condition={!!href}
+      wrap={children => (
+        <a href={href} target='_blank' rel='noreferrer'>
+          {children}
+        </a>
       )}
-      <p className='whitespace-nowrap'>{name}</p>
-    </div>
+    >
+      <div
+        className={
+          'flex items-center justify-between gap-4 rounded-lg border bg-background px-5 py-[7px] font-bold text-muted-foreground md:px-[25px] xl:px-[18px]'
+        }
+      >
+        {connectIndicator && (
+          <div className='-mx-1 h-4 w-1 rounded-sm bg-gradient-to-b from-cyan-400 to-emerald-400'></div>
+        )}
+        <p className='whitespace-nowrap'>{name}</p>
+      </div>
+    </ConditionalWrap>
   );
 };
