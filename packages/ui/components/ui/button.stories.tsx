a9a12a4d0bfa4102f08d4e08d30e26f0a25dea84
Jesse Pinho <jesse@jessepinho.com>
2024-04-10 16:31:01 -0700

    Add a destructiveSecondary variant to Button

---
+++ packages/ui/components/ui/button.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from './button';
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  title: 'Button',
+  tags: ['autodocs'],
+  argTypes: {},
+};
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Basic: Story = {
+  args: {
+    children: 'Save',
+    variant: 'default',
+    size: 'default',
+    asChild: false,
+  },
+};
