99b7182b9606b570857c0d8baa0579531abf2ace
turbocrime <turbocrime@users.noreply.github.com>
2024-04-10 21:30:40 -0500

    chain-registry types build

--- apps/minifront/vite.config.ts
+++ apps/minifront/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import { commitInfoPlugin } from './src/utils/commit-info-vite-plugin';
@@ -6,16 +6,5 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
   base: './',
-  plugins: [react(), splitVendorChunkPlugin(), basicSsl(), commitInfoPlugin(), nodePolyfills()],
-  build: {
-    rollupOptions: {
-      output: {
-        // configure additional chunks based on file path
-        manualChunks: (id: string) => {
-          if (id.includes('@buf')) return 'protobuf';
-          return;
-        },
-      },
-    },
-  },
+  plugins: [nodePolyfills(), react(), basicSsl(), commitInfoPlugin()],
 });
