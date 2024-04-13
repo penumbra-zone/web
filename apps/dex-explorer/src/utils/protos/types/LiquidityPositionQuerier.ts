af8a7dea81027e4aa1a926e90464c7849849f9c9
Phil <pbal@fordham.edu>
2024-04-13 09:32:22 -0700

    Add server side api to query LPs by price and pair

--- apps/dex-explorer/src/utils/protos/types/LiquidityPositionQuerier.ts
+++ apps/dex-explorer/src/utils/protos/types/LiquidityPositionQuerier.ts
@@ -1,5 +1,6 @@
-import { PositionId, Position } from "@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/dex/v1/dex_pb";
+import { PositionId, Position, DirectedTradingPair } from "@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/dex/v1/dex_pb";
 
 export interface LiquidityPositionQuerierInterface {
   liquidityPositionById(id: PositionId): Promise<Position | undefined>;
+  liquidityPositionsByPrice(directedTradingPair: DirectedTradingPair, limit: number): Promise<Position[] | undefined>;
 }
