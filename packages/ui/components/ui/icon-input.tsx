f0c814b9a89e35e3c1f9b723ecba369ddf3303ea
Jesse Pinho <jesse@jessepinho.com>
2024-04-22 14:55:04 -0700

    Add comment

--- packages/ui/components/ui/icon-input.tsx
+++ packages/ui/components/ui/icon-input.tsx
@@ -3,6 +3,10 @@ import { Input } from './input';
 
 const className = 'flex w-full gap-2 rounded-lg border bg-background px-3 py-2';
 
+/**
+ * Use this to render an input with an icon to its left, such as a search field
+ * with a magnifying glass.
+ */
 export const IconInput = ({
   value,
   onChange,
