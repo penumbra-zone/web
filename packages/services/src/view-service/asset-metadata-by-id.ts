e6142366ec45154d36e40c162761a20d0e9a65ca
turbocrime <turbocrime@users.noreply.github.com>
2024-04-29 10:54:14 -0700

    don't throw if metadata unavailble, just return empty response

--- packages/services/src/view-service/asset-metadata-by-id.ts
+++ packages/services/src/view-service/asset-metadata-by-id.ts
@@ -1,19 +1,25 @@
 import type { Impl } from '.';
 import { servicesCtx } from '../ctx/prax';
-import { getAssetMetadata } from './helpers';
 
-export const assetMetadataById: Impl['assetMetadataById'] = async (req, ctx) => {
-  if (!req.assetId) throw new Error('No asset id passed in request');
-  if (!req.assetId.altBaseDenom && !req.assetId.altBech32m && !req.assetId.inner.length) {
+export const assetMetadataById: Impl['assetMetadataById'] = async ({ assetId }, ctx) => {
+  if (!assetId) throw new Error('No asset id passed in request');
+
+  if (!assetId.altBaseDenom && !assetId.altBech32m && !assetId.inner.length)
     throw new Error(
       'Either `inner`, `altBaseDenom`, or `altBech32m` must be set on the asset ID passed in the `assetMetadataById` request',
     );
-  }
 
   const services = ctx.values.get(servicesCtx);
   const { indexedDb, querier } = await services.getWalletServices();
 
-  const denomMetadata = await getAssetMetadata(req.assetId, indexedDb, querier);
+  const localMetadata = await indexedDb.getAssetsMetadata(assetId);
+  if (localMetadata) return { denomMetadata: localMetadata };
 
-  return { denomMetadata };
+  const remoteMetadata = await querier.shieldedPool.assetMetadataById(assetId);
+  if (remoteMetadata) {
+    void indexedDb.saveAssetsMetadata(remoteMetadata);
+    return { denomMetadata: remoteMetadata };
+  }
+
+  return { denomMetadata: undefined };
 };
