a31e7f74fa39f90d0ce9cbce4453aa91b5c8aa33
Jesse Pinho <jesse@jessepinho.com>
2024-05-03 21:39:27 -0700

    Refactor a little

--- packages/services/src/view-service/auctions.ts
+++ packages/services/src/view-service/auctions.ts
@@ -13,7 +13,8 @@ import { assetPatterns } from '@penumbra-zone/constants/assets';
 import { Any, PartialMessage } from '@bufbuild/protobuf';
 import { servicesCtx } from '../ctx/prax';
 import { bech32mAuctionId } from '@penumbra-zone/bech32m/pauctid';
-import { Code, ConnectError } from '@connectrpc/connect';
+import { Code, ConnectError, HandlerContext } from '@connectrpc/connect';
+import { AddressIndex } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/keys/v1/keys_pb';
 
 const getBech32mAuctionId = (
   balancesResponse: PartialMessage<BalancesResponse>,
@@ -29,32 +30,37 @@ const getBech32mAuctionId = (
   return captureGroups.auctionId;
 };
 
+const getAuctionsThisUserControls = async (ctx: HandlerContext, accountFilter?: AddressIndex) => {
+  const auctionsThisUserControls = new Set<string>();
+
+  for await (const balancesResponse of balances(new BalancesRequest({ accountFilter }), ctx)) {
+    const auctionId = getBech32mAuctionId(balancesResponse);
+    if (!auctionId) continue;
+
+    auctionsThisUserControls.add(auctionId);
+  }
+
+  return auctionsThisUserControls;
+};
+
 export const auctions: Impl['auctions'] = async function* (req, ctx) {
   const { includeInactive, queryLatestState, accountFilter } = req;
 
   if (queryLatestState) {
     throw new ConnectError('`queryLatestState` not yet implemented', Code.Unimplemented);
   }
-
   if (includeInactive) {
     throw new ConnectError('`includeInactive` not yet implemented', Code.Unimplemented);
   }
 
-  const bech32mAuctionIds = new Set<string>();
-  for await (const balancesResponse of balances(new BalancesRequest({ accountFilter }), ctx)) {
-    const auctionId = getBech32mAuctionId(balancesResponse);
-    if (!auctionId) continue;
-
-    bech32mAuctionIds.add(auctionId);
-  }
-
-  if (!bech32mAuctionIds.size) return;
+  const auctionsThisUserControls = await getAuctionsThisUserControls(ctx, accountFilter);
+  if (!auctionsThisUserControls.size) return;
 
   const services = ctx.values.get(servicesCtx);
   const { indexedDb } = await services.getWalletServices();
 
   for await (const { id, value } of indexedDb.iterateAuctions()) {
-    if (!bech32mAuctionIds.has(bech32mAuctionId(id))) continue;
+    if (!auctionsThisUserControls.has(bech32mAuctionId(id))) continue;
 
     /** @todo: if (req.includeInactive && auctionIsInactive()) continue; */
 
