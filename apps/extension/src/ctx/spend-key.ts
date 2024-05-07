405ffb8f0ebdb8c8d9e3373e1f03c1401664a3bc
turbocrime <turbocrime@users.noreply.github.com>
2024-05-07 09:40:10 -0700

    review changes

--- apps/extension/src/ctx/spend-key.ts
+++ apps/extension/src/ctx/spend-key.ts
@@ -6,18 +6,16 @@ import { Box } from '@penumbra-zone/types/box';
 import { generateSpendKey } from '@penumbra-zone/wasm/keys';
 
 export const getSpendKey = async () => {
-  const pk = await sessionExtStorage.get('passwordKey');
-  if (!pk) throw new ConnectError('User must login to extension', Code.Unauthenticated);
+  const passKeyJson = await sessionExtStorage.get('passwordKey');
+  if (!passKeyJson) throw new ConnectError('User must login', Code.Unauthenticated);
+  const passKey = await Key.fromJson(passKeyJson);
 
   const wallet0 = (await localExtStorage.get('wallets'))[0];
   if (!wallet0) throw new ConnectError('No wallet found', Code.FailedPrecondition);
 
-  const decryptedSeedPhrase = await (
-    await Key.fromJson(pk)
-  ).unseal(Box.fromJson(wallet0.custody.encryptedSeedPhrase));
+  const seedBox = Box.fromJson(wallet0.custody.encryptedSeedPhrase);
+  const seedPhrase = await passKey.unseal(seedBox);
+  if (!seedPhrase) throw new ConnectError('Unable to decrypt seed phrase', Code.Unauthenticated);
 
-  if (!decryptedSeedPhrase)
-    throw new ConnectError('Unable to decrypt seed phrase', Code.Unauthenticated);
-
-  return generateSpendKey(decryptedSeedPhrase);
+  return generateSpendKey(seedPhrase);
 };
