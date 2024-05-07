5c84341184beac06942c5768131031fffc666364
Gabe Rodriguez <grod220@gmail.com>
2024-05-07 18:12:24 +0200

    Prevent expanding panels (#1056)

--- apps/minifront/src/components/swap/swap/index.tsx
+++ apps/minifront/src/components/swap/swap/index.tsx
@@ -7,16 +7,23 @@ import { UnclaimedSwaps } from './unclaimed-swaps';
 export const Swap = () => {
   return (
     <div className='grid gap-6 md:grid-cols-2 md:gap-4 xl:grid-cols-3 xl:gap-5'>
-      <UnclaimedSwaps />
-      <Card gradient className='order-3 row-span-2 flex-1 p-5 md:order-1 md:p-4 xl:p-5'>
-        <SwapForm />
-      </Card>
-      <EduInfoCard
-        className='row-span-1 md:order-2'
-        src='./swap-icon.svg'
-        label='Shielded Swap'
-        content={EduPanel.SWAP}
-      />
+      {/* Wrapping in divs prevent one from growing and causing the others to expand with it */}
+      <div>
+        <UnclaimedSwaps />
+      </div>
+      <div>
+        <Card gradient className='order-3 row-span-2 flex-1 p-5 md:order-1 md:p-4 xl:p-5'>
+          <SwapForm />
+        </Card>
+      </div>
+      <div>
+        <EduInfoCard
+          className='row-span-1 md:order-2'
+          src='./swap-icon.svg'
+          label='Shielded Swap'
+          content={EduPanel.SWAP}
+        />
+      </div>
     </div>
   );
 };
