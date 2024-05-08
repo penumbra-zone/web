db71c3ba415bf53b3d447c40ae69adb45e8e964e
turbocrime <turbocrime@users.noreply.github.com>
2024-05-08 13:49:59 -0700

    vite configs

--- apps/minifront/vite.config.ts
+++ apps/minifront/vite.config.ts
@@ -1,11 +1,27 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig, loadEnv } from 'vite';
+import path from 'node:url';
 import basicSsl from '@vitejs/plugin-basic-ssl';
-import { commitInfoPlugin } from './src/utils/commit-info-vite-plugin';
+import react from '@vitejs/plugin-react-swc';
 import polyfillNode from 'vite-plugin-node-stdlib-browser';
+import { commitInfoPlugin } from './src/utils/commit-info-vite-plugin';
 
-export default defineConfig({
-  clearScreen: false,
-  base: './',
-  plugins: [polyfillNode(), react(), basicSsl(), commitInfoPlugin()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'PRAX');
+
+  const prax_external = Boolean(JSON.parse(env['PRAX_EXTERNAL'] ?? 'false')) || undefined;
+
+  const config = {
+    clearScreen: false,
+    resolve: {
+      alias: prax_external && {
+        '@penumbra-zone/client/prax': path.fileURLToPath(
+          import.meta.resolve('@penumbra-zone/client-chrome/prax'),
+        ),
+      },
+    },
+    base: './',
+    plugins: [polyfillNode(), react(), basicSsl(), commitInfoPlugin()],
+  };
+
+  return config;
 });
