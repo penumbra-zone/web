5ed02405ead557336f79096747824f89bebcb790
Gabe Rodriguez <grod220@gmail.com>
2024-05-09 16:58:56 +0200

    Support compat addresses for ibc-in

--- packages/bech32m/src/format/convert.ts
+++ packages/bech32m/src/format/convert.ts
@@ -1,4 +1,4 @@
-import { bech32m } from 'bech32';
+import { bech32, bech32m, BechLib } from 'bech32';
 import { StringLength } from './strings';
 import { ByteLength } from './bytes';
 import { Prefix } from './prefix';
@@ -15,69 +15,129 @@ export const fromBech32m = <P extends Prefix>(
   b32mStr: `${P}1${string}`,
   prefix: Prefix = b32mStr.slice(0, b32mStr.lastIndexOf('1')) as P,
 ): Uint8Array => {
+  return fromBechWithLib(b32mStr, prefix, bech32m);
+};
+
+/**
+ * Similar to above, but used to convert from Bech32 for compatibility reasons.
+ *
+ * @param b32mStr input string
+ * @param prefix verified or detected input prefix
+ * @returns byte array of spec length
+ */
+export const fromBech32 = <P extends Prefix>(
+  b32mStr: `${P}1${string}`,
+  prefix: Prefix = b32mStr.slice(0, b32mStr.lastIndexOf('1')) as P,
+): Uint8Array => {
+  return fromBechWithLib(b32mStr, prefix, bech32);
+};
+
+const fromBechWithLib = <P extends Prefix>(
+  b32mStr: `${P}1${string}`,
+  prefix: Prefix,
+  bechLib: BechLib,
+): Uint8Array => {
   const p = b32mStr.slice(0, b32mStr.lastIndexOf('1'));
   if (p !== prefix) throw new TypeError(`Unexpected prefix: expected ${prefix}, got ${p}`);
-  return from(b32mStr, p, StringLength[p], ByteLength[p]);
+  return from({
+    bechLib: bechLib,
+    b32mStr,
+    expectPrefix: p,
+    expectLength: StringLength[p],
+    expectBytes: ByteLength[p],
+  });
 };
 
 /**
  * Internal use. Converts a byte array to a valid bech32m string of spec format,
  * throwing on invalid input.
  *
- * @param data input bytes
+ * @param bData input bytes
  * @param prefix known prefix in spec
  * @returns bech32m string of spec format
  */
-export const toBech32m = <P extends Prefix>(data: Uint8Array, prefix: P): `${P}1${string}` =>
-  to(data, prefix, StringLength[prefix], ByteLength[prefix]);
+export const toBech32m = <P extends Prefix>(bData: Uint8Array, prefix: P): `${P}1${string}` =>
+  to({
+    bechLib: bech32m,
+    bData,
+    prefix,
+    expectLength: StringLength[prefix],
+    expectBytes: ByteLength[prefix],
+  });
+
+/**
+ * Similar to above, but used to convert to Bech32 for compatibility reasons.
+ *
+ * @param bData input bytes
+ * @param prefix known prefix in spec
+ * @returns bech32 string of spec format
+ */
+export const toBech32 = <P extends Prefix>(bData: Uint8Array, prefix: P): `${P}1${string}` =>
+  to({
+    bechLib: bech32,
+    bData,
+    prefix,
+    expectLength: StringLength[prefix],
+    expectBytes: ByteLength[prefix],
+  });
+
+interface FromParams {
+  bechLib: BechLib; // Lib doing decoding
+  b32mStr: string; // input string
+  expectPrefix: string; // verify input prefix
+  expectLength: number; // verify input length
+  expectBytes: number; // expected output length
+}
 
 /**
  * Internal use. Converts a bech32m string to a byte array. Verifies the prefix,
  * input string length, and output byte length.
  *
- * @param bStr input string
- * @param expectPrefix verify input prefix
- * @param expectLength verify input length
- * @param expectBytes expected output length
  * @returns byte array of length `expectBytes`
  */
-const from = (
-  bStr: string,
-  expectPrefix: string,
-  expectLength: number,
-  expectBytes: number,
-): Uint8Array => {
-  if (bStr.length !== expectLength) {
-    throw new TypeError(`Invalid string length: expected ${expectLength}, got ${bStr.length}`);
+const from = ({
+  bechLib,
+  b32mStr,
+  expectPrefix,
+  expectLength,
+  expectBytes,
+}: FromParams): Uint8Array => {
+  if (b32mStr.length !== expectLength) {
+    throw new TypeError(`Invalid string length: expected ${expectLength}, got ${b32mStr.length}`);
   }
 
-  const { prefix, words } = bech32m.decode(bStr, expectLength);
+  const { prefix, words } = bechLib.decode(b32mStr, expectLength);
   if (prefix !== expectPrefix) throw new TypeError('Wrong prefix');
-  const bytes = new Uint8Array(bech32m.fromWords(words));
+  const bytes = new Uint8Array(bechLib.fromWords(words));
   if (bytes.length !== expectBytes) throw new TypeError('Unexpected data length');
   return bytes;
 };
+
+interface ToParams<P extends string> {
+  bechLib: BechLib; // Lib doing encoding
+  bData: Uint8Array; // input bytes
+  prefix: P; // string to use as prefix
+  expectLength: number; // expected output length, including prefix
+  expectBytes: number; // verify input byte length
+}
 
 /**
  * Internal use. Converts a byte array to a bech32m string with specified
  * prefix. Verifies input byte length and output string length.
  *
- * @param bData input bytes
- * @param prefix string to use as prefix
- * @param expectLength expected output length, including prefix
- * @param expectBytes verify input byte length
  * @returns bech32m with prefix and length `expectLength`
  */
-const to = <P extends string>(
-  bData: Uint8Array,
-  prefix: P,
-  expectLength: number,
-  expectBytes: number,
-): `${P}1${string}` => {
+const to = <P extends string>({
+  bechLib,
+  bData,
+  prefix,
+  expectLength,
+  expectBytes,
+}: ToParams<P>): `${P}1${string}` => {
   if (bData.length !== expectBytes) {
     throw new TypeError(`Invalid data length: expected ${expectBytes}, got ${bData.length}`);
   }
-  const bStr = bech32m.encode(prefix, bech32m.toWords(bData), expectLength);
+  const bStr = bechLib.encode(prefix, bechLib.toWords(bData), expectLength);
   if (bStr.length !== expectLength) throw new TypeError('Unexpected string length');
   return bStr as `${P}1${string}`;
 };
