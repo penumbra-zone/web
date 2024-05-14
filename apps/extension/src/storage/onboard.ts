784858a76b2a55c13d7d5e6b844897b3414e60ee
Gabe Rodriguez <grod220@gmail.com>
2024-05-14 16:38:28 -0700

    Update to storage item types

--- apps/extension/src/storage/onboard.ts
+++ apps/extension/src/storage/onboard.ts
@@ -1,6 +1,7 @@
-import { Listener } from '@penumbra-zone/storage/chrome/base';
+import { Listener, StorageItem } from '@penumbra-zone/storage/chrome/base';
 import { localExtStorage } from '@penumbra-zone/storage/chrome/local';
 import { WalletJson } from '@penumbra-zone/types/wallet';
+import { LocalStorageState } from '@penumbra-zone/storage/chrome/types';
 
 /**
  * When a user first onboards with the extension, they won't have chosen a gRPC
@@ -13,13 +14,12 @@ export const onboardGrpcEndpoint = async (): Promise<string> => {
 
   return new Promise(resolve => {
     const storageListener = (changes: Record<string, { newValue?: unknown }>) => {
-      const newValue = changes['grpcEndpoint']?.newValue;
-      const val =
-        typeof newValue === 'object' && newValue != null && 'value' in newValue
-          ? newValue.value
-          : undefined;
-      if (val && typeof val === 'string') {
-        resolve(val);
+      const storageItem = changes['grpcEndpoint']?.newValue as
+        | StorageItem<LocalStorageState['grpcEndpoint']>
+        | undefined;
+      const rpcEndpoint = storageItem?.value;
+      if (rpcEndpoint) {
+        resolve(rpcEndpoint);
         localExtStorage.removeListener(storageListener);
       }
     };
@@ -33,13 +33,12 @@ export const onboardWallet = async (): Promise<WalletJson> => {
 
   return new Promise(resolve => {
     const storageListener: Listener = changes => {
-      const newValue = changes['wallets']?.newValue;
-      const val =
-        typeof newValue === 'object' && newValue != null && 'value' in newValue
-          ? newValue.value
-          : undefined;
-      if (Array.isArray(val) && val[0]) {
-        resolve(val[0] as WalletJson);
+      const storageItem = changes['wallets']?.newValue as
+        | StorageItem<LocalStorageState['wallets']>
+        | undefined;
+      const initialWallet = storageItem?.value[0];
+      if (initialWallet) {
+        resolve(initialWallet);
         localExtStorage.removeListener(storageListener);
       }
     };
