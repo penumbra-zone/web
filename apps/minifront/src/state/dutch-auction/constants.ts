a7d1868fbc8dba4a9a29b41dce15e8e5fd9a9498
Jesse Pinho <jesse@jessepinho.com>
2024-05-15 17:50:45 -0700

    Document the constants

--- apps/minifront/src/state/dutch-auction/constants.ts
+++ apps/minifront/src/state/dutch-auction/constants.ts
@@ -8,13 +8,36 @@ export type DurationOption = (typeof DURATION_OPTIONS)[number];
 
 export const STEP_COUNT = 60n;
 
+/**
+ * When a user creates an auction in minifront, minifront actually creates a
+ * series of "sub-auctions", rather than creating a single auction with the
+ * entirety of the user's input funds. This minimizes the user's exposure to
+ * price fluctuations. The user's input and output amounts are equally divided
+ * into the number of sub-auctions, and the sub-auctions are spaced apart to
+ * take up roughly the duration that the user specifies (although they're spaced
+ * with some randomness to avoid having too many auctions on the same blocks).
+ */
 export interface GdaRecipe {
+  /**
+   * The target overall duration of _all_ sub-auctions, from the start height of
+   * the first to the end height of the last.
+   *
+   * Note that the actual duration won't be exactly equal to this due to the
+   * randomness of a Poisson distribution.
+   */
   durationInBlocks: bigint;
+  /** Used to generate the poisson distribution of sub-auctions. */
   poissonIntensityPerBlock: number;
+  /** The number of sub-auctions for a given overall duration. */
   numberOfSubAuctions: bigint;
+  /** The duration of each sub-auction for a given overall duration. */
   subAuctionDurationInBlocks: bigint;
 }
 
+/**
+ * Configuration parameters for generating sub-auctions based on the user's
+ * desired duration.
+ */
 export const GDA_RECIPES: Record<DurationOption, GdaRecipe> = {
   '10min': {
     durationInBlocks: 10n * BLOCKS_PER_MINUTE,
