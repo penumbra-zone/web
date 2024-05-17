31b2e920da55ee6b3d83264798b1aeb5798c98fb
Valentine <walik19988@gmail.com>
2024-05-17 10:33:01 +0000

    exclude auctionNft assets from dashboard, send and swap (#1123)
    * remove auctionNft assets from dashboard, send and swap
    
    * Update apps/minifront/src/components/dashboard/assets-table/index.tsx
    
    Co-authored-by: Jesse Pinho <jesse@jessepinho.com>
    
    * review fixes
    
    ---------
    
    Co-authored-by: Jesse Pinho <jesse@jessepinho.com>

--- apps/minifront/src/components/send/helpers.ts
+++ apps/minifront/src/components/send/helpers.ts
@@ -1,5 +1,12 @@
 import { isAddress } from '@penumbra-zone/bech32m/penumbra';
 import { Validation } from '../shared/validation-result';
+import { assetPatterns } from '@penumbra-zone/types/assets';
+import { Metadata } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/asset/v1/asset_pb';
+import { getDisplay } from '@penumbra-zone/getters/metadata';
+import { BalancesResponse } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1/view_pb';
+import { getBalances } from '../../fetchers/balances';
+import { getMetadata } from '@penumbra-zone/getters/value-view';
+import { isUnknown } from '../dashboard/assets-table/helpers';
 
 export const penumbraAddrValidation = (): Validation => {
   return {
@@ -7,4 +14,16 @@ export const penumbraAddrValidation = (): Validation => {
     issue: 'invalid address',
     checkFn: (addr: string) => Boolean(addr) && !isAddress(addr),
   };
+};
+
+const nonTransferableAssetPatterns = [assetPatterns.proposalNft, assetPatterns.auctionNft];
+
+export const isTransferable = (metadata: Metadata) =>
+  nonTransferableAssetPatterns.every(pattern => !pattern.matches(getDisplay(metadata)));
+
+export const getTransferableBalancesResponses = async (): Promise<BalancesResponse[]> => {
+  const balancesResponses = await getBalances();
+  return balancesResponses.filter(
+    balance => isUnknown(balance) || isTransferable(getMetadata(balance.balanceView)),
+  );
 };
