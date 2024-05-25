ad0d4ddff5d0d04a6096cdd9ec32170130724b34
Jesse Pinho <jesse@jessepinho.com>
2024-05-24 21:56:25 -0700

    Use single-row layout

--- apps/minifront/src/components/swap/swap-form/token-swap-input.tsx
+++ apps/minifront/src/components/swap/swap-form/token-swap-input.tsx
@@ -1,41 +1,15 @@
 import { BalancesResponse } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1/view_pb';
 import { Box } from '@penumbra-zone/ui/components/ui/box';
 import BalanceSelector from '../../shared/balance-selector';
-import {
-  Metadata,
-  ValueView,
-} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/asset/v1/asset_pb';
 import { ArrowRight } from 'lucide-react';
 import { AssetSelector } from '../../shared/asset-selector';
 import { BalanceValueView } from '@penumbra-zone/ui/components/ui/balance-value-view';
-import { Amount } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/num/v1/num_pb';
-import { groupByAsset } from '../../../fetchers/balances/by-asset';
 import { Input } from '@penumbra-zone/ui/components/ui/input';
 import { joinLoHiAmount } from '@penumbra-zone/types/amount';
 import { getAmount } from '@penumbra-zone/getters/balances-response';
 import { amountMoreThanBalance } from '../../../state/send';
 import { AllSlices } from '../../../state';
 import { useStoreShallow } from '../../../utils/use-store-shallow';
-
-const findMatchingBalance = (
-  metadata: Metadata | undefined,
-  balances: BalancesResponse[],
-): ValueView | undefined => {
-  if (!metadata?.penumbraAssetId) return undefined;
-
-  const match = balances.reduce(groupByAsset, []).find(v => {
-    if (v.valueView.case !== 'knownAssetId') return false;
-    return v.valueView.value.metadata?.penumbraAssetId?.equals(metadata.penumbraAssetId);
-  });
-
-  if (!match) {
-    return new ValueView({
-      valueView: { case: 'knownAssetId', value: { metadata, amount: new Amount() } },
-    });
-  }
-
-  return match;
-};
 
 const isValidAmount = (amount: string, assetIn?: BalancesResponse) =>
   Number(amount) >= 0 && (!assetIn || !amountMoreThanBalance(assetIn, amount));
@@ -68,41 +42,41 @@ export const TokenSwapInput = () => {
     setAssetOut,
     balancesResponses,
   } = useStoreShallow(tokenSwapInputSelector);
-  const balanceOfAssetOut = findMatchingBalance(assetOut, balancesResponses);
   const maxAmount = getAmount.optional()(assetIn);
   let maxAmountAsString: string | undefined;
   if (maxAmount) maxAmountAsString = joinLoHiAmount(maxAmount).toString();
 
   return (
     <Box label='Trade'>
-      <Input
-        value={amount}
-        type='number'
-        inputMode='decimal'
-        variant='transparent'
-        placeholder='Enter an amount...'
-        max={maxAmountAsString}
-        step='any'
-        className={
-          'font-bold leading-10 md:h-8 md:w-[calc(100%-80px)] md:text-xl xl:h-10 xl:w-[calc(100%-160px)] xl:text-3xl'
-        }
-        onChange={e => {
-          if (!isValidAmount(e.target.value, assetIn)) return;
-          setAmount(e.target.value);
-        }}
-      />
-
-      <div className='mt-4 flex items-center justify-between'>
-        <div className='flex flex-col gap-1'>
-          <BalanceSelector value={assetIn} onChange={setAssetIn} balances={balancesResponses} />
+      <div className='flex flex-col items-start gap-4 sm:flex-row'>
+        <div className='flex grow flex-col items-start gap-2'>
+          <Input
+            value={amount}
+            type='number'
+            inputMode='decimal'
+            variant='transparent'
+            placeholder='Enter an amount...'
+            max={maxAmountAsString}
+            step='any'
+            className={'font-bold leading-10 md:h-8 md:text-xl xl:h-10 xl:text-3xl'}
+            onChange={e => {
+              if (!isValidAmount(e.target.value, assetIn)) return;
+              setAmount(e.target.value);
+            }}
+          />
           {assetIn?.balanceView && <BalanceValueView valueView={assetIn.balanceView} />}
         </div>
 
-        <ArrowRight />
+        <div className='flex items-center justify-between gap-4'>
+          <div className='flex flex-col gap-1'>
+            <BalanceSelector value={assetIn} onChange={setAssetIn} balances={balancesResponses} />
+          </div>
 
-        <div className='flex flex-col items-end gap-1'>
-          <AssetSelector assets={swappableAssets} value={assetOut} onChange={setAssetOut} />
-          {balanceOfAssetOut && <BalanceValueView valueView={balanceOfAssetOut} />}
+          <ArrowRight size={16} className='text-muted-foreground' />
+
+          <div className='flex flex-col items-end gap-1'>
+            <AssetSelector assets={swappableAssets} value={assetOut} onChange={setAssetOut} />
+          </div>
         </div>
       </div>
     </Box>
