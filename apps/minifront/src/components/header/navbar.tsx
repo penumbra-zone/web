218e4955dc6ce08b3d50e3ae3bd7647405bfb7cc
Jesse Pinho <jesse@jessepinho.com>
2024-05-27 12:50:48 -0700

    Animate top tabs

--- apps/minifront/src/components/header/navbar.tsx
+++ apps/minifront/src/components/header/navbar.tsx
@@ -1,10 +1,26 @@
-import { cn } from '@penumbra-zone/ui/lib/utils';
-import { headerLinks } from './constants';
+import { HeaderLink, headerLinks } from './constants';
 import { Link } from 'react-router-dom';
 import { usePagePath } from '../../fetchers/page-path';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useId } from 'react';
+
+const ActiveIndicator = ({ layoutId }: { layoutId: string }) => (
+  <motion.div
+    layout
+    layoutId={layoutId}
+    className='absolute inset-0 z-10 rounded-lg bg-button-gradient-secondary px-[30px] py-[10px]'
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+  />
+);
+
+const isActive = (link: HeaderLink, pathname: HeaderLink['href']) =>
+  link.href === pathname || link.subLinks?.includes(pathname);
 
 export const Navbar = () => {
   const pathname = usePagePath();
+  const layoutId = useId();
 
   return (
     <nav className='hidden max-w-xl gap-4 xl:flex xl:grow xl:justify-between'>
@@ -13,13 +29,16 @@ export const Navbar = () => {
           <Link
             key={link.href}
             to={link.href}
-            className={cn(
-              'font-bold py-[10px] px-[30px] select-none rounded-lg',
-              (link.href === pathname || link.subLinks?.includes(pathname)) &&
-                'bg-button-gradient-secondary',
-            )}
+            className='relative select-none rounded-lg px-[30px] py-[10px] font-bold'
           >
-            {link.label}
+            <AnimatePresence>
+              {isActive(link, pathname) && <ActiveIndicator layoutId={layoutId} />}
+            </AnimatePresence>
+
+            <span className='absolute inset-0 z-20 px-[30px] py-[10px]'>{link.label}</span>
+
+            {/* For layout's sake, since other elements are absolute-positioned: */}
+            <span className='text-transparent'>{link.label}</span>
           </Link>
         ) : (
           <div
