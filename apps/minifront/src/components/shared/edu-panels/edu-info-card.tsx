8694ffdd9d6c22c674bec4b76096fcbdd88de6c4
Jesse Pinho <jesse@jessepinho.com>
2024-05-27 22:00:44 -0700

    Support motion in EduInfoCard

--- apps/minifront/src/components/shared/edu-panels/edu-info-card.tsx
+++ apps/minifront/src/components/shared/edu-panels/edu-info-card.tsx
@@ -2,21 +2,23 @@ import { Card } from '@penumbra-zone/ui/components/ui/card';
 import { cn } from '@penumbra-zone/ui/lib/utils';
 import { GradientHeader } from '@penumbra-zone/ui/components/ui/gradient-header';
 import { EduPanel, eduPanelContent } from './content';
+import { motion } from 'framer-motion';
 
 interface HelperCardProps {
   src: string;
   label: string;
   className?: string;
   content: EduPanel;
+  layout?: boolean;
 }
 
-export const EduInfoCard = ({ src, label, className, content }: HelperCardProps) => {
+export const EduInfoCard = ({ src, label, className, content, layout }: HelperCardProps) => {
   return (
-    <Card gradient className={cn('p-5 row-span-1', className)}>
-      <div className='flex gap-2'>
+    <Card gradient className={cn('p-5 row-span-1', className)} layout={layout}>
+      <motion.div layout={layout} className='flex gap-2'>
         <img src={src} alt='icons' className='size-[30px] md:size-8' />
         <GradientHeader>{label}</GradientHeader>
-      </div>
+      </motion.div>
       <p className='mt-4 text-muted-foreground md:mt-2'>{eduPanelContent[content]}</p>
     </Card>
   );
