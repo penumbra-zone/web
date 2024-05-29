9dfc256adfe229c63effa719c2181f5a53a75665
valentine <valentyn1789@gmail.com>
2024-05-30 00:01:30 +0300

    fix symbol

--- packages/ui/components/ui/tx/view/value/index.tsx
+++ packages/ui/components/ui/tx/view/value/index.tsx
@@ -28,7 +28,7 @@ export const ValueViewComponent = ({
     const { amount = new Amount(), metadata } = view.valueView.value;
     const exponent = getDisplayDenomExponent.optional()(metadata);
     const formattedAmount = formatAmount(amount, exponent);
-    const symbol = metadata.symbol || metadata.display || 'Unknown Asset';
+    const symbol = metadata.symbol || metadata.display;
     return (
       <ValueComponent
         formattedAmount={formattedAmount}
