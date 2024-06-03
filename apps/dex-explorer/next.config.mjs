90f54b2d308aa34d104067e99034b36316139f5c
the letter L <134443988+turbocrime@users.noreply.github.com>
2024-06-03 12:50:43 -0700

    fix build (penumbra-zone/dex-explorer/issues/22)
    * fix build
    
    - reconfigure exports in deps
    - use new dep releases
    - rm extraneous deps
    
    * transpile protobuf package
    
    ---------
    
    Co-authored-by: turbocrime <turbocrime@users.noreply.github.com>

--- apps/dex-explorer/next.config.mjs
+++ apps/dex-explorer/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  transpilePackages: ['@penumbra-zone/protobuf'],
   webpack: (config) => {
     config.experiments.asyncWebAssembly = true
     return config;
