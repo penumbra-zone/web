c8e8d15ce1896f625452412b2d567790b0993646
Gabe Rodriguez <grod220@gmail.com>
2024-06-04 13:13:59 +0200

    Properly format if no metadata (#1249)

---
+++ .changeset/shaggy-knives-protect.md
@@ -0,0 +1,5 @@
+---
+'@penumbra-zone/types': patch
+---
+
+Bug fix in getFormattedAmtFromValueView: support known assets without metadata
--- packages/types/src/value-view.ts
+++ packages/types/src/value-view.ts
@@ -5,13 +5,18 @@ import { formatAmount } from './amount';
 
 // Uses exponent in metadata to display amount in terms of display denom
 export const getFormattedAmtFromValueView = (v: ValueView, commas = false): string => {
+  if (!v.valueView.value) {
+    throw new Error(
+      `Cannot derive formatted amount from value view: ${JSON.stringify(v.toJson())}`,
+    );
+  }
+
   if (v.valueView.case === 'knownAssetId' && v.valueView.value.metadata) {
     const { amount = new Amount(), metadata } = v.valueView.value;
     const exponent = getDisplayDenomExponent.optional()(metadata);
     return formatAmount({ amount, exponent, commas });
-  } else if (v.valueView.case === 'unknownAssetId') {
+  } else {
     const { amount = new Amount() } = v.valueView.value;
     return formatAmount({ amount, commas });
   }
-  throw new Error(`Cannot derive formatted amount from value view: ${JSON.stringify(v.toJson())}`);
 };
