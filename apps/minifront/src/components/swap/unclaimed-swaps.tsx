295ea25de12babb90cc9790eb3a44ef17c669dbb
Jesse Pinho <jesse@jessepinho.com>
2024-06-04 15:47:52 -0700

    Coerce types for now

--- apps/minifront/src/components/swap/unclaimed-swaps.tsx
+++ apps/minifront/src/components/swap/unclaimed-swaps.tsx
@@ -2,14 +2,15 @@ import { Button } from '@penumbra-zone/ui/components/ui/button';
 import { Card } from '@penumbra-zone/ui/components/ui/card';
 import { UnclaimedSwapsWithMetadata } from './swap-loader';
 import { AssetIcon } from '@penumbra-zone/ui/components/ui/tx/view/asset-icon';
-import { useStore } from '../../state';
+import { AllSlices, useStore } from '../../state';
 import { useRevalidateUnclaimedSwaps, useUnclaimedSwaps } from '../../state/unclaimed-swaps';
 import { getSwapRecordCommitment } from '@penumbra-zone/getters/swap-record';
 import { uint8ArrayToBase64 } from '@penumbra-zone/types/base64';
 import { GradientHeader } from '@penumbra-zone/ui/components/ui/gradient-header';
+import { StoreApi, UseBoundStore } from 'zustand';
 
 export const UnclaimedSwaps = () => {
-  const unclaimedSwaps = useUnclaimedSwaps(useStore);
+  const unclaimedSwaps = useUnclaimedSwaps(useStore as UseBoundStore<StoreApi<AllSlices>>);
 
   const sortedUnclaimedSwaps = (unclaimedSwaps.data ?? []).sort(
     (a, b) => Number(b.swap.outputData?.height) - Number(a.swap.outputData?.height),
@@ -22,7 +23,7 @@ export const UnclaimedSwaps = () => {
 };
 
 const _UnclaimedSwaps = ({ unclaimedSwaps }: { unclaimedSwaps: UnclaimedSwapsWithMetadata[] }) => {
-  const revalidate = useRevalidateUnclaimedSwaps(useStore);
+  const revalidate = useRevalidateUnclaimedSwaps(useStore as UseBoundStore<StoreApi<AllSlices>>);
   const { claimSwap, isInProgress } = useStore(state => state.unclaimedSwaps);
 
   return (
