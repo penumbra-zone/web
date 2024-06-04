b2875fcabdd82680dacb1953c7976f9aff50e4b4
Jesse Pinho <jesse@jessepinho.com>
2024-06-04 16:40:10 -0700

    Document the parameters

--- apps/minifront/src/lib/z-query/index.ts
+++ apps/minifront/src/lib/z-query/index.ts
@@ -25,10 +25,68 @@ type ZQuery<Name extends string, DataType> = {
 } & Record<Name, ZQueryState<DataType>>;
 
 export const createZQuery = <StoreType, Name extends string, DataType>(
+  /** The name of this property in the state/slice. */
   name: Name,
+  /** A function that executes the query. */
   fetch: () => Promise<DataType>,
+  /**
+   * A function that returns your `useStore` object -- e.g.: `() => useStore`
+   *
+   * If you passed `useStore` directly to `createZQuery`, which is called while
+   * defining `useStore`, you'd get a circular dependency. To work around that,
+   * pass a function that returns `useStore`, so that it can be used later (once
+   * `useStore` is defined).
+   */
   getUseStore: () => UseBoundStore<StoreApi<StoreType>>,
+  /**
+   * A setter that takes an updated ZQuery state object and assigns it to the
+   * location in your overall Zustand state object where this ZQuery state
+   * object is located.
+   *
+   * This ZQuery object doesn't know anything about the store or where this
+   * ZQuery object is located within it, so it can't call `set()` with the
+   * necessary spreads/etc. to ensure that the rest of the state is untouched
+   * when the ZQuery state is updated. So your setter needs to handle that. For
+   * example, if you have a deeply nested ZQuery object (located at
+   * `state.deeply.nested.object`) and you're using Zustand's Immer middleware
+   * to be able to imitate object mutations, you could pass this setter:
+   *
+   * ```ts
+   * createZQuery(
+   *   // ...
+   *   // ...
+   *   // ...
+   *   newValue => {
+   *     // `newValue` is the entire ZQuery state object, and can be assigned
+   *     // as-is to the property that holds the ZQuery state.
+   *     useStore.setState(state => {
+   *       state.deeply.nested.object = newValue;
+   *     })
+   *   },
+   *   // ...
+   * )
+   * ```
+   */
   set: (value: ZQueryState<DataType>) => void,
+  /**
+   * A selector that takes the root Zustand state and returns just this ZQuery
+   * state object.
+   *
+   * This ZQuery object doesn't know anything about the store or where this
+   * ZQuery object is located within it, so it can't call
+   * `getUseStore().getState()` and then navigate to its own location within
+   * state. Thus, you need to pass it a selector so it can find itself.
+   *
+   * ```ts
+   * createZQuery(
+   *   // ...
+   *   // ...
+   *   // ...
+   *   // ...
+   *   state => state.deeply.nested.object,
+   * )
+   * ```
+   */
   get: (state: StoreType) => ZQueryState<DataType>,
 ): ZQuery<Name, DataType> =>
   ({
