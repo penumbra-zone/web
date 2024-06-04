696f13ad928c76534ba63eaeab88c4e12dc3d1d5
Jesse Pinho <jesse@jessepinho.com>
2024-06-04 14:50:05 -0700

    Simplify a bit

--- apps/minifront/src/lib/z-query/index.ts
+++ apps/minifront/src/lib/z-query/index.ts
@@ -17,60 +17,48 @@ export interface ZQueryState<DataType> {
 const capitalize = <Str extends string>(str: Str): Capitalize<Str> =>
   (str.charAt(0).toUpperCase() + str.slice(1)) as Capitalize<Str>;
 
-/**
- * Not exported from Zustand, so we need to copy it here.
- */
-type ExtractState<S> = S extends {
-  getState: () => infer T;
-}
-  ? T
-  : never;
-
 type ZQuery<Name extends string, DataType, StoreType> = {
   [key in `${Name}Selector`]: () => ZQueryState<DataType>;
 } & {
   [key in `revalidate${Capitalize<Name>}Selector`]: () => VoidFunction;
 } & Record<Name, (store: StoreApi<StoreType>) => ZQueryState<DataType>>;
 
-export const createZQuery =
-  <Name extends string, DataType, StoreType>(
-    name: Name,
-    fetch: () => Promise<DataType>,
-  ): ((
-    set: (value: ZQueryState<DataType>) => void,
-    get: (state: ExtractState<StoreApi<StoreType>>) => ZQueryState<DataType>,
-  ) => ZQuery<Name, DataType, StoreType>) =>
-  (set, get) =>
-    ({
-      [`${capitalize(name)}Selector`]: () =>
-        useShallow((state: StoreType) => {
-          const zQuery = get(state);
+export const createZQuery = <StoreType, Name extends string, DataType>(
+  name: Name,
+  fetch: () => Promise<DataType>,
+  set: (value: ZQueryState<DataType>) => void,
+  get: (state: StoreType) => ZQueryState<DataType>,
+): ZQuery<Name, DataType, StoreType> =>
+  ({
+    [`${capitalize(name)}Selector`]: () =>
+      useShallow((state: StoreType) => {
+        const zQuery = get(state);
 
-          return {
-            data: zQuery.data,
-            loading: zQuery.loading,
-            error: zQuery.error,
-          };
-        }),
-
-      [`revalidate${name}Selector`]: () => useShallow((state: StoreType) => get(state).revalidate),
-
-      [name]: (store: StoreApi<StoreType>) => ({
-        data: undefined,
-        loading: false,
-        error: undefined,
-
-        revalidate: () => Promise.resolve(),
-
-        _zQueryInternal: {
-          fetch: async () => {
-            try {
-              const data = await fetch();
-              set({ ...get(store.getState()), data });
-            } catch (error) {
-              set({ ...get(store.getState()), error });
-            }
-          },
-        },
+        return {
+          data: zQuery.data,
+          loading: zQuery.loading,
+          error: zQuery.error,
+        };
       }),
-    }) as ZQuery<Name, DataType, StoreType>;
+
+    [`revalidate${name}Selector`]: () => useShallow((state: StoreType) => get(state).revalidate),
+
+    [name]: (store: StoreApi<StoreType>) => ({
+      data: undefined,
+      loading: false,
+      error: undefined,
+
+      revalidate: () => Promise.resolve(),
+
+      _zQueryInternal: {
+        fetch: async () => {
+          try {
+            const data = await fetch();
+            set({ ...get(store.getState()), data });
+          } catch (error) {
+            set({ ...get(store.getState()), error });
+          }
+        },
+      },
+    }),
+  }) as ZQuery<Name, DataType, StoreType>;
