81b953691e1a82445f3ca83227b8e4b5e97c2d00
the letter L <134443988+turbocrime@users.noreply.github.com>
2024-06-05 17:23:39 -0700

    IBC `Any` ser/de (#1255)

---
+++ .changeset/healthy-kids-taste.md
@@ -0,0 +1,6 @@
+---
+'@penumbra-zone/protobuf': minor
+'@penumbra-zone/wasm': minor
+---
+
+add ibc types to registry, address wasm ser/de of protobuf.Any types
--- packages/wasm/src/transaction.ts
+++ packages/wasm/src/transaction.ts
@@ -1,30 +1,25 @@
-import { transaction_info } from '../wasm';
+import { transaction_perspective_and_view } from '../wasm';
 import {
   Transaction,
   TransactionPerspective,
   TransactionView,
 } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/transaction/v1/transaction_pb';
 import type { IdbConstants } from '@penumbra-zone/types/indexed-db';
-import { JsonValue } from '@bufbuild/protobuf';
 import { FullViewingKey } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/keys/v1/keys_pb';
-
-interface TxInfoWasmResult {
-  txp: JsonValue;
-  txv: JsonValue;
-}
 
 export const generateTransactionInfo = async (
   fullViewingKey: FullViewingKey,
   tx: Transaction,
   idbConstants: IdbConstants,
 ) => {
-  const { txp, txv } = (await transaction_info(
+  const { txp, txv } = await transaction_perspective_and_view(
     fullViewingKey.toBinary(),
     tx.toBinary(),
     idbConstants,
-  )) as TxInfoWasmResult;
+  );
+
   return {
-    txp: TransactionPerspective.fromJson(txp),
-    txv: TransactionView.fromJson(txv),
+    txp: TransactionPerspective.fromBinary(txp),
+    txv: TransactionView.fromBinary(txv),
   };
 };
