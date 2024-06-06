15870c4ca473065de61acbf87773b65e05b4ccfa
Erwan Or <erwan.ounn.84@gmail.com>
2024-06-05 21:45:36 -0400

    minifront(gda): fix mistakes in recipe parameters (#1207)
    * minifront(gda): fix mistakes in recipe parameters
    
    * minifront: rework next event time calculation
    
    * minifront: format

---
+++ apps/minifront/src/state/swap/dutch-auction/time-until-next-event.ts
@@ -0,0 +1,8 @@
+export const timeUntilNextEvent = (lambda: number): number => {
+  const sampleNextEventTime = () => {
+    // This is modeling a
+    return Math.abs(Math.log(Math.random()) / lambda);
+  };
+
+  return sampleNextEventTime();
+};
