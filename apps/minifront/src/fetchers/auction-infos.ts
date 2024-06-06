b1d718776959d3c8c3c1691ec97122d62306d9af
Jesse Pinho <jesse@jessepinho.com>
2024-06-05 17:05:43 -0700

    Use updated ZQuery APi in UnclaimedSwaps and DutchAuction slices

---
+++ apps/minifront/src/fetchers/auction-infos.ts
@@ -0,0 +1,66 @@
+import {
+  AuctionId,
+  DutchAuction,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/auction/v1/auction_pb';
+import { viewClient } from '../clients';
+import { Metadata } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/asset/v1/asset_pb';
+import { getInputAssetId, getOutputAssetId } from '@penumbra-zone/getters/dutch-auction';
+
+export interface AuctionInfo {
+  id: AuctionId;
+  auction: DutchAuction;
+  inputMetadata?: Metadata;
+  outputMetadata?: Metadata;
+}
+
+export const getAuctionInfos = async function* ({
+  queryLatestState = false,
+  abortController,
+}: {
+  queryLatestState?: boolean;
+  abortController?: AbortController;
+}): AsyncGenerator<AuctionInfo> {
+  abortController?.abort();
+  const newAbortController = new AbortController();
+
+  for await (const response of viewClient.auctions(
+    { queryLatestState, includeInactive: true },
+    /**
+     * Weirdly, just passing the newAbortController.signal here doesn't seem to
+     * have any effect, despite the ConnectRPC docs saying that it should
+     * work. I still left this line in, though, since it seems right and
+     * perhaps will be fixed in a later ConnectRPC release. But in the
+     * meantime, returning early from the `for` loop below fixes this issue.
+     *
+     * @see https://connectrpc.com/docs/web/cancellation-and-timeouts/
+     */
+    { signal: newAbortController.signal },
+  )) {
+    if (newAbortController.signal.aborted) return;
+    if (!response.auction || !response.id) continue;
+
+    const auction = DutchAuction.fromBinary(response.auction.value);
+
+    const inputAssetId = getInputAssetId.optional()(auction);
+    const outputAssetId = getOutputAssetId.optional()(auction);
+
+    const inputMetadataPromise = inputAssetId
+      ? viewClient.assetMetadataById({ assetId: inputAssetId })
+      : undefined;
+    const outputMetadataPromise = outputAssetId
+      ? viewClient.assetMetadataById({ assetId: outputAssetId })
+      : undefined;
+
+    const [inputMetadata, outputMetadata] = await Promise.all([
+      inputMetadataPromise,
+      outputMetadataPromise,
+    ]);
+
+    yield {
+      id: response.id,
+      auction,
+      inputMetadata: inputMetadata?.denomMetadata,
+      outputMetadata: outputMetadata?.denomMetadata,
+    };
+  }
+};
--- packages/getters/src/dutch-auction.ts
+++ packages/getters/src/dutch-auction.ts
@@ -4,3 +4,11 @@ import { createGetter } from './utils/create-getter';
 export const getDescription = createGetter(
   (dutchAuction?: DutchAuction) => dutchAuction?.description,
 );
+
+export const getInputAssetId = createGetter(
+  (dutchAuction?: DutchAuction) => dutchAuction?.description?.input?.assetId,
+);
+
+export const getOutputAssetId = createGetter(
+  (dutchAuction?: DutchAuction) => dutchAuction?.description?.outputId,
+);
