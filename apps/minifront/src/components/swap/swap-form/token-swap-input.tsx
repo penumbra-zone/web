cf2594d94fa5bc69d75f012405fc4ddd46376b41
Max Korsunov <maxkors2014@gmail.com>
2024-06-11 19:38:42 +0200

    Fix: #1226: Swap wallet balances (#1252)
    * fix: #1226: change markup of the token swap input
    
    * fix: #1226: display balance of the out-token per selected in-account
    
    * fix: #1225: add changesets
    
    * fix(minifront): #1226: refactor the balance out calculation using getters
    
    * fix(getters): #1226: add changesets
    
    * fix(minifront): #1226: fix linting
    
    * Convert getters to selector
    
    * fix: remove unneeded changesets
    
    * fix(minifront): #1226: Fix after the review
    
    ---------
    
    Co-authored-by: Gabe Rodriguez <grod220@gmail.com>

--- apps/minifront/src/components/swap/swap-form/token-swap-input.tsx
+++ apps/minifront/src/components/swap/swap-form/token-swap-input.tsx
@@ -1,22 +1,55 @@
 import { BalancesResponse } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1/view_pb';
-import { getAmount } from '@penumbra-zone/getters/balances-response';
-import { joinLoHiAmount } from '@penumbra-zone/types/amount';
-import { getFormattedAmtFromValueView } from '@penumbra-zone/types/value-view';
 import { BalanceValueView } from '@penumbra-zone/ui/components/ui/balance-value-view';
 import { Box } from '@penumbra-zone/ui/components/ui/box';
 import { CandlestickPlot } from '@penumbra-zone/ui/components/ui/candlestick-plot';
 import { Input } from '@penumbra-zone/ui/components/ui/input';
+import { joinLoHiAmount } from '@penumbra-zone/types/amount';
+import {
+  getAmount,
+  getBalanceView,
+  getMetadataFromBalancesResponse,
+} from '@penumbra-zone/getters/balances-response';
 import { ArrowRight } from 'lucide-react';
 import { useEffect } from 'react';
 import { getBlockDate } from '../../../fetchers/block-date';
 import { AllSlices } from '../../../state';
 import { amountMoreThanBalance } from '../../../state/send';
 import { useStoreShallow } from '../../../utils/use-store-shallow';
+import { getFormattedAmtFromValueView } from '@penumbra-zone/types/value-view';
+import { getAddressIndex } from '@penumbra-zone/getters/address-view';
+import {
+  Metadata,
+  ValueView,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/asset/v1/asset_pb';
 import { AssetSelector } from '../../shared/asset-selector';
 import BalanceSelector from '../../shared/balance-selector';
+import { Amount } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/num/v1/num_pb';
 
 const isValidAmount = (amount: string, assetIn?: BalancesResponse) =>
   Number(amount) >= 0 && (!assetIn || !amountMoreThanBalance(assetIn, amount));
+
+const getKnownZeroValueView = (metadata?: Metadata) => {
+  return new ValueView({
+    valueView: {
+      case: 'knownAssetId',
+      value: { amount: new Amount({ lo: 0n }), metadata },
+    },
+  });
+};
+
+const assetOutBalanceSelector = ({ swap: { balancesResponses, assetIn, assetOut } }: AllSlices) => {
+  if (!assetIn || !assetOut) return getKnownZeroValueView();
+
+  const match = balancesResponses.find(balance => {
+    const balanceViewMetadata = getMetadataFromBalancesResponse(balance);
+
+    return (
+      balance.accountAddress?.equals(assetIn.accountAddress) && assetOut.equals(balanceViewMetadata)
+    );
+  });
+  const matchedBalance = getBalanceView.optional()(match);
+  return matchedBalance ?? getKnownZeroValueView(assetOut);
+};
 
 const tokenSwapInputSelector = (state: AllSlices) => ({
   swappableAssets: state.swap.swappableAssets,
@@ -29,6 +62,7 @@ const tokenSwapInputSelector = (state: AllSlices) => ({
   balancesResponses: state.swap.balancesResponses,
   priceHistory: state.swap.priceHistory,
   latestKnownBlockHeight: state.status.latestKnownBlockHeight,
+  assetOutBalance: assetOutBalanceSelector(state),
 });
 
 /**
@@ -49,6 +83,7 @@ export const TokenSwapInput = () => {
     balancesResponses,
     priceHistory,
     latestKnownBlockHeight = 0n,
+    assetOutBalance,
   } = useStoreShallow(tokenSwapInputSelector);
 
   useEffect(() => {
@@ -74,54 +109,57 @@ export const TokenSwapInput = () => {
 
   return (
     <Box label='Trade' layout>
-      <div className='gap-4'>
-        <div className='flex flex-col items-start gap-4 sm:flex-row'>
-          <div className='flex grow flex-row items-start gap-2'>
-            <Input
-              value={amount}
-              type='number'
-              inputMode='decimal'
-              variant='transparent'
-              placeholder='Enter an amount...'
-              max={maxAmountAsString}
-              step='any'
-              className={'font-bold leading-10 md:h-8 md:text-xl xl:h-10 xl:text-3xl'}
-              onChange={e => {
-                if (!isValidAmount(e.target.value, assetIn)) return;
-                setAmount(e.target.value);
-              }}
-            />
+      <div className='flex flex-col items-stretch gap-4 sm:flex-row'>
+        <Input
+          value={amount}
+          type='number'
+          inputMode='decimal'
+          variant='transparent'
+          placeholder='Enter an amount...'
+          max={maxAmountAsString}
+          step='any'
+          className={'font-bold leading-10 md:h-8 md:text-xl xl:h-10 xl:text-3xl'}
+          onChange={e => {
+            if (!isValidAmount(e.target.value, assetIn)) return;
+            setAmount(e.target.value);
+          }}
+        />
+
+        <div className='flex gap-4 sm:contents'>
+          {assetIn && (
+            <div className='ml-auto hidden h-full flex-col justify-end self-end sm:flex'>
+              <span className='mr-2 block whitespace-nowrap text-xs text-muted-foreground'>
+                Account #{getAddressIndex(assetIn.accountAddress).account}
+              </span>
+            </div>
+          )}
+
+          <div className='flex h-full flex-col gap-2'>
+            <BalanceSelector value={assetIn} onChange={setAssetIn} balances={balancesResponses} />
             {assetIn?.balanceView && (
-              <div className='h-6'>
-                <BalanceValueView valueView={assetIn.balanceView} onClick={setInputToBalanceMax} />
-              </div>
+              <BalanceValueView valueView={assetIn.balanceView} onClick={setInputToBalanceMax} />
             )}
           </div>
 
-          <div className='flex items-center justify-between gap-4'>
-            <div className='flex flex-col gap-1'>
-              <BalanceSelector value={assetIn} onChange={setAssetIn} balances={balancesResponses} />
-            </div>
-
+          <div className='flex flex-col gap-2 pt-2'>
             <ArrowRight size={16} className='text-muted-foreground' />
+          </div>
 
-            <div className='flex flex-col items-end gap-1'>
-              <AssetSelector assets={swappableAssets} value={assetOut} onChange={setAssetOut} />
-            </div>
+          <div className='flex h-full flex-col gap-2'>
+            <AssetSelector assets={swappableAssets} value={assetOut} onChange={setAssetOut} />
+            {assetOut && <BalanceValueView valueView={assetOutBalance} />}
           </div>
         </div>
-        {priceHistory.startMetadata &&
-          priceHistory.endMetadata &&
-          !!priceHistory.candles.length && (
-            <CandlestickPlot
-              className='h-[480px] w-full bg-charcoal'
-              candles={priceHistory.candles}
-              startMetadata={priceHistory.startMetadata}
-              endMetadata={priceHistory.endMetadata}
-              latestKnownBlockHeight={Number(latestKnownBlockHeight)}
-              getBlockDate={getBlockDate}
-            />
-          )}
+        {priceHistory.startMetadata && priceHistory.endMetadata && priceHistory.candles.length ? (
+          <CandlestickPlot
+            className='h-[480px] w-full bg-charcoal'
+            candles={priceHistory.candles}
+            startMetadata={priceHistory.startMetadata}
+            endMetadata={priceHistory.endMetadata}
+            latestKnownBlockHeight={Number(latestKnownBlockHeight)}
+            getBlockDate={getBlockDate}
+          />
+        ) : null}
       </div>
     </Box>
   );
