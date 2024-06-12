30e4aa5947f92d2c804894de3ba0e728a8bd2063
Jesse Pinho <jesse@jessepinho.com>
2024-06-12 12:33:40 -0700

    Revert unnecessary changes to <Separator />

--- packages/ui/components/ui/separator.tsx
+++ packages/ui/components/ui/separator.tsx
@@ -8,5 +8,5 @@ export const Separator = () => (
   // For some reason, Tailwind's ESLint config wants to change `border-b-[1px]`
   // to `border-b-DEFAULT`, even though that has a different effect!
   // eslint-disable-next-line tailwindcss/no-unnecessary-arbitrary-value
-  <div className='mx-2 h-px min-w-8 grow border-b-[1px] border-dashed border-light-brown' />
+  <div className='mx-2 h-px min-w-8 grow border-b-[1px] border-dotted border-light-brown' />
 );
