3c6b0bf182fb1a36c467cab53ed7690f8747ad97
Gabe Rodriguez <grod220@gmail.com>
2024-06-13 12:32:20 +0200

    Migrate Prax out (#1291)

--- apps/extension/prax.d.ts
+++
@@ -1,5 +0,0 @@
-// these are defined at build time by webpack, using values in .env
-
-declare const PRAX: string;
-declare const PRAX_ORIGIN: string;
-declare const IDB_VERSION: number;
--- apps/extension/src/types/declarations.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.mp4' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.png' {
-  const content: string;
-  export default content;
-}
