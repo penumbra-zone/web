f92493bb85fcb20112228de6dbc4b8dfbcef08b6
Valentine <walik19988@gmail.com>
2024-06-14 21:30:27 +0300

    Update apps/extension/src/shared/components/numeraires-form.tsx
    Co-authored-by: Jesse Pinho <jesse@jessepinho.com>

--- apps/extension/src/shared/components/numeraires-form.tsx
+++ apps/extension/src/shared/components/numeraires-form.tsx
@@ -49,7 +49,6 @@ export const NumeraireForm = ({
   };
 
   return (
-    <>
       <div className='flex flex-col gap-2'>
         <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
           <SelectList>
