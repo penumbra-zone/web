9ee6507903460967896d6f6aa19ec379d5223a93
Jesse Pinho <jesse@jessepinho.com>
2024-06-13 21:46:03 -0700

    Use animation and disabled state to reflect loading status

--- apps/minifront/src/components/swap/auction-list/query-latest-state-button.tsx
+++ apps/minifront/src/components/swap/auction-list/query-latest-state-button.tsx
@@ -5,9 +5,11 @@ import {
   TooltipTrigger,
 } from '@penumbra-zone/ui/components/ui/tooltip';
 import { ReloadIcon } from '@radix-ui/react-icons';
-import { useRevalidateAuctionInfos } from '../../../state/swap/dutch-auction';
+import { useAuctionInfos, useRevalidateAuctionInfos } from '../../../state/swap/dutch-auction';
+import { cn } from '@penumbra-zone/ui/lib/utils';
 
 export const QueryLatestStateButton = () => {
+  const { loading } = useAuctionInfos();
   const revalidate = useRevalidateAuctionInfos();
 
   return (
@@ -16,8 +18,15 @@ export const QueryLatestStateButton = () => {
         <TooltipTrigger
           onClick={() => revalidate({ queryLatestState: true })}
           aria-label='Get the current auction reserves (makes a request to a fullnode)'
+          disabled={loading}
         >
-          <div className='p-2'>
+          <div
+            className={cn(
+              'p-2 transition-transform',
+              loading && 'animate-spin text-muted-foreground',
+              !loading && 'hover:rotate-45',
+            )}
+          >
             <ReloadIcon />
           </div>
         </TooltipTrigger>
