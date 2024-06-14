719c6bf833caac84826bc0365a01d724b7445d52
Jesse Pinho <jesse@jessepinho.com>
2024-06-14 01:09:36 -0700

    Add Promise.withResolvers shim

--- packages/zquery/tests-setup.ts
+++ packages/zquery/tests-setup.ts
@@ -1,5 +1,8 @@
+import withResolvers from 'promise.withresolvers';
 import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
+
+withResolvers.shim();
 
 afterEach(() => {
   // Clear anything rendered by jsdom. (Without this, previous tests can leave
