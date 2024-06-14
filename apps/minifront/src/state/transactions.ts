a46a4bcb15304d6562011e8a714e1d23b55ffc4a
Jesse Pinho <jesse@jessepinho.com>
2024-06-14 14:18:59 -0700

    Update transactions slice to use ZQuery

--- apps/minifront/src/state/transactions.ts
+++ apps/minifront/src/state/transactions.ts
@@ -1,41 +1,62 @@
 import { uint8ArrayToHex } from '@penumbra-zone/types/hex';
-import { SliceCreator } from '.';
+import { SliceCreator, useStore } from '.';
 import { viewClient } from '../clients';
 import { getTransactionClassificationLabel } from '@penumbra-zone/perspective/transaction/classify';
+import { ZQueryState, createZQuery } from '@penumbra-zone/zquery';
+import { TransactionInfoResponse } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1/view_pb';
 
 export interface TransactionSummary {
   height: number;
   hash: string;
   description: string;
 }
+const byHeight = (a: TransactionSummary, b: TransactionSummary) => b.height - a.height;
+
+const getHash = (tx: TransactionInfoResponse) =>
+  tx.txInfo?.id?.inner ? uint8ArrayToHex(tx.txInfo.id.inner) : 'unknown';
+
+export const { summaries, useSummaries } = createZQuery({
+  name: 'summaries',
+  fetch: () => viewClient.transactionInfo({}),
+  stream: () => {
+    const txIdsToKeep = new Set<string>();
+    return {
+      onValue: (prevState: TransactionSummary[] | undefined = [], tx: TransactionInfoResponse) => {
+        const hash = getHash(tx);
+        txIdsToKeep.add(hash);
+
+        const summary = {
+          height: Number(tx.txInfo?.height ?? 0n),
+          hash,
+          description: getTransactionClassificationLabel(tx.txInfo?.view),
+        };
+
+        const existingIndex = prevState.findIndex(summary => summary.hash === hash);
+
+        // Update existing transactions in place, rather than appending
+        // duplicates.
+        if (existingIndex >= 0) return prevState.toSpliced(existingIndex, 1, summary);
+        else return [...prevState, summary].sort(byHeight);
+      },
+
+      onEnd: (prevState: TransactionSummary[] | undefined = []) =>
+        prevState.filter(({ hash }) => txIdsToKeep.has(hash)),
+    };
+  },
+  getUseStore: () => useStore,
+  get: state => state.transactions.summaries,
+  set: setter => {
+    const newState = setter(useStore.getState().transactions.summaries);
+    useStore.setState(state => {
+      state.transactions.summaries = newState;
+    });
+  },
+});
 
 export interface TransactionsSlice {
-  summaries: TransactionSummary[];
-  loadSummaries: () => Promise<void>;
+  summaries: ZQueryState<TransactionSummary[]>;
 }
 
-export const createTransactionsSlice = (): SliceCreator<TransactionsSlice> => (set, get) => ({
-  summaries: [],
-
-  loadSummaries: async () => {
-    set(state => {
-      state.transactions.summaries = [];
-    });
-
-    for await (const tx of viewClient.transactionInfo({})) {
-      const summary = {
-        height: Number(tx.txInfo?.height ?? 0n),
-        hash: tx.txInfo?.id?.inner ? uint8ArrayToHex(tx.txInfo.id.inner) : 'unknown',
-        description: getTransactionClassificationLabel(tx.txInfo?.view),
-      };
-
-      const summaries = [...get().transactions.summaries, summary].sort(
-        (a, b) => b.height - a.height,
-      );
-
-      set(state => {
-        state.transactions.summaries = summaries;
-      });
-    }
-  },
+export const createTransactionsSlice = (): SliceCreator<TransactionsSlice> => () => ({
+  summaries,
 });
