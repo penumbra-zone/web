52df849bc8ddb59f73f8b798b075f8e60867d17b
turbocrime <turbocrime@users.noreply.github.com>
2024-06-17 20:42:40 -0700

    script pack-public

--- pack-public.sh
+++ pack-public.sh
@@ -2,18 +2,15 @@
 
 # pnpm doesn't like to recurse for the pack command, and turbo doesn't
 # understand it as a script.  this script should pack every non-private package.
-pnpm turbo build --filter="./packages/*"
-pnpm pack -C packages/bech32m && \
-pnpm pack -C packages/client && \
-pnpm pack -C packages/crypto && \
-pnpm pack -C packages/getters && \
-pnpm pack -C packages/keys && \
-pnpm pack -C packages/perspective && \
-pnpm pack -C packages/protobuf && \
-pnpm pack -C packages/query && \
-pnpm pack -C packages/services && \
-pnpm pack -C packages/storage && \
-pnpm pack -C packages/transport-chrome && \
-pnpm pack -C packages/transport-dom && \
-pnpm pack -C packages/types && \
-pnpm pack -C packages/wasm
+
+pnpm turbo run build --filter="./packages/*"
+
+for packageJ in packages/*/package.json; do
+    package=$(dirname $packageJ)
+    private=$(pnpm pkg get private -C "$package")
+    if [ "$private" != "true" ]; then
+        pnpm pack -C "$package" &
+    fi
+done
+
+wait
