f7b93a6455403bdcf8b6b26dc51ceaf9c93b3787
Jesse Pinho <jesse@jessepinho.com>
2024-06-20 16:21:01 -0700

    Use ZQuery for loading chains

--- apps/minifront/src/fetchers/registry.ts
+++ apps/minifront/src/fetchers/registry.ts
@@ -1,4 +1,4 @@
-import { ChainRegistryClient, Registry } from '@penumbra-labs/registry';
+import { Chain, ChainRegistryClient, Registry } from '@penumbra-labs/registry';
 import { useQuery } from '@tanstack/react-query';
 import { getChainId } from './chain-id';
 import { getAssetMetadataById } from './assets';
@@ -32,7 +32,7 @@ export const getStakingTokenMetadata = async () => {
   return stakingAssetsMetadata;
 };
 
-export const getIbcConnections = async () => {
+export const getChains = async (): Promise<Chain[]> => {
   const chainId = await getChainId();
   if (!chainId) throw new Error('Could not fetch chain id');
 
