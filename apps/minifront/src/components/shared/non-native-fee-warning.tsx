0e1b348825940e986d33d254d1d79381eb140cec
Jesse Pinho <jesse@jessepinho.com>
2024-06-20 14:32:10 -0700

    Extract a NonNativeFeeWarning component

---
+++ apps/minifront/src/components/shared/non-native-fee-warning.tsx
@@ -0,0 +1,47 @@
+import { BalancesResponse } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1/view_pb';
+import { Metadata } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/asset/v1/asset_pb';
+import { getAssetIdFromValueView } from '@penumbra-zone/getters/value-view';
+import { getAssetId } from '@penumbra-zone/getters/metadata';
+import { useStakingTokenMetadata } from '../../state/shared';
+import { ReactNode } from 'react';
+
+const hasStakingToken = (
+  balancesResponses: BalancesResponse[] = [],
+  stakingAssetMetadata?: Metadata,
+): boolean => {
+  return balancesResponses.some(asset =>
+    getAssetIdFromValueView(asset.balanceView).equals(getAssetId(stakingAssetMetadata)),
+  );
+};
+
+export const useShouldRender = (balancesResponses: BalancesResponse[] = [], amount: number) => {
+  const stakingTokenMetadata = useStakingTokenMetadata();
+  const userHasStakingToken = hasStakingToken(balancesResponses, stakingTokenMetadata.data);
+  const showNonNativeFeeWarning = amount > 0 && !userHasStakingToken;
+
+  return showNonNativeFeeWarning;
+};
+
+export const NonNativeFeeWarning = ({
+  balancesResponses,
+  amount,
+  wrap = children => children,
+}: {
+  balancesResponses?: BalancesResponse[];
+  amount: number;
+  wrap?: (children: ReactNode) => ReactNode;
+}) => {
+  const shouldRender = useShouldRender(balancesResponses, amount);
+
+  if (!shouldRender) return null;
+
+  return wrap(
+    <div className='rounded border border-yellow-500 bg-gray-800 p-4 text-yellow-500'>
+      <strong>Privacy Warning:</strong>
+      <span className='block'>
+        Using non-native tokens for transaction fees may pose a privacy risk. It is recommended to
+        use the native token (UM) for better privacy and security.
+      </span>
+    </div>,
+  );
+};
