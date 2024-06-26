fd5b32b2515041f5618e1412d6a114646eef7e77
Jesse Pinho <jesse@jessepinho.com>
2024-06-26 10:55:00 -0700

    Shim Promise.withResolvers() for ZQuery tests
    Not sure how this got removed.

--- packages/zquery/tests-setup.ts
+++ packages/zquery/tests-setup.ts
@@ -1,5 +1,8 @@
 import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
+import withResolvers from 'promise.withresolvers';
+
+withResolvers.shim();
 
 afterEach(() => {
   // Clear anything rendered by jsdom. (Without this, previous tests can leave
