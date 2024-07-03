ffe956e3e00353e1c7057fd1fcce2cb8bfd7feca
Jesse Pinho <jesse@jessepinho.com>
2024-07-03 13:13:25 -0700

    Move the Account column to the left of asset selectors; fix a few UI issues (#1402)

--- apps/minifront/src/components/shared/selectors/balance-item.tsx
+++ apps/minifront/src/components/shared/selectors/balance-item.tsx
@@ -1,56 +1,64 @@
 import { BalanceOrMetadata, isBalance, isMetadata } from './helpers';
 import { getAddressIndex } from '@penumbra-zone/getters/address-view';
-import { getMetadataFromBalancesResponseOptional } from '@penumbra-zone/getters/balances-response';
+import {
+  getMetadataFromBalancesResponse,
+  getMetadataFromBalancesResponseOptional,
+} from '@penumbra-zone/getters/balances-response';
 import { useMemo } from 'react';
 import { DialogClose } from '@repo/ui/components/ui/dialog';
 import { cn } from '@repo/ui/lib/utils';
 import { AssetIcon } from '@repo/ui/components/ui/asset-icon';
 import { ValueViewComponent } from '@repo/ui/components/ui/value';
+import { TableCell, TableRow } from '@repo/ui/components/ui/table';
+import { BalancesResponse } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1/view_pb';
 
 interface BalanceItemProps {
   asset: BalanceOrMetadata;
-  value?: BalanceOrMetadata;
+  value?: BalancesResponse;
   onSelect: (value: BalanceOrMetadata) => void;
 }
 
 export const BalanceItem = ({ asset, value, onSelect }: BalanceItemProps) => {
   const account = isBalance(asset) ? getAddressIndex(asset.accountAddress).account : undefined;
-  const metadata = isMetadata(asset) ? asset : getMetadataFromBalancesResponseOptional(asset);
+  const metadataFromAsset = isMetadata(asset)
+    ? asset
+    : getMetadataFromBalancesResponseOptional(asset);
+  const metadataFromValue = getMetadataFromBalancesResponse.optional()(value);
 
   const isSelected = useMemo(() => {
     if (!value) return false;
-    if (isMetadata(value) && isMetadata(asset)) {
-      return value.equals(asset);
-    }
-    if (isBalance(value) && isBalance(asset)) {
-      return value.equals(asset);
-    }
+    if (isBalance(asset)) return value.equals(asset);
+    if (isMetadata(asset)) return metadataFromValue?.equals(metadataFromAsset);
     return false;
   }, [asset, value]);
 
   return (
-    <div className='flex flex-col'>
-      <DialogClose onClick={() => onSelect(asset)}>
-        <div
-          className={cn(
-            'grid grid-cols-5 py-[10px] gap-3 cursor-pointer hover:bg-light-brown hover:px-4 hover:-mx-4 font-bold text-muted-foreground',
-            isSelected && 'bg-light-brown px-4 -mx-4',
-          )}
-        >
-          <div className='col-span-2 flex items-center justify-start gap-1'>
-            <AssetIcon metadata={metadata} />
-            <p className='truncate'>{metadata?.symbol ? metadata.symbol : 'Unknown asset'}</p>
-          </div>
+    <DialogClose asChild onClick={() => onSelect(asset)}>
+      <TableRow
+        className={cn(
+          'cursor-pointer hover:bg-light-brown font-bold text-muted-foreground',
+          isSelected && 'bg-light-brown',
+        )}
+      >
+        <TableCell>{account}</TableCell>
 
+        <TableCell>
+          <div className='col-span-2 flex items-center justify-start gap-1'>
+            <AssetIcon metadata={metadataFromAsset} />
+            <p className='truncate'>
+              {metadataFromAsset?.symbol ? metadataFromAsset.symbol : 'Unknown asset'}
+            </p>
+          </div>
+        </TableCell>
+
+        <TableCell>
           <div className='col-span-2 flex justify-end'>
             {isBalance(asset) && (
               <ValueViewComponent showIcon={false} showDenom={false} view={asset.balanceView} />
             )}
           </div>
-
-          <p className='flex justify-center'>{account}</p>
-        </div>
-      </DialogClose>
-    </div>
+        </TableCell>
+      </TableRow>
+    </DialogClose>
   );
 };
