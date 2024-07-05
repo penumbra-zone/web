6c2bfdcff214769d794f26995b50ec4d7fb6b2ca
Tal Derei <70081547+TalDerei@users.noreply.github.com>
2024-07-05 12:02:17 -0700

    fix ics20 ibc withdrawals (#1422)
    * fix ics20 withdrawls
    
    * extract asset id from denom

--- packages/services/src/view-service/fees.ts
+++ packages/services/src/view-service/fees.ts
@@ -1,5 +1,6 @@
 import { AssetId } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/asset/v1/asset_pb';
 import { TransactionPlannerRequest } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1/view_pb';
+import { assetIdFromBaseDenom } from '@penumbra-zone/wasm/asset';
 
 // Attempts to extract a fee token, with priority in descending order, from the assets used
 // in the actions of the planner request.
@@ -13,6 +14,9 @@ export const extractAltFee = (request: TransactionPlannerRequest): AssetId => {
 
   const swapAsset = request.swaps.map(assetIn => assetIn.value?.assetId).find(Boolean);
   if (swapAsset) return swapAsset;
+
+  const ics20WithdrawAsset = request.ics20Withdrawals.map(w => w.denom).find(Boolean);
+  if (ics20WithdrawAsset) return assetIdFromBaseDenom(ics20WithdrawAsset.denom);
 
   throw new Error('Could not extract alternative fee assetId from TransactionPlannerRequest');
 };
