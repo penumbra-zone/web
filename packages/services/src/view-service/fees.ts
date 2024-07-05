58e3985ca4e0b10cb716fe79db177735ef963db2
Tal Derei <talderei99@gmail.com>
2024-07-05 07:41:40 -0700

    remove auctions

--- packages/services/src/view-service/fees.ts
+++ packages/services/src/view-service/fees.ts
@@ -4,10 +4,7 @@ import { TransactionPlannerRequest } from '@buf/penumbra-zone_penumbra.bufbuild_
 // Attempts to extract a fee token, with priority in descending order, from the assets used
 // in the actions of the planner request.
 // TODO: expand functionality for other action types like auctions, IBC, governance, etc.
-export const extractAltFee = (
-  request: TransactionPlannerRequest,
-  stakingTokenAssetId: AssetId,
-): AssetId => {
+export const extractAltFee = (request: TransactionPlannerRequest): AssetId => {
   const outputAsset = request.outputs.map(o => o.value?.assetId).find(Boolean);
   if (outputAsset) return outputAsset;
 
@@ -16,25 +13,6 @@ export const extractAltFee = (
 
   const swapAsset = request.swaps.map(assetIn => assetIn.value?.assetId).find(Boolean);
   if (swapAsset) return swapAsset;
-
-  const auctionScheduleAsset = request.dutchAuctionScheduleActions
-    .map(a => a.description?.outputId)
-    .find(Boolean);
-  if (auctionScheduleAsset) {
-    return stakingTokenAssetId;
-  }
-
-  const auctionEndAsset = request.dutchAuctionEndActions.map(a => a.auctionId?.inner).find(Boolean);
-  if (auctionEndAsset) {
-    return stakingTokenAssetId;
-  }
-
-  const auctionWithdrawAsset = request.dutchAuctionWithdrawActions
-    .map(a => a.auctionId?.inner)
-    .find(Boolean);
-  if (auctionWithdrawAsset) {
-    return stakingTokenAssetId;
-  }
 
   throw new Error('Could not extract alternative fee assetId from TransactionPlannerRequest');
 };
