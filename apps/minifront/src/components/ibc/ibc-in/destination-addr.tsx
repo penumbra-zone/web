1d8ee10b20ce0d320889346386e0247e38e280a8
Max Korsunov <maxkors2014@gmail.com>
2024-07-08 14:09:42 +0200

    feat(minifront): #1383: add information about fresh address in ibc-in

--- apps/minifront/src/components/ibc/ibc-in/destination-addr.tsx
+++ apps/minifront/src/components/ibc/ibc-in/destination-addr.tsx
@@ -1,21 +1,41 @@
 import { AllSlices } from '../../../state';
 import { useStoreShallow } from '../../../utils/use-store-shallow';
 import { AccountSwitcher } from '@repo/ui/components/ui/account-switcher';
+import { CopyToClipboard } from '@repo/ui/components/ui/copy-to-clipboard';
+import { useEffect } from 'react';
 
 const addrsSelector = ({ ibcIn }: AllSlices) => ({
   account: ibcIn.account,
+  address: ibcIn.address,
   setAccount: ibcIn.setAccount,
 });
 
 export const DestinationAddr = () => {
-  const { account, setAccount } = useStoreShallow(addrsSelector);
+  const { account, setAccount, address } = useStoreShallow(addrsSelector);
+
+  // Set initial account to trigger address loading
+  useEffect(() => {
+    setAccount(0);
+  }, []);
 
   return (
-    <div className='flex w-full flex-col gap-1 break-all text-stone-700'>
+    <div className='mb-2 flex w-full flex-col gap-1 break-all text-stone-700'>
       <div className='flex items-center justify-between'>
         <p className='font-bold'>Sending to</p>
         <AccountSwitcher compact account={account} onChange={setAccount} />
       </div>
+
+      {address && (
+        <div className='break-words text-sm text-stone-700 [&>button]:inline-block [&>button]:p-0'>
+          The deposit will be made to a freshly generated address belonging to you. You can inspect
+          what account it points to inside of Prax.{' '}
+          <CopyToClipboard
+            text={address}
+            successLabel={<span className='text-sm'>Copied</span>}
+            label={<span className='text-sm text-stone-700 underline'>Copy address</span>}
+          />
+        </div>
+      )}
     </div>
   );
 };
