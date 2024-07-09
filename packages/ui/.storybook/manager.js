1db6ea7334afe26bdb5f6c8c13429df6364d2c27
Jesse Pinho <jesse@jessepinho.com>
2024-07-09 13:05:38 -0700

    Set up theme and manager options

---
+++ packages/ui/.storybook/manager.js
@@ -0,0 +1,11 @@
+import { addons } from '@storybook/manager-api';
+import penumbraTheme from './penumbraTheme';
+
+addons.setConfig({
+  showToolbar: true,
+  theme: penumbraTheme,
+  sidebar: {
+    showRoots: false,
+    collapsedRoots: ['other'],
+  },
+});
