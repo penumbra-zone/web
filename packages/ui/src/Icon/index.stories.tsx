af0a25619245abdba7ffcac779f25d91d21b7ac8
Jesse Pinho <jesse@jessepinho.com>
2024-07-09 13:57:00 -0700

    Create an Icon component and stories

---
+++ packages/ui/src/Icon/index.stories.tsx
@@ -0,0 +1,31 @@
+import { ArrowRightLeft, Send, Wallet } from 'lucide-react';
+import { Meta, StoryObj } from '@storybook/react';
+
+import { Icon } from '.';
+
+const meta: Meta<typeof Icon> = {
+  component: Icon,
+  tags: ['autodocs'],
+  decorators: [
+    Story => (
+      <div className='text-rust'>
+        <Story />
+      </div>
+    ),
+  ],
+  argTypes: {
+    IconComponent: {
+      options: ['ArrowRightLeft', 'Send', 'Wallet'],
+      mapping: { ArrowRightLeft, Send, Wallet },
+    },
+  },
+};
+
+export default meta;
+
+export const Basic: StoryObj<typeof Icon> = {
+  args: {
+    IconComponent: ArrowRightLeft,
+    size: 'sm',
+  },
+};
