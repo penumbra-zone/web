5c600453d8c444e66c66a455a52dec9784ac12cd
Jesse Pinho <jesse@jessepinho.com>
2024-07-09 13:26:45 -0700

    Set the brand title

--- packages/ui/.storybook/penumbraTheme.js
+++ packages/ui/.storybook/penumbraTheme.js
@@ -4,6 +4,7 @@ import logo from './public/logo.svg';
 const penumbraTheme = create({
   base: 'dark',
   brandImage: logo,
+  brandTitle: 'Penumbra UI library',
   colorPrimary: '#8d5728',
   colorSecondary: '#629994',
 });
