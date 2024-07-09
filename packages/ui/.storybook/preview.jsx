5055616d3931537efbc57c56a6dddfe1c7397f36
Jesse Pinho <jesse@jessepinho.com>
2024-07-09 14:43:56 -0700

    Only add deprecated CSS to deprecated components

--- packages/ui/.storybook/preview.jsx
+++ packages/ui/.storybook/preview.jsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css';
+import React from 'react';
+import globalsCssUrl from '../styles/globals.css?url';
 import penumbraTheme from './penumbraTheme';
 
 /** @type { import('@storybook/react').Preview } */
 const preview = {
+  decorators: [
+    (Story, { title }) => (
+      <>
+        {title.startsWith('Deprecated/') && (
+          <link rel='stylesheet' type='text/css' href={globalsCssUrl} />
+        )}
+        <Story />
+      </>
+    ),
+  ],
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
