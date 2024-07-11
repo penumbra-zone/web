712715cce308a1a316bbfab0cb15ecc6c9ae1559
turbocrime <turbocrime@users.noreply.github.com>
2024-07-10 18:28:15 -0700

    provide type guards from client package

--- packages/client/src/event.ts
+++ packages/client/src/event.ts
@@ -1,9 +1,11 @@
 import { PenumbraInjectionState, PenumbraSymbol } from '.';
 
-export class PenumbraInjectionStateEvent extends CustomEvent<{
+export interface PenumbraInjectionStateEventDetail {
   origin: string;
   state?: PenumbraInjectionState;
-}> {
+}
+
+export class PenumbraInjectionStateEvent extends CustomEvent<PenumbraInjectionStateEventDetail> {
   constructor(injectionProviderOrigin: string, injectionState?: PenumbraInjectionState) {
     super('penumbrastate', {
       detail: {
@@ -13,3 +15,15 @@ export class PenumbraInjectionStateEvent extends CustomEvent<{
     });
   }
 }
+
+export const isPenumbraInjectionStateEvent = (evt: Event): evt is PenumbraInjectionStateEvent =>
+  evt instanceof PenumbraInjectionStateEvent ||
+  ('detail' in evt && isPenumbraInjectionStateEventDetail(evt.detail));
+
+export const isPenumbraInjectionStateEventDetail = (
+  detail: unknown,
+): detail is PenumbraInjectionStateEventDetail =>
+  typeof detail === 'object' &&
+  detail !== null &&
+  'origin' in detail &&
+  typeof detail.origin === 'string';
