f77fee6864d727fd257caa32f48886cace3934ac
Jesse Pinho <jesse@jessepinho.com>
2024-07-11 01:14:44 -0700

    Add docs

--- packages/ui/src/utils/asTransientProps.ts
+++ packages/ui/src/utils/asTransientProps.ts
@@ -8,6 +8,14 @@ type Prefixer<K, T extends string> = {
 
 export type AsTransientProps<T extends Record<string, unknown>> = Prefixer<T, '$'>;
 
+/**
+ * Converts a props object to an object of "transient props" -- i.e., where each
+ * key is prefixed with a `$` to indicate to styled-components that it should
+ * not be passed down to the DOM element.
+ *
+ * @see https://styled-components.com/docs/api#transient-props
+ * @internal
+ */
 export const asTransientProps = <T extends Record<string, unknown>>(
   props: T,
 ): AsTransientProps<T> =>
