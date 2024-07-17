5c9cc44ac9dffb67529fd9c0622b212ff44db7d3
Jesse Pinho <jesse@jessepinho.com>
2024-07-17 14:46:53 -0700

    Add Button/ButtonGroup components

---
+++ packages/ui/src/Button/index.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from '.';
+import { ArrowLeftRight, Check } from 'lucide-react';
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  title: 'Button',
+  tags: ['autodocs', '!dev'],
+  argTypes: {
+    icon: {
+      control: 'select',
+      options: ['None', 'Check', 'ArrowLeftRight'],
+      mapping: { None: undefined, Check, ArrowLeftRight },
+    },
+    onClick: { control: false },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Basic: Story = {
+  args: {
+    size: 'sparse',
+    children: 'Save',
+    actionType: 'default',
+    variant: 'primary',
+    disabled: false,
+    icon: Check,
+    iconOnly: false,
+  },
+};
