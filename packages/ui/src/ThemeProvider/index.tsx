610b6f09e768daa839997bec5fe3f79bc1328fb3
Jesse Pinho <jesse@jessepinho.com>
2024-07-16 22:00:16 -0700

    Add comment

--- packages/ui/src/ThemeProvider/index.tsx
+++ packages/ui/src/ThemeProvider/index.tsx
@@ -3,6 +3,10 @@ import { theme } from './theme';
 import { PropsWithChildren } from 'react';
 import { FontFaces } from './FontFaces';
 
+/**
+ * Place at the root of your app, above all Penumbra UI components, to provide
+ * the theme values that they use.
+ */
 export const ThemeProvider = ({ children }: PropsWithChildren) => (
   <ThemeProviderPrimitive theme={theme}>
     <FontFaces />
