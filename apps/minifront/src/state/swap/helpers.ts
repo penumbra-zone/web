b6c6a3c16f2d1e1c15741bf73b5ad3204dd99ee7
the letter L <134443988+turbocrime@users.noreply.github.com>
2024-07-18 10:21:51 -0700

    use inverse candlestick data in chart (#1475)

--- apps/minifront/src/state/swap/helpers.ts
+++ apps/minifront/src/state/swap/helpers.ts
@@ -50,11 +50,20 @@ export const sendSimulateTradeRequest = ({
   return simulationClient.simulateTrade(req);
 };
 
-export const sendCandlestickDataRequest = async (
+/**
+ * Due to the way price data is recorded, symmetric comparisons do not return
+ * symmetric data. to get the complete picture, a client must combine both
+ * datasets.
+ *  1. query the intended comparison direction (start token -> end token)
+ *  2. query the inverse comparison direction (end token -> start token)
+ *  3. flip the inverse data (reciprocal values, high becomes low)
+ *  4. combine the data (use the highest high, lowest low, sum volumes)
+ */
+export const sendCandlestickDataRequests = async (
   { startMetadata, endMetadata }: Pick<PriceHistorySlice, 'startMetadata' | 'endMetadata'>,
   limit: bigint,
   signal?: AbortSignal,
-): Promise<CandlestickData[] | undefined> => {
+): Promise<CandlestickData[]> => {
   const start = startMetadata?.penumbraAssetId;
   const end = endMetadata?.penumbraAssetId;
 
@@ -65,22 +74,73 @@ export const sendCandlestickDataRequest = async (
     throw new Error('Asset pair equivalent');
   }
 
-  try {
-    const { data } = await dexClient.candlestickData(
-      {
-        pair: { start, end },
-        limit,
-      },
-      { signal },
-    );
-    return data;
-  } catch (err) {
-    if (err instanceof Error && err.name === 'AbortError') {
-      return;
-    } else {
+  const suppressAbort = (err: unknown) => {
+    if (!signal?.aborted) {
       throw err;
     }
-  }
+  };
+
+  const directReq = dexClient
+    .candlestickData({ pair: { start, end }, limit }, { signal })
+    .catch(suppressAbort);
+  const inverseReq = dexClient
+    .candlestickData({ pair: { start: end, end: start }, limit }, { signal })
+    .catch(suppressAbort);
+
+  const directCandles = (await directReq)?.data ?? [];
+  const inverseCandles = (await inverseReq)?.data ?? [];
+
+  // collect candles at each height
+  const collatedByHeight = Map.groupBy(
+    [
+      ...directCandles,
+      ...inverseCandles.map(
+        // flip inverse data to match orientation of direct data
+        inverseCandle => {
+          const correctedCandle = inverseCandle.clone();
+          // comparative values are reciprocal
+          correctedCandle.open = 1 / inverseCandle.open;
+          correctedCandle.close = 1 / inverseCandle.close;
+          // high and low swap places
+          correctedCandle.high = 1 / inverseCandle.low;
+          correctedCandle.low = 1 / inverseCandle.high;
+          return correctedCandle;
+        },
+      ),
+    ],
+    ({ height }) => height,
+  );
+
+  // combine data at each height into a single candle
+  const combinedCandles = Array.from(collatedByHeight.entries()).map(
+    ([height, candlesAtHeight]) => {
+      // TODO: open/close don't diverge much, and when they do it seems to be due
+      // to inadequate number precision. it might be better to just pick one, but
+      // it's not clear which one is 'correct'
+      const combinedCandleAtHeight = candlesAtHeight.reduce((acc, cur) => {
+        // sum volumes
+        acc.directVolume += cur.directVolume;
+        acc.swapVolume += cur.swapVolume;
+
+        // highest high, lowest low
+        acc.high = Math.max(acc.high, cur.high);
+        acc.low = Math.min(acc.low, cur.low);
+
+        // these accumulate to be averaged
+        acc.open += cur.open;
+        acc.close += cur.close;
+        return acc;
+      }, new CandlestickData({ height }));
+
+      // average accumulated open/close
+      combinedCandleAtHeight.open /= candlesAtHeight.length;
+      combinedCandleAtHeight.close /= candlesAtHeight.length;
+
+      return combinedCandleAtHeight;
+    },
+  );
+
+  return combinedCandles;
 };
 
 const byBalanceDescending = (a: BalancesResponse, b: BalancesResponse) => {
