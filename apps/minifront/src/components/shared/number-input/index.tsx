63db6f085cb52b0bac67824ca12b9d78a8485372
Max Korsunov <maxkors2014@gmail.com>
2024-07-22 15:27:07 +0200

    feat(minifront): #1139: add decimals validation

--- apps/minifront/src/components/shared/number-input/index.tsx
+++ apps/minifront/src/components/shared/number-input/index.tsx
@@ -1,8 +1,30 @@
+import type { FC, KeyboardEventHandler } from 'react';
 import { Input, InputProps } from '@repo/ui/components/ui/input';
 import { useWheelPrevent } from './use-wheel-prevent';
 
-export const NumberInput = (props: InputProps) => {
+export interface NumberInputProps extends InputProps {
+  /** If present, prevents users from entering the fractional number part longer than `maxExponent` */
+  maxExponent?: number;
+}
+
+export const NumberInput: FC<NumberInputProps> = ({ maxExponent, ...props }) => {
   const inputRef = useWheelPrevent();
 
-  return <Input ref={inputRef} type='number' {...props} />;
+  const onKeyDown: KeyboardEventHandler<HTMLInputElement> = (event) => {
+    if (maxExponent === 0 && (event.key === '.' || event.key === ',')) {
+      event.preventDefault();
+      return;
+    }
+
+    if (typeof maxExponent !== 'undefined' && typeof props.value === 'string' && !Number.isNaN(Number(event.key))) {
+      const fraction = `${props.value}${event.key}`.split('.')[1]?.length;
+      if (fraction && fraction > maxExponent) {
+        event.preventDefault();
+        return;
+      }
+    }
+    props.onKeyDown?.(event);
+  };
+
+  return <Input ref={inputRef} {...props} type='number' onKeyDown={onKeyDown} />;
 };
