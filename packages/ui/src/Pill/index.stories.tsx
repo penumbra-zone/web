784c8baf064abbe961112232ff2b5e6d134b2542
Jesse Pinho <jesse@jessepinho.com>
2024-07-22 12:51:48 -0700

    Create a Pill component

---
+++ packages/ui/src/Pill/index.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Pill } from '.';
+
+const meta: Meta<typeof Pill> = {
+  component: Pill,
+  tags: ['autodocs', '!dev'],
+};
+export default meta;
+
+type Story = StoryObj<typeof Pill>;
+
+export const Basic: Story = {
+  args: {
+    children: 'Pill',
+    size: 'sparse',
+    variant: 'primary',
+  },
+};
