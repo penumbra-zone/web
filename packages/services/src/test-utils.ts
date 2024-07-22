c1b82f1a2b42ae762a15b3820ce71fa06255902e
Tal Derei <talderei99@gmail.com>
2024-07-22 13:35:20 -0700

    scaffolding for multi-fee unit tests

--- packages/services/src/test-utils.ts
+++ packages/services/src/test-utils.ts
@@ -4,6 +4,53 @@ import {
   SpendKey,
 } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/keys/v1/keys_pb.js';
 import { fullViewingKeyFromBech32m } from '@penumbra-zone/bech32m/penumbrafullviewingkey';
+import {
+  IDB_TABLES,
+  IdbConstants,
+  IdbUpdate,
+  IndexedDbInterface,
+  PenumbraDb,
+} from '@penumbra-zone/types/indexed-db';
+import { AssetId } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/asset/v1/asset_pb.js';
+import {
+  Epoch,
+  Nullifier,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/sct/v1/sct_pb.js';
+import {
+  NotesForVotingResponse,
+  SpendableNoteRecord,
+  SwapRecord,
+  TransactionInfo,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1/view_pb.js';
+import { StateCommitment } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/crypto/tct/v1/tct_pb.js';
+import { TransactionId } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/txhash/v1/txhash_pb.js';
+import { Transaction } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/transaction/v1/transaction_pb.js';
+import {
+  EstimatedPrice,
+  Metadata,
+  Value,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/asset/v1/asset_pb.js';
+import { ScanBlockResult, StateCommitmentTree } from '@penumbra-zone/types/state-commitment-tree';
+import { FmdParameters } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/shielded_pool/v1/shielded_pool_pb.js';
+import { AppParameters } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/app/v1/app_pb.js';
+import { GasPrices } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/fee/v1/fee_pb.js';
+import { PartialMessage } from '@bufbuild/protobuf';
+import {
+  AddressIndex,
+  IdentityKey,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/keys/v1/keys_pb.js';
+import {
+  Position,
+  PositionId,
+  PositionState,
+  TradingPair,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/dex/v1/dex_pb.js';
+import { ValidatorInfo } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/stake/v1/stake_pb.js';
+import {
+  AuctionId,
+  DutchAuctionDescription,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/auction/v1/auction_pb.js';
+import { StoreNames } from 'idb';
 
 export interface IndexedDbMock {
   constants?: Mock;
@@ -35,6 +82,7 @@ export interface IndexedDbMock {
   getAuctionOutstandingReserves?: Mock;
   hasStakingAssetBalance?: Mock;
   stakingTokenAssetId?: Mock;
+  upsertAuction?: Mock;
 }
 
 export interface AuctionMock {
@@ -92,3 +140,238 @@ export const testSpendKey = new SpendKey({
     238, 171, 186, 29, 152, 153, 61, 26, 149, 83, 166,
   ]),
 });
+
+export class IndexedDbMockImpl implements IndexedDbInterface {
+  constructor(public mockDB: IndexedDbMock) {}
+
+  stakingTokenAssetId = new AssetId();
+
+  constants(): IdbConstants {
+    return this.mockDB.constants
+      ? this.mockDB.constants()
+      : { name: 'MockDb', version: 1, tables: IDB_TABLES };
+  }
+
+  async clear(): Promise<void> {
+    return;
+  }
+
+  async getFullSyncHeight(): Promise<bigint | undefined> {
+    return this.mockDB.getFullSyncHeight ? this.mockDB.getFullSyncHeight() : undefined;
+  }
+
+  async getSpendableNoteByNullifier(
+    nullifier: Nullifier,
+  ): Promise<SpendableNoteRecord | undefined> {
+    return this.mockDB.getSpendableNoteByNullifier
+      ? this.mockDB.getSpendableNoteByNullifier(nullifier)
+      : undefined;
+  }
+
+  async getSpendableNoteByCommitment(
+    commitment: StateCommitment,
+  ): Promise<SpendableNoteRecord | undefined> {
+    return this.mockDB.getSpendableNoteByCommitment
+      ? this.mockDB.getSpendableNoteByCommitment(commitment)
+      : undefined;
+  }
+
+  async saveSpendableNote(_note: SpendableNoteRecord): Promise<void> {
+    return;
+  }
+
+  async *iterateSpendableNotes(): AsyncGenerator<SpendableNoteRecord, void, unknown> {
+    return;
+  }
+
+  async saveTransaction(_id: TransactionId, _height: bigint, _tx: Transaction): Promise<void> {
+    return;
+  }
+
+  async getTransaction(txId: TransactionId): Promise<TransactionInfo | undefined> {
+    return this.mockDB.getTransaction ? this.mockDB.getTransaction(txId) : undefined;
+  }
+
+  async *iterateTransactions(): AsyncGenerator<TransactionInfo, void, unknown> {
+    return;
+  }
+
+  async getAssetsMetadata(assetId: AssetId): Promise<Metadata | undefined> {
+    return this.mockDB.getAssetsMetadata ? this.mockDB.getAssetsMetadata(assetId) : undefined;
+  }
+
+  async saveAssetsMetadata(metadata: Metadata): Promise<void> {
+    return this.mockDB.saveAssetsMetadata ? this.mockDB.saveAssetsMetadata(metadata) : undefined;
+  }
+
+  async *iterateAssetsMetadata(): AsyncGenerator<Metadata, void, unknown> {
+    return;
+  }
+
+  async getStateCommitmentTree(): Promise<StateCommitmentTree> {
+    return this.mockDB.getStateCommitmentTree ? this.mockDB.getStateCommitmentTree() : undefined;
+  }
+
+  async saveScanResult(_updates: ScanBlockResult): Promise<void> {
+    return;
+  }
+
+  async getFmdParams(): Promise<FmdParameters | undefined> {
+    return this.mockDB.getFmdParams ? this.mockDB.getFmdParams() : undefined;
+  }
+
+  async saveFmdParams(_params: FmdParameters): Promise<void> {
+    return;
+  }
+
+  async getAppParams(): Promise<AppParameters | undefined> {
+    return this.mockDB.getAppParams ? this.mockDB.getAppParams() : undefined;
+  }
+
+  async saveAppParams(_params: AppParameters): Promise<void> {
+    return;
+  }
+
+  async *iterateSwaps(): AsyncGenerator<SwapRecord, void, unknown> {
+    return;
+  }
+
+  async getSwapByNullifier(nullifier: Nullifier): Promise<SwapRecord | undefined> {
+    return this.mockDB.getSwapByNullifier ? this.mockDB.getSwapByNullifier(nullifier) : undefined;
+  }
+
+  async saveSwap(_note: SwapRecord): Promise<void> {
+    return;
+  }
+
+  async getSwapByCommitment(commitment: StateCommitment): Promise<SwapRecord | undefined> {
+    return this.mockDB.getSwapByCommitment
+      ? this.mockDB.getSwapByCommitment(commitment)
+      : undefined;
+  }
+
+  async getNativeGasPrices(): Promise<GasPrices | undefined> {
+    return this.mockDB.getNativeGasPrices ? this.mockDB.getNativeGasPrices() : undefined;
+  }
+
+  async getAltGasPrices(): Promise<GasPrices[]> {
+    return this.mockDB.getAltGasPrices ? this.mockDB.getAltGasPrices() : [];
+  }
+
+  async saveGasPrices(_value: PartialMessage<GasPrices>): Promise<void> {
+    return;
+  }
+
+  async getNotesForVoting(
+    addressIndex: AddressIndex | undefined,
+    votableAtHeight: bigint,
+  ): Promise<NotesForVotingResponse[]> {
+    return this.mockDB.getNotesForVoting
+      ? this.mockDB.getNotesForVoting(addressIndex, votableAtHeight)
+      : [];
+  }
+
+  async *getOwnedPositionIds(
+    _positionState: PositionState | undefined,
+    _tradingPair: TradingPair | undefined,
+  ): AsyncGenerator<PositionId, void, unknown> {
+    return;
+  }
+
+  async addPosition(_positionId: PositionId, _position: Position): Promise<void> {
+    return;
+  }
+
+  async updatePosition(_positionId: PositionId, _newState: PositionState): Promise<void> {
+    return;
+  }
+
+  async addEpoch(_startHeight: bigint): Promise<void> {
+    return;
+  }
+
+  async getEpochByHeight(height: bigint): Promise<Epoch | undefined> {
+    return this.mockDB.getEpochByHeight ? this.mockDB.getEpochByHeight(height) : undefined;
+  }
+
+  async upsertValidatorInfo(_validatorInfo: ValidatorInfo): Promise<void> {
+    return;
+  }
+
+  async *iterateValidatorInfos(): AsyncGenerator<ValidatorInfo, void, unknown> {
+    return;
+  }
+
+  async getValidatorInfo(identityKey: IdentityKey): Promise<ValidatorInfo | undefined> {
+    return this.mockDB.getValidatorInfo ? this.mockDB.getValidatorInfo(identityKey) : undefined;
+  }
+
+  async updatePrice(
+    _pricedAsset: AssetId,
+    _numeraire: AssetId,
+    _numerairePerUnit: number,
+    _height: bigint,
+  ): Promise<void> {
+    return;
+  }
+
+  async getPricesForAsset(
+    assetMetadata: Metadata,
+    latestBlockHeight: bigint,
+    epochDuration: bigint,
+  ): Promise<EstimatedPrice[]> {
+    return this.mockDB.getPricesForAsset
+      ? this.mockDB.getPricesForAsset(assetMetadata, latestBlockHeight, epochDuration)
+      : [];
+  }
+
+  async clearSwapBasedPrices(): Promise<void> {
+    return;
+  }
+
+  async upsertAuction<T extends DutchAuctionDescription>(
+    _auctionId: AuctionId,
+    _value: { auction?: T; noteCommitment?: StateCommitment; seqNum?: bigint },
+  ): Promise<void> {
+    return;
+  }
+
+  async getAuction(auctionId: AuctionId): Promise<{
+    auction?: DutchAuctionDescription;
+    noteCommitment?: StateCommitment;
+    seqNum?: bigint;
+  }> {
+    return this.mockDB.getAuction ? this.mockDB.getAuction(auctionId) : {};
+  }
+
+  async addAuctionOutstandingReserves(
+    _auctionId: AuctionId,
+    _value: { input: Value; output: Value },
+  ): Promise<void> {
+    return;
+  }
+
+  async deleteAuctionOutstandingReserves(_auctionId: AuctionId): Promise<void> {
+    return;
+  }
+
+  async getAuctionOutstandingReserves(
+    auctionId: AuctionId,
+  ): Promise<{ input: Value; output: Value } | undefined> {
+    return this.mockDB.getAuctionOutstandingReserves
+      ? this.mockDB.getAuctionOutstandingReserves(auctionId)
+      : undefined;
+  }
+
+  async hasStakingAssetBalance(addressIndex: AddressIndex | undefined): Promise<boolean> {
+    return this.mockDB.hasStakingAssetBalance
+      ? this.mockDB.hasStakingAssetBalance(addressIndex)
+      : false;
+  }
+
+  async *subscribe<DBTypes extends PenumbraDb, StoreName extends StoreNames<DBTypes>>(
+    _table: StoreName,
+  ): AsyncGenerator<IdbUpdate<DBTypes, StoreName>, void, unknown> {
+    return;
+  }
+}
--- packages/services/src/view-service/fees.test.ts
+++ packages/services/src/view-service/fees.test.ts
@@ -1,19 +1,67 @@
-import { describe, expect, it } from 'vitest';
+import { Mock, beforeEach, describe, expect, it, vi } from 'vitest';
 import { AssetId } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/asset/v1/asset_pb.js';
 import {
+  SwapRecord,
   TransactionPlannerRequest,
   TransactionPlannerRequest_ActionDutchAuctionEnd,
   TransactionPlannerRequest_ActionDutchAuctionSchedule,
   TransactionPlannerRequest_ActionDutchAuctionWithdraw,
   TransactionPlannerRequest_Output,
   TransactionPlannerRequest_Swap,
+  TransactionPlannerRequest_SwapClaim,
 } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1/view_pb.js';
-import { AuctionId } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/auction/v1/auction_pb.js';
+import {
+  ActionDutchAuctionEnd,
+  AuctionId,
+  DutchAuction,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/auction/v1/auction_pb.js';
+import { extractAltFee } from './fees.js';
+import { StateCommitment } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/crypto/tct/v1/tct_pb.js';
+import { IndexedDbMock, IndexedDbMockImpl } from '../test-utils.js';
+import { uint8ArrayToBase64 } from '@penumbra-zone/types/base64';
+import { processActionDutchAuctionEnd } from '@penumbra-zone/query/helpers/process-action-dutch-auction-end';
+import { IndexedDbInterface } from '@penumbra-zone/types/indexed-db';
 
-// TODO: Need to properly write tests the coverage
 describe('extractAltFee', () => {
-  it('extracts the fee from outputs', () => {
-    const umAssetId = new AssetId({ altBaseDenom: 'UM' });
+  // Create class 'indexedDbMockImpl' that implements 'IndexedDbInterface', and delegate method
+  // calls to underlying 'IndexedDbMock' indexDB instance. This is requireed because extractAltFee
+  // expects 'IndexedDbInterface' as a type signature, and inside the method perform indexDB calls.
+  let indexedDbMockImpl: IndexedDbMockImpl;
+  let mockIndexedDb: IndexedDbMock;
+
+  // IndexDB methods that we want to mock
+  const getSwapByCommitmentMock: Mock = vi.fn();
+  let auctionQuerier: { auctionStateById: Mock };
+  let upsertAuctionMock: Mock = vi.fn();
+  let saveAssetsMetadataMock: Mock = vi.fn();
+
+  const auctionId = new AuctionId({ inner: new Uint8Array([0, 1, 2, 3]) });
+  const action = new ActionDutchAuctionEnd({ auctionId });
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+
+    auctionQuerier = {
+      auctionStateById: vi.fn(),
+    };
+
+    mockIndexedDb = {
+      getSwapByCommitment: getSwapByCommitmentMock,
+      upsertAuction: upsertAuctionMock,
+      saveAssetsMetadata: saveAssetsMetadataMock,
+    };
+
+    // Initialize an instance of 'IndexedDbMockImpl' with 'mockIndexedDb' as the constructor
+    indexedDbMockImpl = new IndexedDbMockImpl(mockIndexedDb);
+  });
+
+  it('extracts the staking asset fee from outputs', async () => {
+    const umAssetId = new AssetId({
+      inner: new Uint8Array([
+        41, 234, 156, 47, 51, 113, 246, 164, 135, 231, 233, 92, 36, 112, 65, 244, 163, 86, 249, 131,
+        235, 6, 78, 93, 43, 59, 207, 50, 44, 169, 106, 16,
+      ]),
+    });
     const request = new TransactionPlannerRequest({
       outputs: [
         new TransactionPlannerRequest_Output({
@@ -21,12 +69,35 @@ describe('extractAltFee', () => {
         }),
       ],
     });
-    const outputAsset = request.outputs.map(o => o.value?.assetId).find(Boolean);
-    expect(outputAsset!.equals(umAssetId)).toBeTruthy();
+    const result = await extractAltFee(request, indexedDbMockImpl);
+    expect(result.equals(umAssetId)).toBeTruthy();
   });
 
-  it('skips over outputs that do not have assetIds', () => {
-    const umAssetId = new AssetId({ altBaseDenom: 'UM' });
+  it('extracts the alternative asset fee from outputs', async () => {
+    const umAssetId = new AssetId({
+      inner: new Uint8Array([
+        29, 109, 132, 171, 117, 25, 85, 32, 109, 182, 133, 48, 82, 47, 204, 82, 209, 59, 174, 189,
+        148, 83, 191, 212, 31, 157, 52, 111, 42, 123, 56, 7,
+      ]),
+    });
+    const request = new TransactionPlannerRequest({
+      outputs: [
+        new TransactionPlannerRequest_Output({
+          value: { assetId: umAssetId },
+        }),
+      ],
+    });
+    const result = await extractAltFee(request, indexedDbMockImpl);
+    expect(result.equals(umAssetId)).toBeTruthy();
+  });
+
+  it('skips over outputs that do not have assetIds', async () => {
+    const umAssetId = new AssetId({
+      inner: new Uint8Array([
+        41, 234, 156, 47, 51, 113, 246, 164, 135, 231, 233, 92, 36, 112, 65, 244, 163, 86, 249, 131,
+        235, 6, 78, 93, 43, 59, 207, 50, 44, 169, 106, 16,
+      ]),
+    });
     const request = new TransactionPlannerRequest({
       outputs: [
         new TransactionPlannerRequest_Output({}),
@@ -35,11 +106,11 @@ describe('extractAltFee', () => {
         }),
       ],
     });
-    const outputAsset = request.outputs.map(o => o.value?.assetId).find(Boolean);
-    expect(outputAsset!.equals(umAssetId)).toBeTruthy();
+    const result = await extractAltFee(request, indexedDbMockImpl);
+    expect(result.equals(umAssetId)).toBeTruthy();
   });
 
-  it('prioritizes outputs over all else', () => {
+  it('prioritizes outputs over all else', async () => {
     const outputAssetId = new AssetId({ altBaseDenom: 'output' });
     const swapAssetId = new AssetId({ altBaseDenom: 'swap' });
     const auctionScheduleAssetId = new AssetId({ altBaseDenom: 'auction-schedule' });
@@ -74,12 +145,19 @@ describe('extractAltFee', () => {
       ],
     });
 
-    const outputAsset = request.outputs.map(o => o.value?.assetId).find(Boolean);
-    expect(outputAsset!.equals(outputAssetId)).toBeTruthy();
+    const result = await extractAltFee(request, indexedDbMockImpl);
+    expect(result.equals(outputAssetId)).toBeTruthy();
   });
 
-  it('extracts the fee from swaps', () => {
-    const swapAssetId = new AssetId({ altBaseDenom: 'swap' });
+  it('extracts the staking asset fee from swaps', async () => {
+    indexedDbMockImpl.mockDB.getSwapByCommitment?.mockResolvedValue(mockSwapNativeStakingToken);
+
+    const swapAssetId = new AssetId({
+      inner: new Uint8Array([
+        41, 234, 156, 47, 51, 113, 246, 164, 135, 231, 233, 92, 36, 112, 65, 244, 163, 86, 249, 131,
+        235, 6, 78, 93, 43, 59, 207, 50, 44, 169, 106, 16,
+      ]),
+    });
     const request = new TransactionPlannerRequest({
       swaps: [
         new TransactionPlannerRequest_Swap({
@@ -88,7 +166,205 @@ describe('extractAltFee', () => {
       ],
     });
 
-    const swapAsset = request.swaps.map(assetIn => assetIn.value?.assetId).find(Boolean);
-    expect(swapAsset!.equals(swapAssetId)).toBeTruthy();
+    const result = await extractAltFee(request, indexedDbMockImpl);
+    expect(result.equals(swapAssetId)).toBeTruthy();
   });
+
+  it('extracts the alternative asset fee from swaps', async () => {
+    indexedDbMockImpl.mockDB.getSwapByCommitment?.mockResolvedValue(mockSwapAlternativeToken);
+
+    const swapAssetId = new AssetId({
+      inner: new Uint8Array([
+        29, 109, 132, 171, 117, 25, 85, 32, 109, 182, 133, 48, 82, 47, 204, 82, 209, 59, 174, 189,
+        148, 83, 191, 212, 31, 157, 52, 111, 42, 123, 56, 7,
+      ]),
+    });
+    const request = new TransactionPlannerRequest({
+      swaps: [
+        new TransactionPlannerRequest_Swap({
+          value: { assetId: swapAssetId },
+        }),
+      ],
+    });
+
+    const result = await extractAltFee(request, indexedDbMockImpl);
+    expect(result.equals(swapAssetId)).toBeTruthy();
+  });
+
+  it('extracts the staking asset fee from swap claims', async () => {
+    indexedDbMockImpl.mockDB.getSwapByCommitment?.mockResolvedValue(mockSwapNativeStakingToken);
+
+    const swapAssetId = new AssetId({
+      inner: new Uint8Array([
+        41, 234, 156, 47, 51, 113, 246, 164, 135, 231, 233, 92, 36, 112, 65, 244, 163, 86, 249, 131,
+        235, 6, 78, 93, 43, 59, 207, 50, 44, 169, 106, 16,
+      ]),
+    });
+
+    const request = new TransactionPlannerRequest({
+      swapClaims: [
+        new TransactionPlannerRequest_SwapClaim({
+          swapCommitment: mockSwapCommitmentNativeStakingToken,
+        }),
+      ],
+    });
+
+    const result = await extractAltFee(request, indexedDbMockImpl);
+    expect(result.equals(swapAssetId)).toBeTruthy();
+  });
+
+  it('extracts the alternative asset fee from swap claims', async () => {
+    indexedDbMockImpl.mockDB.getSwapByCommitment?.mockResolvedValue(mockSwapAlternativeToken);
+
+    const swapAssetId = new AssetId({
+      inner: new Uint8Array([
+        29, 109, 132, 171, 117, 25, 85, 32, 109, 182, 133, 48, 82, 47, 204, 82, 209, 59, 174, 189,
+        148, 83, 191, 212, 31, 157, 52, 111, 42, 123, 56, 7,
+      ]),
+    });
+
+    const request = new TransactionPlannerRequest({
+      swapClaims: [
+        new TransactionPlannerRequest_SwapClaim({
+          swapCommitment: mockSwapCommitmentAlternativeToken,
+        }),
+      ],
+    });
+
+    const result = await extractAltFee(request, indexedDbMockImpl);
+    expect(result.equals(swapAssetId)).toBeTruthy();
+  });
+
+  it('extracts the alternative asset fee from dutchAuctionScheduleActions', async () => {
+    const auctionScheduleAssetId = new AssetId({
+      inner: new Uint8Array([
+        29, 109, 132, 171, 117, 25, 85, 32, 109, 182, 133, 48, 82, 47, 204, 82, 209, 59, 174, 189,
+        148, 83, 191, 212, 31, 157, 52, 111, 42, 123, 56, 7,
+      ]),
+    });
+
+    const request = new TransactionPlannerRequest({
+      dutchAuctionScheduleActions: [
+        new TransactionPlannerRequest_ActionDutchAuctionSchedule({
+          description: {
+            input: {
+              amount: { hi: 0n, lo: 0n },
+              assetId: auctionScheduleAssetId,
+            },
+          },
+        }),
+      ],
+    });
+
+    const result = await extractAltFee(request, indexedDbMockImpl);
+    expect(result.equals(auctionScheduleAssetId)).toBeTruthy();
+  });
+
+  it('extracts the alternative asset fee from dutchAuctionEndActions', async () => {});
+});
+
+const mockSwapCommitmentNativeStakingToken = StateCommitment.fromJson({
+  inner: 'A6VBVkrk+s18q+Sjhl8uEGfS3i0dwF1FrkNm8Db6VAA=',
+});
+
+const mockSwapCommitmentAlternativeToken = StateCommitment.fromJson({
+  inner: 'B6VBVkrk+s18q+Sjhl8uEGfS3i0dwF1FrkNm8Db6VAA=',
+});
+
+const mockSwapNativeStakingToken = SwapRecord.fromJson({
+  swapCommitment: { inner: 'A6VBVkrk+s18q+Sjhl8uEGfS3i0dwF1FrkNm8Db6VAA=' },
+  swap: {
+    tradingPair: {
+      asset1: { inner: 'KeqcLzNx9qSH5+lcJHBB9KNW+YPrBk5dKzvPMiypahA=' },
+      asset2: { inner: 'HW2Eq3UZVSBttoUwUi/MUtE7rr2UU7/UH500byp7OAc=' },
+    },
+    delta1I: {},
+    delta2I: { lo: '1000000' },
+    claimFee: {
+      amount: { hi: '0', lo: '0' },
+      assetId: {
+        inner: uint8ArrayToBase64(
+          new Uint8Array([
+            41, 234, 156, 47, 51, 113, 246, 164, 135, 231, 233, 92, 36, 112, 65, 244, 163, 86, 249,
+            131, 235, 6, 78, 93, 43, 59, 207, 50, 44, 169, 106, 16,
+          ]),
+        ),
+      },
+    },
+    claimAddress: {
+      inner:
+        '2VQ9nQKqga8RylgOq+wAY3/Hmxg96mGnI+Te/BRnXWpr5bSxpLShbpOmzO4pPULf+tGjaBum6InyEpipJ+8wk+HufrvSBa43H9o2ir5WPbk=',
+    },
+    rseed: 'RPuhZ9q2F3XHbTcDPRTHnJjJaMxv8hes4TzJuMbsA/k=',
+  },
+  position: '2383742304257',
+  nullifier: { inner: 'dE7LbhBDgDXHiRvreFyCllcKOOQeuIVsbn2aw8uKhww=' },
+  outputData: {
+    delta1: {},
+    delta2: { lo: '1000000' },
+    lambda1: { lo: '2665239' },
+    lambda2: {},
+    unfilled1: {},
+    unfilled2: {},
+    height: '356591',
+    tradingPair: {
+      asset1: { inner: 'KeqcLzNx9qSH5+lcJHBB9KNW+YPrBk5dKzvPMiypahA=' },
+      asset2: { inner: 'HW2Eq3UZVSBttoUwUi/MUtE7rr2UU7/UH500byp7OAc=' },
+    },
+    epochStartingHeight: '356050',
+  },
+  source: {
+    transaction: {
+      id: '9e1OaxysQAzHUUKsroXMNRCzlPxd6hBWLrqURgNBrmE=',
+    },
+  },
+});
+
+const mockSwapAlternativeToken = SwapRecord.fromJson({
+  swapCommitment: { inner: 'B6VBVkrk+s18q+Sjhl8uEGfS3i0dwF1FrkNm8Db6VAA=' },
+  swap: {
+    tradingPair: {
+      asset1: { inner: 'HW2Eq3UZVSBttoUwUi/MUtE7rr2UU7/UH500byp7OAc=' },
+      asset2: { inner: 'KeqcLzNx9qSH5+lcJHBB9KNW+YPrBk5dKzvPMiypahA=' },
+    },
+    delta1I: {},
+    delta2I: { lo: '1000000' },
+    claimFee: {
+      amount: { hi: '0', lo: '0' },
+      assetId: {
+        inner: uint8ArrayToBase64(
+          new Uint8Array([
+            29, 109, 132, 171, 117, 25, 85, 32, 109, 182, 133, 48, 82, 47, 204, 82, 209, 59, 174,
+            189, 148, 83, 191, 212, 31, 157, 52, 111, 42, 123, 56, 7,
+          ]),
+        ),
+      },
+    },
+    claimAddress: {
+      inner:
+        '2VQ9nQKqga8RylgOq+wAY3/Hmxg96mGnI+Te/BRnXWpr5bSxpLShbpOmzO4pPULf+tGjaBum6InyEpipJ+8wk+HufrvSBa43H9o2ir5WPbk=',
+    },
+    rseed: 'RPuhZ9q2F3XHbTcDPRTHnJjJaMxv8hes4TzJuMbsA/k=',
+  },
+  position: '2383742304258',
+  nullifier: { inner: 'eE7LbhBDgDXHiRvreFyCllcKOOQeuIVsbn2aw8uKhww=' },
+  outputData: {
+    delta1: {},
+    delta2: { lo: '1000000' },
+    lambda1: { lo: '2665239' },
+    lambda2: {},
+    unfilled1: {},
+    unfilled2: {},
+    height: '356591',
+    tradingPair: {
+      asset1: { inner: 'HW2Eq3UZVSBttoUwUi/MUtE7rr2UU7/UH500byp7OAc=' },
+      asset2: { inner: 'KeqcLzNx9qSH5+lcJHBB9KNW+YPrBk5dKzvPMiypahA=' },
+    },
+    epochStartingHeight: '356051',
+  },
+  source: {
+    transaction: {
+      id: '8e1OaxysQAzHUUKsroXMNRCzlPxd6hBWLrqURgNBrmE=',
+    },
+  },
 });
