b09c0831af895a2b20c37ba519ba5b44e4aab45e
Jesse Pinho <jesse@jessepinho.com>
2024-07-22 13:29:01 -0700

    wiop

--- apps/minifront/src/state/swap/price-history.ts
+++ apps/minifront/src/state/swap/price-history.ts
@@ -1,16 +1,41 @@
 import { Metadata } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/asset/v1/asset_pb.js';
 import { CandlestickData } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/dex/v1/dex_pb.js';
 import { getMetadataFromBalancesResponseOptional } from '@penumbra-zone/getters/balances-response';
-import { AllSlices, SliceCreator } from '..';
+import { AllSlices, SliceCreator, useStore } from '..';
 import { sendCandlestickDataRequests } from './helpers';
 import { PRICE_RELEVANCE_THRESHOLDS } from '@penumbra-zone/types/assets';
+import { createZQuery, ZQueryState } from '@penumbra-zone/zquery';
 
 interface Actions {
   load: (ac?: AbortController) => AbortController['abort'];
 }
 
+export const { candles, useCandles, useRevalidateCandles } = createZQuery({
+  name: 'candles',
+  fetch: () => {
+    const { assetIn, assetOut } = useStore.getState().swap;
+    const startMetadata = getMetadataFromBalancesResponseOptional(assetIn);
+    const endMetadata = assetOut;
+    return sendCandlestickDataRequests(
+      { startMetadata, endMetadata },
+      // there's no UI to set limit yet, and any given range won't always happen
+      // to include price records.
+      PRICE_RELEVANCE_THRESHOLDS.default * 2n,
+      useStore.getState().swap.priceHistory.candles._zQueryInternal.abortController?.signal,
+    );
+  },
+  getUseStore: () => useStore,
+  get: state => state.swap.priceHistory.candles,
+  set: setter => {
+    const newState = setter(useStore.getState().swap.priceHistory.candles);
+    useStore.setState(state => {
+      state.swap.priceHistory.candles = newState;
+    });
+  },
+});
+
 interface State {
-  candles: CandlestickData[];
+  candles: ZQueryState<CandlestickData[]>;
   endMetadata?: Metadata;
   startMetadata?: Metadata;
 }
@@ -18,7 +43,7 @@ interface State {
 export type PriceHistorySlice = Actions & State;
 
 const INITIAL_STATE: State = {
-  candles: [],
+  candles,
 };
 
 export const createPriceHistorySlice = (): SliceCreator<PriceHistorySlice> => (set, get) => ({
@@ -37,7 +62,7 @@ export const createPriceHistorySlice = (): SliceCreator<PriceHistorySlice> => (s
       set(({ swap }) => {
         swap.priceHistory.startMetadata = startMetadata;
         swap.priceHistory.endMetadata = endMetadata;
-        swap.priceHistory.candles = candles;
+        // swap.priceHistory.candles = candles;
       });
     });
 
