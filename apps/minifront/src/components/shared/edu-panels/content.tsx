a88b1eb4c6c60333d125ad58da5af390ecc8e64f
Tal Derei <talderei99@gmail.com>
2024-07-25 07:08:53 -0500

    description

--- apps/minifront/src/components/shared/edu-panels/content.tsx
+++ apps/minifront/src/components/shared/edu-panels/content.tsx
@@ -25,7 +25,7 @@ export const eduPanelContent: Record<EduPanel, string> = {
   [EduPanel.IBC_WITHDRAW]:
     'IBC to a connected chain. Note that if the chain is a transparent chain, the transaction will be visible to others.',
   [EduPanel.SWAP]:
-    'Shielded batch swaps between any kind of cryptoasset, with intelligent batch pricing. While the assets and amounts are visible to others, user identities and specific transaction details remain hidden, ensuring long-term privacy.',
+    'Shielded batch swaps between any kind of cryptoasset, using an intelligent batch pricing mechanism. While the assets and amounts are publicly revealed, user identities and specific transaction details remain hidden, ensuring long-term privacy.',
   [EduPanel.SWAP_AUCTION]:
     "Offer a specific quantity of cryptocurrency at decreasing prices until all the tokens are sold. Buyers can place bids at the price they're willing to pay, with the auction concluding when all tokens are sold or when the auction time expires. This mechanism allows for price discovery based on market demand, with participants potentially acquiring tokens at prices lower than initially offered.",
   [EduPanel.STAKING]:
