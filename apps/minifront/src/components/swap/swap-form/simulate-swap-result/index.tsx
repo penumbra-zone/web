2051031979dbee77f428d721266b3889e065a4b5
turbocrime <turbocrime@users.noreply.github.com>
2024-07-26 13:36:47 -0700

    prevent swap NaN

--- apps/minifront/src/components/swap/swap-form/simulate-swap-result/index.tsx
+++ apps/minifront/src/components/swap/swap-form/simulate-swap-result/index.tsx
@@ -18,40 +18,50 @@ import { useStoreShallow } from '../../../../utils/use-store-shallow';
 const HIDE = { clipPath: 'polygon(0 0, 100% 0, 100% 0, 0 0)' };
 const SHOW = { clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)' };
 
-const simulateSwapResultSelector = (state: AllSlices) => ({
-  input: new ValueView({
+const simulateSwapResultSelector = (state: AllSlices) => {
+  if (!state.swap.amount || !state.swap.assetIn?.balanceView) {
+    return undefined;
+  }
+  return new ValueView({
     valueView: {
       case: 'knownAssetId',
       value: {
         amount: toBaseUnit(
           new BigNumber(state.swap.amount),
-          getDisplayDenomExponentFromValueView.optional()(state.swap.assetIn?.balanceView),
+          getDisplayDenomExponentFromValueView(state.swap.assetIn.balanceView),
         ),
-        metadata: getMetadata.optional()(state.swap.assetIn?.balanceView),
+        metadata: getMetadata(state.swap.assetIn.balanceView),
       },
     },
-  }),
-});
+  });
+};
 
 export const SimulateSwapResult = ({ result }: { result: TSimulateSwapResult }) => {
   const { unfilled, output, priceImpact, traces, metadataByAssetId } = result;
-  const { input } = useStoreShallow(simulateSwapResultSelector);
+  const input = useStoreShallow(simulateSwapResultSelector);
 
   const hasUnfilled = joinLoHiAmount(getAmount(unfilled)) > 0n;
 
   return (
-    <motion.div layout initial={HIDE} animate={SHOW} exit={HIDE} className='flex flex-col gap-4'>
-      <div className='flex flex-wrap items-center gap-1'>
-        Filling <ValueViewComponent view={output} size='sm' /> causes a price impact of{' '}
-        <PriceImpact amount={priceImpact} />.
-        {hasUnfilled && (
-          <>
-            <ValueViewComponent view={unfilled} size='sm' /> will remain unfilled.
-          </>
-        )}
-      </div>
+    input && (
+      <motion.div layout initial={HIDE} animate={SHOW} exit={HIDE} className='flex flex-col gap-4'>
+        <div className='flex flex-wrap items-center gap-1'>
+          Filling <ValueViewComponent view={output} size='sm' /> causes a price impact of{' '}
+          <PriceImpact amount={priceImpact} />.
+          {hasUnfilled && (
+            <>
+              <ValueViewComponent view={unfilled} size='sm' /> will remain unfilled.
+            </>
+          )}
+        </div>
 
-      <Traces traces={traces} metadataByAssetId={metadataByAssetId} input={input} output={output} />
-    </motion.div>
+        <Traces
+          traces={traces}
+          metadataByAssetId={metadataByAssetId}
+          input={input}
+          output={output}
+        />
+      </motion.div>
+    )
   );
 };
