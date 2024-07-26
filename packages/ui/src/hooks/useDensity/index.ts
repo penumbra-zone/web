6d32abbd90fab941b2a3d2025e5568e936a274f2
Jesse Pinho <jesse@jessepinho.com>
2024-07-26 09:18:54 -0700

    Simplify useDensity hook

--- packages/ui/src/hooks/useDensity/index.ts
+++ packages/ui/src/hooks/useDensity/index.ts
@@ -3,33 +3,15 @@ import { Density } from '../../types/Density';
 import { DensityContext } from '../../DensityContext';
 
 /**
- * Returns the appropriate `Density` value to use for the component using this
- * hook. If you pass it a defined `Density` value, it will return that.
- * Otherwise, it will grab the `Density` value from `DensityContext`, which is
- * `sparse` by default if no `DensityContext.Provider` exists higher in the
- * component tree.
+ * Returns the `Density` value to use for the component using this hook, by
+ * grabbing the value from `DensityContext`.
  *
  * @example
  * ```tsx
- * const MyComponent = ({ density }: { density?: Density }) => {
- *   density = useDensity(density); // Note the lack of `const` — we're reassigning `density`.
- *
+ * const MyComponent = () => {
+ *   const density = useDensity();
  *   return <SomeStyledComponent $density={density} />
  * }
  * ```
  */
-export const useDensity = (
-  /**
-   * The `density` prop passed into the component using this hook. If passed, it
-   * will override whatever value `<DensityContext />` provides.
-   */
-  density?: Density,
-): Density => {
-  const densityFromContext = useContext(DensityContext);
-
-  if (density) {
-    return density;
-  }
-
-  return densityFromContext;
-};
+export const useDensity = (): Density => useContext(DensityContext);
