358837e624c64d72d94f94938fccdca49f84d30b
Jesse Pinho <jesse@jessepinho.com>
2024-07-26 11:53:27 -0700

    Tweak docs

--- packages/ui/src/types/Density.ts
+++ packages/ui/src/types/Density.ts
@@ -1,8 +1,8 @@
 /**
  * The density for a given layout. Generally, `sparse` is the correct size
- * choice (and is thus the default for any components that accept a `density`
- * prop). But you can use `compact` for layouts containing a lot of data that
- * should be presented in a denser layout.
+ * choice (and is thus the default for any components that use the
+ * `useDensity()` hook). But you can use `compact` for layouts containing a lot
+ * of data that should be presented in a denser layout.
  *
  * See `<DensityContext />`
  */
