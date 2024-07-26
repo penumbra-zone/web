34f5bbde30822092854cb524cb77f9cefec883d5
Jesse Pinho <jesse@jessepinho.com>
2024-07-26 11:29:43 -0700

    Fix docs for DensityContext

--- packages/ui/src/DensityContext/index.ts
+++ packages/ui/src/DensityContext/index.ts
@@ -4,9 +4,6 @@ import { Density } from '../types/Density';
 /**
  * Use this context to set the density of all descendent components in the
  * component tree. This is useful for, e.g., wrapping an entire `<Table />` that
- * will have multiple descendent components that each take a `density` prop.
- *
- * Note that components whose `density` props are manually set will override
- * whatever the context value is.
+ * will have multiple descendent components that each have `density` variants.
  */
 export const DensityContext = createContext<Density>('sparse');
