bc90c9077e7fd3a7c6b0a59ca3358daefea34d16
Max Korsunov <maxkors2014@gmail.com>
2024-07-29 16:36:49 +0200

    feat(protobuf): #1483: bundle all service types together

---
+++ packages/protobuf/src/types/core.ts
@@ -0,0 +1,80 @@
+/**
+ * This file re-exports all types from
+ * `@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/*_pb.ts`
+ * except the duplicated conflicting types (e.g. `AppParametersRequest`).
+ */
+
+export type {
+  AppParameters,
+  TransactionsByHeightRequest,
+  TransactionsByHeightResponse,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/app/v1/app_pb.js';
+
+export type {
+  AssetId,
+  Value,
+  Metadata,
+  AssetImage,
+  AssetImage_Theme,
+  ValueView,
+  Denom,
+  ValueView_KnownAssetId,
+  ValueView_UnknownAssetId,
+  BalanceCommitment,
+  DenomUnit,
+  EquivalentValue,
+  EstimatedPrice,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/asset/v1/asset_pb.js';
+
+export type {
+  Address,
+  AddressView,
+  AddressIndex,
+  AddressView_Decoded,
+  AddressView_Opaque,
+  ConsensusKey,
+  Diversifier,
+  FullViewingKey,
+  GovernanceKey,
+  IdentityKey,
+  PayloadKey,
+  SpendKey,
+  WalletId,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/keys/v1/keys_pb.js';
+
+export type { Amount } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/num/v1/num_pb.js';
+
+export type {
+  Transaction,
+  Action,
+  ActionPlan,
+  ActionView,
+  CluePlan,
+  AuthorizationData,
+  DetectionData,
+  DetectionDataPlan,
+  MemoCiphertext,
+  MemoPlaintext,
+  MemoPlan,
+  MemoView,
+  MemoPlaintextView,
+  MemoView_Opaque,
+  MemoView_Visible,
+  NullifierWithNote,
+  TransactionBody,
+  TransactionBodyView,
+  TransactionParameters,
+  TransactionPerspective,
+  PayloadKeyWithCommitment,
+  TransactionPerspective_ExtendedMetadataById,
+  TransactionPerspective_NullificationTransactionIdByCommitment,
+  TransactionPerspective_CreationTransactionIdByNullifier,
+  TransactionPlan,
+  TransactionView,
+  WitnessData,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/transaction/v1/transaction_pb.js';
+
+export type {
+  EffectHash,
+  TransactionId,
+} from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/txhash/v1/txhash_pb.js';
---
+++ packages/protobuf/src/types/index.ts
@@ -0,0 +1,17 @@
+export * from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1/view_pb.js';
+export * from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/cnidarium/v1/cnidarium_pb.js';
+
+export * from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/crypto/tct/v1/tct_pb.js';
+export * from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/crypto/decaf377_frost/v1/decaf377_frost_pb.js';
+export * from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/crypto/decaf377_fmd/v1/decaf377_fmd_pb.js';
+export * from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/crypto/decaf377_rdsa/v1/decaf377_rdsa_pb.js';
+
+export * from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/custody/threshold/v1/threshold_pb.js';
+export * from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/custody/v1/custody_pb.js';
+
+export * from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/tools/summoning/v1/summoning_pb.js';
+export * from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/util/tendermint_proxy/v1/tendermint_proxy_pb.js';
+
+// Core and Core/component exports
+export * from './component.js';
+export * from './core.js';
