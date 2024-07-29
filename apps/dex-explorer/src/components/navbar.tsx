84540952b7e51c0c25a0aa36895c4bf12973eef1
Phil <pbal@fordham.edu>
2024-07-29 16:20:21 -0700

    Mobile header

--- apps/dex-explorer/src/components/navbar.tsx
+++ apps/dex-explorer/src/components/navbar.tsx
@@ -2,17 +2,17 @@
 
 import Image from "next/image";
 import React from "react";
-import { HamburgerIcon, CloseIcon, SearchIcon } from '@chakra-ui/icons'
-import { Box, Flex, IconButton, Menu, MenuButton, MenuList, useDisclosure, Stack, Text, HStack } from '@chakra-ui/react'
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
+import { Box, Flex, IconButton, useDisclosure, Stack, Text, HStack, Center } from '@chakra-ui/react'
 import { LPSearchBar } from "./lpSearchBar";
-  
+
 interface NavLinkProps {
     children: React.ReactNode
     link: string
 }
-  
-const Links = [{'name': 'Trades', 'link': '/trades'}, {'name': 'Pairs', 'link': '/pair'}]
-  
+
+const Links = [{ 'name': 'Trades', 'link': '/trades' }, { 'name': 'Pairs', 'link': '/pair' }]
+
 const NavLink = (props: NavLinkProps) => {
     return (
         <Box as="a" px={5} py={1} href={props.link} fontFamily={"monospace"} fontWeight={"bold"}>
@@ -35,40 +35,31 @@ export const Navbar = () => {
                     display={{ md: 'none' }}
                     onClick={isOpen ? onClose : onOpen}
                 />
-                <Box>
+                <Center flex={1}>
                     <HStack as="a" href="/">
-                        <Image src="/favicon.ico" alt="Penumbra Logo" width={35} height={35}/>
+                        <Image src="/favicon.ico" alt="Penumbra Logo" width={35} height={35} />
                         <Text fontWeight={"bold"}>Dex Explorer</Text>
                     </HStack>
-                </Box>
-                <HStack as={'nav'} spacing={4} display={{base: 'none', md: 'flex'}}>
+                </Center>
+                <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
                     {Links.map((x) => (<NavLink key={x.name} link={x.link} >{x.name}</NavLink>))}
-                    <Flex paddingLeft={10} display={{base: 'none', md: 'flex'}}>
-                        <LPSearchBar/>
+                    <Flex paddingLeft={10}>
+                        <LPSearchBar />
                     </Flex>
                 </HStack>
-                <Flex alignItems={'center'}  display={{md: 'none'}}>
-                    <Menu>
-                        <MenuButton
-                            rounded={'full'}
-                            cursor={'pointer'}
-                            minW={0}>
-                            {<SearchIcon/>}
-                        </MenuButton>
-                        <MenuList>
-                            <LPSearchBar/>
-                        </MenuList>
-                    </Menu>
-                </Flex>
+                <Box display={{ md: 'none' }} width={10} /> {/* Placeholder for symmetry */}
             </Flex>
 
-            {isOpen ? (
-                <Box pb={4} display={{md: 'none'}}>
+            {isOpen && (
+                <Box pb={4} display={{ md: 'none' }}>
                     <Stack as={'nav'} spacing={4}>
                         {Links.map((x) => (<NavLink key={x.name} link={x.link}>{x.name}</NavLink>))}
                     </Stack>
+                    <Box pt={4}>
+                        <LPSearchBar />
+                    </Box>
                 </Box>
-            ) : null}
+            )}
         </Box>
     )
 }
