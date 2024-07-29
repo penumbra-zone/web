5eb3695ea758f7c56e88ee53be31c0477ef2a4ff
Phil <pbal@fordham.edu>
2024-07-29 16:31:43 -0700

    Fix headers

--- apps/dex-explorer/src/components/lpSearchBar.tsx
+++ apps/dex-explorer/src/components/lpSearchBar.tsx
@@ -1,9 +1,10 @@
 // components/lpSearchBar.tsx
 
 import React, { useState } from "react";
-import { Input } from "@chakra-ui/react";
+import { Input, InputGroup, InputRightElement, IconButton } from "@chakra-ui/react";
+import { SearchIcon } from '@chakra-ui/icons';
 
-export const LPSearchBar = () => {
+export const LPSearchBar = ({ isMobile = false }) => {
   const [LpId, setLpId] = useState("");
 
   const onSearch = (event: React.FormEvent<HTMLFormElement>) => {
@@ -13,16 +14,30 @@ export const LPSearchBar = () => {
   }
 
   return (
-    <form onSubmit={onSearch}>
-      <Input
-        placeholder="Search by Liquidity Position ID"
-        width={"100%"}
-        height={"2.5em"}
-        fontSize={"0.8em"}
-        value={LpId}
-        spellCheck={false}
-        onChange={(e) => setLpId(e.target.value)}
-      />
+    <form onSubmit={onSearch} style={{ width: '100%' }}>
+      <InputGroup size="md">
+        <Input
+          placeholder="Search by Liquidity Position ID"
+          width={isMobile ? "100%" : "37.5em"}
+          height={"2.5em"}
+          fontSize={"0.8em"}
+          value={LpId}
+          onChange={(e) => setLpId(e.target.value)}
+          pr={isMobile ? "4.5rem" : "1rem"}
+          spellCheck={false}
+        />
+        {isMobile && (
+          <InputRightElement width="4.5rem">
+            <IconButton
+              h="1.75rem"
+              size="sm"
+              aria-label="Search"
+              icon={<SearchIcon />}
+              type="submit"
+            />
+          </InputRightElement>
+        )}
+      </InputGroup>
     </form>
   );
 }
