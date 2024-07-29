aafec5e425b7a08ce8912dc1346da1710e417c2e
Phil <pbal@fordham.edu>
2024-07-29 16:20:21 -0700

    Mobile header

--- apps/dex-explorer/src/components/lpSearchBar.tsx
+++ apps/dex-explorer/src/components/lpSearchBar.tsx
@@ -3,34 +3,30 @@
 import React, { useState } from "react";
 import { Input } from "@chakra-ui/react";
 
-
 export const LPSearchBar = () => {
+  const [LpId, setLpId] = useState("");
 
-    const [LpId, setLpId] = useState("");
+  const onSearch = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const sanitizedId = sanitizeInput(LpId)
+    location.assign("/lp/" + sanitizedId)
+  }
 
-   const onSearch = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const sanitizedId = sanitizeInput(LpId)
-        location.assign("/lp/" + sanitizedId)
-    }
-
-    return (
-      // TODO: Massage on mobile
-      <form onSubmit={onSearch}>
-        <Input
-          placeholder="Search by Liquidity Position ID"
-          width={"37.5em"}
-          height={"2.5em"}
-          fontSize={"0.8em"}
-          value={LpId}
-          spellCheck={false}
-          onChange={(e) => setLpId(e.target.value)}
-        />
-      </form>
-    );
+  return (
+    <form onSubmit={onSearch}>
+      <Input
+        placeholder="Search by Liquidity Position ID"
+        width={"100%"}
+        height={"2.5em"}
+        fontSize={"0.8em"}
+        value={LpId}
+        spellCheck={false}
+        onChange={(e) => setLpId(e.target.value)}
+      />
+    </form>
+  );
 }
 
 const sanitizeInput = (input: string): string => {
-    // Trivial sanitation now, should replace with regex for lpId
-    return input.replaceAll(/[&/\\#,+()$~%.^'":*?<>{} ]/g, "");
+  return input.replaceAll(/[&/\\#,+()$~%.^'":*?<>{} ]/g, "");
 }
