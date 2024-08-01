ad27147b1ec3e311c4a69262e2858c6ae561108c
Jesse Pinho <jesse@jessepinho.com>
2024-08-01 12:57:15 -0700

    Start building out assets table

--- apps/minifront/src/components/v2/dashboard-layout/assets-page/index.tsx
+++ apps/minifront/src/components/v2/dashboard-layout/assets-page/index.tsx
@@ -1,1 +1,20 @@
-export const AssetsPage = () => <div>Assets page</div>;
+import { Table } from '@repo/ui/Table';
+
+export const AssetsPage = () => (
+  <div className='flex flex-col gap-1'>
+    <Table title={<div>Account #1</div>}>
+      <Table.Thead>
+        <Table.Tr>
+          <Table.Th>Asset</Table.Th>
+          <Table.Th>Estimate</Table.Th>
+        </Table.Tr>
+      </Table.Thead>
+      <Table.Tbody>
+        <Table.Tr>
+          <Table.Td>test</Table.Td>
+          <Table.Td>test</Table.Td>
+        </Table.Tr>
+      </Table.Tbody>
+    </Table>
+  </div>
+);
