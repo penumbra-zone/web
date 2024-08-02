a0759148044455092a50a1f5d8f716712c2153ef
turbocrime <turbocrime@users.noreply.github.com>
2024-08-02 02:50:28 -0700

    buf gen protos

--- packages/protobuf/src/web.ts
+++ packages/protobuf/src/web.ts
@@ -1,27 +1,40 @@
-import type { IbcChannelService, IbcClientService, IbcConnectionService } from './ibc-core.js';
-import type { CustodyService, ViewService } from './penumbra.js';
-import {
+import type {
+  IbcChannelService,
+  IbcClientService,
+  IbcConnectionService,
+} from './services/cosmos-ibc-core.js';
+import type { CustodyService } from './services/penumbra-custody.js';
+import type { ViewService } from './services/penumbra-view.js';
+import type {
+  AppService,
+  AuctionService,
   CommunityPoolService,
+  CompactBlockService,
   DexService,
+  SimulationService,
   FeeService,
   GovernanceService,
   SctService,
-  SimulationService,
+  ShieldedPoolService,
   StakeService,
-} from './penumbra-core.js';
-import type { TendermintProxyService } from './penumbra-proxy.js';
+} from './services/penumbra-core.js';
+import type { TendermintProxyService } from './services/penumbra-util.js';
 
 export type PenumbraService =
+  | typeof AppService
+  | typeof AuctionService
+  | typeof CommunityPoolService
+  | typeof CompactBlockService
   | typeof CustodyService
   | typeof DexService
+  | typeof FeeService
+  | typeof GovernanceService
   | typeof IbcChannelService
   | typeof IbcClientService
   | typeof IbcConnectionService
   | typeof SctService
+  | typeof ShieldedPoolService
   | typeof SimulationService
   | typeof StakeService
   | typeof TendermintProxyService
-  | typeof ViewService
-  | typeof GovernanceService
-  | typeof CommunityPoolService
-  | typeof FeeService;
+  | typeof ViewService;
