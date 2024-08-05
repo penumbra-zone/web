972cff228018eb0df7a4832113f51229257538a4
valentine <valentyn1789@gmail.com>
2024-08-05 21:04:26 +0300

    smart batches assembly

--- apps/minifront/src/components/swap/auction-list/claim-all-button.tsx
+++ apps/minifront/src/components/swap/auction-list/claim-all-button.tsx
@@ -10,11 +10,43 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from '@repo/ui/components/ui/tooltip';
+import { AuctionInfo } from '../../../fetchers/auction-infos.ts';
 
 const claimAllAuctionButtonSelector = (state: AllSlices) => ({
   endAllAuctions: state.swap.dutchAuction.endAllAuctions,
   withdrawAllAuctions: state.swap.dutchAuction.withdrawAllAuctions,
 });
+
+export interface AuctionsBatch {
+  auctions: AuctionInfo[];
+  source: AddressIndex;
+}
+
+// Assemble batch auctions for end or withdrawal.
+// All auctions in the batch will have the same 'AddressIndex'
+export const assembleAuctionBatch: (
+  auctions: AuctionInfo[],
+  filteredSeqNumber: bigint,
+  batchLimit: number,
+) => AuctionsBatch = (
+  auctions: AuctionInfo[],
+  filteredSeqNumber: bigint,
+  batchLimit: number,
+): AuctionsBatch => {
+  const filteredBySeqAuctions: AuctionInfo[] = auctions.filter(
+    a => a.localSeqNum === filteredSeqNumber,
+  );
+
+  // Get the address index of the first auction in the list and filter other auctions with this address index
+  const firstFoundAddressIndex: AddressIndex = filteredBySeqAuctions[0]?.addressIndex!;
+
+  const filteredBySeqAndAddressIndexAuctions = filterWithLimit(
+    filteredBySeqAuctions,
+    a => a.addressIndex.equals(firstFoundAddressIndex),
+    batchLimit,
+  );
+  return { auctions: filteredBySeqAndAddressIndexAuctions, source: firstFoundAddressIndex };
+};
 
 export const ClaimAllAuctionButton = () => {
   const { endAllAuctions, withdrawAllAuctions } = useStoreShallow(claimAllAuctionButtonSelector);
@@ -28,15 +60,17 @@ export const ClaimAllAuctionButton = () => {
       <TooltipProvider>
         <Tooltip>
           <TooltipTrigger
-            onClick={() =>
+            onClick={() => {
+              // Chain has a transaction size limit, so we can add at most a batch of 48 auctions in a single transaction
+              // see https://github.com/penumbra-zone/web/issues/1166#issuecomment-2263550249
+              const auctionBatch = assembleAuctionBatch(data, 0n, 48);
+
               void endAllAuctions(
-                // Chain has a transaction size limit, so we can add at most a batch of 48 auctions in a single transaction
-                // see https://github.com/penumbra-zone/web/issues/1166#issuecomment-2263550249
-                filterWithLimit(data, a => a.localSeqNum === 0n, 48),
+                auctionBatch.auctions,
                 // TODO Should use the index of the selected account after the account selector for the auction is implemented
-                new AddressIndex({ account: 0 }),
-              )
-            }
+                auctionBatch.source,
+              );
+            }}
             aria-label='End all open auctions, with a limit of 48 auctions per transaction'
           >
             <div className='w-[85px] shrink-0'>
@@ -58,15 +92,16 @@ export const ClaimAllAuctionButton = () => {
       <TooltipProvider>
         <Tooltip>
           <TooltipTrigger
-            onClick={() =>
+            onClick={() => {
+              // Chain has a transaction size limit, so we can add at most a batch of 48 auctions in a single transaction
+              // see https://github.com/penumbra-zone/web/issues/1166#issuecomment-2263550249
+              const auctionBatch = assembleAuctionBatch(data, 1n, 48);
               void withdrawAllAuctions(
-                // Chain has a transaction size limit, so we can add at most a batch of 48 auctions in a single transaction
-                // see https://github.com/penumbra-zone/web/issues/1166#issuecomment-2263550249
-                filterWithLimit(data, a => a.localSeqNum === 1n, 48),
+                auctionBatch?.auctions,
                 // TODO Should use the index of the selected account after the account selector for the auction is implemented
-                new AddressIndex({ account: 0 }),
-              )
-            }
+                auctionBatch?.source,
+              );
+            }}
             aria-label='Withdraw all ended auctions, with a limit of 48 auctions per transaction'
           >
             <div className='w-[95px] shrink-0'>
