2e3af3c8c158c1b54364c9be8beed5600e81eb1c
Jesse Pinho <jesse@jessepinho.com>
2024-08-05 22:27:23 -0700

    Fix spacing issue

--- packages/ui/src/ValueViewComponent/index.tsx
+++ packages/ui/src/ValueViewComponent/index.tsx
@@ -11,21 +11,13 @@ import { useDensity } from '../hooks/useDensity';
 
 type Context = 'default' | 'table';
 
-const Row = styled.span<{ $context: Context; $priority: 'primary' | 'secondary' }>`
+const Row = styled.span`
   display: flex;
   gap: ${props => props.theme.spacing(2)};
   align-items: center;
   width: max-content;
   max-width: 100%;
   text-overflow: ellipsis;
-
-  ${props =>
-    props.$context === 'table' && props.$priority === 'secondary'
-      ? `
-        border-bottom: 2px dashed ${props.theme.color.other.tonalStroke};
-        padding-bottom: ${props.theme.spacing(2)};
-      `
-      : ''};
 `;
 
 const AssetIconWrapper = styled.div`
@@ -37,7 +29,7 @@ const PillMarginOffsets = styled.div<{ $density: Density }>`
   margin-right: ${props => props.theme.spacing(props.$density === 'sparse' ? -1 : 0)};
 `;
 
-const Content = styled.div`
+const Content = styled.div<{ $context: Context; $priority: 'primary' | 'secondary' }>`
   flex-grow: 1;
   flex-shrink: 1;
 
@@ -46,6 +38,11 @@ const Content = styled.div`
   align-items: center;
 
   overflow: hidden;
+
+  ${props =>
+    props.$context === 'table' &&
+    props.$priority === 'secondary' &&
+    `border-bottom: 2px dashed ${props.theme.color.other.tonalStroke};`};
 `;
 
 const SymbolWrapper = styled.div`
@@ -106,12 +103,12 @@ export const ValueViewComponent = <SelectedContext extends Context = 'default'>(
         </Pill>
       )}
     >
-      <Row $context={context ?? 'default'} $priority={priority}>
+      <Row>
         <AssetIconWrapper>
           <AssetIcon metadata={metadata} />
         </AssetIconWrapper>
 
-        <Content>
+        <Content $context={context ?? 'default'} $priority={priority}>
           <Text>{formattedAmount} </Text>
           <SymbolWrapper title={symbol}>
             <Text technical>{symbol}</Text>
