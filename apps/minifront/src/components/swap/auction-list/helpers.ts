6094b6b9fb9497b92cf9f945015fae50a9a08a3f
Valentine <walik19988@gmail.com>
2024-08-07 16:09:57 +0300

    Added end/withdraw all auctions button (#1630)

--- apps/minifront/src/components/swap/auction-list/helpers.ts
+++ apps/minifront/src/components/swap/auction-list/helpers.ts
@@ -1,5 +1,25 @@
 import { AuctionInfo } from '../../../fetchers/auction-infos';
 
+export const filterWithLimit = <T>(
+  array: T[],
+  predicate: (value: T) => boolean,
+  limit: number,
+): T[] => {
+  const result: T[] = [];
+  let count = 0;
+
+  for (const item of array) {
+    if (predicate(item)) {
+      result.push(item);
+      count++;
+      if (count >= limit) {
+        break;
+      }
+    }
+  }
+  return result;
+};
+
 export const byStartHeightAscending = (a: AuctionInfo, b: AuctionInfo) => {
   if (!a.auction.description?.startHeight || !b.auction.description?.startHeight) {
     return 0;
