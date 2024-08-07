e6f019ea34300f32d7f8d96154a7482053a4b27d
Jesse Pinho <jesse@jessepinho.com>
2024-08-07 13:49:33 -0700

    Add Poppins font to the Penumbra Storybook head (#1655)
    * Add Poppins CSS to the Penumbra Storybook head
    
    * Changeset

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,5 +1,11 @@
 import { join, dirname } from 'path';
 
+const poppinsFontCss = `
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+`;
+
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
@@ -39,5 +45,9 @@ const config = {
   typescript: {
     reactDocgen: 'react-docgen-typescript',
   },
+  managerHead: head => `
+    ${head}
+    ${poppinsFontCss}
+  `,
 };
 export default config;
