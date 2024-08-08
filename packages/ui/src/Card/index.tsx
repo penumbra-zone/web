273e182a38c0b7d698fc330270c78615c20bc84a
Jesse Pinho <jesse@jessepinho.com>
2024-08-08 15:30:01 -0700

    Build out <Card /> with stacks of sections

--- packages/ui/src/Card/index.tsx
+++ packages/ui/src/Card/index.tsx
@@ -21,6 +21,10 @@ const Content = styled.div`
   backdrop-filter: blur(${props => props.theme.blur.lg});
   border-radius: ${props => props.theme.borderRadius.xl};
   padding: ${props => props.theme.spacing(3)};
+
+  display: flex;
+  flex-direction: column;
+  gap: ${props => props.theme.spacing(4)};
 `;
 
 export interface CardProps {
@@ -30,13 +34,30 @@ export interface CardProps {
    *
    * @example
    * ```tsx
-   * <Card as='section'>This is a section element with card styling</Card>
+   * <Card as='main'>This is a main element with card styling</Card>
    * ```
    */
   as?: WebTarget;
   title?: ReactNode;
 }
 
+/**
+ * `<Card />`s are rectangular sections of a page set off from the rest of the
+ * page by a background and an optional title. They're useful for presenting
+ * data, or for wrapping a form.
+ *
+ * You can use `<Card.Stack />` and `<Card.Section />` to create a stack of
+ * sections, which are useful for wrapping individual form fields.
+ *
+ * ```tsx
+ * <Card title="This is the card title">
+ *   <Card.Stack>
+ *     <Card.Section><Text>Section one</Text></Card.Section>
+ *     <Card.Section><Text>Section two</Text></Card.Section>
+ *   </Card.Stack>
+ * </Card>
+ * ```
+ */
 export const Card = ({ children, as = 'section', title }: CardProps) => {
   return (
     <Root as={as}>
@@ -46,3 +67,25 @@ export const Card = ({ children, as = 'section', title }: CardProps) => {
     </Root>
   );
 };
+
+const StyledStack = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${props => props.theme.spacing(1)};
+
+  border-radius: ${props => props.theme.borderRadius.sm};
+  overflow: hidden; /** To enforce the border-radius */
+`;
+const Stack = ({ children }: { children?: ReactNode }) => {
+  return <StyledStack>{children}</StyledStack>;
+};
+Card.Stack = Stack;
+
+const StyledSection = styled.div`
+  background-color: ${props => props.theme.color.other.tonalFill5};
+  padding: ${props => props.theme.spacing(3)};
+`;
+const Section = ({ children }: { children?: ReactNode }) => (
+  <StyledSection>{children}</StyledSection>
+);
+Card.Section = Section;
