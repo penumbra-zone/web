1906922c216b80b055b4c1caf97557ae81bca8cd
turbocrime <turbocrime@users.noreply.github.com>
2024-08-21 13:00:57 -0700

    optional access for getter output

--- apps/minifront/src/components/shared/non-native-fee-warning.tsx
+++ apps/minifront/src/components/shared/non-native-fee-warning.tsx
@@ -24,7 +24,7 @@ const hasTokenBalance = ({
   gasPrices: GasPrices[];
   stakingAssetMetadata?: Metadata;
 }): boolean => {
-  const account = getAddressIndex.optional(source).account;
+  const account = getAddressIndex.optional(source)?.account;
   if (typeof account === 'undefined') {
     return false;
   }
@@ -34,8 +34,8 @@ const hasTokenBalance = ({
     asset =>
       getAssetIdFromValueView
         .optional(asset.balanceView)
-        .equals(getAssetId.optional(stakingAssetMetadata)) &&
-      getAddressIndex.optional(asset).account === account,
+        ?.equals(getAssetId.optional(stakingAssetMetadata)) &&
+      getAddressIndex.optional(asset)?.account === account,
   );
 
   if (hasStakingToken) {
@@ -43,7 +43,7 @@ const hasTokenBalance = ({
   }
 
   const accountAssets = balancesResponses.filter(
-    balance => getAddressIndex.optional(balance).account === account,
+    balance => getAddressIndex.optional(balance)?.account === account,
   );
   // Finds the alt tokens in the user's account balances that can be used for fees
   const hasAltTokens = accountAssets.some(balance => {
