d1cecc1c93ee6186d8de31fee93ce30fe880b561
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2024-08-22 16:44:36 +0400

    Rename files

--- packages/ui/components/ui/incompatable-browser-banner/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Banner } from '../banner';
-import { getCompatability } from './get-compatability';
-
-export const IncompatibleBrowserBanner = (props: React.ComponentPropsWithoutRef<'div'>) => {
-  const { isIncompatible, title, content } = getCompatability();
-
-  return isIncompatible && <Banner type='error' title={title} content={content} {...props} />;
-};
--- packages/ui/components/ui/incompatible-browser-banner/index.tsx
+++ packages/ui/components/ui/incompatible-browser-banner/index.tsx
@@ -1,8 +1,15 @@
 import { Banner } from '../banner';
+<<<<<<< HEAD
 import { getCompatibility } from './get-compatibility';
 
 export const IncompatibleBrowserBanner = (props: React.ComponentPropsWithoutRef<'div'>) => {
   const { isIncompatible, title, content } = getCompatibility();
+=======
+import { getCompatability } from './get-compatibility';
+
+export const IncompatibleBrowserBanner = (props: React.ComponentPropsWithoutRef<'div'>) => {
+  const { isIncompatible, title, content } = getCompatability();
+>>>>>>> 1f6ec195 (Rename files)
 
   return isIncompatible && <Banner type='error' title={title} content={content} {...props} />;
 };
