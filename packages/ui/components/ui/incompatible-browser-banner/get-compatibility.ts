74f9a7f786e2c5197374d765b8e39e7fd89784dd
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2024-08-23 18:21:29 +0400

    Support Brave and other Chromium browsers (#1727)
    * Support Brave and other Chromium browsers
    
    * Rename files
    
    * Remove browser & version checks for incompatible browser banner
    
    * Fix rebase conflict
    
    * Fix rebase conflicts 2
    
    * Add changeset

--- packages/ui/components/ui/incompatible-browser-banner/get-compatibility.ts
+++ packages/ui/components/ui/incompatible-browser-banner/get-compatibility.ts
@@ -2,13 +2,6 @@ import { UAParser, UAParserInstance, IDevice } from 'ua-parser-js';
 
 const parser: UAParserInstance = new UAParser();
 
-const CompatibleBrowsers: Record<string, number> = {
-  // browser name: min version
-
-  // same as https://github.com/prax-wallet/web/blob/main/apps/extension/public/manifest.json#L7
-  Chrome: 119,
-};
-
 interface CompatibilityResponse {
   isIncompatible: boolean;
   title: string;
@@ -21,29 +14,7 @@ function isDesktop(device: IDevice): boolean {
 }
 
 function getCompatibility(): CompatibilityResponse {
-  const browser = parser.getBrowser();
   const device = parser.getDevice();
-
-  if (!CompatibleBrowsers[browser.name!]) {
-    return {
-      isIncompatible: true,
-      title: 'Incompatible Browser Detected',
-      content:
-        'You are currently using an incompatible browser. For the best experience, we recommend using Chrome as your browser.',
-    };
-  }
-
-  if (
-    !!CompatibleBrowsers[browser.name!] &&
-    Number(browser.version) < CompatibleBrowsers[browser.name!]!
-  ) {
-    return {
-      isIncompatible: true,
-      title: 'Incompatible Browser Detected',
-      content:
-        'You are currently using an outdated browser. For the best experience, we recommend upgrading Chrome to the latest version.',
-    };
-  }
 
   if (!isDesktop(device)) {
     return {
