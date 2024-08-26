8db9452697482dc288e98c45b9d02c9e2aa22915
Tal Derei <talderei99@gmail.com>
2024-08-25 23:07:57 -0700

    modify tests

--- packages/wasm/crate/tests/test_insufficient_notes.rs
+++ packages/wasm/crate/tests/test_insufficient_notes.rs
@@ -1,8 +1,8 @@
 mod utils;
 use crate::utils::planner_setup::seed_params_in_db;
 use decaf377::Fq;
-use penumbra_asset::asset::{Id, Metadata};
-use penumbra_asset::{Value, STAKING_TOKEN_ASSET_ID};
+use penumbra_asset::asset::Id;
+use penumbra_asset::{Value, STAKING_TOKEN_ASSET_ID, STAKING_TOKEN_DENOM};
 use penumbra_num::Amount;
 use penumbra_proto::core::asset::v1 as pb;
 use penumbra_wasm::database::interface::Database;
@@ -14,45 +14,12 @@ use wasm_bindgen_test::wasm_bindgen_test;
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 use penumbra_proto::DomainType;
 
-#[wasm_bindgen_test]
-// Database throws an error while retrieving
-async fn test_insufficient_notes_failure_to_retrieve() {
-    let mock_db = MockDb::new();
-    let tables = get_mock_tables();
-    seed_params_in_db(&mock_db, &tables).await;
-    let storage = Storage::new(mock_db.clone(), tables.clone()).unwrap();
-
-    let asset_id = *STAKING_TOKEN_ASSET_ID;
-    let key = byte_array_to_base64(&asset_id.to_proto().inner);
-    let metadata_in_db_proto = pb::Metadata {
-        base: "uosmo".to_string(),
-        ..Default::default()
-    };
-    let metadata_in_db: Metadata = metadata_in_db_proto.clone().try_into().unwrap();
-
-    // Store the metadata in the mock database using the base64 key
-    mock_db
-        .put_with_key(&tables.assets, key, &metadata_in_db_proto)
-        .await
-        .unwrap();
-
-    let required = Value {
-        asset_id: metadata_in_db.id(),
-        amount: Amount::from(100u64),
-    };
-
-    let error = insufficient_funds_err(&storage, &required).await;
-    let error_message = format!("{}", error);
-    assert_eq!(
-        error_message,
-        "Transaction failed due to insufficient funds"
-    );
-}
+// TODO: add a test for database error when mock storage can support errors
 
 #[wasm_bindgen_test]
 // Asset id metadata is in storage and displays amount and denom
-async fn test_insufficient_notes_in_storage() {
-    let mock_db = MockDb::new();
+async fn metadata_in_storage_but_no_display_info() {
+    let mock_db: MockDb = MockDb::new();
     let tables = get_mock_tables();
     seed_params_in_db(&mock_db, &tables).await;
 
@@ -81,32 +48,53 @@ async fn test_insufficient_notes_in_storage() {
     assert_eq!(
         format!("{}", error),
         format!(
-            "Transaction failed due to insufficient funds. Required amount: 100 {}.",
-            asset_id_staking_token
+            "Transaction failed due to insufficient funds. Required amount: 100 passet1984fctenw8m2fpl8a9wzguzp7j34d7vravryuhft808nyt9fdggqxmanqm."
         )
     );
 }
 
 #[wasm_bindgen_test]
-// Asset id metadata is not in storage
-async fn test_insufficient_notes_not_in_storage() {
-    let mock_db = MockDb::new();
+// Asset id metadata is in storage and displays amount and denom
+async fn metadata_in_storage() {
+    let mock_db: MockDb = MockDb::new();
     let tables = get_mock_tables();
     seed_params_in_db(&mock_db, &tables).await;
 
     let asset_id_staking_token = *STAKING_TOKEN_ASSET_ID;
-    let asset_id_alternative_token = Id(Fq::rand(&mut OsRng));
 
     let key = byte_array_to_base64(&asset_id_staking_token.to_proto().inner);
-    let metadata_in_db_proto = pb::Metadata {
-        base: asset_id_staking_token.to_string(),
-        ..Default::default()
-    };
+    // Store whole metadata struct
+    let metadata_in_db_proto: pb::Metadata = STAKING_TOKEN_DENOM.clone().into();
 
     mock_db
         .put_with_key(&tables.assets, key.clone(), &metadata_in_db_proto)
         .await
         .unwrap();
+
+    let storage = Storage::new(mock_db.clone(), tables.clone()).unwrap();
+
+    let required = Value {
+        asset_id: asset_id_staking_token,
+        amount: Amount::from(100u64),
+    };
+
+    let error = insufficient_funds_err(&storage, &required).await;
+
+    assert_eq!(
+        format!("{}", error),
+        // Note the decimal change
+        "Transaction failed due to insufficient funds. Required amount: 0.0001 penumbra."
+    );
+}
+
+#[wasm_bindgen_test]
+// Asset id metadata is not in storage
+async fn insufficient_notes_not_in_storage() {
+    let mock_db = MockDb::new();
+    let tables = get_mock_tables();
+    seed_params_in_db(&mock_db, &tables).await;
+
+    let asset_id_alternative_token = Id(Fq::rand(&mut OsRng));
 
     let storage = Storage::new(mock_db.clone(), tables.clone()).unwrap();
 
