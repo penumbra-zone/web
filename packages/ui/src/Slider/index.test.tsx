d93845632b3b7be75e62b4543f7ee216f7bcc6c2
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2024-09-03 15:20:18 +0400

    Add Slider component (#1738)
    * Add slider component'
    
    * Add fontSize and valueDetails props
    
    * Add tests for Slider
    
    * Add changeset
    
    * Use RadixSlider for Slider component
    
    * Add onChange test for Slider

---
+++ packages/ui/src/Slider/index.test.tsx
@@ -0,0 +1,41 @@
+import { render, fireEvent } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
+import { Slider } from '.';
+import { PenumbraUIProvider } from '../PenumbraUIProvider';
+
+window.ResizeObserver = vi.fn().mockImplementation(() => ({
+  disconnect: vi.fn(),
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+}));
+
+describe('<Slider />', () => {
+  it('renders correctly', () => {
+    const { container } = render(
+      <Slider min={0} max={10} step={1} defaultValue={5} leftLabel='left' rightLabel='right' />,
+      {
+        wrapper: PenumbraUIProvider,
+      },
+    );
+
+    expect(container).toHaveTextContent('left');
+    expect(container).toHaveTextContent('right');
+  });
+
+  it('handles onChange correctly', () => {
+    const onChange = vi.fn();
+
+    const { container } = render(
+      <Slider min={0} max={10} step={1} defaultValue={5} onChange={onChange} />,
+      {
+        wrapper: PenumbraUIProvider,
+      },
+    );
+
+    const slider = container.querySelector('[role="slider"]')!;
+    fireEvent.focus(slider);
+    fireEvent.keyDown(slider, { key: 'ArrowRight' });
+
+    expect(onChange).toHaveBeenCalledWith(6);
+  });
+});
