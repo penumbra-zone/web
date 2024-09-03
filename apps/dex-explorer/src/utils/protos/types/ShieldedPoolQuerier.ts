fab73ce584727eefe8b21fd12ef979de73ea0c89
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2024-09-03 15:55:42 +0400

    Update dependencies

--- apps/dex-explorer/src/utils/protos/types/ShieldedPoolQuerier.ts
+++ apps/dex-explorer/src/utils/protos/types/ShieldedPoolQuerier.ts
@@ -1,7 +1,7 @@
 import {
   AssetId,
   Metadata,
-} from "@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/asset/v1/asset_pb";
+} from "@penumbra-zone/protobuf/penumbra/core/asset/v1/asset_pb";
 
 export interface ShieldedPoolQuerierInterface {
   assetMetadata(assetId: AssetId): Promise<Metadata | undefined>;
--- apps/dex-explorer/src/utils/types/block.tsx
+++ apps/dex-explorer/src/utils/types/block.tsx
@@ -1,29 +1,29 @@
-import { SwapExecution } from "@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/dex/v1/dex_pb";
+import { SwapExecution } from "@penumbra-zone/protobuf/penumbra/core/component/dex/v1/dex_pb";
 import { BlockInfo, LiquidityPositionEvent } from "../indexer/types/lps";
 
 export interface BlockSummaryData {
-    openPositionEvents: LiquidityPositionEvent[],
-    closePositionEvents: LiquidityPositionEvent[],
-    withdrawPositionEvents: LiquidityPositionEvent[],
-    swapExecutions: SwapExecution[],
-    arbExecutions: SwapExecution[],
-    createdAt: string
+  openPositionEvents: LiquidityPositionEvent[];
+  closePositionEvents: LiquidityPositionEvent[];
+  withdrawPositionEvents: LiquidityPositionEvent[];
+  swapExecutions: SwapExecution[];
+  arbExecutions: SwapExecution[];
+  createdAt: string;
 }
 
 export interface BlockDetailedSummaryData {
-    openPositionEvents: LiquidityPositionEvent[],
-    closePositionEvents: LiquidityPositionEvent[],
-    withdrawPositionEvents: LiquidityPositionEvent[],
-    otherPositionEvents: LiquidityPositionEvent[],
-    swapExecutions: SwapExecution[],
-    arbExecutions: SwapExecution[],
-    createdAt: string
+  openPositionEvents: LiquidityPositionEvent[];
+  closePositionEvents: LiquidityPositionEvent[];
+  withdrawPositionEvents: LiquidityPositionEvent[];
+  otherPositionEvents: LiquidityPositionEvent[];
+  swapExecutions: SwapExecution[];
+  arbExecutions: SwapExecution[];
+  createdAt: string;
 }
 
 export interface BlockInfoMap {
-    [key: number]: BlockInfo
+  [key: number]: BlockInfo;
 }
 
 export interface BlockSummaryMap {
-    [key: number]: BlockSummaryData
+  [key: number]: BlockSummaryData;
 }
