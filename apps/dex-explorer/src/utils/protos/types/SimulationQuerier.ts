6027350a7c9d711d2fd06d396d8803a7b07309bd
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2024-09-03 15:55:42 +0400

    Update dependencies

--- apps/dex-explorer/src/utils/protos/types/SimulationQuerier.ts
+++ apps/dex-explorer/src/utils/protos/types/SimulationQuerier.ts
@@ -1,7 +1,7 @@
 import {
   SimulateTradeRequest,
-  SwapExecution
-} from "@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/core/component/dex/v1/dex_pb";
+  SwapExecution,
+} from "@penumbra-zone/protobuf/penumbra/core/component/dex/v1/dex_pb";
 
 export interface SimulationServiceInterface {
   simulateTrade(
