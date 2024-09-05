63562dceeb7e10aa0fb74ae423d840d55f4aa045
Max Korsunov <maxkors2014@gmail.com>
2024-09-05 22:24:34 +0500

    feat(ui): #1714: update AssetSelectorTrigger to the latest designs

---
+++ packages/ui/src/AssetSelector/Trigger.tsx
@@ -0,0 +1,139 @@
+import { ForwardedRef, forwardRef, MouseEventHandler } from 'react';
+import styled, { css } from 'styled-components';
+import { ChevronsUpDownIcon } from 'lucide-react';
+import { motion } from 'framer-motion';
+import { BalancesResponse } from '@penumbra-zone/protobuf/penumbra/view/v1/view_pb';
+import { Metadata } from '@penumbra-zone/protobuf/penumbra/core/asset/v1/asset_pb';
+import { getMetadataFromBalancesResponse } from '@penumbra-zone/getters/balances-response';
+import { ActionType, getOutlineColorByActionType } from '../utils/ActionType.ts';
+import { Density } from '../types/Density.ts';
+import { useDensity } from '../hooks/useDensity';
+import { asTransientProps } from '../utils/asTransientProps.ts';
+import { Icon } from '../Icon';
+import { Text } from '../Text';
+import { AssetIcon } from '../AssetIcon';
+import { isMetadata } from './helpers.ts';
+
+const SparseButton = css`
+  height: ${props => props.theme.spacing(12)};
+  padding: 0 ${props => props.theme.spacing(3)};
+`;
+
+const CompactButton = css`
+  height: ${props => props.theme.spacing(8)};
+  padding: 0 ${props => props.theme.spacing(2)};
+`;
+
+const Trigger = styled(motion.button)<{ $density: Density; $actionType: ActionType }>`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: ${props => props.theme.spacing(1)};
+  min-width: ${props => props.theme.spacing(20)};
+  border-radius: ${props => props.theme.borderRadius.none};
+  background: ${props => props.theme.color.other.tonalFill5};
+  transition:
+    background 0.15s,
+    outline 0.15s;
+
+  ${props => (props.$density === 'sparse' ? SparseButton : CompactButton)};
+
+  &:hover {
+    background-color: ${props => props.theme.color.action.hoverOverlay};
+  }
+
+  &:focus {
+    color: ${props => props.theme.color.text.secondary};
+    background: ${props => props.theme.color.other.tonalFill5};
+    outline: 2px solid ${props => getOutlineColorByActionType(props.theme, props.$actionType)};
+  }
+
+  &:disabled {
+    background: linear-gradient(
+        0deg,
+        ${props => props.theme.color.action.disabledOverlay} 0%,
+        ${props => props.theme.color.action.disabledOverlay} 100%
+      ),
+      ${props => props.theme.color.other.tonalFill10};
+  }
+`;
+
+const Value = styled.div<{ $density: Density; $actionType: ActionType }>`
+  display: flex;
+  align-items: center;
+  gap: ${props => props.theme.spacing(props.$density === 'sparse' ? 2 : 1)};
+`;
+
+const IconAdornment = styled.i<{ $disabled?: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: ${props => props.theme.spacing(1)};
+  width: ${props => props.theme.spacing(6)};
+  height: ${props => props.theme.spacing(6)};
+  border-radius: ${props => props.theme.borderRadius.full};
+  background-color: ${props =>
+    props.$disabled ? props.theme.color.action.disabledOverlay : 'transparent'};
+`;
+
+export interface AssetSelectorTriggerProps<
+  ValueType extends (BalancesResponse | Metadata) | Metadata,
+> {
+  value?: ValueType;
+  actionType?: ActionType;
+  disabled?: boolean;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  layoutId?: string;
+}
+
+const AssetSelectorTriggerFunc = <ValueType extends (BalancesResponse | Metadata) | Metadata>(
+  {
+    value,
+    actionType = 'default',
+    disabled,
+    onClick,
+    layoutId,
+  }: AssetSelectorTriggerProps<ValueType>,
+  ref: ForwardedRef<HTMLButtonElement>,
+) => {
+  const density = useDensity();
+
+  const metadata = isMetadata(value) ? value : getMetadataFromBalancesResponse.optional(value);
+
+  return (
+    <Trigger
+      ref={ref}
+      layoutId={layoutId}
+      disabled={disabled}
+      {...asTransientProps({ density, actionType })}
+      onClick={onClick}
+    >
+      {!value ? (
+        <Text small color={color => (disabled ? color.text.muted : color.text.primary)}>
+          Asset
+        </Text>
+      ) : (
+        <Value {...asTransientProps({ density, actionType })}>
+          <AssetIcon metadata={metadata} size={density === 'sparse' ? 'lg' : 'md'} />
+          <Text color={color => (disabled ? color.text.muted : color.text.primary)}>
+            {metadata?.symbol ?? 'Unknown'}
+          </Text>
+        </Value>
+      )}
+
+      <IconAdornment $disabled={disabled}>
+        <Icon
+          IconComponent={ChevronsUpDownIcon}
+          size='sm'
+          color={color => (disabled ? color.text.muted : color.text.primary)}
+        />
+      </IconAdornment>
+    </Trigger>
+  );
+};
+
+export const AssetSelectorTrigger = forwardRef(AssetSelectorTriggerFunc) as <
+  ValueType extends (BalancesResponse | Metadata) | Metadata,
+>(
+  props: AssetSelectorTriggerProps<ValueType> & { ref?: ForwardedRef<HTMLButtonElement> },
+) => ReturnType<typeof AssetSelectorTriggerFunc>;
--- packages/ui/src/AssetSelector/index.tsx
+++ packages/ui/src/AssetSelector/index.tsx
@@ -1,36 +1,10 @@
 import { Metadata } from '@penumbra-zone/protobuf/penumbra/core/asset/v1/asset_pb';
 import { BalancesResponse } from '@penumbra-zone/protobuf/penumbra/view/v1/view_pb';
 import { Dialog } from '../Dialog';
-import { Text } from '../Text';
-import styled from 'styled-components';
-import { buttonBase } from '../utils/button';
-import { Density } from '../types/Density';
-import { useDensity } from '../hooks/useDensity';
-import { getMetadataFromBalancesResponse } from '@penumbra-zone/getters/balances-response';
-import { AssetIcon } from '../AssetIcon';
-import { ConditionalWrap } from '../ConditionalWrap';
 import { AssetSelectorDialogContent } from './AssetSelectorDialogContent';
-import { motion } from 'framer-motion';
 import { useId, useState } from 'react';
-import { isMetadata } from './helpers';
-import { Icon } from '../Icon';
-import { ChevronsUpDownIcon } from 'lucide-react';
-
-const Button = styled(motion.button)<{ $density: Density }>`
-  ${buttonBase}
-
-  background-color: ${props => props.theme.color.other.tonalFill5};
-  height: ${props => props.theme.spacing(props.$density === 'sparse' ? 12 : 8)};
-  text-align: left;
-  padding: 0 ${props => props.theme.spacing(props.$density === 'sparse' ? 3 : 2)};
-  width: ${props => (props.$density === 'sparse' ? '100%' : 'max-content')};
-`;
-
-const Row = styled.div<{ $density: Density }>`
-  display: flex;
-  gap: ${props => props.theme.spacing(props.$density === 'sparse' ? 2 : 1)};
-  align-items: center;
-`;
+import { AssetSelectorTrigger } from './Trigger.tsx';
+import { ActionType } from '../utils/ActionType.ts';
 
 export interface AssetSelectorProps<ValueType extends (BalancesResponse | Metadata) | Metadata> {
   /**
@@ -46,6 +20,9 @@ export interface AssetSelectorProps<ValueType extends (BalancesResponse | Metada
   options: ValueType[];
   /** The title to show above the asset selector dialog when it opens. */
   dialogTitle: string;
+
+  actionType?: ActionType;
+  disabled?: boolean;
 }
 
 /**
@@ -59,10 +36,10 @@ export const AssetSelector = <ValueType extends (BalancesResponse | Metadata) | 
   onChange,
   options,
   dialogTitle,
+  actionType,
+  disabled,
 }: AssetSelectorProps<ValueType>) => {
   const layoutId = useId();
-  const density = useDensity();
-  const metadata = isMetadata(value) ? value : getMetadataFromBalancesResponse.optional(value);
 
   const [isOpen, setIsOpen] = useState(false);
 
@@ -73,37 +50,16 @@ export const AssetSelector = <ValueType extends (BalancesResponse | Metadata) | 
 
   return (
     <Dialog isOpen={isOpen} onClose={() => setIsOpen(false)}>
-      <Button $density={density} layoutId={layoutId} key={layoutId} onClick={() => setIsOpen(true)}>
-        {value && (
-          <Row $density={density}>
-            <AssetIcon metadata={metadata} />
-            <ConditionalWrap
-              if={density === 'sparse'}
-              then={children => <Text>{children}</Text>}
-              else={children => <Text small>{children}</Text>}
-            >
-              {metadata?.symbol}
-            </ConditionalWrap>
-            <div className={'ml-auto'}>
-              <Icon
-                IconComponent={ChevronsUpDownIcon}
-                size={'sm'}
-                color={color => color.neutral.contrast}
-              />
-            </div>
-          </Row>
-        )}
-
-        {!value && (
-          <ConditionalWrap
-            if={density === 'sparse'}
-            then={children => <Text>{children}</Text>}
-            else={children => <Text small>{children}</Text>}
-          >
-            Asset
-          </ConditionalWrap>
-        )}
-      </Button>
+      <Dialog.Trigger>
+        <AssetSelectorTrigger
+          value={value}
+          actionType={actionType}
+          disabled={disabled}
+          layoutId={layoutId}
+          key={layoutId}
+          onClick={() => setIsOpen(true)}
+        />
+      </Dialog.Trigger>
 
       <AssetSelectorDialogContent
         title={dialogTitle}
