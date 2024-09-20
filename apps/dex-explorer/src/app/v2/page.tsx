0c13b38952eed34140bc23be1dcc102cc13a5026
Max Korsunov <maxkors2014@gmail.com>
2024-09-20 13:19:23 +0300

    feat: #46: setup v2 routes

---
+++ apps/dex-explorer/src/app/v2/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { Text } from '@penumbra-zone/ui/Text';
+
+const HomePage = () => {
+  return (
+    <section>
+      <h1 className="text-3xl font-bold">
+        Hello world!
+      </h1>
+
+      <Text h2>Hi!</Text>
+    </section>
+  )
+}
+
+export default HomePage;
