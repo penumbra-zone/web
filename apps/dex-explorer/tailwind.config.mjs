49b0bfb7eef4890cd87aa0e50fbbbf2d8d8589f1
Max Korsunov <maxkors2014@gmail.com>
2024-09-20 13:43:16 +0300

    feat: penumbra-zone/dex-explorer/issues/46: use Penumbra UI theme in the tailwind config

--- apps/dex-explorer/tailwind.config.mjs
+++ apps/dex-explorer/tailwind.config.mjs
@@ -1,10 +1,10 @@
+import { tailwindConfig as v2TailwindConfig } from '@penumbra-zone/ui/tailwind';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx,css}",
   ],
-  theme: {
-    extend: {},
-  },
+  theme: v2TailwindConfig.theme,
   plugins: [],
 }
