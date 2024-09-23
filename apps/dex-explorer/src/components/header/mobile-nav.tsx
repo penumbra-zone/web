583ed61f53a0ae3667d3725280024fc5777d2473
Max Korsunov <maxkors2014@gmail.com>
2024-09-23 11:47:51 +0200

    feat: move header nav from minifront

---
+++ apps/dex-explorer/src/components/header/mobile-nav.tsx
@@ -0,0 +1,57 @@
+import { Menu, X } from 'lucide-react';
+import { Button } from '@penumbra-zone/ui/Button';
+import { Dialog } from '@penumbra-zone/ui/Dialog';
+import { Display } from '@penumbra-zone/ui/Display';
+import { MenuItem } from '@penumbra-zone/ui/MenuItem';
+// import { StatusPopover } from './status-popover.tsx';
+import { ProviderPopover } from './provider-popover.tsx';
+import { HeaderLogo } from './logo.tsx';
+import { useState } from 'react';
+import { HEADER_LINKS } from './links.ts';
+import { useRouter } from 'next/navigation';
+
+export const MobileNav = () => {
+  const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onNavigate = (link: string) => {
+    router.push(link);
+    setIsOpen(false);
+  };
+
+  return (
+    <Dialog isOpen={isOpen} onClose={() => setIsOpen(false)}>
+      <Button iconOnly icon={Menu} onClick={() => setIsOpen(true)}>
+        Menu
+      </Button>
+      <Dialog.EmptyContent>
+        <div className='pointer-events-auto h-full overflow-hidden bg-black'>
+          <Display>
+            <nav className='flex items-center justify-between py-5'>
+              <HeaderLogo />
+
+              <div className='flex gap-2'>
+                {/* <StatusPopover />*/}
+                <ProviderPopover />
+                <Button iconOnly icon={X} onClick={() => setIsOpen(false)}>
+                  Close
+                </Button>
+              </div>
+            </nav>
+
+            <div className='flex flex-col gap-4'>
+              {HEADER_LINKS.map(link => (
+                <MenuItem
+                  key={link.value}
+                  label={link.label}
+                  icon={link.icon}
+                  onClick={() => onNavigate(link.value)}
+                />
+              ))}
+            </div>
+          </Display>
+        </div>
+      </Dialog.EmptyContent>
+    </Dialog>
+  );
+};
