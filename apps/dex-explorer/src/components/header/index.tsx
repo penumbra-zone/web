81f5d29fc87334609738c1b41ff3f1bba92d48fc
Max Korsunov <maxkors2014@gmail.com>
2024-09-23 11:47:51 +0200

    feat: move header nav from minifront

---
+++ apps/dex-explorer/src/components/header/index.tsx
@@ -0,0 +1,26 @@
+import { Density } from '@penumbra-zone/ui/Density';
+import { HeaderLogo } from './logo.tsx';
+import { ProviderPopover } from './provider-popover.tsx';
+// import { StatusPopover } from './status-popover.tsx';
+import { MobileNav } from './mobile-nav.tsx';
+import { DesktopNav } from './desktop-nav.tsx';
+
+export const Header = () => {
+  return (
+    <header className='flex items-center justify-between py-5'>
+      <HeaderLogo />
+
+      <DesktopNav />
+
+      <Density compact>
+        <div className='hidden gap-2 lg:flex'>
+          {/* <StatusPopover />*/}
+          <ProviderPopover />
+        </div>
+        <div className='block lg:hidden'>
+          <MobileNav />
+        </div>
+      </Density>
+    </header>
+  );
+};
