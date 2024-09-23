25eacd952232da6cdfbbecdee324ea11f053646e
Max Korsunov <maxkors2014@gmail.com>
2024-09-23 11:46:19 +0200

    feat: update configs to support client libraries

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,14 @@
 const nextConfig = {
   transpilePackages: ["@penumbra-zone/protobuf"],
   webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+
     config.experiments.asyncWebAssembly = true;
+
     return config;
   },
   output: "standalone",
--- src/app/v2/page.tsx
+++ src/app/v2/page.tsx
@@ -5,10 +5,6 @@ import { Text } from '@penumbra-zone/ui/Text';
 const HomePage = () => {
   return (
     <section>
-      <h1 className="text-3xl font-bold text-white">
-        Hello world!
-      </h1>
-
       <Text h2>Hi!</Text>
     </section>
   )
