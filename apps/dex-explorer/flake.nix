2823fcf01547ef80affe532c61201e38c14d0172
Conor Schaefer <conor@ruin.dev>
2024-09-25 09:05:38 -0700

    docs: add quickstart docs for dev env
    Adds a nix config so that tooling like `pnpm` is available immediately.
    Fleshes out the documentation around connecting to managed database
    instances, since merge of #56.

---
+++ apps/dex-explorer/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Dev shell for Penumbra DEX explorer web application";
+  # inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "devShell";
+          nativeBuildInputs = [ pkgs.bashInteractive ];
+          buildInputs = with pkgs; [
+            fd
+            file
+            jq
+            just
+            pnpm
+            postgresql
+          ];
+        };
+      });
+}
