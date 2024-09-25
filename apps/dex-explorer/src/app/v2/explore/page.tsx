031c2d81b1a09b4ff5d171cfb5dba35459b9298f
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2024-09-25 23:09:45 +0400

    Setup pages for v2 routes (#68)
    * Setup pages for v2 routes
    
    * Use redirect fn

---
+++ apps/dex-explorer/src/app/v2/explore/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { Text } from '@penumbra-zone/ui/Text';
+
+const ExplorePage = () => {
+  return (
+    <section>
+      <Text h2>Hi!</Text>
+    </section>
+  );
+};
+
+export default ExplorePage;
---
+++ apps/dex-explorer/src/app/v2/portfolio/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { Text } from '@penumbra-zone/ui/Text';
+
+const PortfolioPage = () => {
+  return (
+    <section>
+      <Text h2>Hi!</Text>
+    </section>
+  );
+};
+
+export default PortfolioPage;
