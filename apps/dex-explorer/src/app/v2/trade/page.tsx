1b56dbbae93f072359d0898e79ffe6f195c417b7
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2024-09-27 17:43:35 +0400

    Responsive trade layout (penumbra-zone/dex-explorer/issues/69)
    * Setup responsive trade layout
    
    * Adjust column widths
    
    * Remove unused Text componet

--- apps/dex-explorer/src/app/v2/trade/page.tsx
+++ apps/dex-explorer/src/app/v2/trade/page.tsx
@@ -1,12 +1,45 @@
 'use client';
 
-import { Text } from '@penumbra-zone/ui/Text';
+import { Card } from '@penumbra-zone/ui/Card';
 
 const TradePage = () => {
   return (
-    <section>
-      <Text h2>Hi!</Text>
-    </section>
+    <div>
+      <div className='flex flex-wrap lg:gap-2'>
+        <div className='w-full lg:w-auto lg:flex-grow mb-2'>
+          <Card title='Chart'>
+            <div className='h-[512px]'>-</div>
+          </Card>
+        </div>
+        <div className='w-full sm:w-1/2 sm:pr-1 lg:w-[336px] lg:pr-0 mb-2'>
+          <Card title='Route Book'>
+            <div className='h-[512px]'>-</div>
+          </Card>
+        </div>
+        <div className='w-full sm:w-1/2 sm:pl-1 lg:w-[304px] lg:pl-0 mb-2'>
+          <Card title='Order Form'>
+            <div className='h-[512px]'>-</div>
+          </Card>
+        </div>
+      </div>
+      <div className='flex flex-wrap lg:gap-2'>
+        <div className='w-full lg:w-auto lg:flex-grow mb-2'>
+          <Card title='Positions'>
+            <div className='h-[256px]'>-</div>
+          </Card>
+        </div>
+        <div className='w-full sm:w-1/2 sm:pr-1 lg:w-[336px] lg:pr-0 mb-2'>
+          <Card title='Market Trades'>
+            <div className='h-[256px]'>-</div>
+          </Card>
+        </div>
+        <div className='w-full sm:w-1/2 sm:pl-1 lg:w-[304px] lg:pl-0 mb-2'>
+          <Card title='Assets'>
+            <div className='h-[256px]'>-</div>
+          </Card>
+        </div>
+      </div>
+    </div>
   );
 };
 
