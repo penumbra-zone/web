a8d3b20d2e206ded2a55fa0e4c2ed64e79e3404e
Tal Derei <talderei99@gmail.com>
2024-10-03 07:03:18 -0700

    assertion cleanup

--- packages/services/src/view-service/transaction-planner/assert-max-spends.ts
+++ packages/services/src/view-service/transaction-planner/assert-max-spends.ts
@@ -8,13 +8,36 @@ export const assertSpendMax = async (
   plan: TransactionPlan,
   indexedDb: IndexedDbInterface,
 ) => {
+  // If it's not a spend transaction request, skip the assertion checks.
   if (req.spends.length === 0) {
     return;
   }
 
+  // Constraint: validate the Spend transaction planner request is constructed in isolation.
+  if (
+    req.outputs.length > 0 ||
+    req.swaps.length > 0 ||
+    req.swapClaims.length > 0 ||
+    req.delegations.length > 0 ||
+    req.undelegations.length > 0 ||
+    req.undelegationClaims.length > 0 ||
+    req.ibcRelayActions.length > 0 ||
+    req.ics20Withdrawals.length > 0 ||
+    req.positionOpens.length > 0 ||
+    req.positionCloses.length > 0 ||
+    req.positionWithdraws.length > 0 ||
+    req.dutchAuctionScheduleActions.length > 0 ||
+    req.dutchAuctionEndActions.length > 0 ||
+    req.dutchAuctionWithdrawActions.length > 0 ||
+    req.delegatorVotes.length > 0 ||
+    req.ibcRelayActions.length > 0
+  ) {
+    throw new ConnectError('Invalid transaction: Spend transaction was constructed improperly.');
+  }
+
   // Constraint: validate that the transaction planner request is constructed with a single spend request.
   if (req.spends.length > 1) {
-    throw new ConnectError('Invalid transaction: The transaction was constructed improperly!');
+    throw new ConnectError('Invalid transaction: Spend transaction was constructed improperly.');
   }
 
   // Constraint: validate that each spend action's asset ID matches the fee asset ID.
@@ -22,12 +45,11 @@ export const assertSpendMax = async (
   if (feeAssetId === undefined) {
     feeAssetId = indexedDb.stakingTokenAssetId;
   }
-
   plan.actions.forEach(action => {
     if (action.action.case === 'spend') {
       // prettier-ignore
       if (!(action.action.value.note?.value?.assetId?.equals(feeAssetId))) {
-        throw new ConnectError('Invalid transaction: The transaction was constructed improperly.');
+        throw new ConnectError('Invalid transaction: Spend transaction was constructed improperly.');
       }
     }
   });
@@ -42,6 +64,12 @@ export const assertSpendMax = async (
     accumulatedBalance.lo !== req.spends[0]?.value?.amount?.lo ||
     accumulatedBalance.hi !== req.spends[0]?.value?.amount?.hi
   ) {
-    throw new ConnectError('Invalid transaction: The transaction was constructed improperly.');
+    throw new ConnectError('Invalid transaction: Spend transaction was constructed improperly.');
+  }
+
+  // Constraint: validate a single output action in the fully-formed transaction.
+  const outputActions = plan.actions.filter(action => action.action.case === 'output');
+  if (outputActions.length > 1) {
+    throw new ConnectError('Invalid transaction: Spend transaction was constructed improperly.');
   }
 };
