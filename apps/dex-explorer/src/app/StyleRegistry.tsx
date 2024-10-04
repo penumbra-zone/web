a9b05530433c16b248c620c264ab5de74ae5cd8c
Max Korsunov <maxkors2014@gmail.com>
2024-10-04 11:47:59 +0100

    fix: penumbra-zone/dex-explorer/issues/87: support styled-components in SSR to solve FOUC (penumbra-zone/dex-explorer/issues/88)

---
+++ apps/dex-explorer/src/app/StyleRegistry.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { useState, ReactNode } from 'react';
+import { useServerInsertedHTML } from 'next/navigation';
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
+
+/**
+ * Needed to ensure that styled-components styles are rendered on the server.
+ * https://nextjs.org/docs/app/building-your-application/styling/css-in-js#styled-components
+ */
+export const StyledComponentsRegistry = ({
+  children,
+}: {
+  children: ReactNode,
+}) => {
+  // Only create stylesheet once with lazy initial state
+  // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
+  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
+
+  useServerInsertedHTML(() => {
+    const styles = styledComponentsStyleSheet.getStyleElement();
+    styledComponentsStyleSheet.instance.clearTag();
+    return <>{styles}</>;
+  });
+
+  if (typeof window !== 'undefined') {
+    return <>{children}</>;
+  }
+
+  return (
+    <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
+      {children}
+    </StyleSheetManager>
+  );
+};
