f4d75be9d87b4e0d4d06871d888d872de6395cf1
Max Korsunov <maxkors2014@gmail.com>
2024-10-04 11:47:59 +0100

    fix: #87: support styled-components in SSR to solve FOUC (#88)

--- apps/dex-explorer/src/app/layout.tsx
+++ apps/dex-explorer/src/app/layout.tsx
@@ -1,11 +1,14 @@
 import { ReactNode } from 'react';
 import '@/v2.css';
+import { StyledComponentsRegistry } from './StyleRegistry';
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en">
       <body>
-        {children}
+        <StyledComponentsRegistry>
+          {children}
+        </StyledComponentsRegistry>
       </body>
     </html>
   )
