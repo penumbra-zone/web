c7265ccc3d87bb21f701778b3604b48006619306
Max Korsunov <maxkors2014@gmail.com>
2024-10-10 10:22:01 +0200

    feat: penumbra-zone/dex-explorer/issues/65: pair selector (penumbra-zone/dex-explorer/issues/85)
    * feat: penumbra-zone/dex-explorer/issues/65: implement pair selector
    
    * feat: penumbra-zone/dex-explorer/issues/65: fill pair selector with account balances
    
    * feat: fill the `PairSelector` with the registry values
    
    * feat: penumbra-zone/dex-explorer/issues/65: use react-query to fetch assets and balances
    
    * refactor: penumbra-zone/dex-explorer/issues/65: use setters in the stores
    
    * fix: penumbra-zone/dex-explorer/issues/65: use react-query instead of mobx to keep the state of the fetchers
    
    * fix: penumbra-zone/dex-explorer/issues/65: apply latest changes
    
    * fix: penumbra-zone/dex-explorer/issues/65: apply PR comments and add better docs
    
    * fix: penumbra-zone/dex-explorer/issues/65: rename balances queryKey

---
+++ apps/dex-explorer/src/app/Providers.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { ReactNode } from 'react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@/shared/queryClient';
+import { StyledComponentsRegistry } from './StyleRegistry';
+
+export const Providers = ({ children }: { children: ReactNode }) => {
+  return (
+    <StyledComponentsRegistry>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </StyledComponentsRegistry>
+  );
+};
