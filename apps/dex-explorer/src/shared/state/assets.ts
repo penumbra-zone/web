9c2bf0113274384a459e1822a462692338322f20
Max Korsunov <maxkors2014@gmail.com>
2024-10-10 10:22:01 +0200

    feat: github.com/penumbra-zone/dex-explorer/issues/65: pair selector (github.com/penumbra-zone/dex-explorer/issues/85)
    * feat: github.com/penumbra-zone/dex-explorer/issues/65: implement pair selector
    
    * feat: github.com/penumbra-zone/dex-explorer/issues/65: fill pair selector with account balances
    
    * feat: fill the `PairSelector` with the registry values
    
    * feat: github.com/penumbra-zone/dex-explorer/issues/65: use react-query to fetch assets and balances
    
    * refactor: github.com/penumbra-zone/dex-explorer/issues/65: use setters in the stores
    
    * fix: github.com/penumbra-zone/dex-explorer/issues/65: use react-query instead of mobx to keep the state of the fetchers
    
    * fix: github.com/penumbra-zone/dex-explorer/issues/65: apply latest changes
    
    * fix: github.com/penumbra-zone/dex-explorer/issues/65: apply PR comments and add better docs
    
    * fix: github.com/penumbra-zone/dex-explorer/issues/65: rename balances queryKey

---
+++ apps/dex-explorer/src/shared/state/assets.ts
@@ -0,0 +1,41 @@
+import { ViewService } from '@penumbra-zone/protobuf';
+import { getDenomMetadata } from '@penumbra-zone/getters/assets-response';
+import { penumbra } from '@/shared/penumbra';
+import { connectionStore } from '@/shared/state/connection';
+import { useQuery } from '@tanstack/react-query';
+import { useRegistry } from '@/fetchers/registry';
+
+/**
+ * Returns the `Metadata[]` based on the provider connection state.
+ * If connected, it fetches the assets from the `ViewService`'s `assets` method.
+ * Otherwise, it fetches the assets from the remote registry.
+ *
+ * Must be used within `observer` mobX HOC
+ **/
+export const useAssets = () => {
+  const { data: registry, error: registryErr } = useRegistry();
+
+  const registryAssets = useQuery({
+    queryKey: ['registry-assets'],
+    enabled: Boolean(registry),
+    queryFn: () => {
+      if (!registry) {
+        throw new Error('Registry not available');
+      }
+      return registry.getAllAssets();
+    },
+  });
+
+  const accountAssets = useQuery({
+    queryKey: ['view-service-assets'],
+    enabled: connectionStore.connected,
+    queryFn: async () => {
+      const responses = await Array.fromAsync(penumbra.service(ViewService).assets({}));
+      return responses.map(getDenomMetadata);
+    },
+  });
+
+  return connectionStore.connected
+    ? accountAssets
+    : { ...registryAssets, error: registryAssets.error ?? registryErr };
+};
