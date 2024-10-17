ebea174501cac85c7bc7ca2c29e4015302e262b6
Max Korsunov <maxkors2014@gmail.com>
2024-10-17 17:55:48 +0200

    feat: allow multiwallet support in v2 (#97)
    * feat: allow multiwallet support in v2
    
    * fix: lint
    
    * fix: formatter

--- apps/dex-explorer/src/shared/penumbra.ts
+++ apps/dex-explorer/src/shared/penumbra.ts
@@ -1,6 +1,3 @@
 import { createPenumbraClient } from '@penumbra-zone/client';
 
-const PRAX_ID = 'lkpmkhpnhknhmibgnmmhdhgdilepfghe';
-export const PRAX_ORIGIN = new URL(`chrome-extension://${PRAX_ID}`).origin;
-
 export const penumbra = createPenumbraClient();
