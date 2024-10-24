f9ae9917237227fa932b7aeaed10836f2144fec5
Gabe Rodriguez <grod220@gmail.com>
2024-10-24 12:19:09 +0200

    Use url route params for assets (#98)
    * Convert asset selector to route params
    
    * pass in route params
    
    * Review updates

--- apps/dex-explorer/src/components/PairSelector/index.tsx
+++ apps/dex-explorer/src/components/PairSelector/index.tsx
@@ -1,63 +1,106 @@
+'use client';
+
 import { observer } from 'mobx-react-lite';
 import { ArrowLeftRight } from 'lucide-react';
-import { AssetSelector, AssetSelectorValue } from '@penumbra-zone/ui/AssetSelector';
+import {
+  AssetSelector,
+  AssetSelectorValue,
+  isBalancesResponse,
+  isMetadata,
+} from '@penumbra-zone/ui/AssetSelector';
 import { Button } from '@penumbra-zone/ui/Button';
 import { useAssets } from '@/shared/state/assets';
 import { useBalances } from '@/shared/state/balances';
+import { useRouter } from 'next/navigation';
+import { PagePath } from '@/shared/pages.ts';
+import { getMetadataFromBalancesResponse } from '@penumbra-zone/getters/balances-response';
+import { usePathToMetadata } from '@/shared/usePagePath';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+
+const handleRouting = ({
+  router,
+  baseAsset,
+  quoteAsset,
+}: {
+  router: AppRouterInstance;
+  baseAsset: AssetSelectorValue | undefined;
+  quoteAsset: AssetSelectorValue | undefined;
+}) => {
+  if (!baseAsset || !quoteAsset) {
+    throw new Error('Url malformed');
+  }
+
+  let primarySymbol: string;
+  let numeraireSymbol: string;
+
+  // TODO: Create new getter in /web repo
+  if (isMetadata(baseAsset)) {
+    primarySymbol = baseAsset.symbol;
+  } else if (isBalancesResponse(baseAsset)) {
+    primarySymbol = getMetadataFromBalancesResponse(baseAsset).symbol;
+  } else {
+    throw new Error('unrecognized metadata for primary asset');
+  }
+
+  if (isMetadata(quoteAsset)) {
+    numeraireSymbol = quoteAsset.symbol;
+  } else if (isBalancesResponse(quoteAsset)) {
+    numeraireSymbol = getMetadataFromBalancesResponse(quoteAsset).symbol;
+  } else {
+    throw new Error('unrecognized metadata for numeraireSymbol asset');
+  }
+
+  router.push(`${PagePath.Trade}/${primarySymbol}/${numeraireSymbol}`);
+};
 
 export interface PairSelectorProps {
-  /** The `Metadata` or `BalancesResponse`, from which the swap should be initiated */
-  from?: AssetSelectorValue;
-  onFromChange?: (value?: AssetSelectorValue) => void;
-
-  /** The `Metadata` or `BalancesResponse`, to which the swap should be made */
-  to?: AssetSelectorValue;
-  onToChange?: (value?: AssetSelectorValue) => void;
-
   dialogTitle?: string;
   disabled?: boolean;
 }
 
-export const PairSelector = observer(
-  ({ from, onFromChange, to, onToChange, disabled, dialogTitle }: PairSelectorProps) => {
-    const { data: assets } = useAssets();
-    const { data: balances } = useBalances();
+export const PairSelector = observer(({ disabled, dialogTitle }: PairSelectorProps) => {
+  const router = useRouter();
+  const { data: assets } = useAssets();
+  const { data: balances } = useBalances();
+  const { baseAsset, quoteAsset, error, isLoading } = usePathToMetadata();
 
-    const onSwap = () => {
-      onFromChange?.(to);
-      onToChange?.(from);
-    };
+  if (error) {
+    return <div>Error loading pair selector: ${String(error)}</div>;
+  }
 
-    return (
-      <div className='flex gap-2'>
-        <AssetSelector
-          value={from}
-          assets={assets}
-          balances={balances}
-          disabled={disabled}
-          dialogTitle={dialogTitle}
-          onChange={onFromChange}
-        />
+  if (isLoading || !baseAsset || !quoteAsset) {
+    return <div>Loading...</div>;
+  }
 
-        <Button
-          priority='primary'
-          iconOnly
-          icon={ArrowLeftRight}
-          disabled={disabled}
-          onClick={onSwap}
-        >
-          Swap
-        </Button>
+  return (
+    <div className='flex gap-2'>
+      <AssetSelector
+        value={baseAsset}
+        assets={assets}
+        balances={balances}
+        disabled={disabled}
+        dialogTitle={dialogTitle}
+        onChange={val => handleRouting({ router, baseAsset: val, quoteAsset: quoteAsset })}
+      />
 
-        <AssetSelector
-          value={to}
-          assets={assets}
-          balances={balances}
-          disabled={disabled}
-          dialogTitle={dialogTitle}
-          onChange={onToChange}
-        />
-      </div>
-    );
-  },
-);
+      <Button
+        priority='primary'
+        iconOnly
+        icon={ArrowLeftRight}
+        disabled={disabled}
+        onClick={() => handleRouting({ router, baseAsset: quoteAsset, quoteAsset: baseAsset })}
+      >
+        Swap
+      </Button>
+
+      <AssetSelector
+        value={quoteAsset}
+        assets={assets}
+        balances={balances}
+        disabled={disabled}
+        dialogTitle={dialogTitle}
+        onChange={val => handleRouting({ router, baseAsset: baseAsset, quoteAsset: val })}
+      />
+    </div>
+  );
+});
--- apps/dex-explorer/src/shared/usePagePath.ts
+++ apps/dex-explorer/src/shared/usePagePath.ts
@@ -1,5 +1,9 @@
-import { usePathname } from 'next/navigation';
+'use client';
+
+import { useParams, usePathname } from 'next/navigation';
 import { PagePath } from './pages';
+import { useAssets } from '@/shared/state/assets.ts';
+import { useQuery } from '@tanstack/react-query';
 
 const removeTrailingSlash = (url: string): string => {
   return url.endsWith('/') ? url.slice(0, -1) : url;
@@ -28,4 +32,34 @@ const matchPagePath = (str: string): PagePath => {
 export const usePagePath = <T extends PagePath>() => {
   const pathname = usePathname();
   return matchPagePath(removeTrailingSlash(pathname ?? '')) as T;
+};
+
+interface PathParams {
+  baseSymbol: string;
+  quoteSymbol: string;
+  [key: string]: string; // required for useParams signature
+}
+
+// Converts symbol to Metadata
+export const usePathToMetadata = () => {
+  const { data, error, isLoading } = useAssets();
+  const params = useParams<PathParams>();
+
+  const query = useQuery({
+    queryKey: ['pathToMetadata', data, params],
+    queryFn: () => {
+      return {
+        baseAsset: data?.find(a => a.symbol === params?.baseSymbol),
+        quoteAsset: data?.find(a => a.symbol === params?.quoteSymbol),
+      };
+    },
+  });
+
+  return {
+    ...query,
+    baseAsset: query.data?.baseAsset,
+    quoteAsset: query.data?.quoteAsset,
+    isLoading: isLoading || query.isLoading,
+    error: error ?? query.error,
+  };
 };
