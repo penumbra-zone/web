d4569cef947b7c28cac742496c788cdba9f346dc
Gabe Rodriguez <grod220@gmail.com>
2024-10-24 12:19:09 +0200

    Use url route params for assets (github.com/penumbra-zone/dex-explorer/issues/98)
    * Convert asset selector to route params
    
    * pass in route params
    
    * Review updates

--- apps/dex-explorer/src/components/header/desktop-nav.tsx
+++ apps/dex-explorer/src/components/header/desktop-nav.tsx
@@ -2,17 +2,17 @@ import { useRouter } from 'next/navigation';
 import { Tabs } from '@penumbra-zone/ui/Tabs';
 import { Density } from '@penumbra-zone/ui/Density';
 import { HEADER_LINKS } from './links';
-import { usePagePath } from '@/shared/usePagePath';
+import { useBasePath } from '@/shared/pages.ts';
 
 export const DesktopNav = () => {
-  const pagePath = usePagePath();
+  const currentPath = useBasePath();
   const router = useRouter();
 
   return (
     <nav className='hidden rounded-full bg-v2-other-tonalFill5 px-4 py-1 backdrop-blur-xl lg:flex'>
       <Density compact>
         <Tabs
-          value={pagePath}
+          value={currentPath}
           onChange={value => router.push(value)}
           options={HEADER_LINKS}
           actionType='accent'
--- apps/dex-explorer/src/shared/pages.ts
+++ apps/dex-explorer/src/shared/pages.ts
@@ -1,7 +1,25 @@
+import { usePagePath } from '@/shared/usePagePath.ts';
+
 export enum PagePath {
   Home = '/v2',
   Explore = '/v2/explore',
   Trade = '/v2/trade',
   Inspect = '/v2/inspect',
   Portfolio = '/v2/portfolio',
+  TradePair = '/v2/trade/:primary/:numeraire',
 }
+
+const basePath: Partial<Record<PagePath, PagePath>> = {
+  [PagePath.TradePair]: PagePath.Trade,
+};
+
+// Used for dynamic routing when wanting to exclude the dynamic elements
+export const useBasePath = (): PagePath => {
+  const path = usePagePath();
+
+  const base = basePath[path];
+  if (base) {
+    return base;
+  }
+  return path;
+};
