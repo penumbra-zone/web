78fe9eaa3218f58e850d16eb2af86088e86a2c85
Gabe Rodriguez <grod220@gmail.com>
2024-11-01 16:23:54 -0700

    Summary data for pairs (github.com/penumbra-zone/dex-explorer/issues/123)
    * Summary api endpoint
    
    * working example
    
    * Delete old code
    
    * Narrow down types to only relevant tables

---
+++ apps/dex-explorer/src/shared/database/index.ts
@@ -0,0 +1,46 @@
+import pkg from 'pg';
+import fs from 'fs';
+import { Kysely, PostgresDialect } from 'kysely';
+import { DB } from '@/shared/database/schema.ts';
+import { AssetId } from '@penumbra-zone/protobuf/penumbra/core/asset/v1/asset_pb';
+const { Pool, types } = pkg;
+
+class Pindexer {
+  private db: Kysely<DB>;
+
+  constructor() {
+    const ca = process.env['PENUMBRA_INDEXER_CA_CERT'];
+    const connectionString = process.env['PENUMBRA_INDEXER_ENDPOINT'];
+    const dbConfig = {
+      connectionString: connectionString,
+      ...(ca && {
+        ssl: {
+          rejectUnauthorized: true,
+          ca: ca.startsWith('-----BEGIN CERTIFICATE-----') ? ca : fs.readFileSync(ca, 'utf-8'),
+        },
+      }),
+    };
+    const dialect = new PostgresDialect({
+      pool: new Pool(dbConfig),
+    });
+
+    this.db = new Kysely<DB>({ dialect });
+
+    const int8TypeId = 20;
+    // Map int8 to number.
+    types.setTypeParser(int8TypeId, val => {
+      return BigInt(val);
+    });
+  }
+
+  async summary(baseAsset: AssetId, quoteAsset: AssetId) {
+    return this.db
+      .selectFrom('dex_ex_summary')
+      .selectAll()
+      .where('asset_start', '=', Buffer.from(baseAsset.inner))
+      .where('asset_end', '=', Buffer.from(quoteAsset.inner))
+      .execute();
+  }
+}
+
+export const pindexer = new Pindexer();
