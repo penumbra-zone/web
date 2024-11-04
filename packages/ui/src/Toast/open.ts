9d543b1e79a76dcabb7d3800e31f3750d68d4892
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2024-11-04 17:12:25 +0400

    Add ReactNode prop type to ToastProps

--- packages/ui/src/Toast/open.ts
+++ packages/ui/src/Toast/open.ts
@@ -16,7 +16,7 @@ const toastFnMap: Record<ToastType, ToastFn> = {
 export interface ToastProps {
   type: ToastType;
   message: string;
-  description?: string;
+  description?: string | ReactNode;
   persistent?: boolean;
   dismissible?: boolean;
   action?: ExternalToast['action'];
