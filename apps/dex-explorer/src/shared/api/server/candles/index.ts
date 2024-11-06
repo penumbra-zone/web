944d4dc693a98025271b2ef461a1bb7125f36932
Gabe Rodriguez <grod220@gmail.com>
2024-11-06 09:52:54 -0800

    Candles v2 (time windows) (github.com/penumbra-zone/dex-explorer/issues/128)
    * Query Pindexer db for candles
    
    * Add symbol hook
    
    * working candles w/ buttons
    
    * Remove old candle api route
    
    * remove more
    
    * docs: update database reqs to pindexer
    
    * Review updates
    
    ---------
    
    Co-authored-by: Conor Schaefer <conor@penumbralabs.xyz>

---
+++ apps/dex-explorer/src/shared/api/server/candles/index.ts
@@ -0,0 +1,73 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { ChainRegistryClient } from '@penumbra-labs/registry';
+import { pindexer } from '@/shared/database';
+import { CandleApiResponse } from '@/shared/api/server/candles/types.ts';
+import { durationWindows, isDurationWindow } from '@/shared/database/schema.ts';
+import { dbCandleToOhlc, mergeCandles } from '@/shared/api/server/candles/utils.ts';
+
+export async function GET(req: NextRequest): Promise<NextResponse<CandleApiResponse>> {
+  const grpcEndpoint = process.env['PENUMBRA_GRPC_ENDPOINT'];
+  const chainId = process.env['PENUMBRA_CHAIN_ID'];
+  if (!grpcEndpoint || !chainId) {
+    return NextResponse.json(
+      { error: 'PENUMBRA_GRPC_ENDPOINT or PENUMBRA_CHAIN_ID is not set' },
+      { status: 500 },
+    );
+  }
+
+  const { searchParams } = new URL(req.url);
+  const baseAssetSymbol = searchParams.get('baseAsset');
+  const quoteAssetSymbol = searchParams.get('quoteAsset');
+  if (!baseAssetSymbol || !quoteAssetSymbol) {
+    return NextResponse.json(
+      { error: 'Missing required baseAsset or quoteAsset' },
+      { status: 400 },
+    );
+  }
+  const durationWindow = searchParams.get('durationWindow');
+  if (!durationWindow || !isDurationWindow(durationWindow)) {
+    return NextResponse.json(
+      { error: `durationWindow missing or invalid window. Options: ${durationWindows.join(', ')}` },
+      { status: 400 },
+    );
+  }
+
+  const registryClient = new ChainRegistryClient();
+  const registry = await registryClient.remote.get(chainId);
+
+  // TODO: Add getMetadataBySymbol() helper to registry npm package
+  const allAssets = registry.getAllAssets();
+  const baseAssetMetadata = allAssets.find(
+    a => a.symbol.toLowerCase() === baseAssetSymbol.toLowerCase(),
+  );
+  const quoteAssetMetadata = allAssets.find(
+    a => a.symbol.toLowerCase() === quoteAssetSymbol.toLowerCase(),
+  );
+  if (!baseAssetMetadata?.penumbraAssetId || !quoteAssetMetadata?.penumbraAssetId) {
+    return NextResponse.json(
+      { error: `Base asset or quoteAsset asset ids not found in registry` },
+      { status: 400 },
+    );
+  }
+
+  // Need to query both directions and aggregate results
+  const candlesFwd = await pindexer.candles(
+    baseAssetMetadata.penumbraAssetId,
+    quoteAssetMetadata.penumbraAssetId,
+    durationWindow,
+  );
+  const candlesReverse = await pindexer.candles(
+    quoteAssetMetadata.penumbraAssetId,
+    baseAssetMetadata.penumbraAssetId,
+    durationWindow,
+  );
+
+  const mergedCandles = mergeCandles(
+    { metadata: baseAssetMetadata, candles: candlesFwd },
+    { metadata: quoteAssetMetadata, candles: candlesReverse },
+  );
+
+  const response = mergedCandles.map(dbCandleToOhlc);
+
+  return NextResponse.json(response);
+}
