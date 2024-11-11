0012a6a4892548fed83bc601c9b2a4672d3b0cff
Max Korsunov <maxkors2014@gmail.com>
2024-11-11 10:09:32 +0400

    feat: apply Tailwind-based UI library (#129)
    * feat: apply Tailwind-based UI library
    
    * fix: use v13 of UI package
    
    * fix: format

--- app/app.tsx
+++ app/app.tsx
@@ -3,12 +3,10 @@
 import { ReactNode } from 'react';
 import { enableStaticRendering } from 'mobx-react-lite';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { PenumbraUIProvider } from '@penumbra-zone/ui/PenumbraUIProvider';
 import { ToastProvider } from '@penumbra-zone/ui/Toast';
 import { Display } from '@penumbra-zone/ui/Display';
 import { Header, SyncBar } from '@/widgets/header';
 import { queryClient } from '@/shared/const/queryClient';
-import { StyledComponentsRegistry } from './StyleRegistry';
 
 // Used so that observer() won't subscribe to any observables used in an SSR environment
 // and no garbage collection problems are introduced.
@@ -16,17 +14,13 @@ enableStaticRendering(typeof window === 'undefined');
 
 export const App = ({ children }: { children: ReactNode }) => {
   return (
-    <StyledComponentsRegistry>
-      <PenumbraUIProvider>
-        <QueryClientProvider client={queryClient}>
-          <Display>
-            <SyncBar />
-            <Header />
-            {children}
-          </Display>
-          <ToastProvider />
-        </QueryClientProvider>
-      </PenumbraUIProvider>
-    </StyledComponentsRegistry>
+    <QueryClientProvider client={queryClient}>
+      <Display>
+        <SyncBar />
+        <Header />
+        {children}
+      </Display>
+      <ToastProvider />
+    </QueryClientProvider>
   );
 };
