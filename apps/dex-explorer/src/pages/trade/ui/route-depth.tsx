964eda4541b2e4794b0bc0bb6c5c51f4076089f9
Max Korsunov <maxkors2014@gmail.com>
2024-11-20 11:08:03 +0400

    feat: penumbra-zone/dex-explorer/issues/131: grid layout (penumbra-zone/dex-explorer/issues/136)
    * feat: penumbra-zone/dex-explorer/issues/107: add styles to 24h stats
    
    * fix: penumbra-zone/dex-explorer/issues/107: improve pair selector styles
    
    * fix: styles
    
    * fix: format
    
    * fix: use `round` instead of `toFixed`
    
    * fix: add shimmering animation to skeleton
    
    * fix: negative percentage color
    
    * feat: implement mobile grid layout partly
    
    * feat: finish the mobile version of the trade page
    
    * feat: prepare the desktop layout
    
    * fix: fix the mobile chart size
    
    * fix: lint and format
    
    * fix: update ui package version
    
    * feat: apply grid to the order book
    
    * fix: apply grid for every possible screen resolution
    
    * fix: integrate order form into the layout
    
    * fix: global z-index

---
+++ apps/dex-explorer/src/pages/trade/ui/route-depth.tsx
@@ -0,0 +1,9 @@
+import { Skeleton } from '@/shared/ui/skeleton';
+
+export const RouteDepth = () => {
+  return (
+    <div className='w-full h-[320px] p-4'>
+      <Skeleton />
+    </div>
+  );
+};
