4d4849c7985126841934e9572b297109d47c5445
Max Korsunov <maxkors2014@gmail.com>
2024-12-02 11:01:00 +0400

    feat: penumbra-zone/dex-explorer/issues/111: trading pairs part 1 (penumbra-zone/dex-explorer/issues/170)
    * feat: display summary data of popular trading pairs
    
    * fix: penumbra-zone/dex-explorer/issues/111: refetching the query
    
    * feat: select only distinct rows from summary table

--- apps/dex-explorer/src/shared/api/server/summary/single.ts
+++ apps/dex-explorer/src/shared/api/server/summary/single.ts
@@ -2,10 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { pindexer } from '@/shared/database';
 import { ChainRegistryClient } from '@penumbra-labs/registry';
 import { durationWindows, isDurationWindow } from '@/shared/utils/duration.ts';
-import { ChangeData, SummaryDataResponse, SummaryResponse } from '@/shared/api/server/types.ts';
-import { round } from '@penumbra-zone/types/round';
-import { toValueView } from '@/shared/utils/value-view.ts';
-import { calculateDisplayPrice } from '@/shared/utils/price-conversion.ts';
+import { SummaryDataResponse, SummaryResponse } from '@/shared/api/server/summary/types.ts';
 
 export async function GET(req: NextRequest): Promise<NextResponse<SummaryResponse>> {
   const chainId = process.env['PENUMBRA_CHAIN_ID'];
@@ -60,40 +57,6 @@ export async function GET(req: NextRequest): Promise<NextResponse<SummaryRespons
     return NextResponse.json({ window: durationWindow, noData: true });
   }
 
-  const priceDiff = summary.price - summary.price_then;
-  const change = {
-    value: calculateDisplayPrice(priceDiff, baseAssetMetadata, quoteAssetMetadata),
-    sign: priceDiffLabel(priceDiff),
-    percent:
-      summary.price === 0
-        ? '0'
-        : round({
-            value: Math.abs(((summary.price - summary.price_then) / summary.price_then) * 100),
-            decimals: 2,
-          }),
-  };
-
-  const dataResponse = new SummaryDataResponse({
-    window: durationWindow,
-    directVolume: toValueView({
-      amount: summary.direct_volume_over_window,
-      metadata: quoteAssetMetadata,
-    }),
-    price: calculateDisplayPrice(summary.price, baseAssetMetadata, quoteAssetMetadata),
-    low: calculateDisplayPrice(summary.low, baseAssetMetadata, quoteAssetMetadata),
-    high: calculateDisplayPrice(summary.high, baseAssetMetadata, quoteAssetMetadata),
-    change,
-  });
-
+  const dataResponse = SummaryDataResponse.build(summary, baseAssetMetadata, quoteAssetMetadata);
   return NextResponse.json(dataResponse.toJson());
 }
-
-const priceDiffLabel = (num: number): ChangeData['sign'] => {
-  if (num === 0) {
-    return 'neutral';
-  }
-  if (num > 0) {
-    return 'positive';
-  }
-  return 'negative';
-};
