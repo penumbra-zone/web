a9af345e291e4fb430a92d257d3d1fbb7d2adb91
Gabe Rodriguez <grod220@gmail.com>
2024-12-05 08:27:23 +0100

    Chart rendering bug fix (#183)
    * Fix chart rendering bug
    
    * re-enable tests
    
    * Easier to click
    
    * renable tests
    
    * test fix

--- apps/dex-explorer/src/pages/trade/api/candles.tsx
+++ apps/dex-explorer/src/pages/trade/api/candles.tsx
@@ -2,44 +2,8 @@ import { useQuery } from '@tanstack/react-query';
 import { useRefetchOnNewBlock } from '@/shared/api/compact-block.ts';
 import { CandleApiResponse } from '@/shared/api/server/candles/types.ts';
 import { usePathSymbols } from '@/pages/trade/model/use-path.ts';
-import { OhlcData, UTCTimestamp } from 'lightweight-charts';
-import { addDurationWindow, DurationWindow } from '@/shared/utils/duration.ts';
-
-/** Insert empty candles so that every timestamp as one candle. */
-const insertEmptyCandles = (
-  window: DurationWindow,
-  data: OhlcData<UTCTimestamp>[],
-): OhlcData<UTCTimestamp>[] => {
-  const out: OhlcData<UTCTimestamp>[] = [];
-  let i = 0;
-  while (i < data.length) {
-    const candle = data[i];
-    if (!candle) {
-      break;
-    }
-    if (out.length > 0) {
-      const prev = out[out.length - 1];
-      if (!prev) {
-        throw new Error('the impossible happened');
-      }
-      const nextTime = (addDurationWindow(window, new Date(prev.time * 1000)).getTime() /
-        1000) as UTCTimestamp;
-      if (nextTime < candle.time) {
-        out.push({
-          time: nextTime,
-          open: prev.close,
-          close: prev.close,
-          low: prev.close,
-          high: prev.close,
-        });
-        continue;
-      }
-    }
-    out.push(candle);
-    i += 1;
-  }
-  return out;
-};
+import { OhlcData } from 'lightweight-charts';
+import { DurationWindow } from '@/shared/utils/duration.ts';
 
 export const useCandles = (durationWindow: DurationWindow) => {
   const { baseSymbol, quoteSymbol } = usePathSymbols();
@@ -59,7 +23,7 @@ export const useCandles = (durationWindow: DurationWindow) => {
       if ('error' in jsonRes) {
         throw new Error(jsonRes.error);
       }
-      return insertEmptyCandles(durationWindow, jsonRes);
+      return jsonRes;
     },
   });
 
