712e7b1bcf074b7c395d050c31d15f3682511492
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2024-12-05 17:27:31 +0400

    Add pnum (#1935)
    * Add pnum
    
    * Add changeset
    
    * Refactor + add test cases
    
    * Remove eslint comment in test file
    
    * Refactor 2 arg to be number or options
    
    * Rename 2nd arg for pnum
    
    * Rename 2nd arg in jsdoc as well

---
+++ packages/types/src/pnum.ts
@@ -0,0 +1,126 @@
+import { BigNumber } from 'bignumber.js';
+import { round } from '@penumbra-zone/types/round';
+import { LoHi, joinLoHi, splitLoHi } from '@penumbra-zone/types/lo-hi';
+import { Amount } from '@penumbra-zone/protobuf/penumbra/core/num/v1/num_pb';
+import { ValueView } from '@penumbra-zone/protobuf/penumbra/core/asset/v1/asset_pb';
+import { getAmount, getDisplayDenomExponentFromValueView } from '@penumbra-zone/getters/value-view';
+import { removeTrailingZeros } from '@penumbra-zone/types/shortify';
+
+/**
+ * pnum (penumbra number)
+ *
+ * In Penumbra a number can be in the form of a base unit (bigint, LoHi, Amount, ValueView)
+ * or a number with decimals for display purposes (string, number)
+ *
+ * This function handles all these cases automatically internally
+ * - when input is a bigint, LoHi, Amount, or ValueView, it is assumed to be in base units
+ * - when input is a string or number, it is multiplied by 10^exponent and converted to base units
+ *
+ * Likewise for all methods, the outputs are
+ * - in base units for bigint, LoHi, Amount and ValueView
+ * - in display form with decimals for string and number
+ *
+ * @param input
+ * @param optionsOrExponent
+ */
+function pnum(
+  input?: string | number | LoHi | bigint | Amount | ValueView | undefined,
+  optionsOrExponent: { exponent?: number } | number = { exponent: 0 },
+) {
+  let value: BigNumber;
+  let exponent =
+    typeof optionsOrExponent === 'number' ? optionsOrExponent : (optionsOrExponent.exponent ?? 0);
+
+  if (typeof input === 'string' || typeof input === 'number') {
+    value = new BigNumber(input).shiftedBy(exponent);
+  } else if (typeof input === 'bigint') {
+    value = new BigNumber(input.toString());
+  } else if (input instanceof ValueView) {
+    const amount = getAmount(input);
+    value = new BigNumber(joinLoHi(amount.lo, amount.hi).toString());
+    exponent =
+      input.valueView.case === 'knownAssetId' ? getDisplayDenomExponentFromValueView(input) : 0;
+  } else if (
+    input instanceof Amount ||
+    (typeof input === 'object' &&
+      'lo' in input &&
+      'hi' in input &&
+      typeof input.lo === 'bigint' &&
+      typeof input.hi === 'bigint')
+  ) {
+    value = new BigNumber(joinLoHi(input.lo, input.hi).toString());
+  } else {
+    value = new BigNumber(0);
+  }
+
+  return {
+    toNumber(): number {
+      const number = value.shiftedBy(-exponent).toNumber();
+      if (!Number.isFinite(number)) {
+        throw new Error('Number exceeds JavaScript numeric limits, convert to other type instead.');
+      }
+      return number;
+    },
+
+    toRoundedNumber(decimals = exponent): number {
+      const number = value.shiftedBy(-exponent).toNumber();
+      if (!Number.isFinite(number)) {
+        throw new Error('Number exceeds JavaScript numeric limits, convert to other type instead.');
+      }
+      return Number(round({ value: number, decimals }));
+    },
+
+    toString(): string {
+      return value.shiftedBy(-exponent).toString();
+    },
+
+    toRoundedString(decimals = exponent): string {
+      return round({ value: value.shiftedBy(-exponent).toString(), decimals, trailingZeros: true });
+    },
+
+    toFormattedString(
+      options: {
+        commas?: boolean;
+        decimals?: number;
+        trailingZeros?: boolean;
+      } = {},
+    ): string {
+      const defaultOptions = {
+        commas: true,
+        decimals: exponent,
+        trailingZeros: true,
+      };
+
+      const { commas, decimals, trailingZeros } = {
+        ...defaultOptions,
+        ...options,
+      };
+
+      const number = value.shiftedBy(-exponent).toFormat(decimals, {
+        decimalSeparator: '.',
+        groupSeparator: commas ? ',' : '',
+        groupSize: 3,
+      });
+
+      return trailingZeros ? number : removeTrailingZeros(number);
+    },
+
+    toBigInt(): bigint {
+      return BigInt(value.toFixed(0));
+    },
+
+    toBigNumber(): BigNumber {
+      return value.shiftedBy(-exponent);
+    },
+
+    toLoHi(): LoHi {
+      return splitLoHi(BigInt(value.toFixed(0)));
+    },
+
+    toAmount(): Amount {
+      return new Amount(splitLoHi(BigInt(value.toFixed(0))));
+    },
+  };
+}
+
+export { pnum };
