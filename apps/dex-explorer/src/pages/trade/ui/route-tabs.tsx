fa5086a9f958daf70604bc5d33cf514e87d539da
Josef <vacekj@outlook.com>
2024-12-10 12:53:17 +0100

    fix: routebook visual updates (#187)
    * fix: padding in route book
    
    * refactor: update skeleton loading rows and format price/number functions in trade components
    
    - Replaced static loading placeholders with a reusable Skeleton component for better consistency in the RouteBook.
    - Adjusted table header padding for improved alignment.
    - Introduced formatPrice and formatNumber functions to standardize price and number formatting across TradeRow and RouteBook components.
    - Updated TradeRow to utilize new formatting functions for displaying prices, amounts, and totals.
    
    * feat: almost there
    
    * fix: no more tables, grid is king
    
    * fix: lints
    
    * fix: lints

--- apps/dex-explorer/src/pages/trade/ui/route-tabs.tsx
+++ apps/dex-explorer/src/pages/trade/ui/route-tabs.tsx
@@ -15,7 +15,7 @@ export const RouteTabs = () => {
   const [tab, setTab] = useState<RouteTabsType>(RouteTabsType.Book);
 
   return (
-    <div ref={parent} className='flex flex-col gap-2 h-full overflow-hidden'>
+    <div ref={parent} className='flex flex-col h-full'>
       <div className='flex justify-between gap-2 px-4 lg:pt-2 border-b border-b-other-solidStroke'>
         <Density medium>
           <Tabs
