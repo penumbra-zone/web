46214be3e1c2228f91030d60993e8d4555438c22
Tal Derei <70081547+TalDerei@users.noreply.github.com>
2024-12-11 12:02:06 -0800

    ui: display relevant accounts (#202)
    * update npm deps
    
    * view service balance rpc
    
    * simplify account index logic
    
    * balances query key and loading spinner
    
    * ui density updates
    
    * other density updates
    
    * align loading state with button width

--- src/widgets/header/api/subaccounts.ts
+++ src/widgets/header/api/subaccounts.ts
@@ -6,14 +6,29 @@ import {
 import { useQuery } from '@tanstack/react-query';
 import { ViewService } from '@penumbra-zone/protobuf';
 import { penumbra } from '@/shared/const/penumbra';
+import { useBalances } from '@/shared/api/balances';
+import { BalancesResponse } from '@penumbra-zone/protobuf/penumbra/view/v1/view_pb';
 
-const ACCOUNT_INDEXES = [0, 1, 2, 3, 4, 5];
-
-const fetchQuery = async (): Promise<AddressView[]> => {
+const fetchQuery = async (balances: BalancesResponse[]): Promise<AddressView[]> => {
   const service = penumbra.service(ViewService);
 
+  // Include main account for fresh wallets to display address view
+  let accountIndexes: number[] = [0];
+
+  for (const balance of balances) {
+    if (
+      balance.accountAddress?.addressView.case === 'decoded' &&
+      balance.accountAddress.addressView.value.index?.account !== undefined
+    ) {
+      accountIndexes.push(balance.accountAddress.addressView.value.index.account);
+    }
+  }
+
+  // Filter by unique account indices
+  accountIndexes = accountIndexes.filter((value, index, self) => self.indexOf(value) === index);
+
   return Promise.all(
-    ACCOUNT_INDEXES.map(async index => {
+    accountIndexes.map(async index => {
       const response = await service.addressByIndex({ addressIndex: { account: index } });
 
       return new AddressView({
@@ -30,8 +45,27 @@ const fetchQuery = async (): Promise<AddressView[]> => {
 };
 
 export const useSubaccounts = () => {
-  return useQuery({
-    queryKey: ['view-service-accounts'],
-    queryFn: fetchQuery,
+  // Query account balances from view service
+  const { data: balances, isLoading: balanceLoading } = useBalances();
+
+  const query = useQuery({
+    // 'balances' cache query to enable refecthing balances
+    queryKey: ['view-service-accounts', balances],
+    queryFn: () => {
+      if (!balances) {
+        return [];
+      }
+      return fetchQuery(balances);
+    },
+    enabled: !balanceLoading,
   });
+
+  // Combines loading states from balances and subaccounts to prevent
+  // flickering during balance refetches
+  const isLoading = balanceLoading || query.isLoading;
+
+  return {
+    ...query,
+    isLoading,
+  };
 };
