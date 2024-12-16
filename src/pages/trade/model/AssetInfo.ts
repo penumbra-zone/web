699f741b8a84ae16b19465c5814bc388986c9c80
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2024-12-16 20:23:02 +0400

    Limit Orders (#181)
    * Add limit order form
    
    * Construct limit order position
    
    * Refactor order form stores & introduce pnum util
    
    * Update pnum description
    
    * Use remote pnum
    
    * Fix lint issues
    
    * Fix handling of input fields numbers
    
    * tmp commit
    
    * Convert to base units
    
    * Start fleshing out shared position logic
    
    * Add tests for position math
    
    * Integrate position logic into range liquidity form
    
    * Define limit order position function
    
    * Add some documentation to position math
    
    * Wire up limit position form
    
    * Remove slider from limit order form
    
    * Remove key error
    
    * Apply same p,q,r1,r2 values as rust code
    
    * Construct positions taking into account asset order
    
    * Have position plans take in display units for reserves
    
    * Fix limit order amount handling + gas fee calc
    
    * Fix display amounts for range liquidity form
    
    * Bug: don't display weird text in order input
    
    * Define PriceLinkedInputs store
    
    This store will be very useful for market and limit orders, and already addresses
    some yet unreported sources of UX oddities, like the inputs not updating with the price.
    
    * Define new store for MarketOrderForm
    
    * Move AssetInfo to separate file
    
    * Add store for limit order position
    
    * Add RangeOrderFormStore
    
    * Basic wiring of form refactor
    
    * Implement order submission
    
    * Fix range liquidity fee tier selection
    
    * Correct range liquidity
    
    * Gas fee calculation, remove unused stuff
    
    * Fix remaining lints
    
    * Correct order of p and q in position construction
    
    * Use a more robust method of getting p and q in range
    
    This should handle cases where they're out of range by losing only the necessary precision, and it should handle cases where the price gets flattened to 0
    
    * Remove wheel input changes
    
    * Fix market slider input change
    
    * Refactor setter methods for mobx strict mode, fix rerenders
    
    * Fix hydration issue caused by connectionStore.connected
    
    * Fix inputs disappearing
    
    * Add active states to select groups
    
    * Remove setup connection hoc
    
    * Wrapp app with observer
    
    ---------
    
    Co-authored-by: Lucas Meier <lucas@cronokirby.com>

---
+++ src/pages/trade/model/AssetInfo.ts
@@ -0,0 +1,57 @@
+import { AssetId, Metadata, Value } from '@penumbra-zone/protobuf/penumbra/core/asset/v1/asset_pb';
+import { Amount } from '@penumbra-zone/protobuf/penumbra/core/num/v1/num_pb';
+import { pnum } from '@penumbra-zone/types/pnum';
+
+/** A basic utility class containing information we need about an asset.
+ *
+ * This extracts out the useful components we might need for the current
+ * asset.
+ */
+export class AssetInfo {
+  /**
+   * @param balance the balance, in display units.
+   * @param the exponent to convert from base units to display units.
+   */
+  constructor(
+    public id: AssetId,
+    public exponent: number,
+    public symbol: string,
+    public balance?: number,
+  ) {}
+
+  static fromMetadata(metadata: Metadata, balance?: Amount): undefined | AssetInfo {
+    const displayDenom = metadata.denomUnits.find(x => x.denom === metadata.display);
+    if (!displayDenom || !metadata.penumbraAssetId) {
+      return undefined;
+    }
+    return new AssetInfo(
+      metadata.penumbraAssetId,
+      displayDenom.exponent,
+      metadata.symbol,
+      balance && pnum(balance, displayDenom.exponent).toNumber(),
+    );
+  }
+
+  /** Convert an amount, in display units, into a Value (of this asset). */
+  value(display: number): Value {
+    return new Value({
+      amount: pnum(display, this.exponent).toAmount(),
+      assetId: this.id,
+    });
+  }
+
+  /** Format an amount (in display units) as a simple string. */
+  formatDisplayAmount(amount: number): string {
+    const amountString = pnum(amount, this.exponent).toFormattedString({
+      commas: true,
+      decimals: 4,
+      trailingZeros: false,
+    });
+    return `${amountString} ${this.symbol}`;
+  }
+
+  /** Format the balance of this asset as a simple string. */
+  formatBalance(): undefined | string {
+    return this.balance !== undefined ? this.formatDisplayAmount(this.balance) : undefined;
+  }
+}
--- src/shared/api/balances.ts
+++ src/shared/api/balances.ts
@@ -3,19 +3,20 @@ import { BalancesResponse } from '@penumbra-zone/protobuf/penumbra/view/v1/view_
 import { penumbra } from '@/shared/const/penumbra';
 import { connectionStore } from '@/shared/model/connection';
 import { useQuery } from '@tanstack/react-query';
+import { AddressIndex } from '@penumbra-zone/protobuf/penumbra/core/keys/v1/keys_pb';
 
-const fetchQuery = async (): Promise<BalancesResponse[]> => {
-  return Array.fromAsync(penumbra.service(ViewService).balances({}));
+const fetchQuery = (accountFilter?: AddressIndex) => async (): Promise<BalancesResponse[]> => {
+  return Array.fromAsync(penumbra.service(ViewService).balances({ accountFilter }));
 };
 
 /**
  * Fetches the `BalancesResponse[]` based on the provider connection state.
  * Must be used within the `observer` mobX HOC
  */
-export const useBalances = () => {
+export const useBalances = (accountFilter?: AddressIndex) => {
   return useQuery({
     queryKey: ['view-service-balances'],
-    queryFn: fetchQuery,
+    queryFn: fetchQuery(accountFilter),
     enabled: connectionStore.connected,
   });
 };
