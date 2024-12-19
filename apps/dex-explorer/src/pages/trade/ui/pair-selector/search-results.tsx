61f19d9187b7e9d64c8b6f075536fabf50792aca
Max Korsunov <maxkors2014@gmail.com>
2024-12-19 16:35:59 +0400

    feat: github.com/penumbra-zone/dex-explorer/issues/163: pair selector (github.com/penumbra-zone/dex-explorer/issues/204)
    * fix: apply new UI changes of density
    
    * feat: set up new PairSelector
    
    * feat: add star-pair feature
    
    * feat: implement basic pair selection
    
    * refactor: create more components related to pair selector
    
    * feat: implement filtering input
    
    * feat: implement `/api/pairs` endpoint to fetch the most popular pairs
    
    * feat: implement recent assets in search results
    
    * feat: use balances in the search results
    
    * fix: small bugs
    
    * fix: merge
    
    * fix: ts
    
    * fix: handle the pr feedback

---
+++ apps/dex-explorer/src/pages/trade/ui/pair-selector/search-results.tsx
@@ -0,0 +1,196 @@
+import { observer } from 'mobx-react-lite';
+import { Search } from 'lucide-react';
+import { Metadata } from '@penumbra-zone/protobuf/penumbra/core/asset/v1/asset_pb';
+import { BalancesResponse } from '@penumbra-zone/protobuf/penumbra/view/v1/view_pb';
+import {
+  getAddressIndex,
+  getBalanceView,
+  getMetadataFromBalancesResponse,
+} from '@penumbra-zone/getters/balances-response';
+import { Text } from '@penumbra-zone/ui/Text';
+import { Dialog } from '@penumbra-zone/ui/Dialog';
+import { AssetIcon } from '@penumbra-zone/ui/AssetIcon';
+import {
+  groupAndSortBalances,
+  AssetSelectorValue,
+  isBalancesResponse,
+} from '@penumbra-zone/ui/AssetSelector';
+import { Button } from '@penumbra-zone/ui/Button';
+import { useAssets } from '@/shared/api/assets';
+import { useBalances } from '@/shared/api/balances';
+import { connectionStore } from '@/shared/model/connection';
+import { ValueViewComponent } from '@penumbra-zone/ui/ValueView';
+import { recentPairsStore } from './store';
+
+export interface SearchResultsProps {
+  onSelect: (asset: Metadata) => void;
+  onClear: VoidFunction;
+  search?: string;
+  showConfirm: boolean;
+  onConfirm: VoidFunction;
+}
+
+const filterAsset = (asset: Metadata, search: string): boolean => {
+  return (
+    asset.symbol.toLowerCase().includes(search.toLowerCase()) ||
+    asset.description.toLowerCase().includes(search.toLowerCase())
+  );
+};
+
+const useFilteredAssets = (options: AssetSelectorValue[], search: string) => {
+  return options.filter(option => {
+    if (isBalancesResponse(option)) {
+      const metadata = getMetadataFromBalancesResponse(option);
+      return filterAsset(metadata, search);
+    }
+    return filterAsset(option, search);
+  });
+};
+
+const mergeOptions = (
+  assets: Metadata[],
+  balances: BalancesResponse[],
+  account: number,
+): AssetSelectorValue[] => {
+  const grouped = groupAndSortBalances(balances);
+  const balancesPerAccount = grouped.find(group => group[0] === account.toString())?.[1] ?? [];
+  const filteredAssets = assets
+    .filter(
+      asset =>
+        !balancesPerAccount.some(
+          balance => getMetadataFromBalancesResponse(balance).symbol === asset.symbol,
+        ),
+    )
+    .sort((a, b) => Number(b.priorityScore) - Number(a.priorityScore));
+  return [...balancesPerAccount, ...filteredAssets];
+};
+
+export const SearchResults = observer(
+  ({ onSelect, onClear, search, showConfirm, onConfirm }: SearchResultsProps) => {
+    const { recent, add } = recentPairsStore;
+    const { subaccount } = connectionStore;
+
+    const { data: assets } = useAssets();
+    const { data: balances } = useBalances();
+
+    const merged = mergeOptions(assets ?? [], balances ?? [], subaccount);
+    const filtered = useFilteredAssets(merged, search ?? '');
+
+    const onClick = (asset: Metadata) => {
+      add(asset);
+      onSelect(asset);
+    };
+
+    if (!filtered.length) {
+      return (
+        <div className='grow flex flex-col items-center justify-center gap-2 py-4 text-text-secondary'>
+          <Search className='size-8' />
+          <Text small>No results</Text>
+        </div>
+      );
+    }
+
+    return (
+      <>
+        {!search && !!recent.length && (
+          <div className='flex flex-col gap-2 text-text-secondary'>
+            <Text small>Recent</Text>
+            <Dialog.RadioGroup>
+              <div className='flex flex-col gap-1'>
+                {recent.map(asset => (
+                  <Dialog.RadioItem
+                    key={asset.symbol}
+                    value={asset.symbol}
+                    startAdornment={<AssetIcon metadata={asset} size='lg' />}
+                    title={
+                      <div className={asset.name ? '' : 'h-10 flex items-center'}>
+                        <Text color='text.primary'>{asset.symbol}</Text>
+                      </div>
+                    }
+                    description={
+                      asset.name && (
+                        <div className='-mt-2'>
+                          <Text detail color='text.secondary'>
+                            {asset.name}
+                          </Text>
+                        </div>
+                      )
+                    }
+                    onSelect={() => onClick(asset)}
+                  />
+                ))}
+              </div>
+            </Dialog.RadioGroup>
+          </div>
+        )}
+
+        <div className='flex flex-col gap-2 text-text-secondary'>
+          <Text small>Search results</Text>
+          <Dialog.RadioGroup>
+            <div className='flex flex-col gap-1'>
+              {filtered.map(option => {
+                const asset = isBalancesResponse(option)
+                  ? getMetadataFromBalancesResponse(option)
+                  : option;
+                const balance = isBalancesResponse(option)
+                  ? {
+                      addressIndexAccount: getAddressIndex.optional(option)?.account,
+                      valueView: getBalanceView.optional(option),
+                    }
+                  : undefined;
+
+                return (
+                  <Dialog.RadioItem
+                    key={asset.symbol}
+                    value={asset.symbol}
+                    startAdornment={<AssetIcon metadata={asset} size='lg' />}
+                    endAdornment={
+                      balance && (
+                        <div className='[&_img]:hidden'>
+                          <ValueViewComponent
+                            showSymbol={false}
+                            context='table'
+                            valueView={balance.valueView}
+                          />
+                        </div>
+                      )
+                    }
+                    title={
+                      <div className={asset.name ? '' : 'h-10 flex items-center'}>
+                        <Text color='text.primary'>{asset.symbol}</Text>
+                      </div>
+                    }
+                    description={
+                      asset.name && (
+                        <div className='-mt-2'>
+                          <Text detail color='text.secondary'>
+                            {asset.name}
+                          </Text>
+                        </div>
+                      )
+                    }
+                    onSelect={() => onClick(asset)}
+                  />
+                );
+              })}
+            </div>
+          </Dialog.RadioGroup>
+        </div>
+
+        <div className='flex flex-col gap-4 sticky bottom-0 w-full rounded-sm z-10'>
+          {showConfirm && (
+            <Button onClick={onConfirm} priority='primary' actionType='accent'>
+              Confirm
+            </Button>
+          )}
+
+          <div className='bg-neutral-dark'>
+            <Button onClick={onClear} priority='primary'>
+              Clear
+            </Button>
+          </div>
+        </div>
+      </>
+    );
+  },
+);
