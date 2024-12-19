8aad7dae079116735eeeccdb31eafed81aea0b1a
Max Korsunov <maxkors2014@gmail.com>
2024-12-19 16:35:59 +0400

    feat: #163: pair selector (#204)
    * fix: apply new UI changes of density
    
    * feat: set up new PairSelector
    
    * feat: add star-pair feature
    
    * feat: implement basic pair selection
    
    * refactor: create more components related to pair selector
    
    * feat: implement filtering input
    
    * feat: implement `/api/pairs` endpoint to fetch the most popular pairs
    
    * feat: implement recent assets in search results
    
    * feat: use balances in the search results
    
    * fix: small bugs
    
    * fix: merge
    
    * fix: ts
    
    * fix: handle the pr feedback

--- global.d.ts
+++ global.d.ts
@@ -2,3 +2,9 @@ declare module '*.css' {
   const content: Record<string, string>;
   export default content;
 }
+
+declare module '*.svg' {
+  import { FC, SVGAttributes } from 'react';
+  const value: FC<SVGAttributes<SVGElement>>;
+  export default value;
+}
---
+++ src/shared/api/server/summary/pairs.ts
@@ -0,0 +1,81 @@
+import { NextResponse } from 'next/server';
+import { pindexer } from '@/shared/database';
+import { serialize, Serialized } from '@/shared/utils/serializer';
+import {
+  AssetId,
+  Metadata,
+  ValueView,
+} from '@penumbra-zone/protobuf/penumbra/core/asset/v1/asset_pb';
+import { ChainRegistryClient } from '@penumbra-labs/registry';
+import { toValueView } from '@/shared/utils/value-view';
+import { getDisplayDenomExponent } from '@penumbra-zone/getters/metadata';
+
+const getAssetById = (allAssets: Metadata[], id: Buffer): Metadata | undefined => {
+  return allAssets.find(asset => {
+    return asset.penumbraAssetId?.equals(new AssetId({ inner: id }));
+  });
+};
+
+export interface PairData {
+  baseAsset: Metadata;
+  quoteAsset: Metadata;
+  volume: ValueView;
+}
+
+export type PairsResponse = Serialized<PairData>[] | { error: string };
+
+export async function GET(): Promise<NextResponse<PairsResponse>> {
+  const chainId = process.env['PENUMBRA_CHAIN_ID'];
+  if (!chainId) {
+    return NextResponse.json({ error: 'PENUMBRA_CHAIN_ID is not set' }, { status: 500 });
+  }
+
+  const registryClient = new ChainRegistryClient();
+  const registry = await registryClient.remote.get(chainId);
+  const allAssets = registry.getAllAssets();
+
+  const stablecoins = allAssets.filter(asset => ['USDT', 'USDC', 'USDY'].includes(asset.symbol));
+  const usdc = stablecoins.find(asset => asset.symbol === 'USDC');
+
+  const results = await pindexer.pairs({
+    // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style -- usdc is defined
+    usdc: usdc?.penumbraAssetId as AssetId,
+    stablecoins: stablecoins.map(asset => asset.penumbraAssetId) as AssetId[],
+  });
+
+  const pairs = (await Promise.all(
+    results.map(summary => {
+      const baseAsset = getAssetById(allAssets, summary.asset_start);
+      const quoteAsset = getAssetById(allAssets, summary.asset_end);
+      if (!baseAsset || !quoteAsset) {
+        return undefined;
+      }
+
+      let volume = toValueView({
+        amount: summary.liquidity,
+        metadata: quoteAsset,
+      });
+
+      if (summary.usdc_price) {
+        // Custom volume calculation for USDC pairs
+        // TODO: change to a better method (probably create `pnum.multiply()` method and use it)
+        const expDiff = Math.abs(
+          getDisplayDenomExponent(quoteAsset) - getDisplayDenomExponent(usdc),
+        );
+        const result = summary.liquidity * summary.usdc_price * 10 ** expDiff;
+        volume = toValueView({
+          amount: Math.floor(result),
+          metadata: quoteAsset,
+        });
+      }
+
+      return serialize({
+        baseAsset,
+        quoteAsset,
+        volume,
+      });
+    }),
+  )) as Serialized<PairData>[];
+
+  return NextResponse.json(pairs.filter(Boolean));
+}
