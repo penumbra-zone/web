bfd7ccaf3a08f09da63d318d7229c81c20478963
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2025-01-08 18:05:52 +0400

    Positions pricing (github.com/penumbra-zone/dex-explorer/issues/226)
    * Remove position mutation from api
    
    * Update position model
    
    * Convert to an actual Table
    
    * Add current values
    
    * Add DisplayPosition and DisplayAsset types
    
    * Display multiple orders
    
    * Remove unused code
    
    * Change var name
    
    * Update packages
    
    * Fix lint issues
    
    * Add nowrap to fee tier heading
    
    * Update ui package & fix double import

--- apps/dex-explorer/src/pages/trade/api/positions.ts
+++ apps/dex-explorer/src/pages/trade/api/positions.ts
@@ -7,109 +7,11 @@ import {
   PositionId,
   PositionState_PositionStateEnum,
 } from '@penumbra-zone/protobuf/penumbra/core/component/dex/v1/dex_pb';
-import { AssetId, ValueView } from '@penumbra-zone/protobuf/penumbra/core/asset/v1/asset_pb';
-import { Amount } from '@penumbra-zone/protobuf/penumbra/core/num/v1/num_pb';
-import { registryQueryFn } from '@/shared/api/registry.ts';
-import { isZero } from '@penumbra-zone/types/amount';
-
-export interface Order {
-  side: 'Buy' | 'Sell';
-  tradeAmount: ValueView;
-  effectivePrice: ValueView;
-}
-
-export interface PositionData {
-  positionId: PositionId;
-  positionState: PositionState_PositionStateEnum;
-  fee: number;
-  orders: Order[];
-}
-
-const assetIdToValueView = async (assetId?: AssetId, amount?: Amount) => {
-  if (!assetId) {
-    throw new Error('No asset id found to convert to ValueView');
-  }
-
-  const registry = await registryQueryFn();
-  const metadata = registry.tryGetMetadata(assetId);
-  if (metadata) {
-    return new ValueView({
-      valueView: { case: 'knownAssetId', value: { amount, metadata } },
-    });
-  } else {
-    return new ValueView({
-      valueView: { case: 'unknownAssetId', value: { amount, assetId } },
-    });
-  }
-};
-
-const getOrders = async (position: Position): Promise<Order[]> => {
-  const asset1Id = position.phi?.pair?.asset1;
-  const asset1Amount = position.reserves?.r1;
-
-  const asset2Id = position.phi?.pair?.asset2;
-  const asset2Amount = position.reserves?.r2;
-
-  const asset1IsPresent = asset1Amount && !isZero(asset1Amount);
-  const asset2IsPresent = asset2Amount && !isZero(asset2Amount);
-
-  // TODO: Properly calculate effectivePrice (need to subtract fee)
-
-  // Sell order: Offering to sell asset1
-  if (asset1IsPresent && !asset2IsPresent) {
-    return [
-      {
-        side: 'Sell',
-        tradeAmount: await assetIdToValueView(asset1Id, asset1Amount),
-        effectivePrice: await assetIdToValueView(asset2Id, asset2Amount),
-      },
-    ];
-  }
-
-  // Buy order: Offering to buy asset1
-  if (!asset1IsPresent && asset2IsPresent) {
-    return [
-      {
-        side: 'Buy',
-        tradeAmount: await assetIdToValueView(asset1Id, asset1Amount),
-        effectivePrice: await assetIdToValueView(asset2Id, asset2Amount),
-      },
-    ];
-  }
-
-  // Mixed order: offering to sell both assets
-  if (asset1IsPresent && asset2IsPresent) {
-    return [
-      {
-        side: 'Sell',
-        tradeAmount: await assetIdToValueView(asset1Id, asset1Amount),
-        effectivePrice: await assetIdToValueView(asset2Id, asset2Amount),
-      },
-      {
-        side: 'Sell',
-        tradeAmount: await assetIdToValueView(asset2Id, asset2Amount),
-        effectivePrice: await assetIdToValueView(asset1Id, asset1Amount),
-      },
-    ];
-  }
-
-  // If no valid orders are found, return an empty array
-  return [];
-};
-
-export const getPositionData = async (id: PositionId, position: Position) => {
-  return {
-    positionId: id,
-    positionState: position.state?.state ?? PositionState_PositionStateEnum.UNSPECIFIED,
-    fee: position.phi?.component?.fee ?? 0,
-    orders: await getOrders(position),
-  };
-};
 
 // 1) Query prax to get position ids
 // 2) Take those position ids and get position info from the node
 // Context on two-step fetching process: https://github.com/penumbra-zone/penumbra/pull/4837
-const fetchQuery = async (): Promise<PositionData[]> => {
+const fetchQuery = async (): Promise<Map<PositionId, Position>> => {
   const ownedRes = await Array.fromAsync(penumbra.service(ViewService).ownedPositionIds({}));
   const positionIds = ownedRes.map(r => r.positionId).filter(Boolean) as PositionId[];
 
@@ -121,19 +23,16 @@ const fetchQuery = async (): Promise<PositionData[]> => {
   }
   const positions = positionsRes.map(r => r.data).filter(Boolean) as Position[];
 
-  const positionData: PositionData[] = [];
-  // The responses are in the same order as the requests. Hence, the index matching.
-  for (let i = 0; i < positions.length; i++) {
-    const id = positionIds[i];
-    const position = positions[i];
-    if (!id || !position) {
-      throw new Error(`No corresponding position or id for index ${i}`);
+  const positionsById = new Map<PositionId, Position>();
+  positions.forEach((position, index) => {
+    // The responses are in the same order as the requests. Hence, the index matching.
+    const positionId = positionIds[index];
+    if (positionId) {
+      positionsById.set(positionId, position);
     }
-    const data = await getPositionData(id, position);
-    positionData.push(data);
-  }
+  });
 
-  return positionData;
+  return positionsById;
 };
 
 /**
