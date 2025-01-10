dc3564d5431e73bee6cb38ee3763b874315102e6
Josef <vacekj@outlook.com>
2025-01-10 21:38:05 +0100

    feat: implement error state designs (#228)
    * Quick commit 2025-01-07 12:49:57
    
    * feat: routebook error state, fix chart height
    
    * feat: positions error state
    
    * feat: correct stacking
    
    * Quick commit 2025-01-09 14:29:32
    
    * fix: make error msgs consistent in routebook and positions.
    
    * feat: add eslint cache to gitignore

--- src/shared/ui/blockchain-error.tsx
+++ src/shared/ui/blockchain-error.tsx
@@ -2,25 +2,39 @@ import { Ban } from 'lucide-react';
 import { Button } from '@penumbra-zone/ui/Button';
 import { Density } from '@penumbra-zone/ui/Density';
 import { Text } from '@penumbra-zone/ui/Text';
+import { useState } from 'react';
+import { ErrorDetailsModal } from './error-details-modal';
+import cn from 'clsx';
 
 interface BlockchainErrorProps {
   message?: string;
-  onDetailsClick?: () => void;
+  direction?: 'row' | 'column';
 }
 
 export function BlockchainError({
   message = 'An error occurred when loading data from the blockchain',
-  onDetailsClick,
+  direction = 'row',
 }: BlockchainErrorProps) {
+  const [isDetailsOpen, setIsDetailsOpen] = useState(false);
+
   return (
-    <Density compact>
-      <div className='flex flex-row items-center w-full justify-center gap-4 flex-wrap'>
-        <Ban className='h-8 w-8 text-red-400' />
-        <Text small color='text.secondary'>
-          {message}
-        </Text>
-        <Button onClick={onDetailsClick}>Details</Button>
-      </div>
-    </Density>
+    <>
+      <Density compact>
+        <div
+          className={cn(
+            'flex items-center w-full justify-center gap-4',
+            direction === 'column' ? 'flex-col' : 'flex-row flex-wrap',
+          )}
+        >
+          <Ban className='h-8 w-8 text-red-400' />
+          <Text small color='text.secondary'>
+            {message}
+          </Text>
+          <Button onClick={() => setIsDetailsOpen(true)}>Details</Button>
+        </div>
+      </Density>
+
+      <ErrorDetailsModal isOpen={isDetailsOpen} onClose={() => setIsDetailsOpen(false)} />
+    </>
   );
 }
