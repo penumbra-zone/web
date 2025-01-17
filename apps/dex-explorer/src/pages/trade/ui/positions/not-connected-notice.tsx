49f77163480916eeb284bf55d39f6a262a672d82
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2025-01-17 18:49:26 +0400

    Fix auto-closing orders withdrawal (penumbra-zone/dex-explorer/issues/282)
    * Split positions into multiple files
    
    * Refactor and add position closes
    
    * Include positionCloses
    
    * Remove state from payload
    
    * Set positionCloses to undefined when there are nonoe
    
    * Fix lint issues
    
    * gas fees: display alternative gas fee metadata (penumbra-zone/dex-explorer/issues/272)
    
    * display alternative gas fee metadata
    
    * use react query
    
    * cleanup
    
    * Update withdrawn amounts & prices to show dash (penumbra-zone/dex-explorer/issues/277)
    
    ---------
    
    Co-authored-by: Tal Derei <70081547+TalDerei@users.noreply.github.com>
    Co-authored-by: Lucas Meier <lucas@cronokirby.com>

---
+++ apps/dex-explorer/src/pages/trade/ui/positions/not-connected-notice.tsx
@@ -0,0 +1,19 @@
+import { Wallet2 } from 'lucide-react';
+import { ConnectButton } from '@/features/connect/connect-button';
+import { Text } from '@penumbra-zone/ui/Text';
+
+export const NotConnectedNotice = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen gap-4'>
+      <div className='w-12 h-12 text-text-secondary'>
+        <Wallet2 className='w-full h-full' />
+      </div>
+      <Text color='text.secondary' small>
+        Connect wallet to see your positions
+      </Text>
+      <div className='w-fit'>
+        <ConnectButton variant='minimal' actionType='default' />
+      </div>
+    </div>
+  );
+};
