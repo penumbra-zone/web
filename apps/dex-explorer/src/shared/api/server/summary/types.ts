58f9dcc0c828df020b64e461249bfafcd00afe58
Lúcás Meier <cronokirby@gmail.com>
2025-01-27 17:56:14 -0800

    Stopgap: treat negative liquidity and volume as positive (#325)
    * Stopgap: treat negative liquidity and volume as positive
    
    This is a data issue in pindexer, but we should handle this in a robust way, instead of keeling over.

--- apps/dex-explorer/src/shared/api/server/summary/types.ts
+++ apps/dex-explorer/src/shared/api/server/summary/types.ts
@@ -43,19 +43,21 @@ export const adaptSummary = (
   candles?: number[],
   candleTimes?: Date[],
 ): SummaryData => {
-  let liquidity = toValueView({
-    amount: Math.floor(summary.liquidity),
-    metadata: quoteAsset,
-  });
-
   const directVolume = toValueView({
-    amount: Math.floor(summary.direct_volume_indexing_denom_over_window),
+    amount: Math.max(Math.floor(summary.direct_volume_indexing_denom_over_window), 0.0),
     metadata: usdc,
   });
 
   // Converts liquidity and trading volume to their equivalent USDC prices if `usdc_price` is available
+  const rawLiquidity = Math.max(Math.floor(summary.liquidity), 0.0);
+  let liquidity: ValueView;
   if (summary.usdc_price) {
-    liquidity = calculateEquivalentInUSDC(summary.liquidity, summary.usdc_price, quoteAsset, usdc);
+    liquidity = calculateEquivalentInUSDC(rawLiquidity, summary.usdc_price, quoteAsset, usdc);
+  } else {
+    liquidity = toValueView({
+      amount: Math.max(Math.floor(rawLiquidity), 0.0),
+      metadata: quoteAsset,
+    });
   }
 
   const priceDiff = summary.price - summary.price_then;
