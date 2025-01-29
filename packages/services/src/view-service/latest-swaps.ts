c8fe8a7e8f119eb8df601a36988772a6689cf527
Tal Derei <talderei99@gmail.com>
2025-01-29 09:08:51 -0800

    flush out latest swaps view service method

--- packages/services/src/view-service/latest-swaps.ts
+++ packages/services/src/view-service/latest-swaps.ts
@@ -1,20 +1,30 @@
 import type { Impl } from './index.js';
 import { servicesCtx } from '../ctx/prax.js';
-import { TransactionInfo } from '@penumbra-zone/protobuf/penumbra/view/v1/view_pb';
-import {
-  generateTransactionInfo,
-  generateTransactionSummary,
-} from '@penumbra-zone/wasm/transaction';
-import { fvkCtx } from '../ctx/full-viewing-key.js';
+import { Value } from '@penumbra-zone/protobuf/penumbra/core/asset/v1/asset_pb';
+import { TransactionId } from '@penumbra-zone/protobuf/penumbra/core/txhash/v1/txhash_pb';
 
-export const latestSwaps: Impl['latestSwaps'] = async function* (req, ctx) {
+export const latestSwaps: Impl['latestSwaps'] = async function* (_req, ctx) {
   const services = await ctx.values.get(servicesCtx)();
   const { indexedDb } = await services.getWalletServices();
 
-  const fvk = ctx.values.get(fvkCtx);
+  // todo: need to account for optional filters in the incoming `LatestSwapsRequest`.
 
-  for await (const txRecord of indexedDb.iterateTransactions()) {
-    // yield { LatestSwapsResponse object } if the transaction requested from storage
-    // is a swap.
+  // yield BSOD existing swaps and unclaimed swaps, where an unclaimed swap
+  // is defined as `heightClaimed === 0n`.
+  for await (const swapRecord of indexedDb.iterateSwaps()) {
+    if (swapRecord.source?.source.case == 'transaction') {
+      yield {
+        input: new Value({
+          amount: swapRecord.outputData?.delta1,
+          assetId: swapRecord.outputData?.tradingPair?.asset1,
+        }),
+        output: new Value({
+          amount: swapRecord.outputData?.delta2,
+          assetId: swapRecord.outputData?.tradingPair?.asset2,
+        }),
+        blockHeight: swapRecord.heightClaimed,
+        id: new TransactionId({ inner: swapRecord.source.source.value.id }),
+      };
+    }
   }
 };
