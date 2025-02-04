8cf7b8cc3f1d66b0dd24b9856749f5c946ae6d0a
Tal Derei <70081547+TalDerei@users.noreply.github.com>
2025-02-04 08:40:51 -0800

    header: use veil logo (#342)
    * header: use veil logo
    
    * formatting

--- apps/dex-explorer/src/widgets/header/ui/logo.tsx
+++ apps/dex-explorer/src/widgets/header/ui/logo.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link';
 import { PagePath } from '@/shared/const/pages';
-import PenumbraLogo from './logo.svg';
+import VeilLogo from '../assets/veil-combined-logo.svg';
 
 export const HeaderLogo = () => {
   return (
     <Link className='flex h-8 items-center' href={PagePath.Explore}>
-      <PenumbraLogo />
+      <VeilLogo width={96} height={15} />
     </Link>
   );
 };
