ba9d477834729c67d42cec5cf58220efb416be01
Tal Derei <70081547+TalDerei@users.noreply.github.com>
2025-02-04 08:45:43 -0800

    middleware: route default home page to explore page (#346)

--- apps/dex-explorer/src/widgets/header/ui/links.ts
+++ apps/dex-explorer/src/widgets/header/ui/links.ts
@@ -2,8 +2,8 @@ import { MoonStar, ArrowUpFromDot, Coins } from 'lucide-react';
 import { PagePath } from '@/shared/const/pages';
 
 export const HEADER_LINKS = [
-  { label: 'Trade', value: PagePath.Trade, icon: ArrowUpFromDot },
   { label: 'Explore', value: PagePath.Explore, icon: Coins },
-  { label: 'Inspect', value: PagePath.Inspect, icon: MoonStar },
   { label: 'Portfolio', value: PagePath.Portfolio, icon: Coins },
+  { label: 'Trade', value: PagePath.Trade, icon: ArrowUpFromDot },
+  { label: 'Inspect', value: PagePath.Inspect, icon: MoonStar },
 ];
