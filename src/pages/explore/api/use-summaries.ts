fa7b6ebd42474d85e7d856ec582e6a8ee554d76e
Max Korsunov <maxkors2014@gmail.com>
2025-02-06 17:25:24 +0400

    feat: improve data requests for trading pairs (#356)

--- src/pages/explore/api/use-summaries.ts
+++ src/pages/explore/api/use-summaries.ts
@@ -4,6 +4,7 @@ import { InfiniteData, QueryKey, useInfiniteQuery } from '@tanstack/react-query'
 import { SummaryData } from '@/shared/api/server/summary/types';
 import { DurationWindow } from '@/shared/utils/duration';
 import { apiFetch } from '@/shared/utils/api-fetch';
+import { useDebounce } from '@/shared/utils/use-debounce';
 
 /// The base limit will need to be increased as more trading pairs are added to the explore page.
 const BASE_LIMIT = 20;
@@ -11,8 +12,10 @@ const BASE_PAGE = 0;
 const BASE_WINDOW: DurationWindow = '1d';
 
 export const useSummaries = (search: string) => {
+  const debouncedSearch = useDebounce(search, 400);
+
   return useInfiniteQuery<SummaryData[], Error, InfiniteData<SummaryData[]>, QueryKey, number>({
-    queryKey: ['summaries', search],
+    queryKey: ['summaries', debouncedSearch],
     staleTime: 1000 * 60 * 5,
     initialPageParam: BASE_PAGE,
     getNextPageParam: (lastPage, _, lastPageParam) => {
@@ -20,7 +23,7 @@ export const useSummaries = (search: string) => {
     },
     queryFn: async ({ pageParam }) => {
       return apiFetch<SummaryData[]>('/api/summaries', {
-        search,
+        search: debouncedSearch,
         limit: BASE_LIMIT.toString(),
         offset: (pageParam * BASE_LIMIT).toString(),
         durationWindow: BASE_WINDOW,
