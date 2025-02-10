8f21471261abbb08a48b4edb5bf519394503d0d6
Tal Derei <talderei99@gmail.com>
2025-02-09 21:21:57 -0800

    indexedDB: lqt historical votes table

--- packages/types/src/indexed-db.ts
+++ packages/types/src/indexed-db.ts
@@ -324,6 +324,16 @@ export interface PenumbraDb extends DBSchema {
       output: Jsonified<Value>;
     };
   };
+  LQT_HISTORICAL_VOTES: {
+    key: string; // epoch index
+    value: {
+      epoch: string;
+      TransactionId: Jsonified<TransactionId>;
+      AssetMetadata: Jsonified<Metadata>;
+      VoteValue: Jsonified<Value>;
+      RewardValue: Jsonified<Amount> | null;
+    };
+  };
 }
 
 // need to store PositionId and Position in the same table
@@ -361,4 +371,5 @@ export const IDB_TABLES: Tables = {
   tree_commitments: 'TREE_COMMITMENTS',
   tree_last_position: 'TREE_LAST_POSITION',
   tree_last_forgotten: 'TREE_LAST_FORGOTTEN',
+  lqt_historical_votes: 'LQT_HISTORICAL_VOTES',
 };
