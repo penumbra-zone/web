8cb7ae72265a2399c140138f32c62240f056623c
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2025-02-12 21:02:04 +0400

    Detect Inspect Input in Search (#362)
    * Detect Inspect Input in Search
    
    * Refactor setLoading
    
    * Change order of statements
    
    * Add coming soon for missing pages

---
+++ apps/dex-explorer/src/pages/inspect/block/index.tsx
@@ -0,0 +1,9 @@
+import { Text } from '@penumbra-zone/ui/Text';
+
+export function InspectBlock() {
+  return (
+    <div className='flex flex-col items-center justify-center h-[400px]'>
+      <Text color='text.muted'>Coming soon...</Text>
+    </div>
+  );
+}
