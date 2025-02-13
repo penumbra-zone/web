69df4187a3d1ec6832c7cac4435362e4ce1b1d0d
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2025-02-13 18:16:12 +0400

    Make Tabs as Links (penumbra-zone/dex-explorer/issues/360)
    * Make tabs as links
    
    * Update @penumbra-zone/ui
    
    * Remove unused Link
    
    * Update @penumbra-zone/ui

--- apps/dex-explorer/src/widgets/header/ui/links.ts
+++ apps/dex-explorer/src/widgets/header/ui/links.ts
@@ -1,9 +1,34 @@
+import Link from 'next/link';
 import { MoonStar, ArrowUpFromDot, Coins } from 'lucide-react';
 import { PagePath } from '@/shared/const/pages';
 
 export const HEADER_LINKS = [
-  { label: 'Explore', value: PagePath.Explore, icon: Coins },
-  { label: 'Portfolio', value: PagePath.Portfolio, icon: Coins },
-  { label: 'Trade', value: PagePath.Trade, icon: ArrowUpFromDot },
-  { label: 'Inspect', value: PagePath.Inspect, icon: MoonStar },
+  {
+    as: Link,
+    tabProps: { href: PagePath.Explore },
+    label: 'Explore',
+    value: PagePath.Explore,
+    icon: Coins,
+  },
+  {
+    as: Link,
+    tabProps: { href: PagePath.Portfolio },
+    label: 'Portfolio',
+    value: PagePath.Portfolio,
+    icon: Coins,
+  },
+  {
+    as: Link,
+    tabProps: { href: PagePath.Trade },
+    label: 'Trade',
+    value: PagePath.Trade,
+    icon: ArrowUpFromDot,
+  },
+  {
+    as: Link,
+    tabProps: { href: PagePath.Inspect },
+    label: 'Inspect',
+    value: PagePath.Inspect,
+    icon: MoonStar,
+  },
 ];
