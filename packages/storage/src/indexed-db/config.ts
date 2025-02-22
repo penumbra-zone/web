0ef6e68e43791cd66e2b0ee856cb612b6aaed71f
Tal Derei <talderei99@gmail.com>
2025-02-21 16:31:54 -0800

    bump idb version

--- packages/storage/src/indexed-db/config.ts
+++ packages/storage/src/indexed-db/config.ts
@@ -2,4 +2,4 @@
  * The version number for the IndexedDB schema. This version number is used to manage
  * database upgrades and ensure that the correct schema version is applied.
  */
-export const IDB_VERSION = 49;
+export const IDB_VERSION = 50;
