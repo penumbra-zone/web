1c5be6f1131dc13dec47d486ecdff89ac8b752cf
Max Korsunov <maxkors2014@gmail.com>
2025-02-24 11:43:02 +0400

    fix: format

--- packages/services/src/view-service/transaction-info-by-hash.ts
+++ packages/services/src/view-service/transaction-info-by-hash.ts
@@ -1,7 +1,10 @@
 import type { Impl } from './index.js';
 import { servicesCtx } from '../ctx/prax.js';
 import { Code, ConnectError } from '@connectrpc/connect';
-import { generateTransactionInfo, generateTransactionSummary } from '@penumbra-zone/wasm/transaction';
+import {
+  generateTransactionInfo,
+  generateTransactionSummary,
+} from '@penumbra-zone/wasm/transaction';
 import { TransactionInfo } from '@penumbra-zone/protobuf/penumbra/view/v1/view_pb';
 import { fvkCtx } from '../ctx/full-viewing-key.js';
 import { txvTranslator } from './util/transaction-view.js';
@@ -36,6 +39,13 @@ export const transactionInfoByHash: Impl['transactionInfoByHash'] = async (req, 
   // Generate transaction info summary from the TxV.
   const summary = await generateTransactionSummary(txv);
 
-  const txInfo = new TransactionInfo({ height, id: req.id, transaction, perspective, view, summary });
+  const txInfo = new TransactionInfo({
+    height,
+    id: req.id,
+    transaction,
+    perspective,
+    view,
+    summary,
+  });
   return { txInfo };
 };
