850594b147540285abad776b01f00792f10d2d8d
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2025-02-26 19:54:10 +0400

    Inspect Block Page (penumbra-zone/dex-explorer/issues/378)
    * Add inspect block page
    
    * Fetch transactions by height
    
    * Setup block summary nextjs api route
    
    * Improve display type for batchSwaps
    
    * Parse batchSwaps from pindexer
    
    * Fix lint issues
    
    * Parse from jsonb
    
    * Update batch swaps type
    
    * Merge branch main into inspect-block
    
    * Replace Table with TableCell
    
    * Address PR comments
    
    * Use serialization functions

--- apps/dex-explorer/src/pages/inspect/block/index.tsx
+++ apps/dex-explorer/src/pages/inspect/block/index.tsx
@@ -1,9 +1,1 @@
-import { Text } from '@penumbra-zone/ui/Text';
-
-export function InspectBlock() {
-  return (
-    <div className='flex flex-col items-center justify-center h-[400px]'>
-      <Text color='text.muted'>Coming soon...</Text>
-    </div>
-  );
-}
+export { InspectBlock } from './ui';
