3baa681cc077337bb5bf0fe67812c43bf9541f8b
turbocrime <turbocrime@users.noreply.github.com>
2025-03-03 19:38:51 -0800

    simplify mock-chrome types, correct error stacks

--- packages/mock-chrome/src/event.mock.ts
+++ packages/mock-chrome/src/event.mock.ts
@@ -1,31 +1,25 @@
 import { vi, MockedFunction, Mocked } from 'vitest';
 
-export const eventMocked = <E extends ChromeEvent>(event: E): MockedChromeEvent<E> =>
-  event as unknown as MockedChromeEvent<E>;
-
 /**
  * Mock the listeners manager for any `chrome.events.Event` type, such as
  * `chrome.runtime.ExtensionConnectEvent` or `chrome.runtime.PortMessageEvent`.
  *
  * Does not support event rules or listeners with `filter` parameters.
  *
- * @param listeners set object, will be used for internal state
- * @returns event manager with mocked features, direct dispatch, and insight to listeners
+ * @param listeners set of existing listeners
+ * @returns an inspectable {@link MockedChromeEvent} providing dispatch and insight
  */
-
-export const mockEvent = <
-  E extends ChromeEvent,
-  L extends E extends ChromeEvent<infer T> ? T : never = E extends ChromeEvent<infer T> ? T : never,
->(
-  listeners = new Set<L>(),
+export const mockEvent = <E extends ChromeEvent>(
+  listeners = new Set<ChromeEventListener<E>>(),
 ): MockedChromeEvent<E> => {
-  // dispatch method to activate the listeners
-  const dispatch = (...i: Parameters<L>) => listeners.forEach(listener => listener(...i));
+  const dispatch = (...i: Parameters<ChromeEventListener<E>>): void =>
+    // eslint-disable-next-line @typescript-eslint/require-await -- sever the exception stack for 'remote' dispatch
+    void (async () => listeners.forEach(listener => listener(...i)))();
 
-  const addListener = (i: L): void => void listeners.add(i);
-  const hasListener = (i: L): boolean => listeners.has(i);
+  const addListener = (i: ChromeEventListener<E>): void => void listeners.add(i);
+  const hasListener = (i: ChromeEventListener<E>): boolean => listeners.has(i);
   const hasListeners = (): boolean => listeners.size > 0;
-  const removeListener = (i: L): void => void listeners.delete(i);
+  const removeListener = (i: ChromeEventListener<E>): void => void listeners.delete(i);
 
   return {
     listeners,
@@ -35,20 +29,30 @@ export const mockEvent = <
     hasListener: vi.fn(hasListener),
     hasListeners: vi.fn(hasListeners),
     removeListener: vi.fn(removeListener),
-  } as unknown as MockedChromeEvent<E>;
+
+    getRules: vi.fn() as never,
+    addRules: vi.fn() as never,
+    removeRules: vi.fn() as never,
+  };
 };
 
+/**
+ * Mock interface for a `chrome.events.Event` listener manager, such as
+ * `chrome.runtime.ExtensionConnectEvent` or `chrome.runtime.PortMessageEvent`.
+ *
+ * Does not support event rules or listeners with `filter` parameters.
+ *
+ * Create with {@link mockEvent}
+ */
 export interface MockedChromeEvent<T extends ChromeEvent = ChromeEvent>
   extends Mocked<ChromeEvent<ChromeEventListener<T>>> {
+  /** inspectable set of attached listeners */
   listeners: Set<ChromeEventListener<T>>;
-
+  /** dispatch the event to activate attached listeners */
   dispatch: MockedFunction<(...args: Parameters<ChromeEventListener<T>>) => void>;
 
   addListener: MockedFunction<(callback: ChromeEventListener<T>) => void>;
-
   hasListener: MockedFunction<(callback: ChromeEventListener<T>) => boolean>;
-
   hasListeners: MockedFunction<() => boolean>;
-
   removeListener: MockedFunction<(callback: ChromeEventListener<T>) => void>;
 }
