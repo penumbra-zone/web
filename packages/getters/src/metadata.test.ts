61fbcbe1f8709f35099512b7504fb7990ecb6149
Max Korsunov <maxkors2014@gmail.com>
2025-03-13 11:52:08 +0400

    refactor(getters): #1622: update protobuf usage

--- packages/getters/src/metadata.test.ts
+++ packages/getters/src/metadata.test.ts
@@ -1,10 +1,11 @@
-import { Metadata } from '@penumbra-zone/protobuf/penumbra/core/asset/v1/asset_pb';
+import { MetadataSchema } from '@penumbra-zone/protobuf/penumbra/core/asset/v1/asset_pb';
+import { create } from '@bufbuild/protobuf';
 import { describe, expect, it } from 'vitest';
 import { getDisplayDenomExponent } from './metadata.js';
 
 describe('getDisplayDenomExponent()', () => {
   it("gets the exponent from the denom unit whose `denom` is equal to the metadata's `display` property", () => {
-    const penumbraMetadata = new Metadata({
+    const penumbraMetadata = create(MetadataSchema, {
       display: 'penumbra',
       denomUnits: [
         {
