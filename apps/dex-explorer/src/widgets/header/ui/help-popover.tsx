c6b0547716dcc46578bd48fb1a0220ddc1505576
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2025-03-25 21:44:22 +0400

    Add links to help menu (penumbra-zone/dex-explorer/issues/404)

--- apps/dex-explorer/src/widgets/header/ui/help-popover.tsx
+++ apps/dex-explorer/src/widgets/header/ui/help-popover.tsx
@@ -1,5 +1,5 @@
 import NextLink from 'next/link';
-import { CircleHelp } from 'lucide-react';
+import { CircleHelp, MessageCircleHeart, BookOpenText, Info } from 'lucide-react';
 import { Popover } from '@penumbra-zone/ui/Popover';
 import { Button } from '@penumbra-zone/ui/Button';
 import { Text } from '@penumbra-zone/ui/Text';
@@ -15,14 +15,48 @@ export const HelpPopover = observer(() => {
         </Button>
       </Popover.Trigger>
       <Popover.Content align='end' side='bottom'>
-        <NextLink href='/portfolio?showOnboarding=true'>
-          <button className='flex px-3 py-2 gap-3 text-text-primary'>
-            <Icon IconComponent={CircleHelp} size='md' />
-            <Text small align='left'>
-              Show getting started guide
-            </Text>
-          </button>
+        <NextLink
+          href='/portfolio?showOnboarding=true'
+          className='flex px-3 py-2 gap-3 text-text-primary'
+        >
+          <Icon IconComponent={CircleHelp} size='md' />
+          <Text small align='left'>
+            Show getting started guide
+          </Text>
         </NextLink>
+        <a
+          href='https://guide.penumbra.zone/dex'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='flex px-3 py-2 gap-3 text-text-primary'
+        >
+          <Icon IconComponent={BookOpenText} size='md' />
+          <Text small align='left'>
+            How Veil works
+          </Text>
+        </a>
+        <a
+          href='https://discord.gg/4dYRd2vgkF'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='flex px-3 py-2 gap-3 text-text-primary'
+        >
+          <Icon IconComponent={MessageCircleHeart} size='md' />
+          <Text small align='left'>
+            Provide feedback
+          </Text>
+        </a>
+        <a
+          href='https://penumbra.zone/'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='flex px-3 py-2 gap-3 text-text-primary'
+        >
+          <Icon IconComponent={Info} size='md' />
+          <Text small align='left'>
+            About Penumbra
+          </Text>
+        </a>
       </Popover.Content>
     </Popover>
   );
