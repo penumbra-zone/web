7f99c10548c6706dc5b2f040da264247a156d247
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2025-03-27 18:03:03 +0400

    Implemented LQT round card

--- apps/veil/src/pages/tournament/ui/round/page.tsx
+++ apps/veil/src/pages/tournament/ui/round/page.tsx
@@ -1,15 +1,13 @@
 'use client';
 
 import { PenumbraWaves } from '@/pages/explore/ui/waves';
-import { MyRewards } from './my-rewards';
-import { LandingCard } from './landing-card';
+import { RoundCard } from './round-card';
 
-export const TournamentPage = () => {
+export const TournamentRoundPage = () => {
   return (
     <section className='flex flex-col gap-6 p-4 max-w-[1168px] mx-auto'>
       <PenumbraWaves />
-      <LandingCard />
-      <MyRewards />
+      <RoundCard />
     </section>
   );
 };
