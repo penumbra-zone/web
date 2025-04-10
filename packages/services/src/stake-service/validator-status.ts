40fe9fa1ff380275beec9e5b1ec26de7eb3cbb2a
turbocrime <turbocrime@users.noreply.github.com>
2025-04-10 10:38:50 -0700

    simplify implementation

--- packages/services/src/stake-service/validator-status.ts
+++ packages/services/src/stake-service/validator-status.ts
@@ -1,28 +1,22 @@
 import { Impl } from './index.js';
 import { servicesCtx } from '../ctx/prax.js';
-import { Code, ConnectError } from '@connectrpc/connect';
 import {
   GetValidatorInfoRequest,
   ValidatorInfo,
 } from '@penumbra-zone/protobuf/penumbra/core/component/stake/v1/stake_pb';
+import { IdentityKey } from '@penumbra-zone/protobuf/penumbra/core/keys/v1/keys_pb';
 
 export const validatorStatus: Impl['validatorStatus'] = async ({ identityKey }, ctx) => {
-  if (!identityKey) {
-    throw ConnectError.from('Validator identity key required', Code.InvalidArgument);
-  }
-
   const services = await ctx.values.get(servicesCtx)();
   const { querier, indexedDb } = await services.getWalletServices();
 
-  const remoteValidatorInfo = querier.stake
-    .validatorInfo(new GetValidatorInfoRequest({ identityKey }))
-    .then(({ validatorInfo }) => validatorInfo);
-
   const { status } = new ValidatorInfo(
-    // try to get latest, but suppress request error
-    (await remoteValidatorInfo.catch()) ??
-      // if unavailable, use local info
-      (await indexedDb.getValidatorInfo(identityKey)),
+    // use local info
+    (await indexedDb.getValidatorInfo(new IdentityKey(identityKey))) ??
+      // or fetch remote info
+      (await querier.stake
+        .validatorInfo(new GetValidatorInfoRequest({ identityKey }))
+        .then(({ validatorInfo }) => validatorInfo)),
   );
 
   return { status };
