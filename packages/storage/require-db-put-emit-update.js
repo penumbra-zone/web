4e3f8866d4b55db749d06df8e9de0da03eff3f9c
turbocrime <turbocrime@users.noreply.github.com>
2025-04-23 14:16:06 -0700

    refactor rule

--- packages/storage/require-db-put-emit-update.js
+++ packages/storage/require-db-put-emit-update.js
@@ -12,86 +12,106 @@ export default {
   },
 
   create(context) {
+    const INSERT_METHODS = ['put', 'add'];
+
+    function isInsertMethod(node) {
+      return (
+        node?.callee?.type === 'MemberExpression' &&
+        INSERT_METHODS.includes(node.callee.property?.name)
+      );
+    }
+
+    function isDbInsertCall(node) {
+      return (
+        isInsertMethod(node) &&
+        node.callee.object?.object?.type === 'ThisExpression' &&
+        node.callee.object.property?.name === 'db'
+      );
+    }
+
+    function isTxInsertCall(node) {
+      return isInsertMethod(node) && node.callee.object?.callee?.property?.name === 'objectStore';
+    }
+
+    function hasEmitUpdate(node) {
+      const parent = node.parent;
+      if (!parent || !parent.parent) {
+        return false;
+      }
+
+      return (
+        parent.type === 'MemberExpression' &&
+        parent.property?.name === 'then' &&
+        parent.parent.type === 'CallExpression' &&
+        parent.parent.arguments?.length === 1 &&
+        parent.parent.arguments[0]?.type === 'CallExpression' &&
+        parent.parent.arguments[0].callee?.object?.type === 'ThisExpression' &&
+        parent.parent.arguments[0].callee.property?.name === 'emitUpdate'
+      );
+    }
+
+    function getStoreName(node) {
+      if (!node?.callee?.object) {
+        return null;
+      }
+
+      const storeArg =
+        node.callee.object.callee?.property?.name === 'objectStore'
+          ? node.callee.object.arguments?.[0]
+          : node.arguments?.[0];
+
+      return storeArg?.type === 'Literal' ? storeArg.value : null;
+    }
+
+    function getTransactionName(node) {
+      if (!isTxInsertCall(node)) {
+        return 'null';
+      }
+
+      // Find the closest parent that's a function call or block statement
+      let current = node;
+      while (current?.parent) {
+        if (current.parent.type === 'CallExpression' || current.parent.type === 'BlockStatement') {
+          break;
+        }
+        current = current.parent;
+      }
+
+      // Look for a variable declaration or parameter that's a transaction
+      const scope = context.getScope();
+      const txVar = scope.variables.find(v =>
+        v.defs.some(
+          def =>
+            def.node?.type === 'VariableDeclarator' &&
+            def.node.init?.type === 'Identifier' &&
+            def.node.init.name === 'transaction',
+        ),
+      );
+
+      return txVar ? txVar.name : 'tx';
+    }
+
     return {
       CallExpression(node) {
-        // Check if this is a this.db.put call or tx.objectStore(...).put call
-        const isDbPut =
-          node.callee.type === 'MemberExpression' &&
-          node.callee.object.type === 'MemberExpression' &&
-          node.callee.object.object.type === 'ThisExpression' &&
-          node.callee.object.property.type === 'Identifier' &&
-          node.callee.object.property.name === 'db' &&
-          node.callee.property.type === 'Identifier' &&
-          ['put', 'add'].includes(node.callee.property.name);
-
-        const isTxPut =
-          node.callee.type === 'MemberExpression' &&
-          node.callee.property.type === 'Identifier' &&
-          ['put', 'add'].includes(node.callee.property.name) &&
-          node.callee.object.type === 'CallExpression' &&
-          node.callee.object.callee.type === 'MemberExpression' &&
-          node.callee.object.callee.property.type === 'Identifier' &&
-          node.callee.object.callee.property.name === 'objectStore';
-
-        if (!isDbPut && !isTxPut) return;
-
-        // Get the parent chain to check for .then(this.emitUpdate(...))
-        let current = node;
-        let foundThen = false;
-        let foundEmitUpdate = false;
-
-        while (current.parent) {
-          // Skip over await expressions
-          if (current.parent.type === 'AwaitExpression') {
-            current = current.parent;
-            continue;
-          }
-
-          if (
-            current.parent.type === 'MemberExpression' &&
-            current.parent.property.type === 'Identifier' &&
-            current.parent.property.name === 'then'
-          ) {
-            foundThen = true;
-            current = current.parent;
-            continue;
-          }
-
-          if (
-            foundThen &&
-            current.parent.type === 'CallExpression' &&
-            current.parent.arguments.length === 1 &&
-            current.parent.arguments[0].type === 'CallExpression' &&
-            current.parent.arguments[0].callee.type === 'MemberExpression' &&
-            current.parent.arguments[0].callee.object.type === 'ThisExpression' &&
-            current.parent.arguments[0].callee.property.type === 'Identifier' &&
-            current.parent.arguments[0].callee.property.name === 'emitUpdate'
-          ) {
-            foundEmitUpdate = true;
-            break;
-          }
-
-          current = current.parent;
+        if (!isDbInsertCall(node) && !isTxInsertCall(node)) {
+          return;
         }
 
-        if (!foundThen || !foundEmitUpdate) {
+        if (!hasEmitUpdate(node)) {
+          const storeName = getStoreName(node);
+          const txName = getTransactionName(node);
+
           context.report({
             node,
-            message: 'db insertion call must be followed by .then(this.emitUpdate(...))',
+            message: `Database insertion (${node.callee.property.name}) must be followed by .then(this.emitUpdate(...)) to ensure UI updates`,
             fix(fixer) {
-              const sourceCode = context.getSourceCode();
-              const putCall = sourceCode.getText(node);
-              const storeName = isDbPut
-                ? node.arguments[0]?.type === 'Literal'
-                  ? node.arguments[0].value
-                  : 'STORE_NAME'
-                : node.callee.object.arguments[0]?.type === 'Literal'
-                  ? node.callee.object.arguments[0].value
-                  : 'STORE_NAME';
-
+              if (!storeName) {
+                return null; // Don't provide a fix if we can't determine the store name
+              }
+              const putCall = context.sourceCode.getText(node);
               return fixer.replaceText(
                 node,
-                `${putCall}.then(this.emitUpdate('${storeName}', ${isTxPut ? 'tx' : 'null'}))`,
+                `${putCall}.then(this.emitUpdate('${storeName}', ${txName}))`,
               );
             },
           });
