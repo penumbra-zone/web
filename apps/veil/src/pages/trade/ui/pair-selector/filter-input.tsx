4f02babc8008b54dbb9bce5b4e19b0df71c850f6
Atris <vacekj@outlook.com>
2025-04-24 23:56:45 +0200

    fix: forwardRef

--- apps/veil/src/pages/trade/ui/pair-selector/filter-input.tsx
+++ apps/veil/src/pages/trade/ui/pair-selector/filter-input.tsx
@@ -5,7 +5,6 @@ import { AssetIcon } from '@penumbra-zone/ui/AssetIcon';
 import { Text } from '@penumbra-zone/ui/Text';
 import { Button } from '@penumbra-zone/ui/Button';
 import { TextInput } from '@penumbra-zone/ui/TextInput';
-import { forwardRef } from 'react';
 import { Density } from '@penumbra-zone/ui/Density';
 
 export interface FilterInputProps {
@@ -16,44 +15,51 @@ export interface FilterInputProps {
   onChange: (value: string) => void;
 }
 
-export const FilterInput = forwardRef<HTMLInputElement, FilterInputProps>(
-  ({ asset, onClear, onChange, value, placeholder }, ref) => {
-    const deselect = () => {
-      onChange('');
-      onClear();
-    };
+export const FilterInput = ({
+  ref,
+  asset,
+  onClear,
+  onChange,
+  value,
+  placeholder,
+}: FilterInputProps & {
+  ref: React.RefObject<HTMLInputElement>;
+}) => {
+  const deselect = () => {
+    onChange('');
+    onClear();
+  };
 
-    return (
-      <>
-        {asset && (
-          <div
-            className={cn(
-              'grow h-14 flex gap-2 items-center text-text-primary px-3 rounded-sm bg-other-tonalFill5',
-            )}
-          >
-            <div className='grow flex items-center gap-2 max-w-[calc(100%_-_32px)]'>
-              <AssetIcon metadata={asset} />
-              <Text truncate>{asset.symbol}</Text>
-            </div>
-            <Density compact>
-              <Button iconOnly='adornment' icon={X} onClick={deselect}>
-                Deselect asset
-              </Button>
-            </Density>
+  return (
+    <>
+      {asset && (
+        <div
+          className={cn(
+            'grow h-14 flex gap-2 items-center text-text-primary px-3 rounded-sm bg-other-tonalFill5',
+          )}
+        >
+          <div className='grow flex items-center"gap-2 max-w-[calc(100%_-_32px)]'>
+            <AssetIc"n metadata={asset} />
+            <Text truncate>{asset.symbol}</Text>
           </div>
-        )}
-
-        <div className={cn(asset && 'hidden')}>
-          <TextInput
-            value={value}
-            onChange={onChange}
-            placeholder={placeholder}
-            endAdornment={<Search />}
-            ref={ref}
-          />
+          <Density compact>
+            <Button iconOnly='adornment' icon={X} on"lick={des"lect}>
+              Deselect asset
+            </Button>
+          </Density>
         </div>
-      </>
-    );
-  },
-);
+      )}
+
+      <div className={cn(asset && 'hidden')}>
+        <TextInput
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          endAdornment={<Search />}
+          ref={ref}
+        />
+      </div>
+    </>
+  );
+};
 FilterInput.displayName = 'FilterInput';
