27a5c42ffcba6743be3a5b5f9b09da206a27dc32
Tal Derei <talderei99@gmail.com>
2025-05-07 21:16:23 -0700

    fix casted votes interpretation

--- apps/veil/src/pages/tournament/ui/delegator-rewards.tsx
+++ apps/veil/src/pages/tournament/ui/delegator-rewards.tsx
@@ -37,6 +37,7 @@ export const DelegatorTotalRewards = observer(() => {
   const { epoch, isLoading: epochLoading } = useCurrentEpoch();
   const {
     totalRewards,
+    data: rewards,
     query: { isLoading: isRewardsLoading },
   } = usePersonalRewards(subaccount, epoch, epochLoading);
   const { data: stakingToken, isLoading: isTokenLoading } = useStakingTokenMetadata();
@@ -48,7 +49,8 @@ export const DelegatorTotalRewards = observer(() => {
 
   // Check if we have all the data needed to display rewards
   const isLoading = isRewardsLoading || isTokenLoading;
-  const hasCompleteData = !isLoading && totalRewards !== undefined && stakingToken;
+  const hasCompleteData =
+    !isLoading && totalRewards !== undefined && rewards !== undefined && stakingToken;
 
   // Memoize the reward view to prevent unnecessary recalculations
   const rewardView = useMemo(() => {
