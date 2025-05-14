b354ac8789393bf978241b753bf6ae789b5de81f
Lucas Meier <lucas@cronokirby.com>
2025-05-14 14:57:15 -0700

    fix: remove lp rewards from delegator table
    We can't know the LPs owned by a particular delegator, so this should
    just display the voting rewards.

--- apps/veil/src/pages/tournament/ui/delegator-table/delegator-rewards.tsx
+++ apps/veil/src/pages/tournament/ui/delegator-table/delegator-rewards.tsx
@@ -1,17 +1,12 @@
-import { useState } from 'react';
 import { Text } from '@penumbra-zone/ui/Text';
 import { Skeleton } from '@penumbra-zone/ui/Skeleton';
 import { ValueViewComponent } from '@penumbra-zone/ui/ValueView';
-import { SegmentedControl } from '@penumbra-zone/ui/SegmentedControl';
 import { Density } from '@penumbra-zone/ui/Density';
 import { useTotalRewards } from '../../api/use-total-rewards';
-import { LpRewards } from '../lp-rewards';
 import { VotingRewards } from '../total-delegator-rewards';
 
 export const DelegatorRewards = () => {
   const { data: total, isLoading } = useTotalRewards();
-
-  const [tab, setTab] = useState<'lp' | 'voting'>('lp');
 
   return (
     <div className='flex flex-col gap-4 p-6 rounded-lg bg-other-tonalFill5 backdrop-blur-lg'>
@@ -21,7 +16,7 @@ export const DelegatorRewards = () => {
             Total Rewards Earned
           </Text>
           <Text small color='text.secondary'>
-            Cumulative from all epochs, voting and LPs rewards
+            Cumulative from all epochs, and voting rewards
           </Text>
         </div>
 
@@ -38,22 +33,7 @@ export const DelegatorRewards = () => {
         )}
       </div>
 
-      <div className='flex flex-col gap-4 mt-4'>
-        <div className='[&_button]:grow'>
-          <SegmentedControl value={tab} onChange={value => setTab(value as typeof tab)}>
-            <SegmentedControl.Item style='filled' value='lp'>
-              LPs Rewards
-            </SegmentedControl.Item>
-            <SegmentedControl.Item style='filled' value='voting'>
-              Voting Rewards
-            </SegmentedControl.Item>
-          </SegmentedControl>
-        </div>
-
-        {tab === 'lp' && <LpRewards />}
-
-        {tab === 'voting' && <VotingRewards />}
-      </div>
+      <VotingRewards />
     </div>
   );
 };
