f3a85231fb0cae8cbfe52dbfb41a7db122a466e8
Lucas Meier <lucas@cronokirby.com>
2025-05-14 11:39:41 -0700

    lp-leaderboard: fix infinite load on current epoch
    The weird logic around the latest block was causing it to load infinitely
    on the current epoch. This fixes that, and uses the existing refetch
    on new block mechanism.

--- apps/veil/src/entities/leaderboard/api/use-lp-leaderboard.ts
+++ apps/veil/src/entities/leaderboard/api/use-lp-leaderboard.ts
@@ -1,13 +1,12 @@
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
 import { apiPostFetch } from '@/shared/utils/api-fetch';
-import { statusStore } from '@/shared/model/status';
-import { useCurrentEpoch } from '@/pages/tournament/api/use-current-epoch';
 import {
   LpLeaderboardRequest,
   LpLeaderboardResponse,
   LpLeaderboardSortKey,
   LpLeaderboardSortDirection,
 } from './utils';
+import { useRefetchOnNewBlock } from '@/shared/api/compact-block';
 
 export const BASE_LIMIT = 10;
 export const BASE_PAGE = 1;
@@ -29,20 +28,9 @@ export const useLpLeaderboard = ({
   isActive: boolean;
   assetId: string | undefined;
 }): UseQueryResult<LpLeaderboardResponse> => {
-  const { latestKnownBlockHeight } = statusStore;
-  const { epoch: currentEpoch } = useCurrentEpoch();
-
+  const queryKey = ['lp-leaderboard', epoch, page, limit, sortKey, sortDirection, assetId];
   const query = useQuery({
-    queryKey: [
-      'lp-leaderboard',
-      epoch,
-      page,
-      limit,
-      sortKey,
-      sortDirection,
-      assetId,
-      ...(epoch === currentEpoch ? [Number(latestKnownBlockHeight)] : []),
-    ],
+    queryKey,
     staleTime: Infinity,
     queryFn: async () => {
       return apiPostFetch<LpLeaderboardResponse>('/api/tournament/lp-leaderboard', {
@@ -54,11 +42,9 @@ export const useLpLeaderboard = ({
         assetId,
       } as LpLeaderboardRequest);
     },
-    enabled:
-      typeof epoch === 'number' &&
-      isActive &&
-      (epoch === currentEpoch ? !!latestKnownBlockHeight : true),
+    enabled: typeof epoch === 'number' && isActive,
   });
+  useRefetchOnNewBlock('lp-leaderboard', query);
 
   return query;
 };
