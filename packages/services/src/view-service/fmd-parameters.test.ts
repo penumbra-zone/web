31e774b6948645e7f569d9731e06f7e6c25384d0
turbocrime <turbocrime@users.noreply.github.com>
2025-05-15 14:33:31 -0700

    correct mock and satisfy linter

--- packages/services/src/view-service/fmd-parameters.test.ts
+++ packages/services/src/view-service/fmd-parameters.test.ts
@@ -6,22 +6,19 @@ import {
 import { createContextValues, createHandlerContext, HandlerContext } from '@connectrpc/connect';
 import { ViewService } from '@penumbra-zone/protobuf';
 import { servicesCtx } from '../ctx/prax.js';
-import { IndexedDbMock, MockServices } from '../test-utils.js';
+import { mockIndexedDb, MockServices } from '../test-utils.js';
 import { FmdParameters } from '@penumbra-zone/protobuf/penumbra/core/component/shielded_pool/v1/shielded_pool_pb';
 import { fMDParameters } from './fmd-parameters.js';
 import type { ServicesInterface } from '@penumbra-zone/types/services';
 
 describe('FmdParameters request handler', () => {
   let mockServices: MockServices;
-  let mockIndexedDb: IndexedDbMock;
+
   let mockCtx: HandlerContext;
 
   beforeEach(() => {
     vi.resetAllMocks();
 
-    mockIndexedDb = {
-      getFmdParams: vi.fn(),
-    };
     mockServices = {
       getWalletServices: vi.fn(() =>
         Promise.resolve({ indexedDb: mockIndexedDb }),
@@ -40,7 +37,7 @@ describe('FmdParameters request handler', () => {
   });
 
   test('should successfully get fmdParameters when idb has them', async () => {
-    mockIndexedDb.getFmdParams?.mockResolvedValue(testData);
+    mockIndexedDb.getFmdParams.mockResolvedValue(testData);
     const fmdParameterResponse = new FMDParametersResponse(
       await fMDParameters(new FMDParametersRequest(), mockCtx),
     );
@@ -48,7 +45,7 @@ describe('FmdParameters request handler', () => {
   });
 
   test('should fail to get fmdParameters when idb has none', async () => {
-    mockIndexedDb.getFmdParams?.mockResolvedValue(undefined);
+    mockIndexedDb.getFmdParams.mockResolvedValue(undefined);
     await expect(fMDParameters(new FMDParametersRequest(), mockCtx)).rejects.toThrow();
   });
 });
