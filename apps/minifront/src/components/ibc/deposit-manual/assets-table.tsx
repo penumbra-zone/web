penumbra-zone/web

commit dcfbe8a035cd25c565953a04d3f9608b48a0f4d2

Feat/v2 portfolio balance and assets + React 19 upgrade (#2364)
## Description of Changes

feat(ui): Added v2 Portfolio Page layout including PortfolioBalance and
AssetCard components. Also had to setup a dual-theme build process to
avoid tailwind conflicts in Minifront app (legacy vs. v2 theme).

Furthermore, React was upgraded to 19 incl. cleanup of forwardRef
utilizations.

## Related Issue

[Link to the issue this PR addresses.
](https://github.com/penumbra-zone/web/issues/2363)

apps/minifront/src/index.css
@@ -1,0 +1,32 @@
+/* apps/minifront/src/index.css */
+
+/* Import the prefixed CSS for v2 components */
+@import '@penumbra-zone/ui/style-prefixed.css';
+
+/* Tailwind directives for minifront's own styling */
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+/* Fix for border styling using the --light-brown variable */
+* {
+  border-color: var(--light-brown);
+}
+
+.pe-0 {
+  padding-inline-end: 0px !important;
+}
+
+.ps-0 {
+  padding-inline-start: 0px !important;
+}
+
+body {
+  min-height: 100vh;
+  background-color: black;
+}
+
+.v1-bg {
+  min-height: 100vh;
+  background-color: black;
+}

apps/minifront/src/main.tsx
@@ -11,6 +11,11 @@ import { RouterProvider } from 'react-router-dom';
 import { rootRouter } from './components/root-router';
 import { PenumbraUIProvider } from '@penumbra-zone/ui-deprecated/PenumbraUIProvider';
 
+// Add back the unprefixed CSS import that was working for the header
+import '@penumbra-zone/ui/style.css';
+// Then also import our custom CSS which contains the prefixed CSS import for v2 components
+import './index.css';
+
 const Main = () => {
   const [queryClient] = useState(() => new QueryClient());
 

packages/ui-deprecated/components/ui/button/index.tsx
@@ -46,16 +46,14 @@ export interface ButtonProps
    * @see https://www.radix-ui.com/primitives/docs/utilities/slot#slot
    */
   asChild?: boolean;
+  ref?: React.Ref<HTMLButtonElement>;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : 'button';
-    return (
-      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
-    );
-  },
-);
+// Converted from forwardRef to regular component
+const Button = ({ className, variant, size, asChild = false, ref, ...props }: ButtonProps) => {
+  const Comp = asChild ? Slot : 'button';
+  return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
+};
 Button.displayName = 'Button';
 
 export { Button, buttonVariants };

packages/ui-deprecated/components/ui/command/index.tsx
@@ -7,10 +7,11 @@ import { Search } from 'lucide-react';
 import { cn } from '../../../lib/utils';
 import { Dialog, DialogContent } from '../dialog';
 
-const Command = React.forwardRef<
-  React.ElementRef<typeof CommandPrimitive>,
-  React.ComponentPropsWithoutRef<typeof CommandPrimitive>
->(({ className, ...props }, ref) => (
+export interface CommandProps extends React.ComponentPropsWithoutRef<typeof CommandPrimitive> {
+  ref?: React.Ref<HTMLDivElement>;
+}
+
+const Command = ({ className, ref, ...props }: CommandProps) => (
   <CommandPrimitive
     ref={ref}
     className={cn(
@@ -19,7 +20,7 @@ const Command = React.forwardRef<
     )}
     {...props}
   />
-));
+);
 Command.displayName = CommandPrimitive.displayName;
 
 type CommandDialogProps = DialogProps;
@@ -36,10 +37,12 @@ const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   );
 };
 
-const CommandInput = React.forwardRef<
-  React.ElementRef<typeof CommandPrimitive.Input>,
-  React.ComponentPropsWithoutRef<typeof CommandPrimitive.Input>
->(({ className, ...props }, ref) => (
+export interface CommandInputProps
+  extends React.ComponentPropsWithoutRef<typeof CommandPrimitive.Input> {
+  ref?: React.Ref<HTMLInputElement>;
+}
+
+const CommandInput = ({ className, ref, ...props }: CommandInputProps) => (
   // eslint-disable-next-line react/no-unknown-property -- TODO: justify
   <div className='flex items-center border-b px-3' cmdk-input-wrapper=''>
     <Search className='mr-2 size-4 shrink-0 opacity-50' />
@@ -52,36 +55,43 @@ const CommandInput = React.forwardRef<
       {...props}
     />
   </div>
-));
-
+);
 CommandInput.displayName = CommandPrimitive.Input.displayName;
 
-const CommandList = React.forwardRef<
-  React.ElementRef<typeof CommandPrimitive.List>,
-  React.ComponentPropsWithoutRef<typeof CommandPrimitive.List>
->(({ className, ...props }, ref) => (
+export interface CommandListProps
+  extends React.ComponentPropsWithoutRef<typeof CommandPrimitive.List> {
+  ref?: React.Ref<HTMLDivElement>;
+}
+
+const CommandList = ({ className, ref, ...props }: CommandListProps) => (
   <CommandPrimitive.List
     ref={ref}
     className={cn('max-h-[300px] overflow-y-auto overflow-x-hidden', className)}
     {...props}
   />
-));
-
+);
 CommandList.displayName = CommandPrimitive.List.displayName;
 
-const CommandEmpty = React.forwardRef<
-  React.ElementRef<typeof CommandPrimitive.Empty>,
-  React.ComponentPropsWithoutRef<typeof CommandPrimitive.Empty>
->((props, ref) => (
-  <CommandPrimitive.Empty ref={ref} className='py-6 text-center text-sm' {...props} />
-));
+export interface CommandEmptyProps
+  extends React.ComponentPropsWithoutRef<typeof CommandPrimitive.Empty> {
+  ref?: React.Ref<HTMLDivElement>;
+}
 
+const CommandEmpty = ({ ref, className, ...props }: CommandEmptyProps & { className?: string }) => (
+  <CommandPrimitive.Empty
+    ref={ref}
+    className={cn('py-6 text-center text-sm', className)}
+    {...props}
+  />
+);
 CommandEmpty.displayName = CommandPrimitive.Empty.displayName;
 
-const CommandGroup = React.forwardRef<
-  React.ElementRef<typeof CommandPrimitive.Group>,
-  React.ComponentPropsWithoutRef<typeof CommandPrimitive.Group>
->(({ className, ...props }, ref) => (
+export interface CommandGroupProps
+  extends React.ComponentPropsWithoutRef<typeof CommandPrimitive.Group> {
+  ref?: React.Ref<HTMLDivElement>;
+}
+
+const CommandGroup = ({ className, ref, ...props }: CommandGroupProps) => (
   <CommandPrimitive.Group
     ref={ref}
     className={cn(
@@ -90,26 +100,29 @@ const CommandGroup = React.forwardRef<
     )}
     {...props}
   />
-));
-
+);
 CommandGroup.displayName = CommandPrimitive.Group.displayName;
 
-const CommandSeparator = React.forwardRef<
-  React.ElementRef<typeof CommandPrimitive.Separator>,
-  React.ComponentPropsWithoutRef<typeof CommandPrimitive.Separator>
->(({ className, ...props }, ref) => (
+export interface CommandSeparatorProps
+  extends React.ComponentPropsWithoutRef<typeof CommandPrimitive.Separator> {
+  ref?: React.Ref<HTMLDivElement>;
+}
+
+const CommandSeparator = ({ className, ref, ...props }: CommandSeparatorProps) => (
   <CommandPrimitive.Separator
     ref={ref}
     className={cn('-mx-1 h-px bg-border', className)}
     {...props}
   />
-));
+);
 CommandSeparator.displayName = CommandPrimitive.Separator.displayName;
 
-const CommandItem = React.forwardRef<
-  React.ElementRef<typeof CommandPrimitive.Item>,
-  React.ComponentPropsWithoutRef<typeof CommandPrimitive.Item>
->(({ className, ...props }, ref) => (
+export interface CommandItemProps
+  extends React.ComponentPropsWithoutRef<typeof CommandPrimitive.Item> {
+  ref?: React.Ref<HTMLDivElement>;
+}
+
+const CommandItem = ({ className, ref, ...props }: CommandItemProps) => (
   <CommandPrimitive.Item
     ref={ref}
     className={cn(
@@ -118,8 +131,7 @@ const CommandItem = React.forwardRef<
     )}
     {...props}
   />
-));
-
+);
 CommandItem.displayName = CommandPrimitive.Item.displayName;
 
 const CommandShortcut = ({ className, ...props }: React.HTMLAttributes<HTMLSpanElement>) => {

apps/minifront/src/components/send/send-form/index.tsx
@@ -9,7 +9,7 @@ import InputToken from '../../shared/input-token';
 import { GasFee } from '../../shared/gas-fee';
 import { useBalancesResponses, useStakingTokenMetadata } from '../../../state/shared';
 import { transferableBalancesResponsesSelector } from '../../../state/send/helpers';
-import { useRefreshFee } from '../../v2/transfer-layout/send-page/use-refresh-fee';
+import { useRefreshFee } from '../../v2/transfer/send-page/use-refresh-fee';
 
 export const SendForm = () => {
   // Retrieve the staking token metadata and gas prices from the zustand

apps/minifront/src/components/v2/portfolio-layout/assets-page/index.tsx
@@ -1,107 +1,0 @@
-import { Density } from '@penumbra-zone/ui-deprecated/Density';
-import { Table } from '@penumbra-zone/ui-deprecated/Table';
-import { BalancesByAccount, groupByAccount, useBalancesResponses } from '../../../../state/shared';
-import { shouldDisplay } from '../../../../fetchers/balances/should-display';
-import { sortByPriorityScore } from '../../../../fetchers/balances/by-priority-score';
-import { BalancesResponse } from '@penumbra-zone/protobuf/penumbra/view/v1/view_pb';
-import { getMetadataFromBalancesResponse } from '@penumbra-zone/getters/balances-response';
-import { PagePath } from '../../../metadata/paths';
-import { getAddressIndex } from '@penumbra-zone/getters/address-view';
-import { AbridgedZQueryState } from '@penumbra-zone/zquery/src/types';
-import { ValueViewComponent } from '@penumbra-zone/ui-deprecated/ValueViewComponent';
-import { EquivalentValues } from './equivalent-values';
-import { TableTitle } from './table-title';
-import { Link } from 'react-router-dom';
-import { Button } from '@penumbra-zone/ui-deprecated/Button';
-import { ArrowRightLeft } from 'lucide-react';
-import { useAnimationDeferredValue } from '@penumbra-zone/ui-deprecated/hooks/useAnimationDeferredValue';
-import { ConditionalWrap } from '@penumbra-zone/ui-deprecated/ConditionalWrap';
-import { LayoutGroup } from 'framer-motion';
-
-const getTradeLink = (balance: BalancesResponse): string => {
-  const metadata = getMetadataFromBalancesResponse.optional(balance);
-  const accountIndex = getAddressIndex(balance.accountAddress).account;
-  const accountQuery = accountIndex ? `&account=${accountIndex}` : '';
-  return metadata ? `${PagePath.SWAP}?from=${metadata.symbol}${accountQuery}` : PagePath.SWAP;
-};
-
-const filteredBalancesByAccountSelector = (
-  zQueryState: AbridgedZQueryState<BalancesResponse[]>,
-): BalancesByAccount[] =>
-  zQueryState.data?.filter(shouldDisplay).sort(sortByPriorityScore).reduce(groupByAccount, []) ??
-  [];
-
-const BUTTON_CELL_WIDTH_PX = '56px';
-
-export const AssetsPage = () => {
-  const balancesByAccount = useBalancesResponses({
-    select: filteredBalancesByAccountSelector,
-    shouldReselect: (before, after) => before?.data !== after.data,
-  });
-
-  const deferredBalancesByAccount = useAnimationDeferredValue(balancesByAccount);
-
-  return deferredBalancesByAccount?.map((account, index) => (
-    <ConditionalWrap
-      key={account.account}
-      // Only wrap the first table in the `<LayoutGroup />`, since that's the
-      // only one that will be animating when transitioning to the assets table.
-      if={index === 0}
-      then={children => <LayoutGroup id='portfolioContent'>{children}</LayoutGroup>}
-    >
-      <Table
-        tableLayout='fixed'
-        title={<TableTitle account={account} />}
-        motion={{ layoutId: index === 0 ? 'table' : undefined }}
-      >
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th
-              width={`calc(50% - (${BUTTON_CELL_WIDTH_PX} / 2))`}
-              motion={{ layoutId: index === 0 ? 'th1' : undefined }}
-            >
-              Asset
-            </Table.Th>
-            <Table.Th
-              width={`calc(50% - (${BUTTON_CELL_WIDTH_PX} / 2))`}
-              motion={{ layoutId: index === 0 ? 'th2' : undefined }}
-            >
-              Estimate
-            </Table.Th>
-            <Table.Th
-              width={BUTTON_CELL_WIDTH_PX}
-              motion={{ layoutId: index === 0 ? 'th3' : undefined }}
-            />
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>
-          {account.balances.map((balance, index) => (
-            <Table.Tr key={index}>
-              <Table.Td vAlign='top'>
-                <ValueViewComponent valueView={balance.balanceView} context='table' />
-              </Table.Td>
-              <Table.Td vAlign='top'>
-                <EquivalentValues valueView={balance.balanceView} />
-              </Table.Td>
-
-              <Table.Td>
-                <div className='h-8 w-10 overflow-hidden'>
-                  <Link
-                    to={getTradeLink(balance)}
-                    className='block translate-x-full opacity-0 transition [tr:hover>td>div>&]:translate-x-0 [tr:hover>td>div>&]:opacity-100'
-                  >
-                    <Density compact>
-                      <Button icon={ArrowRightLeft} iconOnly>
-                        Trade
-                      </Button>
-                    </Density>
-                  </Link>
-                </div>
-              </Table.Td>
-            </Table.Tr>
-          ))}
-        </Table.Tbody>
-      </Table>
-    </ConditionalWrap>
-  ));
-};

apps/minifront/src/components/v2/portfolio-layout/index.tsx
@@ -1,51 +1,0 @@
-import { Card } from '@penumbra-zone/ui-deprecated/Card';
-import { Outlet, useNavigate } from 'react-router-dom';
-import { Grid } from '@penumbra-zone/ui-deprecated/Grid';
-import { Tabs } from '@penumbra-zone/ui-deprecated/Tabs';
-import { usePagePath } from '../../../fetchers/page-path';
-import { PagePath } from '../../metadata/paths';
-import { AssetsCardTitle } from './assets-card-title';
-import { TransactionsCardTitle } from './transactions-card-title';
-import { motion } from 'framer-motion';
-import { getV2Link } from '../get-v2-link.ts';
-
-const CARD_TITLE_BY_PATH = {
-  [getV2Link(PagePath.DASHBOARD)]: <AssetsCardTitle />,
-  [getV2Link(PagePath.TRANSACTIONS)]: <TransactionsCardTitle />,
-};
-
-const TABS_OPTIONS = [
-  { label: 'Assets', value: getV2Link(PagePath.DASHBOARD) },
-  { label: 'Transactions', value: getV2Link(PagePath.TRANSACTIONS) },
-];
-
-export const PortfolioLayout = () => {
-  const pagePath = usePagePath();
-  const navigate = useNavigate();
-
-  return (
-    <Grid container>
-      <Grid mobile={0} tablet={2} desktop={3} xl={4} />
-
-      <Grid tablet={8} desktop={6} xl={4}>
-        <Card
-          title={CARD_TITLE_BY_PATH[getV2Link(pagePath)]}
-          motion={{ layout: true, layoutId: 'main' }}
-        >
-          <motion.div layout>
-            <Tabs
-              value={getV2Link(pagePath)}
-              onChange={value => navigate(value)}
-              options={TABS_OPTIONS}
-              actionType='accent'
-            />
-          </motion.div>
-
-          <Outlet />
-        </Card>
-      </Grid>
-
-      <Grid mobile={0} tablet={2} desktop={3} xl={4} />
-    </Grid>
-  );
-};

apps/minifront/src/components/v2/portfolio/assets/AssetCard/index.tsx
@@ -1,0 +1,62 @@
+import { ReactNode } from 'react';
+import { Card } from '@penumbra-zone/ui';
+import { AssetList } from './AssetList';
+import { AccountMock, mockAccounts } from './mock';
+import { InfoDialog } from '../InfoDialog';
+
+export interface AssetCardProps {
+  /**
+   * Title of the card
+   */
+  title?: string;
+  /**
+   * List of accounts with assets
+   */
+  accounts?: AccountMock[];
+  /**
+   * Optional header action component
+   */
+  headerAction?: ReactNode;
+  /**
+   * Optional content to display at the end of the title row
+   * This is useful for displaying icons, buttons, or other elements
+   * alongside the title
+   */
+  /**
+   * Whether to show the InfoButton
+   * When true, the InfoButton will be displayed next to the title
+   */
+  showInfoButton?: boolean;
+}
+
+/**
+ * AssetCard component displays a list of assets grouped by accounts.
+ * It follows the Penumbra design system and uses the Card component as a container.
+ *
+ * By default, it shows an InfoButton that provides information about the shielded portfolio.
+ */
+export const AssetCard = ({
+  title = 'Your Assets',
+  accounts = mockAccounts,
+  headerAction,
+  showInfoButton = true,
+}: AssetCardProps) => {
+  // Use InfoButton as endContent if showInfoButton is true and no custom endContent is provided
+  const finalEndContent = showInfoButton ? <InfoDialog /> : undefined;
+
+  return (
+    <Card title={title} headerAction={headerAction} endContent={finalEndContent}>
+      <AssetList accounts={accounts} />
+    </Card>
+  );
+};
+
+// Re-export sub-components and types
+export { AssetList } from './AssetList';
+export type { AssetListProps } from './AssetList';
+export { AssetListItem } from './AssetListItem';
+export type { AssetListItemProps } from './AssetListItem';
+export { AccountSection } from './AccountSection';
+export type { AccountSectionProps } from './AccountSection';
+export type { AccountMock, AssetMock } from './mock';
+export { mockAccounts } from './mock';

commit 76a1a912d2f7fc5d6e71b2c9edea89a0ac3d201f

IBC: fix manual flow for shielding assets (#2426)
## Description of Changes

- fixes side-effect of a regression from
https://github.com/penumbra-zone/web/pull/2364 where assets couldn't be
selected when manually shielding,
- truncates overflow IBC asset id for manual deposits,
- QoL improvements towards better, easier to understanding naming
conventions

-----------------------------


https://github.com/user-attachments/assets/e01bf6a5-81a7-4fac-82b5-37e1f260ae95

## Related Issue

Closes #2420

Closes #2422

Closes #2382

https://github.com/penumbra-zone/web/issues/2420 and
https://github.com/penumbra-zone/web/issues/2382 (part of broader
tracking issue with IBC-related bugs:
https://github.com/penumbra-zone/web/issues/2427)

## Checklist Before Requesting Review

- [x] I have ensured that any relevant minifront changes do not cause
the existing extension to break.

---------

Co-authored-by: Max Korsunov <maxkors2014@gmail.com>

apps/minifront/src/components/ibc/deposit-manual/assets-table.tsx
@@ -45,7 +45,7 @@ export const AssetsTable = () => {
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHead className='w-[100px]'>Denom</TableHead>
+            <TableHead className='w-[100px]'>Asset</TableHead>
             <TableHead className='text-right'>Amount</TableHead>
           </TableRow>
         </TableHeader>

apps/minifront/src/components/ibc/deposit-manual/chain-dropdown.tsx
@@ -71,7 +71,7 @@ export const ChainDropdown = () => {
               <span className='mt-0.5'>{selected?.label}</span>
             </div>
           ) : (
-            'Shield assets from'
+            'Select Source Chain'
           )}
           <ChevronsUpDown className='ml-2 size-4 shrink-0 opacity-50' />
         </Button>

apps/minifront/src/components/ibc/deposit-manual/ibc-in-request.tsx
@@ -38,7 +38,7 @@ export const IbcInRequest = () => {
 
   return (
     <div className='flex flex-col items-center gap-2'>
-      <div className='font-bold text-white'>Issue Ibc-in Request</div>
+      <div className='font-bold text-white'>Initiate Shielding Transfer</div>
       {isUnsupportedAsset && (
         <div className='justify-center rounded bg-amber-200 p-2 text-center italic text-stone-700'>
           Note: only <b>native</b> assets at this time are eligible for ibc&apos;ing in. Unwind them
@@ -53,12 +53,14 @@ export const IbcInRequest = () => {
           <SelectContent className='max-w-52 bg-white text-stone-700'>
             {data.map(b => (
               <SelectItem value={b.displayDenom} key={b.displayDenom}>
-                <div className='flex items-center gap-2 text-stone-700'>
+                <div className='flex max-w-[180px] items-center gap-2 text-stone-700'>
                   <Avatar className='size-6'>
                     <AvatarImage src={getIconWithUmFallback(b)} />
                     <Identicon uniqueIdentifier={b.displayDenom} type='gradient' size={22} />
                   </Avatar>
-                  <span className=''>{b.displayDenom}</span>
+                  <span className='truncate text-sm' title={b.displayDenom}>
+                    {b.displayDenom}
+                  </span>
                 </div>
               </SelectItem>
             ))}

apps/minifront/src/components/ibc/tabs.ts
@@ -3,7 +3,7 @@ import { PagePath } from '../metadata/paths.ts';
 export type ShieldTab = PagePath.DEPOSIT_SKIP | PagePath.DEPOSIT_MANUAL | PagePath.WITHDRAW;
 
 export const shieldTabs = [
-  { title: 'Deposit (Skip)', href: PagePath.DEPOSIT_SKIP, enabled: true },
-  { title: 'Deposit (Manual)', href: PagePath.DEPOSIT_MANUAL, enabled: true },
-  { title: 'Withdraw', href: PagePath.WITHDRAW, enabled: true },
+  { title: 'Deposit with Skip', href: PagePath.DEPOSIT_SKIP, enabled: true },
+  { title: 'Manual Deposit', href: PagePath.DEPOSIT_MANUAL, enabled: true },
+  { title: 'Withdraw Funds', href: PagePath.WITHDRAW, enabled: true },
 ];

apps/minifront/src/index.css
@@ -1,8 +1,5 @@
 /* apps/minifront/src/index.css */
 
-/* Import the prefixed CSS for v2 components */
-@import '@penumbra-zone/ui/style-prefixed.css';
-
 /* Tailwind directives for minifront's own styling */
 @tailwind base;
 @tailwind components;

apps/minifront/src/main.tsx
@@ -10,11 +10,7 @@ import { useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { rootRouter } from './components/root-router';
 import { PenumbraUIProvider } from '@penumbra-zone/ui-deprecated/PenumbraUIProvider';
-
-// Add back the unprefixed CSS import that was working for the header
 import '@penumbra-zone/ui/style.css';
-// Then also import our custom CSS which contains the prefixed CSS import for v2 components
-import './index.css';
 
 const Main = () => {
   const [queryClient] = useState(() => new QueryClient());

packages/ui-deprecated/components/ui/button/index.tsx
@@ -50,10 +50,14 @@ export interface ButtonProps
 }
 
 // Converted from forwardRef to regular component
-const Button = ({ className, variant, size, asChild = false, ref, ...props }: ButtonProps) => {
-  const Comp = asChild ? Slot : 'button';
-  return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
-};
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'button';
+    return (
+      <Comp ref={ref} className={cn(buttonVariants({ variant, size, className }))} {...props} />
+    );
+  },
+);
 Button.displayName = 'Button';
 
 export { Button, buttonVariants };

packages/ui-deprecated/components/ui/command/index.tsx
@@ -11,16 +11,12 @@ export interface CommandProps extends React.ComponentPropsWithoutRef<typeof Comm
   ref?: React.Ref<HTMLDivElement>;
 }
 
-const Command = ({ className, ref, ...props }: CommandProps) => (
-  <CommandPrimitive
-    ref={ref}
-    className={cn(
-      'flex h-full w-full flex-col overflow-hidden rounded-md bg-popover text-popover-foreground',
-      className,
-    )}
-    {...props}
-  />
-);
+const Command = React.forwardRef<
+  React.ComponentRef<typeof CommandPrimitive>,
+  React.ComponentPropsWithoutRef<typeof CommandPrimitive>
+>(({ className, ...props }, ref) => (
+  <CommandPrimitive ref={ref} className={cn(className)} {...props} />
+));
 Command.displayName = CommandPrimitive.displayName;
 
 type CommandDialogProps = DialogProps;
@@ -42,20 +38,22 @@ export interface CommandInputProps
   ref?: React.Ref<HTMLInputElement>;
 }
 
-const CommandInput = ({ className, ref, ...props }: CommandInputProps) => (
-  // eslint-disable-next-line react/no-unknown-property -- TODO: justify
-  <div className='flex items-center border-b px-3' cmdk-input-wrapper=''>
+const CommandInput = React.forwardRef<
+  React.ComponentRef<typeof CommandPrimitive.Input>,
+  React.ComponentPropsWithoutRef<typeof CommandPrimitive.Input>
+>(({ className, ...props }, ref) => (
+  <div className='flex items-center border-b px-3'>
     <Search className='mr-2 size-4 shrink-0 opacity-50' />
     <CommandPrimitive.Input
       ref={ref}
       className={cn(
-        'flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50',
+        'flex h-11 w-full bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground',
         className,
       )}
       {...props}
     />
   </div>
-);
+));
 CommandInput.displayName = CommandPrimitive.Input.displayName;
 
 export interface CommandListProps
@@ -63,13 +61,16 @@ export interface CommandListProps
   ref?: React.Ref<HTMLDivElement>;
 }
 
-const CommandList = ({ className, ref, ...props }: CommandListProps) => (
+const CommandList = React.forwardRef<
+  React.ComponentRef<typeof CommandPrimitive.List>,
+  React.ComponentPropsWithoutRef<typeof CommandPrimitive.List>
+>(({ className, ...props }, ref) => (
   <CommandPrimitive.List
     ref={ref}
     className={cn('max-h-[300px] overflow-y-auto overflow-x-hidden', className)}
     {...props}
   />
-);
+));
 CommandList.displayName = CommandPrimitive.List.displayName;
 
 export interface CommandEmptyProps
@@ -91,16 +92,16 @@ export interface CommandGroupProps
   ref?: React.Ref<HTMLDivElement>;
 }
 
-const CommandGroup = ({ className, ref, ...props }: CommandGroupProps) => (
+const CommandGroup = React.forwardRef<
+  React.ComponentRef<typeof CommandPrimitive.Group>,
+  React.ComponentPropsWithoutRef<typeof CommandPrimitive.Group>
+>(({ className, ...props }, ref) => (
   <CommandPrimitive.Group
     ref={ref}
-    className={cn(
-      'overflow-hidden p-1 text-foreground [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground',
-      className,
-    )}
+    className={cn('overflow-hidden p-1 text-foreground', className)}
     {...props}
   />
-);
+));
 CommandGroup.displayName = CommandPrimitive.Group.displayName;
 
 export interface CommandSeparatorProps
@@ -122,16 +123,19 @@ export interface CommandItemProps
   ref?: React.Ref<HTMLDivElement>;
 }
 
-const CommandItem = ({ className, ref, ...props }: CommandItemProps) => (
+const CommandItem = React.forwardRef<
+  React.ComponentRef<typeof CommandPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof CommandPrimitive.Item>
+>(({ className, ...props }, ref) => (
   <CommandPrimitive.Item
     ref={ref}
     className={cn(
-      'relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+      'relative flex cursor-default select-none items-center rounded-sm px-3 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground',
       className,
     )}
     {...props}
   />
-);
+));
 CommandItem.displayName = CommandPrimitive.Item.displayName;
 
 const CommandShortcut = ({ className, ...props }: React.HTMLAttributes<HTMLSpanElement>) => {