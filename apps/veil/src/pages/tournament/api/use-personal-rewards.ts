afa150d79919c921d0b019c99b9d902144496caf
Tal Derei <talderei99@gmail.com>
2025-05-27 15:04:56 -0700

    don't use stale inifnity in personal rewards to use correct rewards in social card

--- apps/veil/src/pages/tournament/api/use-personal-rewards.ts
+++ apps/veil/src/pages/tournament/api/use-personal-rewards.ts
@@ -106,7 +106,6 @@ export const usePersonalRewards = (
 
   const query = useQuery({
     queryKey: ['total-voting-rewards', subaccount, page, limit, sortKey, sortDirection, epoch],
-    staleTime: Infinity,
     enabled:
       connectionStore.connected &&
       !!epoch &&
