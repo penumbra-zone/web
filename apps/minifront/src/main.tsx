3dedc19d17f2f1018c28fbc33f1739f9856250c2
Dave <56094367+DDX1@users.noreply.github.com>
2025-06-05 17:01:10 +0200

    fix(minifront): double global.css import after merging main

--- apps/minifront/src/main.tsx
+++ apps/minifront/src/main.tsx
@@ -4,7 +4,6 @@
 // references.
 import './state';
 import '@penumbra-zone/ui-deprecated/styles/globals.css';
-import './index.css';
 
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -12,7 +11,6 @@ import { useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { rootRouter } from './components/root-router';
 import { PenumbraUIProvider } from '@penumbra-zone/ui-deprecated/PenumbraUIProvider';
-import '@penumbra-zone/ui-deprecated/styles/globals.css';
 
 const Main = () => {
   const [queryClient] = useState(() => new QueryClient());
