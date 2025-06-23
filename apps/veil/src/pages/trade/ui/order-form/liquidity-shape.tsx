dbde400c481f9694b13e759ef44a9514b79a82fd
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2025-06-23 14:37:18 -0300

    Liquidity shape (#2485)
    ## Description of Changes
    
    Closes https://github.com/penumbra-zone/web/issues/2403
    
    Implements the liquidity shape to the order form:
    
    <img width="329" alt="image"
    src="https://github.com/user-attachments/assets/287fe2d9-7db1-4767-bce0-69bea82062cf"
    />
    
    
    Pyramid shape (stable kind)
    <img width="920" alt="image"
    src="https://github.com/user-attachments/assets/089d3565-a0e6-40d3-b61f-14e8f0430722"
    />
    
    
    
    ## Related Issue
    
    Link to the issue this PR addresses.
    
    ## Checklist Before Requesting Review
    
    - [ ] I have ensured that any relevant minifront changes do not cause
    the existing extension to break.

---
+++ apps/veil/src/pages/trade/ui/order-form/liquidity-shape.tsx
@@ -0,0 +1,102 @@
+import { LiquidityDistributionShape } from '@/shared/math/position';
+import { Text } from '@penumbra-zone/ui/Text';
+import cn from 'clsx';
+import ConcentratedDefault from '@/shared/assets/liquidity-shapes/Type=Concentrated, State=Default.svg';
+import ConcentratedHover from '@/shared/assets/liquidity-shapes/Type=Concentrated, State=Hover.svg';
+import ConcentratedSelected from '@/shared/assets/liquidity-shapes/Type=Concentrated, State=Selected.svg';
+import StablekindDefault from '@/shared/assets/liquidity-shapes/Type=Stablekind, State=Default.svg';
+import StablekindHover from '@/shared/assets/liquidity-shapes/Type=Stablekind, State=Hover.svg';
+import StablekindSelected from '@/shared/assets/liquidity-shapes/Type=Stablekind, State=Selected.svg';
+import VolatileDefault from '@/shared/assets/liquidity-shapes/Type=Volatile, State=Default.svg';
+import VolatileHover from '@/shared/assets/liquidity-shapes/Type=Volatile, State=Hover.svg';
+import VolatileSelected from '@/shared/assets/liquidity-shapes/Type=Volatile, State=Selected.svg';
+import { useState } from 'react';
+import { Tooltip } from '@penumbra-zone/ui/Tooltip';
+
+const shapeMapping = {
+  [LiquidityDistributionShape.FLAT]: {
+    text: 'Linear',
+    default: ConcentratedDefault,
+    hover: ConcentratedHover,
+    selected: ConcentratedSelected,
+    tooltipMessage: '',
+    disabled: true,
+  },
+  [LiquidityDistributionShape.PYRAMID]: {
+    text: 'Locally stable',
+    default: StablekindDefault,
+    hover: StablekindHover,
+    selected: StablekindSelected,
+    tooltipMessage:
+      'Concentrates liquidity around the current price. Best for pairs with low volatility or tight correlation.',
+    disabled: false,
+  },
+  [LiquidityDistributionShape.INVERTED_PYRAMID]: {
+    text: 'Volatile',
+    default: VolatileDefault,
+    hover: VolatileHover,
+    selected: VolatileSelected,
+    tooltipMessage:
+      'Spreads liquidity across a wider range to capture more price movement. Ideal for volatile pairs.',
+    disabled: false,
+  },
+};
+
+export const LiquidityShape = ({
+  shape,
+  onClick,
+  selected,
+}: {
+  shape: LiquidityDistributionShape;
+  onClick: () => void;
+  selected: boolean;
+}) => {
+  const {
+    text,
+    default: Default,
+    hover: Hover,
+    selected: Selected,
+    disabled,
+    tooltipMessage,
+  } = shapeMapping[shape];
+  const [isHovered, setIsHovered] = useState(false);
+
+  if (disabled) {
+    return null;
+  }
+
+  return (
+    <button
+      key={shape}
+      className={cn(
+        'p-3 rounded-sm flex-1 min-h-10 border flex flex-col items-center justify-center active:opacity-80 active:duration-[0.1s]',
+        selected ? 'bg-other-tonalFill5' : undefined,
+        selected ? 'border-primary-main' : 'border-transparent',
+      )}
+      onClick={onClick}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <Tooltip message={tooltipMessage}>
+        <div className='relative flex justify-center w-full mb-1'>
+          <Default />
+          <Hover
+            className={cn(
+              'absolute z-10 inset-0 w-full h-full transition-opacity duration-200',
+              isHovered && !selected ? 'opacity-100' : 'opacity-0',
+            )}
+          />
+          <Selected
+            className={cn(
+              'absolute z-20 inset-0 w-full h-full transition-opacity duration-200',
+              selected ? 'opacity-100' : 'opacity-0',
+            )}
+          />
+        </div>
+        <Text detail color='text.secondary'>
+          {text}
+        </Text>
+      </Tooltip>
+    </button>
+  );
+};
