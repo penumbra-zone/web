23ed9e2c73ac2663a3186495d9498ee8d0e4a6d2
Max Korsunov <maxkors2014@gmail.com>
2025-06-23 15:34:12 +0400

    fix(ui): bring back theme font info

--- packages/ui/src/theme/theme.ts
+++ packages/ui/src/theme/theme.ts
@@ -10,6 +10,27 @@ export const theme = {
     lg: 1200,
     xl: 1600,
   },
+  font: {
+    default: 'Poppins',
+    mono: 'Iosevka Term, monospace',
+    heading: 'Work Sans',
+  },
+  fontSize: {
+    text9xl: '8rem',
+    text8xl: '6rem',
+    text7xl: '4.5rem',
+    text6xl: '3.75rem',
+    text5xl: '3rem',
+    text4xl: '2.25rem',
+    text3xl: '1.875rem',
+    text2xl: '1.5rem',
+    textXl: '1.25rem',
+    textLg: '1.125rem',
+    textBase: '1rem',
+    textSm: '0.875rem',
+    textXs: '0.75rem',
+    textXxs: '0.6875rem',
+  },
   color: {
     neutral: {
       main: '',
