d16475801e68fed76ad2c3b8882bb4b4c870ddf7
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2025-07-02 18:23:00 -0300

    Add pair icons & expansion row

--- apps/veil/src/entities/liquidity/ui/grouped-liquidity-row.tsx
+++ apps/veil/src/entities/liquidity/ui/grouped-liquidity-row.tsx
@@ -4,8 +4,10 @@ import { ChevronDown, ChevronUp } from 'lucide-react';
 import { TableCell } from '@penumbra-zone/ui/TableCell';
 import { Button } from '@penumbra-zone/ui/Button';
 import { Text } from '@penumbra-zone/ui/Text';
+import { useRegistry } from '@/shared/api/registry';
 import { DisplayLP } from '../model/get-display-lps';
 import { LiquidityRow } from './liquidity-row';
+import Image from 'next/image';
 
 interface GroupedLiquidityRowProps {
   pair: string;
@@ -13,6 +15,7 @@ interface GroupedLiquidityRowProps {
 }
 
 export const GroupedLiquidityRow = ({ pair, lps }: GroupedLiquidityRowProps) => {
+  const [base, quote] = pair.split('/');
   const params = useParams<{
     baseSymbol: string;
     quoteSymbol: string;
@@ -23,11 +26,18 @@ export const GroupedLiquidityRow = ({ pair, lps }: GroupedLiquidityRowProps) => 
   const toggleExpanded = () => setIsExpanded(!isExpanded);
 
   useEffect(() => {
-    const [base, quote] = pair.split('/');
     if (baseSymbol === base && quoteSymbol === quote) {
       setIsExpanded(true);
     }
-  }, [baseSymbol, quoteSymbol, pair]);
+  }, [baseSymbol, quoteSymbol, base, quote]);
+
+  const { data: registry } = useRegistry();
+  const baseAsset = registry
+    .getAllAssets()
+    .find((asset: { symbol: string }) => asset.symbol === base);
+  const quoteAsset = registry
+    .getAllAssets()
+    .find((asset: { symbol: string }) => asset.symbol === quote);
 
   return (
     <>
@@ -36,6 +46,30 @@ export const GroupedLiquidityRow = ({ pair, lps }: GroupedLiquidityRowProps) => 
         onClick={() => setIsExpanded(!isExpanded)}
       >
         <TableCell>
+          <div className='relative mr-1 shrink-0 pr-2'>
+            {baseAsset?.images[0]?.svg && (
+              <Image
+                className='relative z-10 h-4 w-4 rounded-full'
+                src={baseAsset.images[0].svg}
+                alt={baseAsset.symbol}
+                width={16}
+                height={16}
+              />
+            )}
+            {quoteAsset?.images[0]?.svg && (
+              <Image
+                className={
+                  baseAsset?.images[0]?.svg
+                    ? 'absolute top-0 left-2 z-0 h-4 w-4 rounded-full'
+                    : 'relative z-10 h-4 w-4 rounded-full'
+                }
+                src={quoteAsset.images[0].svg}
+                alt={quoteAsset.symbol}
+                width={16}
+                height={16}
+              />
+            )}
+          </div>
           <Text detailTechnical whitespace='nowrap'>
             {pair} ({lps.length})
           </Text>
--- apps/veil/src/entities/liquidity/ui/liquidity-row.tsx
+++ apps/veil/src/entities/liquidity/ui/liquidity-row.tsx
@@ -6,6 +6,7 @@ import { ValueViewComponent } from '@penumbra-zone/ui/ValueView';
 import { ThemeColor } from '@penumbra-zone/ui/theme';
 import { DisplayLP } from '../model/get-display-lps';
 import { useState } from 'react';
+import { Density } from '@penumbra-zone/ui/Density';
 
 interface LiquidityRowProps {
   lp: DisplayLP;
@@ -27,75 +28,117 @@ export const LiquidityRow = ({ lp }: LiquidityRowProps) => {
   const toggleExpanded = () => setIsExpanded(!isExpanded);
 
   return (
-    <div
-      className={`col-span-10 grid grid-cols-subgrid ${isExpanded ? 'bg-other-tonal-fill10' : 'bg-other-tonal-fill5'}`}
-    >
-      <TableCell cell>
-        <Text xs whitespace='nowrap'>
-          {lp.date}
-        </Text>
-      </TableCell>
-      <TableCell cell>
-        <Text xs whitespace='nowrap'>
-          {lp.liquidityShape}
-        </Text>
-      </TableCell>
-      <TableCell cell>
-        <Text xs whitespace='nowrap' color={getStatusColor(lp.status)}>
-          {lp.status}
-        </Text>
-      </TableCell>
-      <TableCell cell>
-        <ValueViewComponent valueView={lp.minPrice} />
-      </TableCell>
-      <TableCell cell>
-        <ValueViewComponent valueView={lp.maxPrice} />
-      </TableCell>
-      <TableCell cell>
-        <ValueViewComponent valueView={lp.currentValue} />
-      </TableCell>
-      <TableCell cell>
-        <ValueViewComponent valueView={lp.volume} />
-      </TableCell>
-      <TableCell cell>
-        <ValueViewComponent valueView={lp.feesEarned} />
-      </TableCell>
-      <TableCell cell>
-        <Text
-          xs
-          whitespace='nowrap'
-          color={lp.pnlPercentage >= 0 ? 'success.light' : 'destructive.light'}
-        >
-          {lp.pnlPercentage > 0 ? '+' : ''}
-          {lp.pnlPercentage}%
-        </Text>
-      </TableCell>
-      <TableCell justify='end'>
-        {lp.status === 'Closed' ? (
-          <Button
-            iconOnly
-            icon={ArrowBigDownIcon}
-            actionType='success'
-            priority='secondary'
-            onClick={toggleExpanded}
+    <>
+      <div
+        className={`col-span-10 grid grid-cols-subgrid ${isExpanded ? 'bg-other-tonal-fill10' : 'bg-other-tonal-fill5'}`}
+      >
+        <TableCell cell>
+          <Text xs whitespace='nowrap'>
+            {lp.date}
+          </Text>
+        </TableCell>
+        <TableCell cell>
+          <Text xs whitespace='nowrap'>
+            {lp.liquidityShape}
+          </Text>
+        </TableCell>
+        <TableCell cell>
+          <Text xs whitespace='nowrap' color={getStatusColor(lp.status)}>
+            {lp.status}
+          </Text>
+        </TableCell>
+        <TableCell cell>
+          <ValueViewComponent valueView={lp.minPrice} />
+        </TableCell>
+        <TableCell cell>
+          <ValueViewComponent valueView={lp.maxPrice} />
+        </TableCell>
+        <TableCell cell>
+          <ValueViewComponent valueView={lp.currentValue} />
+        </TableCell>
+        <TableCell cell>
+          <ValueViewComponent valueView={lp.volume} />
+        </TableCell>
+        <TableCell cell>
+          <ValueViewComponent valueView={lp.feesEarned} />
+        </TableCell>
+        <TableCell cell>
+          <Text
+            xs
+            whitespace='nowrap'
+            color={lp.pnlPercentage >= 0 ? 'success.light' : 'destructive.light'}
           >
+            {lp.pnlPercentage > 0 ? '+' : ''}
+            {lp.pnlPercentage}%
+          </Text>
+        </TableCell>
+        <TableCell justify='end'>
+          {lp.status === 'Closed' ? (
+            <Button
+              iconOnly
+              icon={ArrowBigDownIcon}
+              actionType='success'
+              priority='secondary'
+              onClick={toggleExpanded}
+            >
+              &nbsp;
+            </Button>
+          ) : (
+            <Button
+              iconOnly
+              icon={X}
+              actionType='destructive'
+              priority='secondary'
+              onClick={toggleExpanded}
+            >
+              &nbsp;
+            </Button>
+          )}
+          <Button iconOnly icon={isExpanded ? ChevronUp : ChevronDown} onClick={toggleExpanded}>
             &nbsp;
           </Button>
-        ) : (
-          <Button
-            iconOnly
-            icon={X}
-            actionType='destructive'
-            priority='secondary'
-            onClick={toggleExpanded}
-          >
-            &nbsp;
-          </Button>
-        )}
-        <Button iconOnly icon={isExpanded ? ChevronUp : ChevronDown} onClick={toggleExpanded}>
-          &nbsp;
-        </Button>
-      </TableCell>
-    </div>
+        </TableCell>
+      </div>
+      {isExpanded && (
+        <Density compact>
+          <div className={`col-span-10 grid grid-cols-subgrid bg-other-tonal-fill10`}>
+            <TableCell>&nbsp;</TableCell>
+            <TableCell>&nbsp;</TableCell>
+            <TableCell>&nbsp;</TableCell>
+            <TableCell>&nbsp;</TableCell>
+            <TableCell>&nbsp;</TableCell>
+            <TableCell cell>
+              {/* TODO: replace with asset 1 & 2 */}
+              <div className='flex flex-col gap-2'>
+                <Density slim>
+                  <ValueViewComponent valueView={lp.currentValue} />
+                  <ValueViewComponent valueView={lp.currentValue} />
+                </Density>
+              </div>
+            </TableCell>
+            <TableCell cell>
+              {/* TODO: replace with asset 1 & 2 */}
+              <div className='flex flex-col gap-2'>
+                <Density slim>
+                  <ValueViewComponent valueView={lp.volume} />
+                  <ValueViewComponent valueView={lp.volume} />
+                </Density>
+              </div>
+            </TableCell>
+            <TableCell cell>
+              {/* TODO: replace with asset 1 & 2 */}
+              <div className='flex flex-col gap-2'>
+                <Density slim>
+                  <ValueViewComponent valueView={lp.feesEarned} />
+                  <ValueViewComponent valueView={lp.feesEarned} />
+                </Density>
+              </div>
+            </TableCell>
+            <TableCell>&nbsp;</TableCell>
+            <TableCell>&nbsp;</TableCell>
+          </div>
+        </Density>
+      )}
+    </>
   );
 };
