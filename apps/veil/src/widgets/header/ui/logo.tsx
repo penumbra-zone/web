89a3df925e5575d3aafe89218feef7ad589a75ff
Erwan Or <erwanor@penumbralabs.xyz>
2025-07-07 09:51:27 -0400

    veil(explore): try new logo

--- apps/veil/src/widgets/header/ui/logo.tsx
+++ apps/veil/src/widgets/header/ui/logo.tsx
@@ -1,11 +1,17 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { PagePath } from '@/shared/const/pages';
-import VeilLogo from '../assets/veil-combined-logo.svg';
 
 export const HeaderLogo = () => {
   return (
     <Link className='flex h-8 items-center' href={PagePath.Explore}>
-      <VeilLogo width={96} height={15} />
+      <Image 
+        src='/assets/veil.svg' 
+        alt='Veil' 
+        width={120} 
+        height={40}
+        priority
+      />
     </Link>
   );
 };
