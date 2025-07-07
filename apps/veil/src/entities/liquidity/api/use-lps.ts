5ac3bda7dbe1adb0419260c5918ae3a5b0b4cf93
Tal Derei <talderei99@gmail.com>
2025-07-07 09:58:23 -0700

    add other fields to useLP api

--- apps/veil/src/entities/liquidity/api/use-lps.ts
+++ apps/veil/src/entities/liquidity/api/use-lps.ts
@@ -3,19 +3,34 @@ import { AddressIndex } from '@penumbra-zone/protobuf/penumbra/core/keys/v1/keys
 import { useQuery } from '@tanstack/react-query';
 import { ViewService } from '@penumbra-zone/protobuf/penumbra/view/v1/view_connect';
 import { LpPositionBundleResponse_Entry } from '@penumbra-zone/protobuf/penumbra/view/v1/view_pb';
-import { PositionMetadata } from '@penumbra-zone/protobuf/penumbra/core/component/dex/v1/dex_pb';
+import {
+  PositionMetadata,
+  PositionState,
+  TradingPair,
+} from '@penumbra-zone/protobuf/penumbra/core/component/dex/v1/dex_pb';
 
 /**
  * Must be used within the `observer` mobX HOC
  */
-export const useLps = ({ subaccount }: { subaccount: number }) => {
+export const useLps = ({
+  subaccount,
+  positionMetadata,
+  positionState,
+  tradingPair,
+}: {
+  subaccount: number;
+  positionMetadata?: PositionMetadata;
+  positionState?: PositionState;
+  tradingPair?: TradingPair;
+}) => {
   const { data: positionStrategyBundles } = useQuery({
     queryKey: ['lp-position-bundle', subaccount],
     queryFn: async () => {
       const stream = penumbra.service(ViewService).lpPositionBundle({
         subaccount: new AddressIndex({ account: subaccount }),
-        // Dummy.
-        positionMetadata: new PositionMetadata({ strategy: 4, identifier: 0 }),
+        positionMetadata,
+        positionState,
+        tradingPair,
       });
 
       const entries: LpPositionBundleResponse_Entry[] = [];
