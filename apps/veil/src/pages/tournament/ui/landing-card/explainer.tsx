2c074f18c629b78f4fcc2bc8d8a8eba34c556357
Max Korsunov <maxkors2014@gmail.com>
2025-07-08 16:38:25 +0400

    fix(veil): #2578: learn more (#2586)
    Closes #2578 
    
    Learn More button in LQT explainer section

--- apps/veil/src/pages/tournament/ui/landing-card/explainer.tsx
+++ apps/veil/src/pages/tournament/ui/landing-card/explainer.tsx
@@ -1,5 +1,9 @@
 import Image from 'next/image';
+import { ExternalLink } from 'lucide-react';
 import { Text } from '@penumbra-zone/ui/Text';
+import { Button } from '@penumbra-zone/ui/Button';
+
+const LQT_MARKETING_URL = 'https://penumbra.zone/tournament';
 
 export const Explainer = () => {
   return (
@@ -62,6 +66,13 @@ export const Explainer = () => {
             </Text>
           </div>
         </div>
+
+        {/* eslint-disable-next-line react/jsx-no-target-blank -- we want analytics to see referrers */}
+        <a href={LQT_MARKETING_URL} target='_blank'>
+          <Button icon={ExternalLink} priority='primary'>
+            Learn More
+          </Button>
+        </a>
       </div>
     </div>
   );
