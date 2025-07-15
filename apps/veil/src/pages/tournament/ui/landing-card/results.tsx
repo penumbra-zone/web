9c407a17ed4e35395f7b00780486de7b520fb474
Max Korsunov <maxkors2014@gmail.com>
2025-07-15 18:30:21 +0300

    fix(veil): #2610: update gauge voting designs

--- apps/veil/src/pages/tournament/ui/landing-card/results.tsx
+++ apps/veil/src/pages/tournament/ui/landing-card/results.tsx
@@ -1,7 +1,14 @@
+import { useMemo } from 'react';
+import { InfoIcon } from 'lucide-react';
 import { Text } from '@penumbra-zone/ui/Text';
+import { Tooltip } from '@penumbra-zone/ui/Tooltip';
 import { Skeleton } from '@penumbra-zone/ui/Skeleton';
 import type { MappedGauge } from '../../server/previous-epochs';
-import { VoteAssetIcon, VoteAssetContent } from '../vote-dialog/vote-dialog-asset';
+import {
+  VoteAssetIcon,
+  VoteAssetContent,
+  VOTING_THRESHOLD,
+} from '../vote-dialog/vote-dialog-asset';
 import { ProvideLiquidityButton } from '../shared/provide-liquidity-button';
 
 export interface TournamentResultsProps {
@@ -14,12 +21,34 @@ const TournamentResultsAsset = ({ asset }: { asset: MappedGauge }) => {
     <div key={asset.asset.symbol} className='flex items-center gap-3'>
       <VoteAssetIcon asset={asset} />
       <VoteAssetContent asset={asset} />
-      <ProvideLiquidityButton symbol={asset.asset.symbol} />
+      <ProvideLiquidityButton
+        symbol={asset.asset.symbol}
+        primary={asset.portion >= VOTING_THRESHOLD}
+      />
     </div>
   );
 };
 
 export const TournamentResults = ({ loading, results }: TournamentResultsProps) => {
+  const { below, above } = useMemo(() => {
+    if (!results.length || loading) {
+      return {
+        below: [],
+        above: [],
+      };
+    }
+
+    const belowIndex = results.findIndex(asset => asset.portion < VOTING_THRESHOLD);
+    const above = results.slice(0, belowIndex === -1 ? results.length : belowIndex);
+    let below = results.slice(belowIndex === -1 ? results.length : belowIndex);
+    below = above.length > 5 ? [] : below.slice(0, 5 - above.length);
+
+    return {
+      below,
+      above,
+    };
+  }, [results, loading]);
+
   if (!loading && !results.length) {
     return <div className='grow' />;
   }
@@ -36,13 +65,34 @@ export const TournamentResults = ({ loading, results }: TournamentResultsProps) 
         </Text>
       )}
 
-      {loading
-        ? new Array(5).fill({}).map((_, index) => (
-            <div key={index} className='h-8 w-full rounded'>
-              <Skeleton />
-            </div>
-          ))
-        : results.map(asset => <TournamentResultsAsset asset={asset} key={asset.asset.base} />)}
+      {loading ? (
+        new Array(5).fill({}).map((_, index) => (
+          <div key={index} className='h-8 w-full rounded'>
+            <Skeleton />
+          </div>
+        ))
+      ) : (
+        <>
+          {above.slice(0, 5).map(asset => (
+            <TournamentResultsAsset asset={asset} key={asset.asset.base} />
+          ))}
+        </>
+      )}
+
+      {!!below.length && (
+        <div className='flex items-center gap-2'>
+          <Text small color='text.secondary'>
+            Below threshold ({'<'}5%)
+          </Text>
+          <Tooltip message='LPs for assets below the 5% threshold are not eligible for rewards'>
+            <InfoIcon className='size-3 text-neutral-light' />
+          </Tooltip>
+        </div>
+      )}
+
+      {below.map(asset => (
+        <TournamentResultsAsset asset={asset} key={asset.asset.base} />
+      ))}
     </div>
   );
 };
--- apps/veil/src/pages/tournament/ui/shared/provide-liquidity-button.tsx
+++ apps/veil/src/pages/tournament/ui/shared/provide-liquidity-button.tsx
@@ -1,21 +1,65 @@
+import { useState } from 'react';
 import Link from 'next/link';
+import { Text } from '@penumbra-zone/ui/Text';
 import { Button } from '@penumbra-zone/ui/Button';
+import { Dialog } from '@penumbra-zone/ui/Dialog';
+import { Density } from '@penumbra-zone/ui/Density';
 import { useStakingTokenMetadata } from '@/shared/api/registry';
 import { getTradePairPath } from '@/shared/const/pages';
 
 export interface ProvideLiquidityButtonProps {
   symbol: string;
+  primary: boolean;
 }
 
-export const ProvideLiquidityButton = ({ symbol }: ProvideLiquidityButtonProps) => {
+export const ProvideLiquidityButton = ({ symbol, primary }: ProvideLiquidityButtonProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const umMetadata = useStakingTokenMetadata();
   const link = getTradePairPath(umMetadata.data.symbol, symbol, {
     highlight: 'liquidity',
   });
 
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  if (!primary) {
+    return (
+      <>
+        <Button actionType='default' density='slim' onClick={() => setIsOpen(true)}>
+          Provide Liquidity
+        </Button>
+
+        <Dialog isOpen={isOpen} onClose={handleClose}>
+          <Dialog.Content
+            title='LP Rewards Not Guaranteed'
+            buttons={
+              <div className='flex items-center gap-2 px-6 pb-8 [&>*]:w-1/2'>
+                <Density sparse>
+                  <Button actionType='default' onClick={handleClose}>
+                    Cancel
+                  </Button>
+                  <Link href={link}>
+                    <Button actionType='accent'>Continue</Button>
+                  </Link>
+                </Density>
+              </div>
+            }
+          >
+            <Text>
+              This asset is currently below the 5% incentive threshold, and providing liquidity may
+              not result in earning rewards this epoch.
+            </Text>
+          </Dialog.Content>
+        </Dialog>
+      </>
+    );
+  }
+
   return (
     <Link href={link}>
-      <Button actionType='default' density='slim'>
+      <Button actionType='accent' density='slim'>
         Provide Liquidity
       </Button>
     </Link>
