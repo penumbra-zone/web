545cb7ca112b89ba1e5e3128dfdf333a78194aa2
erwanor <erwanor@penumbralabs.xyz>
2025-07-23 15:42:37 +0200

    veil: only display version footer on `/inspect`

--- apps/veil/src/widgets/footer/ui/footer.tsx
+++ apps/veil/src/widgets/footer/ui/footer.tsx
@@ -1,10 +1,17 @@
+'use client';
+
 import { VeilVersion } from './veil-version';
+import { useBasePath } from '@/shared/const/pages';
+import { PagePath } from '@/shared/const/pages';
 
 export const Footer = () => {
+  const currentPath = useBasePath();
+  const isInspectPage = currentPath === PagePath.Inspect;
+
   return (
     <footer className='mt-auto border-t border-t-other-solid-stroke px-6 py-4'>
       <div className='flex justify-center'>
-        <VeilVersion />
+        {isInspectPage && <VeilVersion />}
       </div>
     </footer>
   );
