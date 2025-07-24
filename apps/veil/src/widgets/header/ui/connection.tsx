0419a6756d028d89591360bc77f631d663a47bbd
Max Korsunov <maxkors2014@gmail.com>
2025-07-24 13:38:37 +0500

    fix(veil): #2598: add loading state to header connector

--- apps/veil/src/widgets/header/ui/connection.tsx
+++ apps/veil/src/widgets/header/ui/connection.tsx
@@ -1,4 +1,5 @@
 import { observer } from 'mobx-react-lite';
+import { Skeleton } from '@penumbra-zone/ui/Skeleton';
 import { connectionStore } from '@/shared/model/connection';
 import { ConnectButton } from '@/features/connect/connect-button';
 import { SubaccountSelector } from '@/widgets/header/ui/subaccount-selector';
@@ -8,6 +9,14 @@ export interface ConnectionProps {
 }
 
 export const Connection = observer(({ mobile }: ConnectionProps) => {
+  if (connectionStore.connectedLoading) {
+    return mobile ? null : (
+      <div className='h-12 w-28'>
+        <Skeleton />
+      </div>
+    );
+  }
+
   if (!connectionStore.connected) {
     return <ConnectButton variant={mobile ? 'mobile' : 'default'} />;
   }
